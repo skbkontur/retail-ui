fc93d243ed6f1b96396baf473079e22729a99932
Pavel Volokitin <pavelvolokitin@gmail.com>
2015-02-25 17:02:36 +0500

    Introduce `class list` utility.

---
+++ components/cx.js
@@ -0,0 +1,19 @@
+module.exports = function(prefix) {
+  return function(classNames) {
+    if (typeof classNames === 'string' && arguments.length === 1) {
+      return doPrefix(classNames);
+    }
+
+    if (typeof classNames === 'object') {
+      return Object.keys(classNames).filter(function(className) {
+        return classNames[className];
+      }).map(doPrefix).join(' ');
+    } else {
+      return Array.prototype.map.call(classNames, doPrefix).join(' ');
+    }
+  };
+
+  function doPrefix(className) {
+    return className ? (prefix + '-' + className) : prefix;
+  }
+};
