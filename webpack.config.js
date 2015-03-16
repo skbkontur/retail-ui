70f13a65f6e3a4caa40b745767fc08f4142e1e5d
Pavel Volokitin <pavelvolokitin@gmail.com>
2015-03-16 10:19:22 +0500

    Init.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: __dirname + '/src',
+  entry: './showcase',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/dist/',
+    filename: 'showcase.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader?loose&optional=runtime',
+        exclude: /node_modules\/react/
+      },
+      {
+        test: /\.(css|less)$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {test: /\.less$/, loader: 'less-loader'},
+      {test: /\.(woff|eot)$/, loader: "file-loader"}
+    ]
+  },
+  resolve: {
+    fallback: path.join(__dirname, 'node_modules'),
+    alias: {
+      ui: 'retail-ui/components'
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  resolveLoader: {
+    fallback: path.join(__dirname, 'node_modules')
+  },
+  plugins: [
+    new ExtractTextPlugin('showcase.css')
+  ]
+};
