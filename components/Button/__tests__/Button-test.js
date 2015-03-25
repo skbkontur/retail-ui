1e2d58253549bbfdde40205ecd94632e206e5c5c
Pavel Volokitin <pavelvolokitin@gmail.com>
2015-03-25 14:48:57 +0500

    Add basic test for Button.

---
+++ components/Button/__tests__/Button-test.js
@@ -0,0 +1,26 @@
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+
+jest.dontMock('../Button.jsx');
+var Button = require('../Button.jsx');
+
+describe('Button', function() {
+  it('has correct label', function() {
+    var button = TestUtils.renderIntoDocument(
+      <Button>Foo</Button>
+    );
+
+    expect(button.getDOMNode().textContent).toBe('Foo');
+  });
+
+  it ('handles click event', function() {
+    var handler = jest.genMockFn();
+    var button = TestUtils.renderIntoDocument(
+      <Button onClick={handler} />
+    );
+
+    TestUtils.Simulate.click(button.getDOMNode());
+
+    expect(handler.mock.calls.length).toBe(1);
+  });
+});
