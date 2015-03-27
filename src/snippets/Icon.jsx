d367869be48ecd647254dcc74007cc792b4790da
Pavel Volokitin <pavelvolokitin@gmail.com>
2015-03-27 16:31:34 +0500

    Improve icons demo looking.

--- src/snippets/Icon.jsx
+++ src/snippets/Icon.jsx
@@ -1,8 +1,9 @@
+var style = {display: 'inline-block', margin: '10px 10px 0 0'};
 React.render(
   <div>
     <div>Имя показывается при наведении:</div>
     {Icon.getAllNames().map(name => (
-      <span key={name} title={name} style={{margin: 5}}>
+      <span key={name} title={name} style={style}>
         <Icon name={name} />{' '}
       </span>
     ))}
