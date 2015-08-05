38edda9097811421eee492fa1687779eb7b707c0
Pavel Volokitin <pavelvolokitin@gmail.com>
2015-08-05 09:50:24 +0500

    [DatePicker] Handle date from the input.

--- components/DatePicker/DatePicker.jsx
+++ components/DatePicker/DatePicker.jsx
@@ -12,6 +12,7 @@ var DatePicker = React.createClass({
   getInitialState() {
     return {
       value: null,
+      textValue: '',
       opened: false,
     };
   },
@@ -30,7 +31,9 @@ var DatePicker = React.createClass({
     return (
       <span className={styles.root}>
         <Group width={150}>
-          <Input ref="input" mainInGroup value={formatDate(this.state.value)} />
+          <Input ref="input" mainInGroup value={this.state.textValue}
+              placeholder="дд.мм.гггг" onChange={this.handleChange}
+              onBlur={this.handleBlur} />
           <Button narrow active={this.state.opened} onClick={this.open}>
             <Icon name="calendar" />
           </Button>
@@ -40,6 +43,24 @@ var DatePicker = React.createClass({
     );
   },
 
+  handleChange(event) {
+    let value = event.target.value;
+    this.setState({textValue: value});
+
+    let date = parseDate(value);
+    if (date) {
+      this.setState({value: date});
+    }
+  },
+
+  handleBlur() {
+    let value = parseDate(this.state.textValue);
+    this.setState({
+      value,
+      textValue: formatDate(value),
+    });
+  },
+
   handlePickerKey(event) {
     if (event.key === 'Escape') {
       this.close(true);
@@ -47,7 +68,7 @@ var DatePicker = React.createClass({
   },
 
   handlePick(date) {
-    this.setState({value: date});
+    this.setState({value: date, textValue: formatDate(date)});
     this.close(true);
   },
 
@@ -71,8 +92,20 @@ function formatDate(date) {
   if (!date) return '';
 
   let day = formatNumber(date.getDate());
-  let month = formatNumber(date.getMonth());
+  let month = formatNumber(date.getMonth() + 1);
   return `${day}.${month}.${date.getFullYear()}`;
+}
+
+function parseDate(str) {
+  let match = str.match(/^(\d{1,2})\.(\d{1,2})\.(\d{4})$/);
+  if (match) {
+    let date = new Date(0);
+    date.setFullYear(parseInt(match[3], 10));
+    date.setMonth(parseInt(match[2], 10) - 1);
+    date.setDate(parseInt(match[1], 10));
+    return date;
+  }
+  return null;
 }
 
 function formatNumber(value) {
