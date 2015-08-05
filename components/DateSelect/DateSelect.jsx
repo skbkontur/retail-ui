cc365abd2cc20435b66ae02d9580dbd5e509fe74
Pavel Volokitin <pavelvolokitin@gmail.com>
2015-08-05 16:18:48 +0500

    [DateSelect] Add caps.

--- components/DateSelect/DateSelect.jsx
+++ components/DateSelect/DateSelect.jsx
@@ -32,6 +32,8 @@ const DateSelect = React.createClass({
     return {
       opened: false,
       pos: 0,
+      topCapped: false,
+      botCapped: false,
       current: 0,
     };
   },
@@ -79,7 +81,6 @@ const DateSelect = React.createClass({
     }
     let style = {
       top: top - 2,
-      height,
     };
 
     let shiftStyle = {
@@ -87,23 +88,32 @@ const DateSelect = React.createClass({
       top: -shift,
     };
 
+    let holderClass = classNames({
+      [styles.menuHolder]: true,
+      [styles.isTopCapped]: this.state.topCapped,
+    });
+
     return (
-      <div className={styles.menuHolder} style={style}
-          onKeyDown={this.handleKey} onWheel={this.handleWheel}>
-        <div style={shiftStyle}>{items}</div>
-        <div className={styles.menuOverlay}
-            onMouseDown={this.handleItemClick}
-            onMouseMove={this.handleMouseMove}
-            onMouseLeave={this.handleMouseLeave} />
+      <div className={holderClass} style={style} onKeyDown={this.handleKey}>
+        {!this.state.topCapped && (
+          <div className={styles.menuUp} onClick={this.handleUp} />
+        )}
+        <div className={styles.itemsHolder} style={{height}}>
+          <div style={shiftStyle}>{items}</div>
+          <div className={styles.menuOverlay}
+              onMouseDown={this.handleItemClick}
+              onMouseMove={this.handleMouseMove}
+              onMouseLeave={this.handleMouseLeave}
+              onWheel={this.handleWheel} />
+          </div>
+        {!this.state.botCapped && (
+          <div className={styles.menuDown} onClick={this.handleDown} />
+        )}
       </div>
     );
   },
 
   handleWheel(event) {
-    if (this.props.type === 'month') {
-      return;
-    }
-
     event.preventDefault();
 
     let deltaY = event.deltaY;
@@ -113,8 +123,7 @@ const DateSelect = React.createClass({
       deltaY *= HEIGHT * 4;
     }
     let pos = this.state.pos += deltaY;
-    this.resetSize();
-    this.setState({pos});
+    this.resetSize(pos);
   },
 
   handleMouseMove(event) {
@@ -179,6 +188,14 @@ const DateSelect = React.createClass({
     }
   },
 
+  handleUp() {
+    this.resetSize(this.state.pos - HEIGHT);
+  },
+
+  handleDown() {
+    this.resetSize(this.state.pos + HEIGHT);
+  },
+
   getItem(index) {
     let value = this.props.value + index;
     if (this.props.type === 'month') {
@@ -190,10 +207,9 @@ const DateSelect = React.createClass({
   open() {
     if (this.state.opened) return;
 
-    this.resetSize();
+    this.resetSize(0);
     this.setState({
       opened: true,
-      pos: 0,
       current: 0,
     });
   },
@@ -204,15 +220,43 @@ const DateSelect = React.createClass({
     this.setState({opened: false});
   },
 
-  resetSize() {
+  resetSize(pos) {
     let top = -5 * HEIGHT;
     let height = 11 * HEIGHT;
     if (this.props.type === 'month') {
       top = -this.props.value * HEIGHT;
       height = 12 * HEIGHT;
     }
-    this.setState({top, height});
+
+    let minPos = this.getMinPos() - top;
+    let maxPos = this.getMaxPos() - top - height + HEIGHT;
+    if (minPos >= pos) {
+      pos = minPos;
+    }
+    if (maxPos <= pos) {
+      pos = maxPos;
+    }
+    let topCapped = pos <= minPos;
+    let botCapped = pos >= maxPos;
+
+    this.setState({pos, top, height, topCapped, botCapped});
   },
+
+  getMinPos() {
+    if (this.props.type === 'month') {
+      return -this.props.value * HEIGHT;
+    } else if (this.props.type === 'year') {
+      return (1900 - this.props.value) * HEIGHT;
+    }
+  },
+
+  getMaxPos() {
+    if (this.props.type === 'month') {
+      return (11 - this.props.value) * HEIGHT;
+    } else if (this.props.type === 'year') {
+      return (2100 - this.props.value) * HEIGHT;
+    }
+  }
 });
 
 module.exports = DateSelect;
