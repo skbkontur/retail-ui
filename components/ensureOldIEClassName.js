d5811c2c8d87405f2a9fb104435f5a3cccac42c2
Pavel Volokitin <pavelvolokitin@gmail.com>
2015-09-22 12:51:44 +0500

    Old IE class names.

---
+++ components/ensureOldIEClassName.js
@@ -0,0 +1,7 @@
+const div = document.createElement("div");
+div.innerHTML = '<!--[if IE 8]>8<![endif]--><!--[if IE 9]>9<![endif]-->';
+const ieVerison = parseInt(div.innerHTML);
+
+if (ieVerison) {
+  document.body.className += ' rt-ie' + ieVerison;
+}
