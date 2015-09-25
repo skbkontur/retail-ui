b2528c445c2156d639fa275e1f0542818811687e
Pavel Volokitin <pavelvolokitin@gmail.com>
2015-09-25 13:44:29 +0500

    Use Gapped for Tooltip example.

--- src/snippets/Tooltip.jsx
+++ src/snippets/Tooltip.jsx
@@ -2,18 +2,31 @@ function render() {
   return <div>Hey there!</div>;
 }
 
-React.render(<div>
-  <Tooltip render={render} pos="top right">top right</Tooltip>
-  <br /><br />
-  <Tooltip render={render} pos="top center" trigger="click">top center</Tooltip>
-  <br /><br />
-  <Tooltip render={render} pos="bottom left">bottom left</Tooltip>
-  <br /><br />
-  <Tooltip render={render} pos="right top">right top</Tooltip>
-  <br /><br />
-  <Tooltip render={render} pos="right middle">right middle</Tooltip>
-  <br /><br />
-  <Tooltip render={render}>
-    <span style={{border: '1px solid'}}>?</span>
-  </Tooltip>
-</div>, mountNode);
+React.render(
+  <Gapped vertical>
+    <Tooltip render={render} pos="right top">
+      <div style={{height: 100, background: '#f99'}} />
+    </Tooltip>
+
+    <Tooltip render={render} pos="right top">
+      <div style={{width: 100, height: 100, background: '#f99'}} />
+    </Tooltip>
+
+    <Tooltip render={render} pos="top right">top right</Tooltip>
+
+    <Tooltip render={render} pos="top center" trigger="click">
+      top center (click)
+    </Tooltip>
+
+    <Tooltip render={render} pos="bottom left">bottom left</Tooltip>
+
+    <Tooltip render={render} pos="right top">right top</Tooltip>
+
+    <Tooltip render={render} pos="right middle">right middle</Tooltip>
+
+    <Tooltip render={render}>
+      <span style={{border: '1px solid'}}>?</span>
+    </Tooltip>
+  </Gapped>,
+  mountNode
+);
