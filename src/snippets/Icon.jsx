2415fa9f9ef2c8518f76ba9c892cbaa9ae84f075
Pavel Volokitin <pavelvolokitin@gmail.com>
2015-09-25 13:35:04 +0500

    Use Tooltip in the Icon snippet.

--- src/snippets/Icon.jsx
+++ src/snippets/Icon.jsx
@@ -1,12 +1,13 @@
-var style = {display: 'inline-block', margin: '10px 10px 0 0'};
 React.render(
-  <div>
+  <Gapped vertical>
     <div>Имя показывается при наведении:</div>
-    {Icon.getAllNames().map(name => (
-      <span key={name} title={name} style={style}>
-        <Icon name={name} />{' '}
-      </span>
-    ))}
-  </div>,
+    <Gapped>
+      {Icon.getAllNames().map((name) => (
+        <Tooltip key={name} render={() => name}>
+          <Icon name={name} />
+        </Tooltip>
+      ))}
+    </Gapped>
+  </Gapped>,
   mountNode
 );
