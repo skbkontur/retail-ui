eb186ea1b12986ea2e6f8d8554941fec9c617898
scalder <scalder@skbkontur.ru>
2015-11-15 04:41:37 +0500

    spinner - добавил в менюшку, привел код в соответствие с правилами eslint в проекте

--- components/Spinner/settings.js
+++ components/Spinner/settings.js
@@ -1,7 +1,7 @@
 export const types = {
-  big: "big",
-  mini: "mini",
-  normal: "normal"
+  big: 'big',
+  mini: 'mini',
+  normal: 'normal',
 };
 
 export const sizeMaps = {
@@ -9,23 +9,23 @@ export const sizeMaps = {
     height: 16,
     width: 16,
     viewBox: null,
-    strokeWidth: 1.5
+    strokeWidth: 1.5,
   },
   [types.normal]: {
     height: 35,
     width: 47,
     viewBox: null,
-    strokeWidth: 2
+    strokeWidth: 2,
   },
   [types.big]: {
     height: 70,
     width: 94,
-    viewBox: "0 0 47 35",
-    strokeWidth: 2
-  }
+    viewBox: '0 0 47 35',
+    strokeWidth: 2,
+  },
 };
 
 export default {
   types,
-  sizeMaps
+  sizeMaps,
 };
--- components/Spinner/spinner.jsx
+++ components/Spinner/spinner.jsx
@@ -1,46 +1,65 @@
 import classnames from 'classnames';
-import { Component, PropTypes } from 'react';
+import {Component, PropTypes} from 'react';
 import styles from "./Spinner.less";
-import { types, sizeMaps } from "./settings";
+import {types, sizeMaps} from "./settings";
 
 /**
- * Стандартный спиннер
+ * DRAFT - инлайн-лоадер
  */
 class Spinner extends Component {
   renderCloud() {
-    const { type } = this.props;
+    const {type} = this.props;
     const params = sizeMaps[type];
 
+    const svgPath = `M32.0297086,9.1495774 L31.5978628,8.5870774 C29.3570968,
+      5.67148577 25.9305165,4 22.1999329,4 C17.3429265,
+      4 12.9026663,7.04270463 11.154144,11.5717304 L10.901479,
+      12.2253114 L10.2421341,12.4725311 C6.50853057,13.8727758 4,
+      17.4719751 4,21.428492 C4,26.7061833 8.32047079,
+      31 13.6314689,31 L32.0297086,31 C38.078569,31 43,26.1036477 43,
+      20.0862989 C43,14.3602091 38.493302,9.5769573 32.7403918,
+      9.19661922 L32.0297086,9.1495774 Z`;
+
     return (
-      <svg className={styles.cloud} width={params.width} height={params.height} viewBox={params.viewBox}>
+      <svg className={styles.cloud}
+           width={params.width}
+           height={params.height}
+           viewBox={params.viewBox}>
         <path className={styles.bg}
               strokeWidth={params.strokeWidth}
-              d="M32.0297086,9.1495774 L31.5978628,8.5870774 C29.3570968,5.67148577 25.9305165,4 22.1999329,4 C17.3429265,4 12.9026663,7.04270463 11.154144,11.5717304 L10.901479,12.2253114 L10.2421341,12.4725311 C6.50853057,13.8727758 4,17.4719751 4,21.428492 C4,26.7061833 8.32047079,31 13.6314689,31 L32.0297086,31 C38.078569,31 43,26.1036477 43,20.0862989 C43,14.3602091 38.493302,9.5769573 32.7403918,9.19661922 L32.0297086,9.1495774 Z"></path>
+              d={svgPath}></path>
         <path className={styles.path}
               strokeWidth={params.strokeWidth}
-              d="M32.0297086,9.1495774 L31.5978628,8.5870774 C29.3570968,5.67148577 25.9305165,4 22.1999329,4 C17.3429265,4 12.9026663,7.04270463 11.154144,11.5717304 L10.901479,12.2253114 L10.2421341,12.4725311 C6.50853057,13.8727758 4,17.4719751 4,21.428492 C4,26.7061833 8.32047079,31 13.6314689,31 L32.0297086,31 C38.078569,31 43,26.1036477 43,20.0862989 C43,14.3602091 38.493302,9.5769573 32.7403918,9.19661922 L32.0297086,9.1495774 Z"></path>
+              d={svgPath}></path>
       </svg>
     );
   }
 
   renderCircle() {
-    const { type } = this.props;
+    const {type} = this.props;
     const params = sizeMaps[type];
 
     return (
-      <svg className={styles.circle} width={params.width} height={params.height}>
-        <circle class="path" cx="8" cy="8" r="6" stroke-miterlimit="10" strokeWidth={params.strokeWidth} />
+      <svg className={styles.circle}
+           width={params.width}
+           height={params.height}>
+
+        <circle cx="8" cy="8" r="6" stroke-miterlimit="10"
+                className="path"
+                strokeWidth={params.strokeWidth} />
       </svg>
     );
   }
 
   render() {
-    const { type } = this.props;
+    const {type} = this.props;
 
     return (
       <div className={styles.spinner}>
-        { type === types.mini ? this.renderCircle() : this.renderCloud() }
-        { type === types.mini ? <span className={styles.caption}>{this.props.caption}</span> : null }
+        {type === types.mini ? this.renderCircle() : this.renderCloud()}
+        {type === types.mini ?
+          <span className={styles.caption}>{this.props.caption}</span>
+          : null}
       </div>
     )
   }
@@ -52,21 +71,21 @@ Spinner.propTypes = {
    *
    * Значение по-умолчанию - normal
    *
-   * enum со всеми типами - Spinner.types
+   * Spinner.types - все доступные типы
    */
   type: PropTypes.oneOf(Object.values(types)),
 
   /**
    * Текст рядом с мини-лоадером.
    *
-   * Значение по-умолчанию - "Загрузка"
+   * "Загрузка" - значение по-умолчанию
    */
-  caption: PropTypes.string
+  caption: PropTypes.string,
 };
 
 Spinner.defaultProps = {
   type: types.normal,
-  caption: "Загрузка"
+  caption: "Загрузка",
 };
 
 Spinner.types = types;
