223fdb47b200a998fcf5556f41e4c7b600236958
Pavel Volokitin <pavelvolokitin@gmail.com>
2016-02-02 16:28:13 +0500

    Update packages.

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -22,7 +22,11 @@ module.exports = {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'stage-0', 'react'],
+          presets: [
+            require.resolve('babel-preset-es2015'),
+            require.resolve('babel-preset-stage-0'),
+            require.resolve('babel-preset-react'),
+          ],
         },
         include: [
           path.join(__dirname, 'src'),
