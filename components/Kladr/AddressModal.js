8850a075b8f7eeb70ddf61ebe9f7e82022ed4298
Pavel Volokitin <pavelvolokitin@gmail.com>
2016-02-17 12:11:44 +0500

    [Kladr] Autocomplete addresses fields.

--- components/Kladr/AddressModal.js
+++ components/Kladr/AddressModal.js
@@ -3,12 +3,14 @@
 import React from 'react';
 
 import Button from '../Button';
+import ComboBox from '../ComboBox';
 import Gapped from '../Gapped';
 import Input from '../Input';
 import Modal from '../Modal';
 import Select from '../Select';
 
-import type {Address} from './Types';
+import {search} from './KladrAPI';
+import type {Address, PlaceDescription} from './Types';
 
 import styles from './AddressModal.less';
 
@@ -21,15 +23,83 @@ type State = {
   address: Address,
 };
 
+type SourceFunction = (searchText: string) => Promise<PlaceDescription>;
+type HandlerFunction = (e: any, value: PlaceDescription) => void;
+type FieldProps = {
+  source: SourceFunction,
+  onChange: HandlerFunction,
+  renderItem: Function,
+  renderValue: Function,
+  recover: Function,
+}
+
 export default class AddressModal extends React.Component {
   props: Props;
   state: State;
+
+  _regionProps: FieldProps;
+  _districtProps: FieldProps;
+  _cityProps: FieldProps;
+  _settlementProps: FieldProps;
+  _streetProps: FieldProps;
 
   constructor(props: Props) {
     super(props);
 
     this.state = {
-      address: props.address,
+      address: props.address || {},
+    };
+
+    this._regionProps = this.createFieldProps('region', [], 'Region');
+    this._districtProps = this.createFieldProps(
+      'district', ['region'], 'District'
+    );
+    this._cityProps = this.createFieldProps(
+      'city', ['region', 'district'], 'City'
+    );
+    this._settlementProps = this.createFieldProps(
+      'settlement', ['region', 'district', 'city'], 'Settlement'
+    );
+    this._streetProps = this.createFieldProps(
+      'street', ['region', 'district', 'city', 'settlement'], 'Street'
+    );
+  }
+
+  createFieldProps(field: string, parents: Array<string>, level: string): FieldProps {
+    return {
+      source: this.createSource(field, parents, level),
+      onChange: this.createHandler(field),
+      renderItem: renderValueOrItem,
+      renderValue: renderValueOrItem,
+      recover: recover,
+    }
+  }
+
+  createHandler(place: string): HandlerFunction {
+    return (e, value) => {
+      this.setState({
+        address: {
+          ...this.state.address,
+          [place]: value,
+        }
+      })
+    }
+  }
+
+  createSource(field: string, parents: Array<string>, level: string): SourceFunction {
+    return (searchText) => {
+      let parentCode = null;
+      for (let parentName of parents) {
+        const parent = this.state.address[parentName];
+        if (parent && parent.code) {
+          parentCode = parent.code;
+          break;
+        }
+      }
+
+      return search(searchText, `[${level}]`, parentCode).then((values) => ({
+        values: values.map(address => address[field]),
+      }));
     };
   }
 
@@ -62,31 +132,39 @@ export default class AddressModal extends React.Component {
         <div className={styles.row}>
           <div className={styles.label}>Регион</div>
           <div className={styles.field}>
-            <Input width="100%" />
+            <ComboBox value={this.state.address.region} {...this._regionProps} />
           </div>
         </div>
         <div className={styles.row}>
           <div className={styles.label}>Район</div>
           <div className={styles.field}>
-            <Input width="100%" />
+            <ComboBox value={this.state.address.district}
+              {...this._districtProps}
+            />
           </div>
         </div>
         <div className={styles.row}>
           <div className={styles.label}>Город</div>
           <div className={styles.field}>
-            <Input width="100%" />
+            <ComboBox value={this.state.address.city}
+              {...this._cityProps}
+              />
           </div>
         </div>
         <div className={styles.row}>
           <div className={styles.label}>Населенный пункт</div>
           <div className={styles.field}>
-            <Input width="100%" />
+            <ComboBox value={this.state.address.settlement}
+              {...this._settlementProps}
+              />
           </div>
         </div>
         <div className={styles.row}>
           <div className={styles.label}>Улица</div>
           <div className={styles.field}>
-            <Input width="100%" />
+            <ComboBox value={this.state.address.street}
+              {...this._streetProps}
+            />
           </div>
         </div>
 
@@ -117,4 +195,14 @@ export default class AddressModal extends React.Component {
       </Gapped>
     );
   }
+}
+
+function renderValueOrItem(place) {
+  return place ? place.name : 'null';
+}
+
+function recover(searchText) {
+  return {
+    value: {name: searchText},
+  };
 }
---
+++ components/Kladr/KladrAPI.js
@@ -0,0 +1,40 @@
+/* @flow */
+
+const kladrUrl = 'https://kladr.kontur.ru/v1/';
+const LIMIT = 50;
+
+export function search(searchText: string, levels: string, parentCode: string) {
+  const data = createQuery({
+    prefix: searchText,
+    parentKladr: parentCode || '',
+    limit: LIMIT,
+    desiredAoLevels: levels,
+    strictSubordination: 'false',
+  });
+  return fetch(`${kladrUrl}suggest?${data}`)
+    .then(response => response.json())
+    .then(json => toJS(json));
+}
+
+function createQuery(data){
+  let params = [];
+  for (const key in data) {
+    if (data.hasOwnProperty) {
+      params.push(`${key}=${encodeURIComponent(data[key])}`);
+    }
+  }
+  return params.join('&');
+}
+
+function toJS(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(toJS);
+  } else if (obj && typeof obj === 'object') {
+    const ret = {};
+    for (const key in obj) {
+      ret[key.charAt(0).toLowerCase() + key.substr(1)] = toJS(obj[key]);
+    }
+    return ret;
+  }
+  return obj;
+}
