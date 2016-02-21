c7d4fe73142d2728a489d4830b45037474edeb13
Хазамов Теймураз Артурович <t1mmaas@skbkontur.ru>
2016-02-21 15:33:50 +0500

    svg updated

--- components/Logotype/Logotype.jsx
+++ components/Logotype/Logotype.jsx
@@ -1,26 +1,20 @@
-import React, {PropTypes} from 'react';
-import modernizr from './modernizr-inlinesvg';
+import React, {PropTypes} from 'react'
+import modernizr from './modernizr-inlinesvg'
 
-//noinspection JSUnresolvedVariable
-import styles from './Logotype.less';
+// noinspection JSUnresolvedVariable
+import styles from './Logotype.less'
 
 const createCloudSVG = (color) => (
-  <svg
-    className={styles.cloud}
-    id="cloud"
-    x="0px"
-    y="0px"
-    viewBox="423.4 585.2 36 26"
-  >
+  <svg width="25px" height="19px" viewBox="9 0 25 19" className={styles.cloud}>
     <path
       fill={color}
-      d="M440.3,587.8c2.8,0,5.4,1.3,7.1,3.5l0.7,1h1.2c4.3,0.3,7.6,3.8,7.6,8.1c0,4.5-3.6,8.1-8.2,8.1h-15.8 c-3.8,0-6.9-3.1-6.9-7c0-3,1.8-5.5,4.6-6.6l1.2-0.3l0.5-1.2C433.4,590.2,436.7,587.8,440.3,587.8 M440.3,585.2  c-4.9,0-9,3.1-10.7,7.5c-3.6,1.3-6.2,5-6.2,8.9c0,5.3,4.3,9.6,9.5,9.6h15.8c5.9,0,10.7-4.8,10.7-10.8c0-5.8-4.4-10.4-10-10.8  C447.4,587,444.1,585.2,440.3,585.2L440.3,585.2z"
-    />
+      d="M21.1744364,0.854871967 C17.8731636,0.854871967 15.0510424,2.9310477 13.9323455,5.85111464 C11.4838,6.7743113 9.73773939,9.14737406 9.73773939,11.9149305 C9.73773939,15.4958762 12.6410727,18.3976418 16.2103455,18.3976418 L26.8491333,18.3976418 C30.8488303,18.3976418 34.1054364,15.1440854 34.1054364,11.1401774 C34.1054364,7.29081339 31.1005879,4.13689707 27.3221939,3.88677992 C25.8908303,2.00785105 23.6554667,0.854871967 21.1744364,0.854871967 M21.1744364,2.60772552 C23.0646485,2.60772552 24.7985273,3.4617841 25.9314364,4.95028619 L26.4166788,5.58066276 L27.2064667,5.6376 C30.0955879,5.81857908 32.3573455,8.23637824 32.3573455,11.1401774 C32.3573455,14.1761523 29.888497,16.6549556 26.8491333,16.6549556 L16.2103455,16.6549556 C13.607497,16.6549556 11.4858303,14.5279431 11.4858303,11.9149305 C11.4858303,9.95872971 12.7202545,8.18350795 14.5475273,7.48806025 L15.2845273,7.20947448 L15.5687697,6.46929038 C16.4519515,4.15926527 18.7035576,2.60772552 21.1744364,2.60772552"
+      id="Fill-8" />
   </svg>
-);
+)
 
 const createCloudVML = (color) => {
-  document.namespaces.add('v', 'urn:schemas-microsoft-com:vml', '#default#VML');
+  document.namespaces.add('v', 'urn:schemas-microsoft-com:vml', '#default#VML')
   const cloud = `
     <v:shape
       style="position: absolute;left: 0;bottom: -1px; width: 0.66px;height: 0.66px;z-index: 1;"
@@ -32,15 +26,17 @@ const createCloudVML = (color) => {
       <v:skew on="t" matrix="1,0,0,1,0,0" offset="-424,-586" />
       <v:fill type="solid" />
     </v:shape>
-  `;
-  return <span
-    style={{position: 'relative', width: 23, display: 'inline-block'}}
-    dangerouslySetInnerHTML={{__html: cloud}}
-  />;
-};
+  `
+  return (
+    <span
+      style={{position: 'relative', width: 23, display: 'inline-block'}}
+      dangerouslySetInnerHTML={{__html: cloud}}
+    />
+  )
+}
 
-//noinspection JSUnresolvedVariable
-const createCloud = modernizr.inlinesvg ? createCloudSVG : createCloudVML;
+// noinspection JSUnresolvedVariable
+const createCloud = modernizr.inlinesvg ? createCloudSVG : createCloudVML
 
 const Logotype = ({color = '#D92932', suffix}) => (
   <a href="/" tabIndex="-1" className={styles.root}>
@@ -49,7 +45,7 @@ const Logotype = ({color = '#D92932', suffix}) => (
     <span>нтур.</span>
     <span style={{color}}>{suffix}</span>
   </a>
-);
+)
 
 Logotype.propTypes = {
   /**
@@ -61,6 +57,6 @@ Logotype.propTypes = {
    * Суффикс сервиса
    */
   suffix: PropTypes.string.isRequired,
-};
+}
 
-module.exports = Logotype;
+module.exports = Logotype
