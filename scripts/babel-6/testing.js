e6c8c4a3bdb886973f33b669d07362e8060838f1
Pavel Volokitin <pavelvolokitin@gmail.com>
2016-03-10 14:42:52 +0500

    Fix testing plugin for babel 6.

--- scripts/babel-6/testing.js
+++ scripts/babel-6/testing.js
@@ -1,6 +1,13 @@
 module.exports = function(babel) {
   const t = babel.types;
 
+  function getTid(attr) {
+    if (t.isJSXExpressionContainer(attr)) {
+      return attr.expression;
+    }
+    return attr;
+  }
+
   const buildRefFunc = babel.template(`global.ReactTesting.ref(TID)`);
   const buildPassFunc = babel.template(`global.ReactTesting.pass(this)`);
 
@@ -9,8 +16,10 @@ module.exports = function(babel) {
       JSXAttribute(path) {
         if (path.node.name.name === 'tid') {
           path.node.name.name = 'ref';
+
+          const tid = getTid(path.node.value);
           path.node.value = t.JSXExpressionContainer(
-            buildRefFunc({TID: path.node.value}).expression
+            buildRefFunc({TID: tid}).expression
           );
         } else if (path.node.name.name === 'tid-pass') {
           path.node.name.name = 'ref';
