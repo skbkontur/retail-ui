5962755ee31791a8d78e66132562b0cb42090ab4
Pavel Volokitin <pavelvolokitin@gmail.com>
2016-03-15 16:34:01 +0500

    Use testing babel plugin in tests.

--- preprocessor.js
+++ preprocessor.js
@@ -1,5 +1,7 @@
 const babel = require('babel-core');
 
+const testingPlugin = require('./scripts/babel-6/testing');
+
 module.exports = {
   process: function(src, path) {
     if (path.match(/.less$/)) {
@@ -9,6 +11,7 @@ module.exports = {
       return babel.transform(src, {
         filename: path,
         presets: ['jest', 'es2015', 'react', 'stage-0'],
+        plugins: [testingPlugin],
         retainLines: true,
       }).code;
     }
