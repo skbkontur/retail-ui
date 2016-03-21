ed3c7aa47455004d5a8997a030ba23ec8269b0e3
Pavel Volokitin <pavelvolokitin@gmail.com>
2016-03-21 16:14:14 +0500

    [SearchSelect] Remove from the docs.

--- docs/src/components.js
+++ docs/src/components.js
@@ -102,12 +102,6 @@ module.exports = [
     src: require('!raw!./snippets/RadioGroup.jsx'),
   },
   {
-    name: 'SearchSelect',
-    component: require('ui/SearchSelect'),
-    info: require('!docgen!ui/SearchSelect/SearchSelect.js'),
-    src: require('!raw!./snippets/SearchSelect.jsx'),
-  },
-  {
     name: 'Select',
     component: require('ui/Select'),
     info: require('!docgen!ui/Select/Select.js'),
