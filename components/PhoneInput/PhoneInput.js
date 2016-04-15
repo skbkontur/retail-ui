9d0d970430a338974c30df98b6da1db8432b5e8c
Правосуд Сергей Николаевич <pravosud@skbkontur.ru>
2016-04-15 12:40:03 +0500

    components/PhoneInput

---
+++ components/PhoneInput/PhoneInput.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Input from '../Input';
+
+import styles from './PhoneInput.less';
+
+/**
+ * Все пропсы пробрасываются во внутренний Input
+ */
+class PhoneInput extends React.Component {
+  static defaultProps = {
+    width: 160,
+  };
+
+  render() {
+    return (
+      <div className={styles.phoneWrapper}>
+        <Input
+          {...this.props}
+          width={this.props.width}
+          mask="999\u2009\999-99-99"
+          maskChar={null}
+        />
+      </div>
+    );
+  }
+}
+
+export default PhoneInput;
---
+++ components/PhoneInput/index.js
@@ -0,0 +1,2 @@
+import PhoneInput from './PhoneInput';
+module.exports = PhoneInput;
