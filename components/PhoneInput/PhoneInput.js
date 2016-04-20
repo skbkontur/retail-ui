32a66a2ef5d97df6148c704bd537d2eb26b0efb4
Правосуд Сергей Николаевич <pravosud@skbkontur.ru>
2016-04-20 12:52:41 +0500

    Удалены стили и дефолтный width в компоненте PhoneInput

--- components/PhoneInput/PhoneInput.js
+++ components/PhoneInput/PhoneInput.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import Input from '../Input';
 
-import styles from './PhoneInput.less';
 
 /**
  * Все пропсы пробрасываются во внутренний Input
  */
 class PhoneInput extends React.Component {
-  static defaultProps = {
-    width: 160,
-  };
 
   render() {
     return (
       <Input
         {...this.props}
-        width={this.props.width}
         mask="+7 999 999-99-99"
         maskChar={null}
         placeholder="+7"
