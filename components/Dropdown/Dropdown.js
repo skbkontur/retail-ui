58f52dc9b854367da0a23205b419260a520f5e65
Pavel Volokitin <pavelvolokitin@gmail.com>
2016-05-03 22:52:40 +0500

    Use React.Element for static items in Dropdown and Select.

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,6 +1,8 @@
 import React, {PropTypes} from 'react';
 
 import filterProps from '../filterProps';
+import MenuItem from '../MenuItem/MenuItem';
+import MenuSeparator from '../MenuSeparator/MenuSeparator';
 import Select from '../Select';
 
 const PASS_PROPS = {
@@ -46,50 +48,23 @@ export default class Dropdown extends React.Component {
   }
 
   render() {
-    const items = [];
-    React.Children.forEach(this.props.children, (child) => {
-      if (child.type === Separator) {
-        items.push(Select.SEP);
-      } else {
-        items.push(child);
-      }
-    });
+    const items = React.Children.map(this.props.children, item => item);
 
     return (
       <Select
         {...filterProps(this.props, PASS_PROPS)}
-        value={this.props.caption} items={items} renderValue={renderValue}
-        renderItem={renderItem} onChange={this._handleChange}
+        value={this.props.caption}
+        items={items}
+        renderValue={renderValue}
+        renderItem={renderItem}
       />
     );
   }
-
-  _handleChange = (event, value) => {
-    if (value.props.onClick) {
-      value.props.onClick();
-    }
-  };
 }
 
 function renderValue(value) {
   return value;
 }
 
-function renderItem(value) {
-  return value.props.children;
-}
-
-class MenuItem extends React.Component {
-  render() {
-    return null;
-  }
-}
-
-class Separator extends React.Component {
-  render() {
-    return null;
-  }
-}
-
 Dropdown.MenuItem = MenuItem;
-Dropdown.Separator = Separator;
+Dropdown.Separator = MenuSeparator;
