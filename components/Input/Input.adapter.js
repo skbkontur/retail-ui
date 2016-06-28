71ee8b81b7873f65abc32701ca7de065fca26caf
Pavel Volokitin <pavelvolokitin@gmail.com>
2016-06-28 20:04:57 +0500

    [FxInput] Add adapter.

--- components/Input/Input.adapter.js
+++ components/Input/Input.adapter.js
@@ -1,17 +1,18 @@
-/* @flow */
+// @flow
 
 import Input from './Input.js';
 
 const InputAdapter = {
-  getValue(inst) {
+  getValue(inst: Input) {
     return inst.state.value;
   },
 
-  setValue(inst, value) {
+  setValue(inst: Input, value: string) {
     inst.handleChange({target: {value}});
   },
 };
 
 Input.__ADAPTER__ = InputAdapter;
 
+export {InputAdapter};
 export default Input;
