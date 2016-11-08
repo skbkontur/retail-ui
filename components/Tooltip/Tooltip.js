6051f45202c42b4689dd3825c67c32d8a7543ad8
Хазамов Теймураз Артурович <t1mmaas@skbkontur.ru>
2016-11-08 14:12:05 +0500

    [Tooltip] flowtype fix

--- components/Tooltip/Tooltip.js
+++ components/Tooltip/Tooltip.js
@@ -14,6 +14,8 @@ type Pos = 'top left' | 'top center' | 'top right'
 type Props = {
   children: React.Element<any>,
 
+  className: string,
+
   closeButton?: bool,
 
   render: () => ?React.Element<any>,
@@ -115,6 +117,7 @@ export default class Tooltip extends React.Component {
   render() {
     const props = {};
     const {className} = this.props;
+
     if (this.props.trigger === 'hover') {
       props.onMouseOver = this._handleMouseOver;
       props.onMouseLeave = this._handleMouseLeave;
