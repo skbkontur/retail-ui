f3efba1cfc9dc744bd7ece3d51aa6ac6e94c6437
Хазамов Теймураз Артурович <t1mmaas@skbkontur.ru>
2016-11-14 11:06:14 +0500

    [Docs] regeneratorRuntime fix

--- docs/src/regeneratorRuntime.js
+++ docs/src/regeneratorRuntime.js
@@ -1,3 +1,2 @@
 // Remove this when babel-polyfill upgrades regenerator-runtime to > 0.9.6
-global.regeneratorRuntime = null;
-require('regenerator-runtime/runtime');
+global.regeneratorRuntime = require('regenerator-runtime');
