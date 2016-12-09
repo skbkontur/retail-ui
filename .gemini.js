9aa76c799090921ad5cea22fcffbda87f6aa79ec
Хазамов Теймураз Артурович <t1mmaas@skbkontur.ru>
2016-12-09 17:41:31 +0500

    [Gemini] added tests

---
+++ .gemini.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rootUrl: 'http://localhost:6006/',
+
+    browsers: {
+        chrome: {
+            desiredCapabilities: {
+                browserName: 'chrome'
+            }
+        }
+    }
+};
