369a24e10f57ea8d389b8d03f9fb6d2bb99dc8f4
tihonove <tihonove@skbkontur.ru>
2017-02-07 12:28:41 +0500

    update docs

--- docs/src/components/Layout/Layout.jsx
+++ docs/src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-//import { Logotype } from 'ui';
+import Logotype from 'retail-ui/components/Logotype';
 
 import cn from './Layout.less';
 
@@ -14,17 +14,21 @@ export default function Layout({ children }: LayoutProps): React.Element<*> {
     return (
         <div className={cn('root')}>
             <div className={cn('navigation-bar')}>
-                {/*<Logotype suffix='ритейл' color='#fff' textColor='#fff' />*/}
-                <Link activeClassName={cn('active')} to='/OnBlurValidations'>OnBlurValidations</Link>
-                <Link activeClassName={cn('active')} to='/OnBlurValidationsWithSubmitValidation'>
-                    OnBlurValidationsWithSubmitValidation
-                </Link>
-                <Link activeClassName={cn('active')} to='/DifferentMessages'>DifferentMessages</Link>
-                <Link activeClassName={cn('active')} to='/SimpleTextMessages'>SimpleTextMessages</Link>
-                <Link activeClassName={cn('active')} to='/ScrollDifferentMessages'>
-                    ScrollDifferentMessages
-                </Link>
-                <Link activeClassName={cn('active')} to='/ManyEditors'>ManyEditors</Link>
+                <div className={cn('logo')}>
+                    <Logotype suffix='ui-validations' href='#/' color='#fff' textColor='#fff' />
+                </div>
+                <div className={cn('sections')}>
+                    <Link activeClassName={cn('active')} to='/OnBlurValidations'>OnBlurValidations</Link>
+                    <Link activeClassName={cn('active')} to='/OnBlurValidationsWithSubmitValidation'>
+                        OnBlurValidationsWithSubmitValidation
+                    </Link>
+                    <Link activeClassName={cn('active')} to='/DifferentMessages'>DifferentMessages</Link>
+                    <Link activeClassName={cn('active')} to='/SimpleTextMessages'>SimpleTextMessages</Link>
+                    <Link activeClassName={cn('active')} to='/ScrollDifferentMessages'>
+                        ScrollDifferentMessages
+                    </Link>
+                    <Link activeClassName={cn('active')} to='/ManyEditors'>ManyEditors</Link>
+                </div>
             </div>
             <div className={cn('content')}>
                 {children}
