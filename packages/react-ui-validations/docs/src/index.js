98ffe41ead23e94c193f1ae7132a9d713b08c0b5
tihonove <tihonove@skbkontur.ru>
2017-02-12 12:01:12 +0500

    quick validation demo

--- packages/react-ui-validations/docs/src/index.js
+++ packages/react-ui-validations/docs/src/index.js
@@ -10,6 +10,7 @@ import Api from './components/Pages/Api.md';
 import GettingStarted from './components/Pages/GettingStarted.md';
 import Description from './components/Pages/Description.md';
 import Examples from './components/Pages/Examples';
+import Concepts from './components/Pages/Concepts';
 
 import './styles/reset.less';
 import './styles/typography.less';
@@ -20,7 +21,8 @@ ReactDom.render(
     <Router history={history}>
         <Route path='/' component={Layout}>
             <IndexRoute component={Description} />
-            {Examples.map(example => <Route key={example.url} path={example.url} component={example.component} />)}
+            {Examples.map(page => <Route key={page.url} path={page.url} component={page.component} />)}
+            {Concepts.map(page => <Route key={page.url} path={page.url} component={page.component} />)}
             <Route path='api' component={Api}>
                 <Route path='*' />
             </Route>
