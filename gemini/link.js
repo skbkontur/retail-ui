54f3657564b7c0d39eb70fa194bfb80be27ec55e
Правосуд Сергей Николаевич <pravosud@skbkontur.ru>
2017-05-03 15:34:10 +0500

    removed pressed and clicked link captures from gemini

--- gemini/link.js
+++ gemini/link.js
@@ -41,38 +41,28 @@ gemini.suite('link danger', suite => {
     });
 });
 
-gemini.suite('link Grayed', (suite) => {
-  suite.setUrl(pathTo('Link', 'Grayed'))
-	.setCaptureElements('#test-element')
-	.capture('idle')
+gemini.suite('link Grayed', suite => {
+  suite
+    .setUrl(pathTo('Link', 'Grayed'))
+    .setCaptureElements('#test-element')
+    .capture('idle')
     .capture('hover', (actions, find) => {
       actions.mouseMove(find('a'));
     })
     .capture('mouseLeave', (actions, find) => {
       actions.mouseMove(find('body'), [0, 0]);
-    })
-    .capture('pressed', (actions, find) => {
-      actions.mouseDown(find('a'));
-    })
-    .capture('clicked', (actions, find) => {
-      actions.mouseUp(find('a'));
     });
 });
 
-gemini.suite('link Disabled', (suite) => {
-  suite.setUrl(pathTo('Link', 'Disabled'))
-	.setCaptureElements('#test-element')
-	.capture('idle')
+gemini.suite('link Disabled', suite => {
+  suite
+    .setUrl(pathTo('Link', 'Disabled'))
+    .setCaptureElements('#test-element')
+    .capture('idle')
     .capture('hover', (actions, find) => {
       actions.mouseMove(find('a'));
     })
     .capture('mouseLeave', (actions, find) => {
       actions.mouseMove(find('body'), [0, 0]);
-    })
-    .capture('pressed', (actions, find) => {
-      actions.mouseDown(find('a'));
-    })
-    .capture('clicked', (actions, find) => {
-      actions.mouseUp(find('a'));
     });
 });
