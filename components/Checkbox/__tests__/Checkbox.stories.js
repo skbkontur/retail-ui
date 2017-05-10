ee40d20f6956c7226e2743c49642f00b9c30af62
Правосуд Сергей Николаевич <pravosud@skbkontur.ru>
2017-05-10 13:01:37 +0500

    updated Checkbox.stories

--- components/Checkbox/__tests__/Checkbox.stories.js
+++ components/Checkbox/__tests__/Checkbox.stories.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { storiesOf } from '@kadira/storybook';
 import Checkbox from '../Checkbox';
 
-class PlainComponent extends Component {
+class PlainCheckbox extends Component {
   state = {
     checked: false
   };
@@ -15,14 +15,14 @@ class PlainComponent extends Component {
         onChange={() => this.setState({ checked: !checked })}
         checked={checked}
       >
-        Plain checkbox
+        {this.props.children}
       </Checkbox>
     );
   }
 }
 
 storiesOf('Checkbox', module)
-  .add('plain', () => <PlainComponent />)
+  .add('plain', () => <PlainCheckbox>Plain checkbox</PlainCheckbox>)
   .add('unchecked', () => <Checkbox>Unchecked</Checkbox>)
   .add('checked', () => <Checkbox checked>Checked</Checkbox>)
   .add('disabled', () => <Checkbox disabled>Disabled</Checkbox>)
@@ -37,4 +37,15 @@ storiesOf('Checkbox', module)
     >
       Hover me
     </Checkbox>
+  ))
+  .add('with a long label', () => (
+    <PlainCheckbox>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </PlainCheckbox>
   ));
