fb446198bd0ad9c2c8de76f30ea8c51a89f6bfe6
Хазамов Теймураз Артурович <t1mmaas@skbkontur.ru>
2017-06-14 21:20:02 +0500

    added retry to tests

--- .gemini.js
+++ .gemini.js
@@ -5,19 +5,22 @@ module.exports = {
       desiredCapabilities: {
         browserName: 'chrome',
         version: '50'
-      }
+      },
+      retry: 5
     },
 		firefox: {
       desiredCapabilities: {
         browserName: 'firefox',
         version: '47'
-      }
+      },
+      retry: 5
 		},
 		ie11: {
       desiredCapabilities: {
         browserName: 'internet explorer',
 				version: '11'
-      }
+      },
+      retry: 5
 		}
   },
 	system: {
