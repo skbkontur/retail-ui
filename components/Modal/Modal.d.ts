499adf8f128122e06c40a3a20a70a0cdea6e3c2d
slawwan <slawwan@kontur>
2017-07-16 02:28:29 +0500

    добавил .d.ts для Modal

---
+++ components/Modal/Modal.d.ts
@@ -0,0 +1,47 @@
+import * as React from 'react';
+
+export interface ModalProps {
+  ignoreBackgroundClick?: boolean;
+  noClose?: boolean;
+  width?: number;
+  onClose?: () => void;
+}
+
+export interface ModalState {
+  shadowed: boolean;
+}
+
+export interface ModalHeaderProps {}
+
+export interface ModalHeaderState {}
+
+export interface ModalBodyProps {}
+
+export interface ModalBodyState {}
+
+export interface ModalFooterProps {
+  panel?: boolean;
+}
+
+export interface ModalFooterState {}
+
+export class ModalHeader extends React.Component<
+  ModalHeaderProps,
+  ModalHeaderState
+> {}
+
+export class ModalBody extends React.Component<
+  ModalBodyProps,
+  ModalBodyState
+> {}
+
+export class ModalFooter extends React.Component<
+  ModalFooterProps,
+  ModalFooterState
+> {}
+
+export default class Modal extends React.Component<ModalProps, ModalState> {
+  static Header: typeof ModalHeader;
+  static Body: typeof ModalBody;
+  static Footer: typeof ModalFooter;
+}
---
+++ components/Modal/index.d.ts
@@ -0,0 +1,3 @@
+import MenuItem from './MenuItem';
+
+export = MenuItem;
