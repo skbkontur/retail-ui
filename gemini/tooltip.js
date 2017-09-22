25220dc41e9b18c7cefbc66214c293f87170b3aa
Правосуд Сергей Николаевич <pravosud@skbkontur.ru>
2017-09-22 15:01:47 +0500

    updated Tooltip screenshot tests

--- gemini/tooltip.js
+++ gemini/tooltip.js
@@ -1,9 +1,30 @@
 /* global gemini */
 var pathTo = require('./utils').pathTo;
 
-gemini.suite('Tooltip', (suite) => {
+gemini.suite('Tooltip', suite => {
   suite
     .setUrl(pathTo('Tooltip', 'static tooltip'))
-    .setCaptureElements(['[class^="Box-root"]'])
+    .setCaptureElements('#test-element')
     .capture('simple');
+});
+
+gemini.suite('Tooltip left', suite => {
+  suite
+    .setUrl(pathTo('Tooltip', 'tooltip left'))
+    .setCaptureElements('#test-element')
+    .capture('plain');
+});
+
+gemini.suite('Tooltip right', suite => {
+  suite
+    .setUrl(pathTo('Tooltip', 'tooltip right'))
+    .setCaptureElements('#test-element')
+    .capture('plain');
+});
+
+gemini.suite('Tooltip bottom', suite => {
+  suite
+    .setUrl(pathTo('Tooltip', 'tooltip bottom'))
+    .setCaptureElements('#test-element')
+    .capture('plain');
 });
