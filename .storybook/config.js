fe159a9127fb86581f9d957898010108d1115022
Timur Khazamov <t1mmaas@skbkontur.ru>
2017-11-10 15:05:58 +0500

    Reverted storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,10 @@
 import 'babel-polyfill';
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
-import { ThemeProvider } from 'react-jss';
-import theme from '../components/theme';
 
 addDecorator(story => (
   <div id="test-element" style={{ display: 'inline-block', padding: 4 }}>
-    <ThemeProvider theme={theme}>{story()}</ThemeProvider>
+    {story()}
   </div>
 ));
 
