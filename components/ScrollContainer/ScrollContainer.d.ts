8585eb8eb70f905ea5f3ab6eb85ff6dd3ef70345
stepanoff <stepanoff@skbkontur.ru>
2018-01-22 17:26:52 +0500

    [Menu, ScrollContainer] Added string type for maxHeight prop

--- components/ScrollContainer/ScrollContainer.d.ts
+++ components/ScrollContainer/ScrollContainer.d.ts
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export interface ScrollContainerProps {
   invert?: boolean;
-  maxHeight?: number;
+  maxHeight?: number | string;
 }
 
 export interface ScrollContainerState {}
