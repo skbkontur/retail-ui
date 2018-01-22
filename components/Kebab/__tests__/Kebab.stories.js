db292554be3d01a26059c8a0d5c8949508443f1a
stepanoff <stepanoff@skbkontur.ru>
2018-01-22 17:27:30 +0500

    [Kebab] Added story to test fixed height case

--- components/Kebab/__tests__/Kebab.stories.js
+++ components/Kebab/__tests__/Kebab.stories.js
@@ -1,9 +1,15 @@
 // @flow
-import React, { Component } from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import React, {Component} from 'react';
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
 import Kebab from '../Kebab';
 import MenuItem from '../../MenuItem';
+import { manyItemsList, defaultItemsList } from './Kebab.items';
+
+type KebabItem = {
+  text: string,
+  action: string
+};
 
 storiesOf('Kebab', module)
   .addDecorator(story => (
@@ -17,27 +23,47 @@ storiesOf('Kebab', module)
       {story()}
     </div>
   ))
-  .add('14px', () => <SomethingWithKebab size="small" />)
-  .add('20px', () => <SomethingWithKebab size="large" />)
-  .add('20px-disabled', () => <SomethingWithKebab size="large" disabled />);
+  .add('14px', () => <SomethingWithKebab size="small"/>)
+  .add('20px', () => <SomethingWithKebab size="large"/>)
+  .add('20px-disabled', () => <SomethingWithKebab size="large" disabled/>)
+  .add('With fixed menu height', () => (
+    <SomethingWithKebab
+      size="large"
+      menuMaxHeight={'200px'}
+      items={manyItemsList}
+    />
+      )
+  );
 
 class SomethingWithKebab extends Component<{
   size: 'small' | 'large',
-  disabled?: boolean
+  disabled?: boolean,
+  items?: KebabItem[]
 }> {
   render() {
+    const itemsList = this.props.items || defaultItemsList;
+    const menuItems = itemsList.map((item, index) => {
+      return (
+        <MenuItem
+          key={index}
+          onClick={action(item.action)}
+        >
+          {item.text}
+        </MenuItem>
+      )
+    });
+
     return (
-      <div style={{ width: 200, textAlign: 'center' }}>
+      <div style={{width: 200, textAlign: 'center'}}>
         Pikachu{' '}
         <Kebab
           size={this.props.size}
           onOpen={action('open')}
           onClose={action('close')}
           disabled={this.props.disabled}
+          menuMaxHeight={this.props.menuMaxHeight}
         >
-          <MenuItem onClick={action('First')}>First</MenuItem>
-          <MenuItem onClick={action('Second')}>Second</MenuItem>
-          <MenuItem onClick={action('Uno')}>Uno</MenuItem>
+          {menuItems}
         </Kebab>
       </div>
     );
