7286f60cc436977c757093b6f58f95bb4d674785
slawwan <slawwan@skbkontur.ru>
2018-01-25 22:07:52 +0500

    [Select] Возможность переопределять отображаемое значение для null #397

--- components/Select/__tests__/Select.stories.js
+++ components/Select/__tests__/Select.stories.js
@@ -7,6 +7,7 @@ class SelectWrapper extends React.Component<{}, *> {
   state = {
     value: { label: 'One', value: 1 }
   };
+
   render() {
     return (
       <div>
@@ -26,14 +27,42 @@ class SelectWrapper extends React.Component<{}, *> {
   }
 }
 
+class SelectWithNull extends React.Component<*, *> {
+  state = {
+    value: null
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          value: <b>{JSON.stringify(this.state.value)}</b>
+        </div>
+        <Select
+          items={[
+            [null, 'Any'],
+            Select.SEP,
+            [1, 'First'],
+            [2, 'Second'],
+            [3, 'Third']
+          ]}
+          value={this.state.value}
+          onChange={(_, value) => this.setState({ value })}
+        />
+      </div>
+    );
+  }
+}
+
 storiesOf('Select', module)
-  .addDecorator(story =>
+  .addDecorator(story => (
     <div
       className="dropdown-test-container"
       style={{ height: 150, width: 200, padding: 4 }}
     >
       {story()}
     </div>
-  )
+  ))
   .add('Simple', () => <Select items={['one', 'two', 'three']} />)
-  .add('Complex values', () => <SelectWrapper />);
+  .add('Complex values', () => <SelectWrapper />)
+  .add('With null', () => <SelectWithNull />);
