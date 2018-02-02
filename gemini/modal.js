2e9ca580cd39f780b8d3284f5af4dafea467b05f
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-02-02 21:52:06 +0500

    [Storybook] updated font updated screenshots

--- gemini/modal.js
+++ gemini/modal.js
@@ -5,12 +5,11 @@ var pathTo = require('./utils').pathTo;
 gemini.suite('modal', suite => {
   suite
     .setUrl(pathTo('Modal', 'Modal over another modal'))
-    .setCaptureElements('body')
+    .setCaptureElements('html')
     .capture('open first modal', (actions, find) => {
       actions.click(find('button'));
     })
     .capture('open second modal', (actions, find) => {
       actions.click(find('[class^="Modal-root"] button'));
-      actions.mouseMove(find('body'), [0, 0]);
     });
 });
