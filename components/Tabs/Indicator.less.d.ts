59a956c892c3a2983b12c9d82463f94c37f6d979
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-03-21 16:03:48 +0500

    Compile target -> es3

--- components/Tabs/Indicator.less.d.ts
+++ components/Tabs/Indicator.less.d.ts
@@ -1,1 +1,5 @@
 export const root: string;
+export const primary: string;
+export const success: string;
+export const warning: string;
+export const error: string;
--- components/Tabs/Tab.less.d.ts
+++ components/Tabs/Tab.less.d.ts
@@ -4,3 +4,7 @@ export const active: string;
 export const focus: string;
 export const input: string;
 export const disabled: string;
+export const primary: string;
+export const success: string;
+export const warning: string;
+export const error: string;
