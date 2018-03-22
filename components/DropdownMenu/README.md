28335c842f9d95fd1748790921811b8d6f8b4b39
21alexander21 <21alexander21@gmail.com>
2018-03-22 18:08:49 +0500

    update docs

--- components/DropdownMenu/README.md
+++ components/DropdownMenu/README.md
@@ -1,85 +1,67 @@
-DropdownMenu example
+Простой пример
 
 ```js
-const Gapped = require("../Gapped").default;
-const Link = require("../Link").default;
-const Menu = require("../Menu/Menu").default;
+<DropdownMenu
+  caption={<Button use="primary">Открыть меню</Button>}
+>
+  <MenuHeader>Заголовок меню</MenuHeader>
+  <MenuSeparator />
+  <MenuItem>Раз</MenuItem>
+  <MenuItem>Два</MenuItem>
+  <MenuItem>Три</MenuItem>
+  <MenuSeparator />
+  <MenuItem>Раз</MenuItem>
+  <MenuItem>Два</MenuItem>
+  <MenuItem>Три</MenuItem>
+</DropdownMenu>
+```
+
+С указанием ширины меню
+
+```js
+<DropdownMenu
+  caption={<Button use="primary">Открыть меню</Button>}
+  menuWidth={350}
+>
+  <MenuHeader>Заголовок меню</MenuHeader>
+  <MenuSeparator />
+  <MenuItem>Раз</MenuItem>
+  <MenuItem>Два</MenuItem>
+  <MenuItem>Три</MenuItem>
+  <MenuSeparator />
+  <MenuItem>Раз</MenuItem>
+  <MenuItem>Два</MenuItem>
+  <MenuItem>Три</MenuItem>
+</DropdownMenu>
+```
+
+С указанием максимальной высоты меню
+
+```js
+<DropdownMenu
+  caption={<Button use="primary">Открыть меню</Button>}
+  menuMaxHeight={150}
+>
+  <MenuHeader>Заголовок меню</MenuHeader>
+  <MenuSeparator />
+  <MenuItem>Раз</MenuItem>
+  <MenuItem>Два</MenuItem>
+  <MenuItem>Три</MenuItem>
+  <MenuSeparator />
+  <MenuItem>Раз</MenuItem>
+  <MenuItem>Два</MenuItem>
+  <MenuItem>Три</MenuItem>
+</DropdownMenu>
+```
+
+В ```caption``` можно передать любой элемент
+
+```js
 const Icon = require("../Icon").default;
 
-
-<Gapped>
-  <DropdownMenu
-      caption={<Button use="primary">Открыть меню</Button>}
-      menuMaxHeight={50}
-      menuWidth={350}
-  >
-    <MenuHeader>Заголовок меню</MenuHeader>
-    <MenuSeparator />
-    <MenuItem>Раз</MenuItem>
-    <MenuItem>Два</MenuItem>
-    <MenuItem>Три</MenuItem>
-    <MenuSeparator />
-    <MenuItem>Раз</MenuItem>
-    <MenuItem>Два</MenuItem>
-    <MenuItem>Три</MenuItem>
-  </DropdownMenu>
-  <DropdownMenu
-      onItemClick={(any) => { console.log('Клик по айтему:', any); }}
-      caption={<Link use="danger" >Ссылкой открыть меню</Link>}
-  >
-    <MenuHeader>Заголовок меню</MenuHeader>
-    <MenuSeparator />
-    <MenuItem>Раз</MenuItem>
-    <MenuItem>Два</MenuItem>
-    <MenuItem>Три</MenuItem>
-  </DropdownMenu>
-  <Menu>
-    <MenuHeader>Заголовок меню</MenuHeader>
-    <MenuSeparator />
-    <DropdownMenu
-        onItemClick={(any) => { console.log('Клик по айтему:', any); }}
-        caption={<MenuItem>Пункт меню 1</MenuItem>}
-    >
-      <MenuHeader>Заголовок меню</MenuHeader>
-      <MenuSeparator />
-      <MenuItem>Раз</MenuItem>
-      <MenuItem>Два</MenuItem>
-      <MenuItem>Три</MenuItem>
-    </DropdownMenu>
-    <DropdownMenu
-        onItemClick={(any) => { console.log('Клик по айтему:', any); }}
-        caption={<MenuItem>Пункт меню 2</MenuItem>}
-    >
-      <MenuHeader>Заголовок меню</MenuHeader>
-      <MenuSeparator />
-      <MenuItem>Раз</MenuItem>
-      <MenuItem>Два</MenuItem>
-      <MenuItem>Три</MenuItem>
-    </DropdownMenu>
-    <DropdownMenu
-        onItemClick={(any) => { console.log('Клик по айтему:', any); }}
-        caption={<MenuItem>Пункт меню 3</MenuItem>}
-    >
-      <MenuHeader>Заголовок меню</MenuHeader>
-      <MenuSeparator />
-      <MenuItem>Раз</MenuItem>
-      <MenuItem>Два</MenuItem>
-      <MenuItem>Три</MenuItem>
-    </DropdownMenu>
-  </Menu>
-  <DropdownMenu
-    caption={<Icon name="Error" size={32} />}
-  >
-  </DropdownMenu>
-  <DropdownMenu
-    caption={<Icon name="Dot12" size={32} />}
-    menuWidth="300px"
-  >
-    <MenuItem>Раз</MenuItem>
-    <MenuSeparator />
-    <MenuItem>Два</MenuItem>
-    <MenuSeparator />
-    <MenuItem>Три</MenuItem>
-  </DropdownMenu>
-</Gapped>
+<DropdownMenu caption={<Icon name="Menu" size={32} />} menuWidth="300px">
+  <MenuItem>Раз</MenuItem>
+  <MenuItem>Два</MenuItem>
+  <MenuItem>Три</MenuItem>
+</DropdownMenu>
 ```
--- components/DropdownMenu/__stories__/DropdownMenu.stories.js
+++ components/DropdownMenu/__stories__/DropdownMenu.stories.js
@@ -5,89 +5,58 @@ import MenuHeader from '../../MenuHeader';
 import MenuSeparator from '../../MenuSeparator';
 import DropdownMenu from '../DropdownMenu';
 import Button from '../../Button';
-import Gapped from '../../Gapped';
-import Link from '../../Link';
 import Icon from '../../Icon';
-import Menu from '../../Menu/Menu';
 
-storiesOf('DropdownMenu', module).add('Simple example', () => {
-  return (
-    <Gapped>
-      <DropdownMenu
-        caption={<Button use="primary">Открыть меню</Button>}
-        menuMaxHeight={50}
-        menuWidth={350}
-      >
-        <MenuHeader>Заголовок меню</MenuHeader>
-        <MenuSeparator />
-        <MenuItem>Раз</MenuItem>
-        <MenuItem>Два</MenuItem>
-        <MenuItem>Три</MenuItem>
-        <MenuSeparator />
-        <MenuItem>Раз</MenuItem>
-        <MenuItem>Два</MenuItem>
-        <MenuItem>Три</MenuItem>
-      </DropdownMenu>
-      <DropdownMenu
-        onItemClick={any => {
-          console.log('Клик по айтему:', any);
-        }}
-        caption={<Link use="danger">Ссылкой открыть меню</Link>}
-      >
-        <MenuHeader>Заголовок меню</MenuHeader>
-        <MenuSeparator />
-        <MenuItem>Раз</MenuItem>
-        <MenuItem>Два</MenuItem>
-        <MenuItem>Три</MenuItem>
-      </DropdownMenu>
-      <Menu>
-        <MenuHeader>Заголовок меню</MenuHeader>
-        <MenuSeparator />
-        <DropdownMenu
-          onItemClick={any => {
-            console.log('Клик по айтему:', any);
-          }}
-          caption={<MenuItem>Пункт меню 1</MenuItem>}
-        >
-          <MenuHeader>Заголовок меню</MenuHeader>
-          <MenuSeparator />
-          <MenuItem>Раз</MenuItem>
-          <MenuItem>Два</MenuItem>
-          <MenuItem>Три</MenuItem>
-        </DropdownMenu>
-        <DropdownMenu
-          onItemClick={any => {
-            console.log('Клик по айтему:', any);
-          }}
-          caption={<MenuItem>Пункт меню 2</MenuItem>}
-        >
-          <MenuHeader>Заголовок меню</MenuHeader>
-          <MenuSeparator />
-          <MenuItem>Раз</MenuItem>
-          <MenuItem>Два</MenuItem>
-          <MenuItem>Три</MenuItem>
-        </DropdownMenu>
-        <DropdownMenu
-          onItemClick={any => {
-            console.log('Клик по айтему:', any);
-          }}
-          caption={<MenuItem>Пункт меню 3</MenuItem>}
-        >
-          <MenuHeader>Заголовок меню</MenuHeader>
-          <MenuSeparator />
-          <MenuItem>Раз</MenuItem>
-          <MenuItem>Два</MenuItem>
-          <MenuItem>Три</MenuItem>
-        </DropdownMenu>
-      </Menu>
-      <DropdownMenu caption={<Icon name="Error" size={32} />} />
-      <DropdownMenu caption={<Icon name="Dot12" size={32} />} menuWidth="300px">
-        <MenuItem>Раз</MenuItem>
-        <MenuSeparator />
-        <MenuItem>Два</MenuItem>
-        <MenuSeparator />
-        <MenuItem>Три</MenuItem>
-      </DropdownMenu>
-    </Gapped>
-  );
-});
+storiesOf('DropdownMenu', module)
+  .add('Простой пример', () => (
+    <DropdownMenu caption={<Button use="primary">Открыть меню</Button>}>
+      <MenuHeader>Заголовок меню</MenuHeader>
+      <MenuSeparator />
+      <MenuItem>Раз</MenuItem>
+      <MenuItem>Два</MenuItem>
+      <MenuItem>Три</MenuItem>
+      <MenuSeparator />
+      <MenuItem>Раз</MenuItem>
+      <MenuItem>Два</MenuItem>
+      <MenuItem>Три</MenuItem>
+    </DropdownMenu>
+  ))
+  .add('С указанием ширины меню', () => (
+    <DropdownMenu
+      caption={<Button use="primary">Открыть меню</Button>}
+      menuWidth={350}
+    >
+      <MenuHeader>Заголовок меню</MenuHeader>
+      <MenuSeparator />
+      <MenuItem>Раз</MenuItem>
+      <MenuItem>Два</MenuItem>
+      <MenuItem>Три</MenuItem>
+      <MenuSeparator />
+      <MenuItem>Раз</MenuItem>
+      <MenuItem>Два</MenuItem>
+      <MenuItem>Три</MenuItem>
+    </DropdownMenu>
+  ))
+  .add('С указанием максимальной высоты меню', () => (
+    <DropdownMenu
+      caption={<Button use="primary">Открыть меню</Button>}
+      menuMaxHeight={150}
+    >
+      <MenuHeader>Заголовок меню</MenuHeader>
+      <MenuSeparator />
+      <MenuItem>Раз</MenuItem>
+      <MenuItem>Два</MenuItem>
+      <MenuItem>Три</MenuItem>
+      <MenuSeparator />
+      <MenuItem>Раз</MenuItem>
+      <MenuItem>Два</MenuItem>
+      <MenuItem>Три</MenuItem>
+    </DropdownMenu>
+  ))
+  .add('В caption можно передать любой элемент', () => (
+    <DropdownMenu caption={<Icon name="Menu" size={32} />} menuWidth="300px">
+      <MenuItem>Раз</MenuItem>
+      <MenuItem>Два</MenuItem>
+      <MenuItem>Три</MenuItem>
+    </DropdownMenu>
+  ));
