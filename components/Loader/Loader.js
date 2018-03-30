79bda63db5c0de808b42e3fdf918330bbc979bd9
21alexander21 <21alexander21@gmail.com>
2018-03-30 17:03:17 +0500

    пример реализации липкого лоадера

--- components/Loader/Loader.js
+++ components/Loader/Loader.js
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import debounce from 'lodash.debounce';
 
 import Spinner from '../Spinner';
 import styles from './Loader.less';
@@ -14,10 +15,15 @@ type Props = {
   type?: 'mini' | 'normal' | 'big'
 };
 
+type State = {
+  spinnerTopOffset: number,
+  stickySpinner: boolean
+};
+
 /**
  * DRAFT - лоадер-контейнер
  */
-class Loader extends React.Component<Props> {
+class Loader extends React.Component<Props, State> {
   static defaultProps = {
     type: Spinner.Types.normal
   };
@@ -50,9 +56,77 @@ class Loader extends React.Component<Props> {
     type: PropTypes.oneOf(Object.keys(Spinner.Types))
   };
 
+  _containerNode: ?HTMLDivElement;
+
+  state = {
+    spinnerTopOffset: 0,
+    stickySpinner: false
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', debounce(this._moveSpinner, 10));
+    this._moveSpinner();
+  }
+
+  _moveSpinner = () => {
+    if (!this._containerNode) {
+      return;
+    }
+
+    const {
+      top: containerTop,
+      height: containerHeight
+    } = this._containerNode.getBoundingClientRect();
+
+    const hiddenTop = Math.abs(Math.min(0, containerTop));
+    const hiddenBottom = Math.abs(
+      Math.min(0, window.innerHeight - containerTop - containerHeight)
+    );
+    const visibleHeight = containerHeight - hiddenBottom - hiddenTop;
+
+    if (visibleHeight < 0) {
+      this.setState({
+        stickySpinner: false,
+        spinnerTopOffset: 0
+      });
+      return;
+    }
+
+    if (hiddenTop || hiddenBottom) {
+      const spinnerTop = hiddenTop + visibleHeight / 2;
+
+      if (spinnerTop < 30) {
+        return;
+      }
+
+      if (spinnerTop > containerHeight - 30) {
+        return;
+      }
+
+      this.setState({
+        stickySpinner: true,
+        spinnerTopOffset: spinnerTop
+      });
+    } else {
+      this.setState({
+        stickySpinner: false,
+        spinnerTopOffset: 0
+      });
+    }
+  };
+
   _renderSpinner(type, caption) {
     return (
-      <span className={styles.spinnerContainerCenter}>
+      <span
+        className={
+          this.state.stickySpinner
+            ? styles.spinnerContainerSticky
+            : styles.spinnerContainerCenter
+        }
+        style={{
+          top: this.state.spinnerTopOffset
+        }}
+      >
         <Spinner type={type} caption={caption} />
       </span>
     );
@@ -65,7 +139,12 @@ class Loader extends React.Component<Props> {
     });
 
     return (
-      <div className={loaderClassName}>
+      <div
+        className={loaderClassName}
+        ref={elelment => {
+          this._containerNode = elelment;
+        }}
+      >
         {this.props.children}
 
         {active && this._renderSpinner(type, caption)}
