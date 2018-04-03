fd86535a4fac5deddbb0b238116e4b1200def9fd
Mikhail Shatikhin <shatikhin@skbkontur.ru>
2018-04-03 17:18:04 +0500

    Rename some icon names 'ok' -> 'Ok'

--- components/Link/__tests__/Link.stories.js
+++ components/Link/__tests__/Link.stories.js
@@ -6,14 +6,14 @@ import Toast from '../../Toast';
 
 storiesOf('Link', module)
   .add('Simple', () => <Link>Simple Link</Link>)
-  .add('With Icon', () => <Link icon="ok">Simple Link</Link>)
-  .add('Danger', () =>
-    <Link icon="ok" use="danger">
+  .add('With Icon', () => <Link icon="Ok">Simple Link</Link>)
+  .add('Danger', () => (
+    <Link icon="Ok" use="danger">
       Simple Link
     </Link>
-  )
+  ))
   .add('Grayed', () => <Link use="grayed">Simple link</Link>)
   .add('Disabled', () => <Link disabled>Simple link</Link>)
-  .add('With onClick', () =>
+  .add('With onClick', () => (
     <Link onClick={() => Toast.push('Clicked!')}>Simple Link</Link>
-  );
+  ));
