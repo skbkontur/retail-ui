a458cca54d50e28d4c6f2d17d272b29e620cefc3
Timur Khazamov <bboytx@gmail.com>
2018-04-28 16:58:31 +0500

    Revert "Toggle (#517)"
    This reverts commit cac62be63d5c812e61557213dc97dbbeeb8d4b14.

--- components/Toggle/Toggle.js
+++ components/Toggle/Toggle.js
@@ -3,39 +3,23 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Upgrades from '../../lib/Upgrades';
-
-const isFlatDesign = Upgrades.isFlatDesignEnabled();
-
-const CssStyles = require('./Toggle.less');
-
-let styles: typeof CssStyles;
-styles = isFlatDesign
-  ? require('./Toggle.flat.less')
-  : require('./Toggle.less');
+import styles from './Toggle.less';
 
 type Props = {
   checked: boolean,
   disabled?: boolean,
-  onChange?: (value: boolean) => void,
-  warning?: boolean,
-  error?: boolean,
-  loading?: boolean
+  onChange?: (value: boolean) => void
 };
 
 class Toggle extends React.Component<Props> {
   static propTypes = {
     checked: PropTypes.bool.isRequired,
     disabled: PropTypes.bool,
-    error: PropTypes.bool,
-    loading: PropTypes.bool,
-    warning: PropTypes.bool,
     onChange: PropTypes.func
   };
 
   static defaultProps = {
-    disabled: false,
-    loading: false
+    disabled: false
   };
 
   handleClick = () => {
@@ -47,26 +31,19 @@ class Toggle extends React.Component<Props> {
   };
 
   render() {
-    const { checked, disabled, warning, error, loading } = this.props;
+    const { checked, disabled } = this.props;
 
     const containerClassName = classNames(styles.container, {
       [styles.isChecked]: checked,
-      [styles.isDisabled]: disabled,
-      [styles.isDefault]: true,
-      [styles.isWarning]: warning,
-      [styles.isError]: error,
-      [styles.isLoading]: loading
+      [styles.isDisabled]: disabled
     });
 
     return (
       <span className={containerClassName} onClick={this.handleClick}>
-        <span className={styles.bg__container}>
-          <span className={styles.bg__wrap}>
-            <span className={styles.bg__left} />
-            <span className={styles.bg__right} />
-          </span>
+        <span className={styles.handle}>
+          <div className={styles.bg} />
+          <span className={styles.hinge} />
         </span>
-        <span className={styles.handle} />
       </span>
     );
   }
