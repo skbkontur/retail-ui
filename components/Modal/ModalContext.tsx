cebfc208e09aaac505dd0c44f7b218029db1763b
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-05-31 18:34:02 +0500

    [build] — Fixed build

--- components/Modal/ModalContext.tsx
+++ components/Modal/ModalContext.tsx
@@ -1,3 +1,5 @@
+// FIXME: remove after upgrading ts to 2.9.1
+// @ts-ignore Used for generating declarations
 import * as React from 'react';
 import createReactContext = require('create-react-context');
 
