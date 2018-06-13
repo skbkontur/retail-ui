988c137044ff48c43d6fc72a538107541afdc05d
Timur Khazamov <t1mmaas@skbkontur.ru>
2018-06-13 17:42:48 +0500

    added sh script

---
+++ codemodes/flow-to-ts/ts.convert-dir.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "================================================================"
+echo "=                      RENAME                                  ="
+echo "================================================================"
+node scripts/rename.js "$@"
+
+
+echo "================================================================"
+echo "=                      APPLY CODE SHIFT                        ="
+echo "================================================================"
+yarn jscodeshift -t transform-stage-1.js --extensions tsx --parser flow "$@"
+yarn jscodeshift -t transform-stage-2.js --extensions tsx --parser babylon "$@"
+
+echo "================================================================"
+echo "=                      DONE                                    ="
+echo "================================================================"
