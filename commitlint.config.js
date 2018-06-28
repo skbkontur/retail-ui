d396f7327203f871a63678a1ec71032b1b0b61bc
21alexander21 <21alexander21@gmail.com>
2018-06-28 19:10:28 +0500

    chore: rule for scope name

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   extends: [
     '@commitlint/config-conventional'
-  ]
+  ],
+  utils: { getComponents },
+	rules: {
+		'scope-enum': ctx => [2, 'always', getComponents(ctx)]
+	}
 };
+
+function getComponents(context) {
+  const ctx = context || {};
+  const cwd = ctx.cwd || process.cwd();
+  const componentsDir = path.join(cwd, 'packages/retail-ui/components/');
+  const components = fs.readdirSync(componentsDir).map(item => path.join(componentsDir, item)).filter(item => fs.lstatSync(item).isDirectory());
+  
+  return components || [];
+}
