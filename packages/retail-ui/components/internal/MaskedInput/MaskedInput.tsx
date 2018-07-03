a40dfae0cbbb82dbedd3d1e1286c5ff25f033fb3
21alexander21 <21alexander21@gmail.com>
2018-07-03 19:05:09 +0500

    chore(masked-input): мелкие работы по инпуту
    - почищены код
    - перемещение каретки вперед при вводе значения

--- packages/retail-ui/components/internal/MaskedInput/MaskedInput.tsx
+++ packages/retail-ui/components/internal/MaskedInput/MaskedInput.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import InputMask from 'inputmask-core';
-import Input, { InputProps } from '../../Input';
+import { InputProps } from '../../Input';
 import styles = require('./MaskedInput.less');
+
+export interface Selection {
+  start: number;
+  end: number;
+}
 
 export type MaskedInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   mask: string;
@@ -10,9 +15,8 @@ export type MaskedInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 };
 
 export interface MaskedInputState {
-  value: MaskedInputProps['value'];
-  maskedValue: string;
-  focus: boolean;
+  value: React.InputHTMLAttributes<HTMLInputElement>['value'];
+  selection: Selection;
 }
 
 export default class MaskedInput extends React.Component<
@@ -21,8 +25,10 @@ export default class MaskedInput extends React.Component<
 > {
   public state: MaskedInputState = {
     value: this.props.value || '',
-    maskedValue: '',
-    focus: false
+    selection: {
+      start: 0,
+      end: 0
+    }
   };
 
   private input: Nullable<HTMLInputElement>;
@@ -32,134 +38,139 @@ export default class MaskedInput extends React.Component<
     placeholderChar: this.props.maskChar || ''
   });
 
+  private emptyValue: string = this.mask.emptyValue;
+
+  public componentDidMount() {
+    this.mask.pattern.isRevealingMask = true;
+  }
+
+  public componentDidUpdate(
+    _prevProps: MaskedInputProps,
+    prevState: MaskedInputState
+  ) {
+    if (
+      prevState.value !== this.state.value &&
+      this.state.value &&
+      prevState.value
+    ) {
+      this.moveCaret(
+        prevState.value.toString().length < this.state.value.toString().length
+      );
+    }
+  }
+
   public render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', padding: '4px' }}>
-        <input
-          onChange={this.handleChange}
-          onBlur={this.handleBlur}
-          value={this.state.value}
-          ref={element => {
-            this.input = element;
-          }}
-          style={{
-            position: 'absolute',
-            width: 0,
-            height: 0,
-            padding: 0,
-            outline: 'none',
-            opacity: 0
-          }}
-        />
-        <span
-          tabIndex={1}
-          onFocus={this.handleFocus}
-          style={{
-            display: 'inline-block',
-            border: '1px solid #d9d9d9',
-            borderTopColor: '#b2b2b2',
-            fontSize: '14px',
-            lineHeight: '20px',
-            padding: '6px 10px',
-            background: '#fff'
-          }}
-        >
-          {this.mask.getValue()}
-        </span>
-        <p>{this.getFormatedValue()}</p>
-        <div
-          className={styles.inputContainer}
-          style={{ padding: '10px', position: 'relative' }}
-        >
-          <Input value={this.getFormatedValue()} />
-          <span
+      <div
+        style={{ display: 'flex', flexDirection: 'column', padding: '4px' }}
+        className={styles.container}
+      >
+        <div style={{ position: 'relative' }}>
+          <input
+            className={styles.container}
+            value={this.state.value}
+            onChange={this.handleChange}
             style={{
-              position: 'absolute',
-              width: '100%',
-              top: 9,
-              left: 9,
-              padding: '8px 12px',
+              display: 'inline-block',
+              border: '1px solid #d9d9d9',
+              borderTopColor: '#b2b2b2',
+              fontSize: '14px',
               lineHeight: '20px',
-              pointerEvents: 'none',
-              opacity: 0.7
+              padding: '6px 10px',
+              background: '#fff',
+              font: 'inherit'
             }}
-          >
-            {this.mask.emptyValue}
+            ref={element => {
+              this.input = element;
+            }}
+          />
+          <span className={styles.inputMask}>
+            <span
+              style={{
+                color: 'transparent'
+              }}
+            >
+              {this.emptyValue.slice(0, this.mask.getValue().length)}
+            </span>
+            {this.emptyValue.slice(this.mask.getValue().length)}
           </span>
         </div>
       </div>
     );
   }
 
-  private getFormatedValue = () => {
-    const formatedInput: string = this.mask.getValue();
-    const { placeholderChar } = this.mask.pattern;
-
-    return formatedInput
-      .split('')
-      .map((char, index) => {
-        if (char === placeholderChar) {
-          return ' ';
-        }
-
-        return char;
-      })
-      .join('');
-  };
-
-  private handleFocus = () => {
-    if (this.input) {
-      this.setState({ focus: true }, () => {
-        if (this.input) {
-          this.input.focus();
-        }
-      });
-    }
-  };
-
-  private handleBlur = () => {
-    this.setState({ focus: false });
-  };
-
   private handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    this.mask.setValue(value);
+    let { value } = event.target;
+
+    if (!this.input) {
+      return;
+    }
+
+    const selection = {
+      start: this.input.selectionStart || 0,
+      end: this.input.selectionEnd || 0
+    };
+
+    if (value.length < this.mask.getValue().length) {
+      if (!this.mask.pattern.isEditableIndex(this.input.selectionEnd)) {
+        value = value.slice(0, value.length - 1);
+      }
+    }
+
+    this.mask.setValue(this.getRawValue(value));
+
     this.setState({
-      value: this.getTrimedValue(),
-      maskedValue: this.mask.getValue()
+      value: this.mask.getValue(),
+      selection
     });
   };
 
-  private getTrimedValue = () => {
-    const rawValue = this.mask.getRawValue();
+  private getRawValue = (value: string) =>
+    value
+      .split('')
+      .filter((char, index) => {
+        const isValidChar = this.mask.input(char);
 
-    return rawValue.replace(
-      new RegExp(this.mask.pattern.placeholderChar, 'g'),
-      ''
-    );
-  };
+        if (isValidChar) {
+          this.mask.backspace();
+          return true;
+        }
 
-  private findFirstNoMaskChar = (
-    position: number,
-    direction: 'left' | 'right'
-  ) => {
-    let firstPosition = direction === 'left' ? position - 1 : position + 1;
+        return false;
+      })
+      .join('');
 
-    if (direction === 'left' && firstPosition < 0) {
-      return 0;
+  private moveCaret = (moveBack?: boolean) => {
+    if (!this.state.value) {
+      return;
+    }
+    const { start } = this.state.selection;
+    let newCaretStart = start;
+    // let newCaretEnd = end;
+
+    this.state.value
+      .toString()
+      .split('')
+      .forEach((_char, index) => {
+        if (index < start) {
+          return;
+        }
+
+        if (newCaretStart !== start) {
+          return;
+        }
+
+        if (this.mask.pattern.isEditableIndex(index)) {
+          newCaretStart = index + 1;
+        }
+      });
+
+    if (this.input) {
+      this.input.setSelectionRange(newCaretStart, newCaretStart);
     }
 
-    if (direction === 'right' && firstPosition > this.mask.emptyValue.length) {
-      return this.mask.emptyValue.length;
+    if (moveBack) {
+      return;
     }
-
-    if (
-      this.mask.emptyValue[firstPosition] &&
-      this.mask.emptyValue[firstPosition] !== this.mask.pattern.placeholderChar
-    ) {
-      firstPosition = this.findFirstNoMaskChar(firstPosition, direction);
-    }
-
-    return firstPosition;
   };
 }
