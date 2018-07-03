3df8eee5603ffa468c41eb65e7300acffdbb45ee
21alexander21 <21alexander21@gmail.com>
2018-07-03 12:59:40 +0500

    chore(masked-input): инпут отдельно, маска отдельно

--- packages/retail-ui/components/internal/MaskedInput/MaskedInput.tsx
+++ packages/retail-ui/components/internal/MaskedInput/MaskedInput.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import InputMask from 'inputmask-core';
-import { InputProps } from '../../Input';
+import Input, { InputProps } from '../../Input';
+import styles = require('./MaskedInput.less');
 
 export type MaskedInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   mask: string;
@@ -11,6 +12,7 @@ export type MaskedInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
 export interface MaskedInputState {
   value: MaskedInputProps['value'];
   maskedValue: string;
+  focus: boolean;
 }
 
 export default class MaskedInput extends React.Component<
@@ -19,8 +21,11 @@ export default class MaskedInput extends React.Component<
 > {
   public state: MaskedInputState = {
     value: this.props.value || '',
-    maskedValue: ''
+    maskedValue: '',
+    focus: false
   };
+
+  private input: Nullable<HTMLInputElement>;
 
   private mask: any = new InputMask({
     pattern: this.props.mask.replace(/9/g, '1'),
@@ -29,14 +34,92 @@ export default class MaskedInput extends React.Component<
 
   public render() {
     return (
-      <div>
-        <input onChange={this.handleChange} value={this.state.value} />
-        <div>
-          <strong>{this.mask.getValue()}</strong>
+      <div style={{ display: 'flex', flexDirection: 'column', padding: '4px' }}>
+        <input
+          onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          value={this.state.value}
+          ref={element => {
+            this.input = element;
+          }}
+          style={{
+            position: 'absolute',
+            width: 0,
+            height: 0,
+            padding: 0,
+            outline: 'none',
+            opacity: 0
+          }}
+        />
+        <span
+          tabIndex={1}
+          onFocus={this.handleFocus}
+          style={{
+            display: 'inline-block',
+            border: '1px solid #d9d9d9',
+            borderTopColor: '#b2b2b2',
+            fontSize: '14px',
+            lineHeight: '20px',
+            padding: '6px 10px',
+            background: '#fff'
+          }}
+        >
+          {this.mask.getValue()}
+        </span>
+        <p>{this.getFormatedValue()}</p>
+        <div
+          className={styles.inputContainer}
+          style={{ padding: '10px', position: 'relative' }}
+        >
+          <Input value={this.getFormatedValue()} />
+          <span
+            style={{
+              position: 'absolute',
+              width: '100%',
+              top: 9,
+              left: 9,
+              padding: '8px 12px',
+              lineHeight: '20px',
+              pointerEvents: 'none',
+              opacity: 0.7
+            }}
+          >
+            {this.mask.emptyValue}
+          </span>
         </div>
       </div>
     );
   }
+
+  private getFormatedValue = () => {
+    const formatedInput: string = this.mask.getValue();
+    const { placeholderChar } = this.mask.pattern;
+
+    return formatedInput
+      .split('')
+      .map((char, index) => {
+        if (char === placeholderChar) {
+          return ' ';
+        }
+
+        return char;
+      })
+      .join('');
+  };
+
+  private handleFocus = () => {
+    if (this.input) {
+      this.setState({ focus: true }, () => {
+        if (this.input) {
+          this.input.focus();
+        }
+      });
+    }
+  };
+
+  private handleBlur = () => {
+    this.setState({ focus: false });
+  };
 
   private handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
