d977352ac9ac0ee6a316dec1f0a7a8062ea52eea
21alexander21 <21alexander21@gmail.com>
2018-07-06 14:57:09 +0500

    refactor(fx-input): refactor types

--- packages/retail-ui/components/FxInput/FxInput.tsx
+++ packages/retail-ui/components/FxInput/FxInput.tsx
@@ -10,13 +10,16 @@ import CurrencyInput, { CurrencyInputProps } from '../CurrencyInput';
 import { createPropsGetter } from '../internal/createPropsGetter';
 import { InputType } from '../Input/Input';
 
-export interface FxInputProps extends Omit<CurrencyInputProps & InputProps, 'type' | 'value' | 'onChange'> {
-  auto?: boolean,
-  type?: 'currency' | InputType;
-  value?: React.ReactText | CurrencyInputProps['value'] | InputProps['value'];
-  onRestore?: () => void;
-  onChange?: CurrencyInputProps['onChange'] | InputProps['onChange'];
-};
+export type FxInputProps = Override<
+  CurrencyInputProps,
+  {
+    auto?: boolean;
+    type?: 'currency' | InputProps['type'];
+    onRestore?: () => void;
+    onChange: CurrencyInputProps['onChange'] | InputProps['onChange'];
+    value?: React.ReactText;
+  }
+>;
 
 export interface FxInputDefaultProps {
   width: FxInputProps['width'];
@@ -31,16 +34,17 @@ class FxInput extends React.Component<FxInputProps> {
 
   public static defaultProps: FxInputDefaultProps = {
     width: 250,
-    type: 'text' as 'currency' | InputType
+    type: 'text'
   };
 
   private getProps = createPropsGetter(FxInput.defaultProps);
 
   public render(): JSX.Element {
+    const { type, ...rest } = this.props;
     const inputProps: {
-      align: InputProps['align'],
-      mainInGroup: boolean,
-      leftIcon?: React.ReactNode
+      align: InputProps['align'];
+      mainInGroup: boolean;
+      leftIcon?: React.ReactNode;
     } = {
       align: 'right',
       mainInGroup: true
@@ -65,22 +69,22 @@ class FxInput extends React.Component<FxInputProps> {
     return (
       <Group width={this.props.width}>
         {button}
-        {
-          this.getProps().type === 'currency' ?
+        {this.getProps().type === 'currency' ? (
           <CurrencyInput
             {...inputProps}
-            {...this.props}
+            {...rest}
             value={this.props.value as CurrencyInputProps['value']}
             onChange={this.props.onChange as CurrencyInputProps['onChange']}
-          /> :
+          />
+        ) : (
           <Input
             {...inputProps}
-            {...this.props}
+            {...rest}
             type={this.props.type as InputType}
             value={this.props.value as InputProps['value']}
             onChange={this.props.onChange as InputProps['onChange']}
           />
-        }
+        )}
       </Group>
     );
   }
