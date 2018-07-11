b7fd374294183590c8061aa53d07c9c0c18edc12
21alexander21 <21alexander21@gmail.com>
2018-07-11 15:15:19 +0500

    chore(input): remove className propery from interface

--- packages/retail-ui/components/Input/Input.tsx
+++ packages/retail-ui/components/Input/Input.tsx
@@ -44,6 +44,7 @@ export type InputProps = Override<
     onMouseOver?: React.MouseEventHandler<HTMLLabelElement>;
     type?: InputType;
     value?: string;
+    className?: undefined;
   }
 >;
 
@@ -162,7 +163,6 @@ class Input extends React.Component<InputProps, InputState> {
     const labelProps = {
       className: classNames({
         [classes.root]: true,
-        [className || '']: true,
         [classes.disabled]: disabled,
         [classes.error]: error,
         [classes.warning]: warning,
