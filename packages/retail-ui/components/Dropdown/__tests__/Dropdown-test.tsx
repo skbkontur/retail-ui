6de4998c61335e2a0d826b3169d368ac28325f0b
21alexander21 <21alexander21@gmail.com>
2018-07-24 17:35:19 +0500

    test(Dropdown): еще несколько тестов

--- packages/retail-ui/components/Dropdown/__tests__/Dropdown-test.tsx
+++ packages/retail-ui/components/Dropdown/__tests__/Dropdown-test.tsx
@@ -2,7 +2,7 @@ import { mount } from 'enzyme';
 import * as React from 'react';
 import Dropdown from '../';
 import MenuItem from '../../MenuItem';
-import Select from '../../Select';
+import Select, { SelectState } from '../../Select';
 
 describe('Dropdown', () => {
   it('It renders', () => {
@@ -15,6 +15,25 @@ describe('Dropdown', () => {
     expect(wrapper.exists());
   });
 
+  it('It opens and closes', () => {
+    const button = <button id="test-button">Open</button>;
+    const menuItem = <MenuItem>Menu item</MenuItem>;
+    const wrapper = mount(<Dropdown caption={button}>{menuItem}</Dropdown>);
+
+    wrapper.find('#test-button').simulate('click');
+
+    const selectInstance = wrapper.find(Select).instance();
+
+    expect(
+      (selectInstance.state as SelectState<JSX.Element>).opened
+    ).toBeTruthy();
+
+    wrapper.find('#test-button').simulate('click');
+    expect(
+      (selectInstance.state as SelectState<JSX.Element>).opened
+    ).toBeFalsy();
+  });
+
   it('Pass props to select', () => {
     const button = <button id="test-button">Open</button>;
     const menuItem = <MenuItem>Menu item</MenuItem>;
@@ -24,6 +43,7 @@ describe('Dropdown', () => {
 
     expect(select.prop('value')).toEqual(button);
     expect(select.prop('items')).toHaveLength(1);
-    // expect(select.prop('items')[0]).toEqual(menuItem);
+
+    expect(React.isValidElement(select.prop('items')[0])).toBeTruthy();
   });
 });
