1a9300366c547cb264c082e140216fa9d3a2b44e
21alexander21 <21alexander21@gmail.com>
2018-08-13 18:40:41 +0500

    feat(Tooltip): переделан компонент
    - В основе чекбокс
    - Фокус по табу
    - Переключение по пробелу
    - Новая верстка и стили
    - `color` - кастомный цвет
    - `changeEventHandler` - хэндлер нативного события
    - обновлены скриншоты gemini

--- packages/retail-ui/components/Toggle/Toggle.tsx
+++ packages/retail-ui/components/Toggle/Toggle.tsx
@@ -10,18 +10,44 @@ import FlatStyles = require('./Toggle.flat.less');
 
 const styles = isFlatDesign ? FlatStyles : DefaultStyles;
 
+let isListening: boolean;
+let tabPressed: boolean;
+
+const KEYCODE_TAB = 9;
+
+function listenTabPresses() {
+  const handler = (event: KeyboardEvent) => {
+    tabPressed = event.keyCode === KEYCODE_TAB;
+  };
+
+  if (!isListening) {
+    document.addEventListener('keydown', handler);
+    isListening = true;
+  }
+}
+
 export interface ToggleProps {
-  checked: boolean;
+  checked?: boolean;
   disabled?: boolean;
   onChange?: (value: boolean) => void;
+  changeEventHandler?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   warning?: boolean;
   error?: boolean;
   loading?: boolean;
+  autoFocus?: boolean;
+  onFocus?: React.FocusEventHandler<HTMLInputElement>;
+  onBlur?: React.FocusEventHandler<HTMLInputElement>;
+  color?: React.CSSProperties['color'];
 }
 
-class Toggle extends React.Component<ToggleProps> {
+export interface ToggleState {
+  checked: boolean;
+  focusByTab?: boolean;
+}
+
+export default class Toggle extends React.Component<ToggleProps, ToggleState> {
   public static propTypes = {
-    checked: PropTypes.bool.isRequired,
+    checked: PropTypes.bool,
     disabled: PropTypes.bool,
     error: PropTypes.bool,
     loading: PropTypes.bool,
@@ -31,46 +57,173 @@ class Toggle extends React.Component<ToggleProps> {
 
   public static defaultProps = {
     disabled: false,
-    loading: false
+    loading: false,
+    checked: false
   };
 
+  private input: HTMLInputElement | null = null;
+
+  constructor(props: ToggleProps) {
+    super(props);
+
+    this.state = {
+      checked: props.checked || false
+    };
+  }
+
+  public componentDidMount() {
+    listenTabPresses();
+
+    if (this.props.autoFocus) {
+      tabPressed = true;
+      this.focus();
+    }
+  }
+
   public render() {
-    const { checked, warning, error, loading } = this.props;
+    const { warning, error, loading, color } = this.props;
     const disabled = this.isDisabled();
 
-    const containerClassName = classNames(styles.container, {
-      [styles.isChecked]: checked,
-      [styles.isDisabled]: disabled,
-      [styles.isDefault]: true,
-      [styles.isWarning]: warning,
-      [styles.isError]: error,
-      [styles.isLoading]: loading
+    const containerClassNames = classNames(styles.container, {
+      [styles.isWarning]: !color && warning,
+      [styles.isError]: !color && error,
+      [styles.isLoading]: loading,
+      [styles.focused]: !disabled && !loading && this.state.focusByTab
     });
 
     return (
-      <span className={containerClassName} onClick={this.handleClick}>
-        <span className={styles.bg__container}>
-          <span className={styles.bg__wrap}>
-            <span className={styles.bg__left} />
-            <span className={styles.bg__right} />
-          </span>
-        </span>
-        <span className={styles.handle} />
-      </span>
+      <label
+        className={classNames(styles.wrapper, {
+          [styles.isDisabled]: disabled
+        })}
+      >
+        <input
+          type="checkbox"
+          checked={this.state.checked}
+          onChange={this.handleChange}
+          className={styles.input}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+          ref={this.inputRef}
+        />
+        <div
+          className={containerClassNames}
+          style={
+            this.state.checked && this.props.color
+              ? {
+                  backgroundColor: color,
+                  borderColor: color
+                }
+              : undefined
+          }
+        >
+          <div
+            className={styles.activeBackground}
+            style={
+              this.state.checked && this.props.color
+                ? {
+                    backgroundColor: color
+                  }
+                : undefined
+            }
+          />
+        </div>
+        <div className={styles.handle} />
+      </label>
     );
   }
 
-  private handleClick = () => {
-    const { checked, onChange } = this.props;
-    const disabled = this.isDisabled();
-    if (!disabled && onChange) {
-      onChange(!checked);
+  public componentWillReceiveProps(nextProps: ToggleProps) {
+    if (nextProps.checked && nextProps.checked !== this.state.checked) {
+      this.setState({
+        checked: nextProps.checked
+      });
     }
+  }
+
+  public focus = () => {
+    if (this.input) {
+      tabPressed = true;
+      this.input.focus();
+    }
+  };
+
+  private inputRef = (element: HTMLInputElement) => {
+    this.input = element;
+  };
+
+  private handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (this.isDisabled()) {
+      event.preventDefault();
+      return;
+    }
+
+    if (this.props.changeEventHandler) {
+      event.persist();
+    }
+
+    this.setState(
+      {
+        checked: event.target.checked
+      },
+      () => {
+        if (this.props.onChange) {
+          this.props.onChange(this.state.checked);
+        }
+
+        if (this.props.changeEventHandler) {
+          this.props.changeEventHandler(event);
+        }
+      }
+    );
+  };
+
+  private handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (this.isDisabled()) {
+      event.preventDefault();
+      return;
+    }
+
+    if (this.props.onFocus) {
+      event.persist();
+    }
+
+    // tslint:disable-next-line:no-console
+    console.log('tabPressed', tabPressed);
+
+    if (tabPressed) {
+      this.setState(
+        {
+          focusByTab: true
+        },
+        () => {
+          if (this.props.onFocus) {
+            this.props.onFocus(event);
+          }
+          tabPressed = false;
+        }
+      );
+    }
+  };
+
+  private handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (this.props.onBlur) {
+      event.persist();
+    }
+
+    this.setState(
+      {
+        focusByTab: false
+      },
+      () => {
+        if (this.props.onBlur) {
+          this.props.onBlur(event);
+        }
+      }
+    );
   };
 
   private isDisabled() {
     return this.props.disabled || this.props.loading;
   }
 }
-
-export default Toggle;
