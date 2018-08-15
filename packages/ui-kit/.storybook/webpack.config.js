fad6c197f6aa73ab36554b2a7ed452ab25bdb501
21alexander21 <21alexander21@gmail.com>
2018-08-15 18:12:48 +0500

    chore(ui-kit): ts конфиг как в retai-ui

--- packages/ui-kit/.storybook/webpack.config.js
+++ packages/ui-kit/.storybook/webpack.config.js
@@ -3,7 +3,10 @@ const path = require("path");
 module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve("ts-loader")
+    loader: require.resolve("ts-loader"),
+    options: {
+      configFile: path.resolve(__dirname, "../prod.tsconfig.json")
+    }
   });
 
   config.resolve.extensions.push(".ts", ".tsx");
