a72f72cbda759b95561e4be9a1a5e265a4b62097
21alexander21 <21alexander21@gmail.com>
2018-08-17 14:14:52 +0500

    test(Button): скриншотный тест для состояния error

--- packages/react-ui-screenshot-tests/gemini/button.js
+++ packages/react-ui-screenshot-tests/gemini/button.js
@@ -1,58 +1,66 @@
 /* global gemini */
 
-const pathTo = require('./utils').pathTo;
+const pathTo = require("./utils").pathTo;
 
 const testScenario = suite => {
   suite
-    .capture('idle')
-    .capture('hover', (actions, find) => {
-      actions.mouseMove(find('button'));
+    .capture("idle")
+    .capture("hover", (actions, find) => {
+      actions.mouseMove(find("button"));
     })
-    .capture('mouseLeave', (actions, find) => {
-      actions.mouseMove(find('body'), [0, 0]);
+    .capture("mouseLeave", (actions, find) => {
+      actions.mouseMove(find("body"), [0, 0]);
     })
-    .capture('pressed', (actions, find) => {
-      actions.mouseDown(find('button'));
+    .capture("pressed", (actions, find) => {
+      actions.mouseDown(find("button"));
     })
-    .capture('clicked', (actions, find) => {
-      actions.mouseUp(find('button'));
+    .capture("clicked", (actions, find) => {
+      actions.mouseUp(find("button"));
     })
-    .capture('clickedOutside', (actions, find) => {
-      actions.click(find('body'), 0, [1, 1]);
+    .capture("clickedOutside", (actions, find) => {
+      actions.click(find("body"), 0, [1, 1]);
     })
-    .capture('tabPress', (actions, find) => {
+    .capture("tabPress", (actions, find) => {
       actions.sendKeys(gemini.TAB);
     });
 };
 
-gemini.suite('button', suite => {
+gemini.suite("button", suite => {
   testScenario(
     suite
-      .setUrl(pathTo('Button', 'playground'))
-      .setCaptureElements('#test-element')
+      .setUrl(pathTo("Button", "playground"))
+      .setCaptureElements("#test-element")
   );
 });
 
-gemini.suite('button use link', suite => {
+gemini.suite("button use link", suite => {
   testScenario(
     suite
-      .setUrl(pathTo('Button', 'use link'))
-      .setCaptureElements('#test-element')
+      .setUrl(pathTo("Button", "use link"))
+      .setCaptureElements("#test-element")
   );
 });
 
-gemini.suite('button use link with icon', suite => {
+gemini.suite("button use link with icon", suite => {
   testScenario(
     suite
-      .setUrl(pathTo('Button', 'use link with icon'))
-      .setCaptureElements('#test-element')
+      .setUrl(pathTo("Button", "use link with icon"))
+      .setCaptureElements("#test-element")
   );
 });
 
-gemini.suite('button link multiline', suite => {
+gemini.suite("button link multiline", suite => {
   testScenario(
     suite
-      .setUrl(pathTo('Button', 'multiline text with link button'))
-      .setCaptureElements('#test-element')
+      .setUrl(pathTo("Button", "multiline text with link button"))
+      .setCaptureElements("#test-element")
+  );
+});
+
+gemini.suite("with error", suite => {
+  testScenario(
+    suite
+      .setUrl(pathTo("Button", "with error"))
+      .setCaptureElements("#test-element")
   );
 });
