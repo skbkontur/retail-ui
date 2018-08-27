0275a3c42411790734c997210d5c4b77a146ae59
21alexander21 <21alexander21@gmail.com>
2018-08-27 11:58:39 +0500

    test(CurrencyInput): fix typings

--- packages/retail-ui/components/CurrencyInput/CurrencyHelper.tsx
+++ packages/retail-ui/components/CurrencyInput/CurrencyHelper.tsx
@@ -71,7 +71,7 @@ export default class CurrencyHelper {
   }
 
   public static format(
-    value: Nullable<number>,
+    value: Nullable<number | string>,
     options?: Nullable<DecimalFormattingOptions>
   ): string {
     if (value == null) {
@@ -161,7 +161,7 @@ export default class CurrencyHelper {
   public static isValidString(
     value: string,
     fractionDigits: Nullable<number>,
-    unsigned: Nullable<boolean>
+    unsigned?: Nullable<boolean>
   ) {
     value = CurrencyHelper.unformatString(value);
     const destructed = CurrencyHelper.destructString(value);
