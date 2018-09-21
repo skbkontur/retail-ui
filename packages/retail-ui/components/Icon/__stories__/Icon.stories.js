17807655139431f82f71c2f2b66cba924bf7a1e5
wKich <nflazzer@gmail.com>
2018-09-21 15:09:42 +0500

    test(Icon): add screenshot test

--- packages/retail-ui/components/Icon/__stories__/Icon.stories.js
+++ packages/retail-ui/components/Icon/__stories__/Icon.stories.js
@@ -3,17 +3,36 @@ import * as React from 'react';
 
 import Icon from '../Icon';
 
+const rootStyle: Partial<React.CSSProperties> = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  alignItems: 'baseline',
+  width: '1000px'
+};
+
+const iconStyle = {
+  width: '14px',
+  display: 'inline-block',
+  margin: '5px'
+};
+
 storiesOf('Icon', module).add('All icons', () => (
-  <table>
-    <tbody>
-      {Icon.getAllNames().map(name => (
-        <tr key={name}>
-          <td>
+  <div style={rootStyle}>
+    {Icon.getAllNames()
+      .sort()
+      .map(name => (
+        <div
+          key={name}
+          style={{
+            flexBasis: '200px',
+            height: name === 'Space' ? '0' : '30px'
+          }}
+        >
+          <div style={iconStyle}>
             <Icon name={name} />
-          </td>
-          <td>{name}</td>
-        </tr>
+          </div>
+          {name}
+        </div>
       ))}
-    </tbody>
-  </table>
+  </div>
 ));
