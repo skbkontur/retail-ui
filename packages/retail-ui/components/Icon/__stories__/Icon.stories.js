9e298285fe142cc6e15bba5905c870d6289fe5d5
wKich <nflazzer@gmail.com>
2018-09-28 11:08:25 +0500

    chore(Icon): simplify icon stoires

--- packages/retail-ui/components/Icon/__stories__/Icon.stories.js
+++ packages/retail-ui/components/Icon/__stories__/Icon.stories.js
@@ -1,7 +1,7 @@
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
-import Icon from '../Icon';
+import Icon, { IconName } from '../Icon';
 
 const rootStyle: Partial<React.CSSProperties> = {
   display: 'flex',
@@ -10,29 +10,47 @@ const rootStyle: Partial<React.CSSProperties> = {
   width: '1000px'
 };
 
+const iconUpdates: IconName[][] = [['Delta', 'UserAdd', 'Youtube2']];
+
+interface TestIconProps {
+  name: IconName;
+}
+
+class TestIcon extends React.Component<TestIconProps> {
+  public render() {
+    const { name } = this.props;
+    return (
+      <div
+        style={{
+          flexBasis: '200px',
+          height: name === 'Space' ? '0' : '60px'
+        }}
+      >
+        <div>
+          <Icon name={name} /> <span>{name}</span>
+        </div>
+        <div>
+          <span>{name.slice(0, name.length / 2)}</span> <Icon name={name} />{' '}
+          <span>{name.slice(name.length / 2)}</span>
+        </div>
+        <div>
+          <span>{name}</span> <Icon name={name} />
+        </div>
+      </div>
+    );
+  }
+}
+
 storiesOf('Icon', module).add('All icons', () => (
   <div style={rootStyle}>
     {Icon.getAllNames()
+      .filter(name => !iconUpdates.every(update => update.includes(name)))
       .sort()
       .map(name => (
-        <div
-          key={name}
-          style={{
-            flexBasis: '200px',
-            height: name === 'Space' ? '0' : '60px'
-          }}
-        >
-          <div>
-            <Icon name={name} /> <span>{name}</span>
-          </div>
-          <div>
-            <span>{name.slice(0, name.length / 2)}</span> <Icon name={name} />{' '}
-            <span>{name.slice(name.length / 2)}</span>
-          </div>
-          <div>
-            <span>{name}</span> <Icon name={name} />
-          </div>
-        </div>
+        <TestIcon key={name} name={name} />
       ))}
+    {iconUpdates.map(update =>
+      update.map(name => <TestIcon key={name} name={name} />)
+    )}
   </div>
 ));
