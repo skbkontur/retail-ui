2c3e15c4650127a1326b17503b5da39ff0a38d76
vkochev <vkochev>
2018-10-11 18:01:51 +0500

    fix: confict regexp

--- packages/react-ui-testing/SeleniumTesting/nuget-push.js
+++ packages/react-ui-testing/SeleniumTesting/nuget-push.js
@@ -1,16 +1,16 @@
 const exec = require("child_process").exec;
-const path = require('path');
+const path = require("path");
 const nugetSource = "https://api.nuget.org/v3/index.json";
 const apikey = process.env["NUGET_API_KEY"];
 
 exec(
     `nuget push Kontur.ReactUI.SeleniumTesting.*.nupkg -source ${nugetSource} -ApiKey ${apikey} -ForceEnglishOutput`,
-    {cwd: path.join(__dirname, 'Output')},
+    { cwd: path.join(__dirname, "Output") },
     function(error, stdout, stderr) {
         if (
             error &&
-            !error.message.includes(
-                "status code does not indicate success: 409"
+            !error.message.match(
+                /409 \(A package with ID '.*' and version '.*' already exists and cannot be modified\.\)/
             )
         ) {
             throw error;
