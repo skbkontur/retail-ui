09c6bb0755dd10c8c53cb40f3fbc10a0807c3b5f
wKich <nflazzer@gmail.com>
2018-10-30 16:09:26 +0500

    chore(react-ui-screenshot-tests): use encodeURIComponent for stories

--- packages/react-ui-screenshot-tests/gemini/utils.js
+++ packages/react-ui-screenshot-tests/gemini/utils.js
@@ -1,7 +1,9 @@
 var host = "/iframe";
 
 function pathTo(kind, story) {
-  return `${host}?selectedKind=${kind}&selectedStory=${story}`;
+  return `${host}?selectedKind=${encodeURIComponent(
+    kind
+  )}&selectedStory=${encodeURIComponent(story)}`;
 }
 
 module.exports = {
