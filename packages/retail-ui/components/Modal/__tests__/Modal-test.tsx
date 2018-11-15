f68728bf4dda2f2d5a298dfc9a7e5aee9bf9deaa
21alexander21 <21alexander21@gmail.com>
2018-11-15 14:05:21 +0500

    chore(Modal): fix linter error

--- packages/retail-ui/components/Modal/__tests__/Modal-test.tsx
+++ packages/retail-ui/components/Modal/__tests__/Modal-test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Modal, { isHeader, isFooter } from '../Modal';
 
 describe('Modal', () => {
