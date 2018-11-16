408b7b95c5e141f36372d94626e8146654317fea
Alexander Kuzmin <21alexander21@gmail.com>
2018-11-16 12:27:26 +0500

    [Modal] опциональное залипание шапки и футера (#911)
    - `Modal.Header` и `Modal.Footer` получили проп `sticky` (по-дуфолту **true**) для включения/выключения залипающего поведения
    - Улучшены проверки `isHeader` и `isFooter`, исправлены ошибки типов в них
    - Элемент, слушающий клик на фоне вынесен в отдельный компонент `ModalClickTrap`,
    чтобы получить к нему доступ из unit-тестов
    - Добавлены юнит-тесты
    - Добавлен скриншотный тест для non-sticky поведения
    
    * feat(Modal): optional sticky header
    
    * feat(Modal): optional sticky footer
    
    * docs(Modal): doc and example about non-sticky behavior
    
    * refactor(Modal): some improvements
    
    * test(Modal): add unit-tests
    
    * chore(Modal): fix linter error
    
    * chore(Modal): fix typo in storybook
    
    * test(Modal): add screenshot tests for non-stiky elements
    
    * test(Modal): react-focus-lock mock moved to test-setup

--- packages/retail-ui/components/Modal/ModalHeader.tsx
+++ packages/retail-ui/components/Modal/ModalHeader.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ModalContext } from './ModalContext';
+import { ModalContext, CloseProps } from './ModalContext';
 import Sticky from '../Sticky';
 import classNames from 'classnames';
 import Close from './ModalClose';
@@ -8,36 +8,51 @@ import styles = require('./Modal.less');
 
 export interface HeaderProps {
   close?: boolean;
+  sticky: boolean;
 }
 
 export class Header extends React.Component<HeaderProps> {
+  public static defaultProps = {
+    sticky: true
+  };
+
   public render(): JSX.Element {
     return (
       <ModalContext.Consumer>
-        {({ close, additionalPadding }) => (
-          <Sticky side="top" allowChildWithMargins>
-            {fixed => (
-              <div
-                className={classNames(
-                  styles.header,
-                  fixed && styles.fixedHeader,
-                  additionalPadding && styles.headerAddPadding
-                )}
-              >
-                {close && (
-                  <div className={styles.absoluteClose}>
-                    <Close
-                      requestClose={close.requestClose}
-                      disableClose={close.disableClose}
-                    />
-                  </div>
-                )}
-                {this.props.children}
-              </div>
-            )}
-          </Sticky>
-        )}
+        {({ close, additionalPadding }) => {
+          if (this.props.sticky) {
+            return (
+              <Sticky side="top" allowChildWithMargins>
+                {this.renderContent(close, additionalPadding)}
+              </Sticky>
+            );
+          }
+
+          return this.renderContent(close, additionalPadding)();
+        }}
       </ModalContext.Consumer>
     );
   }
+
+  private renderContent = (close?: CloseProps, additionalPadding?: boolean) => (
+    fixed = false
+  ) => (
+    <div
+      className={classNames(
+        styles.header,
+        fixed && styles.fixedHeader,
+        additionalPadding && styles.headerAddPadding
+      )}
+    >
+      {close && (
+        <div className={styles.absoluteClose}>
+          <Close
+            requestClose={close.requestClose}
+            disableClose={close.disableClose}
+          />
+        </div>
+      )}
+      {this.props.children}
+    </div>
+  );
 }
