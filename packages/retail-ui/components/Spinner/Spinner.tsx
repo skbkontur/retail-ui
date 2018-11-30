b0ba0cd43a58fa4acd87199f1ad6e409f58adf47
21alexander21 <21alexander21@gmail.com>
2018-11-30 15:36:50 +0500

    feat(Spinner): поддержка ReactNode в caption

--- packages/retail-ui/components/Spinner/Spinner.tsx
+++ packages/retail-ui/components/Spinner/Spinner.tsx
@@ -14,7 +14,7 @@ export const SpinnerConfig = {
 export type SpinnerType = 'mini' | 'normal' | 'big';
 
 export interface SpinnerProps {
-  caption?: string;
+  caption?: React.ReactNode;
   dimmed?: boolean;
   type?: SpinnerType;
 }
@@ -29,7 +29,7 @@ class Spinner extends React.Component<SpinnerProps> {
      *
      * 'Загрузка' - значение по-умолчанию
      */
-    caption: PropTypes.string,
+    caption: PropTypes.node,
 
     dimmed: PropTypes.bool,
 
@@ -130,7 +130,7 @@ class Spinner extends React.Component<SpinnerProps> {
     return this._renderCloud(type);
   };
 
-  private _renderCaption = (type: SpinnerType, caption: string) => {
+  private _renderCaption = (type: SpinnerType, caption: React.ReactNode) => {
     const spanClassName = classnames({
       [styles.captionRight]: type === types.mini,
       [styles.captionBottom]: type !== types.mini
