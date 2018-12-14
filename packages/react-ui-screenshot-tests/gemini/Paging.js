9ad712d2bd1b7ea1aaad1670d4542787143d2ac7
21alexander21 <21alexander21@gmail.com>
2018-12-14 18:55:25 +0500

    test(Paging): тест управления клавиатурой

--- packages/react-ui-screenshot-tests/gemini/Paging.js
+++ packages/react-ui-screenshot-tests/gemini/Paging.js
@@ -16,4 +16,44 @@ gemini.suite("Paging", () => {
         actions.click(find('[class^="Paging-forwardLink"]'));
       });
   });
+
+  gemini.suite("Keyboard control", () => {
+    gemini.suite("Sample", suite => {
+      suite
+        .setUrl(pathTo("Paging", "GoToAbsensePage"))
+        .setCaptureElements("#test-element")
+        .capture("focused", (actions, find) => {
+          actions.focus(find('[class^="Paging-paging"]'));
+        })
+        .capture("Move focus right", actions => {
+          actions.sendKeys(gemini.ARROW_RIGHT);
+        })
+        .capture("Move to page by Ender", actions => {
+          actions.sendKeys(gemini.ENTER);
+        })
+        .capture("Next page", actions => {
+          actions.sendKeys([gemini.CONTROL, gemini.ARROW_RIGHT]);
+        })
+        .skip.in(
+          "ie11",
+          'в ie11 не получается "нажать" `gemini.CONTROL + gemini.ARROW_RIGHT`'
+        );
+    });
+
+    gemini.suite("Sample ie11", suite => {
+      suite
+        .setUrl(pathTo("Paging", "GoToAbsensePage"))
+        .setCaptureElements("#test-element")
+        .capture("focused", (actions, find) => {
+          actions.focus(find('[class^="Paging-paging"]'));
+        })
+        .capture("Move focus right", actions => {
+          actions.sendKeys(gemini.ARROW_RIGHT);
+        })
+        .capture("Move to page by Ender", actions => {
+          actions.sendKeys(gemini.ENTER);
+        })
+        .only.in("ie11");
+    });
+  });
 });
