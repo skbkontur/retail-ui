8a078a6bf1f5945b95889bc35e007a694034fbdd
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2018-12-28 13:28:59 +0500

    fix(SidePage): emit LayoutEvent on Body update #961

--- packages/retail-ui/components/SidePage/SidePageBody.tsx
+++ packages/retail-ui/components/SidePage/SidePageBody.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
-
+import LayoutEvents from '../../lib/LayoutEvents';
 import styles = require('./SidePage.less');
 
 export default class SidePageBody extends React.Component {
+  public componentDidUpdate() {
+    LayoutEvents.emit();
+  }
+
   public render(): JSX.Element {
     return <div className={styles.body}>{this.props.children}</div>;
   }
