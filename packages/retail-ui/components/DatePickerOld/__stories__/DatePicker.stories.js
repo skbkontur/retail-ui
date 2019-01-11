9a7a2c9133d466eb8c3ce1de1811eea4552a0941
wKich <nflazzer@gmail.com>
2019-01-11 14:43:35 +0500

    fix(DatePickerOld): can't blur in ie11

--- packages/retail-ui/components/DatePickerOld/__stories__/DatePicker.stories.js
+++ packages/retail-ui/components/DatePickerOld/__stories__/DatePicker.stories.js
@@ -4,6 +4,8 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import DatePicker from '../DatePickerOld';
+import { Nullable } from '../../../typings/utility-types';
+import Button from '../../Button';
 
 type State = {
   error: boolean,
@@ -32,6 +34,44 @@ class DatePickerWithError extends React.Component<{}, State> {
   }
 }
 
+class DatePickerWithFocusBlur extends React.Component {
+  public state = {
+    value: new Date()
+  };
+
+  private datepicker: Nullable<DatePicker> = null;
+
+  public render() {
+    return (
+      <div>
+        <DatePicker
+          value={this.state.value}
+          onChange={this.handleChange}
+          ref={this.refDatePicker}
+        />
+        <Button
+          onClick={() => {
+            action('focus');
+            this.datepicker!.focus();
+            setTimeout(() => {
+              action('blur');
+              this.datepicker!.blur();
+            }, 1000);
+          }}
+        >
+          Focus and blur
+        </Button>
+      </div>
+    );
+  }
+
+  private refDatePicker = (el: Nullable<DatePicker>) => (this.datepicker = el);
+
+  private handleChange = (_: any, value: Date | string | null) => {
+    this.setState({ value });
+  };
+}
+
 storiesOf('DatePickerOld', module)
   .addDecorator(story => (
     <div>
@@ -43,11 +83,12 @@ storiesOf('DatePickerOld', module)
     <div style={{ paddingTop: 200 }}>
       <DatePicker
         value={new Date('2017-01-02')}
-        onMouseEnter={() => console.count('enter')}
-        onMouseLeave={() => console.count('leave')}
+        onMouseEnter={action('enter')}
+        onMouseLeave={action('leave')}
         onChange={action('change')}
       />
       <button>ok</button>
     </div>
   ))
-  .add('DatePickerWithError', () => <DatePickerWithError />);
+  .add('DatePickerWithError', () => <DatePickerWithError />)
+  .add('DatePickerWithFocusBlur', () => <DatePickerWithFocusBlur />);
