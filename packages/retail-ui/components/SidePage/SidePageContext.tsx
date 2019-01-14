7fe9290a3100e47c7e59c530ceae390c0ce9ba4c
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2019-01-14 12:36:43 +0500

    feat(SidePage): add updateLayout method

---
+++ packages/retail-ui/components/SidePage/SidePageContext.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import createReactContext = require('create-react-context');
+import { SidePageFooter } from './SidePageFooter';
+
+export interface SidePageContextType {
+  requestClose: () => void;
+  width: number | string;
+  footerRef: (ref: SidePageFooter | null) => void;
+}
+
+export const SidePageContext = createReactContext<SidePageContextType>({
+  requestClose: () => undefined,
+  width: 'auto',
+  footerRef: () => undefined
+});
+
+export const withContext = <P extends {}>(
+  BaseComponent: React.ComponentType<P & { context?: SidePageContextType }>
+) => (props: P) => (
+  <SidePageContext.Consumer>
+    {(context: SidePageContextType) => (
+      <BaseComponent {...props} context={context} />
+    )}
+  </SidePageContext.Consumer>
+);
