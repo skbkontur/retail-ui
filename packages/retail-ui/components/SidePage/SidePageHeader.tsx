bce7251e2dd05b8cf6682fe81d36f4026aedcbe8
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2019-01-17 15:46:11 +0500

    fix(SidePage): remove empty space from fixed header

--- packages/retail-ui/components/SidePage/SidePageHeader.tsx
+++ packages/retail-ui/components/SidePage/SidePageHeader.tsx
@@ -9,27 +9,120 @@ export interface SidePageHeaderProps {
   children?: React.ReactNode | ((fixed: boolean) => React.ReactNode);
 }
 
+export interface SidePageHeaderState {
+  isReadyToFix: boolean;
+  headerHeight?: number | null;
+}
+
 export default class SidePageHeader extends React.Component<
   SidePageHeaderProps
 > {
+  public state = {
+    isReadyToFix: false,
+    headerHeight: null
+  };
+
+  private wrapper: HTMLElement | null = null;
+  private normalHeader: HTMLElement | null = null;
+  private fixedHeader: HTMLElement | null = null;
+
+  public componentDidMount() {
+    window.addEventListener('scroll', this.onScroll, true);
+    this.onScroll();
+  }
+
+  public componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll, true);
+  }
+
+  public componentDidUpdate() {
+    this.onScroll();
+    if (this.state.headerHeight !== this.normalHeaderHeight) {
+      this.setState({
+        headerHeight: this.normalHeaderHeight
+      });
+    }
+  }
+
+  public get scrollTop() {
+    return this.wrapper ? -this.wrapper.getBoundingClientRect().top : 0;
+  }
+
+  public get normalHeaderHeight() {
+    return this.normalHeader
+      ? this.normalHeader.getBoundingClientRect().height
+      : 0;
+  }
+
+  public get fixedHeaderHeight() {
+    return this.fixedHeader
+      ? this.fixedHeader.getBoundingClientRect().height
+      : 0;
+  }
+
+  public get closeOffset() {
+    if (!this.fixedHeader) {
+      return 0;
+    }
+    const closeElement = this.fixedHeader.getElementsByClassName(
+      styles.close
+    )[0];
+    return closeElement
+      ? closeElement.getBoundingClientRect().top -
+          this.fixedHeader.getBoundingClientRect().top
+      : 0;
+  }
+
   public render(): JSX.Element {
+    const { isReadyToFix, headerHeight } = this.state;
     return (
-      <Sticky side="top">
-        {fixed => (
-          <div className={classNames(styles.header, fixed && styles.fixed)}>
-            {this.renderClose()}
-            <div className={classNames(styles.title, fixed && styles.fixed)}>
-              {typeof this.props.children === 'function'
-                ? this.props.children(fixed)
-                : this.props.children}
-            </div>
-          </div>
+      <div ref={this.wrapperRef}>
+        {this.renderHiddenHeader({ fixed: false, ref: this.normalHeaderRef })}
+        {this.renderHiddenHeader({ fixed: true, ref: this.fixedHeaderRef })}
+        {isReadyToFix ? (
+          <Sticky side="top" height={headerHeight}>
+            {fixed => this.renderHeader({ fixed })}
+          </Sticky>
+        ) : (
+          this.renderHeader({ fixed: false })
         )}
-      </Sticky>
+      </div>
     );
   }
 
-  public renderClose() {
+  private renderHeader({
+    fixed,
+    hidden
+  }: {
+    fixed?: boolean;
+    hidden?: boolean;
+  }) {
+    const closeOffset = !fixed ? this.closeOffset : 0;
+    return (
+      <div
+        className={classNames({
+          [styles.header]: true,
+          [styles.fixed]: fixed,
+          [styles.hidden]: hidden
+        })}
+      >
+        {!hidden ? (
+          <Sticky side="top" offset={closeOffset}>
+            {this.renderClose()}
+          </Sticky>
+        ) : (
+          this.renderClose()
+        )}
+        <div className={classNames(styles.title, fixed && styles.fixed)}>
+          {typeof this.props.children === 'function'
+            ? this.props.children(Boolean(fixed))
+            : this.props.children}
+        </div>
+      </div>
+    );
+  }
+
+  private renderClose() {
     return (
       <SidePageContext.Consumer>
         {({ requestClose }) => (
@@ -40,4 +133,45 @@ export default class SidePageHeader extends React.Component<
       </SidePageContext.Consumer>
     );
   }
+
+  /**
+   * Render hidden headers to calc their heights
+   */
+  private renderHiddenHeader(options: {
+    fixed?: boolean;
+    ref?: (el: HTMLElement | null) => void;
+  }) {
+    const { fixed, ref } = options;
+    return React.cloneElement(this.renderHeader({ fixed, hidden: true }), {
+      ref
+    });
+  }
+
+  private onScroll = () => {
+    const { scrollTop, normalHeaderHeight, fixedHeaderHeight } = this;
+    const isReadyToFix = scrollTop > normalHeaderHeight - fixedHeaderHeight;
+    if (this.state.isReadyToFix !== isReadyToFix) {
+      this.setState({
+        isReadyToFix
+      });
+    }
+  };
+
+  private wrapperRef = (el: HTMLElement | null) => {
+    if (el) {
+      this.wrapper = el;
+    }
+  };
+
+  private normalHeaderRef = (el: HTMLElement | null) => {
+    if (el) {
+      this.normalHeader = el;
+    }
+  };
+
+  private fixedHeaderRef = (el: HTMLElement | null) => {
+    if (el) {
+      this.fixedHeader = el;
+    }
+  };
 }
