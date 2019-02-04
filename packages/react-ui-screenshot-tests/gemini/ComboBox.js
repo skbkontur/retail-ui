487a1b58c1fead6edfa5a56085d585e82cf93618
wKich <nflazzer@gmail.com>
2019-02-04 17:29:59 +0500

    test(ComboBox): fix test selector

--- packages/react-ui-screenshot-tests/gemini/ComboBox.js
+++ packages/react-ui-screenshot-tests/gemini/ComboBox.js
@@ -112,12 +112,12 @@ gemini.suite("ComboBox", () => {
         .setCaptureElements("#test-element")
         .capture("initial value")
         .capture("reset value", (action, find) => {
-          const resetButton = find('[class^="Button-wrap"]:nth-of-type(2)');
+          const resetButton = find('[class^="Button-wrap"]:nth-child(3)');
 
           action.click(resetButton);
         })
         .capture("set value", (action, find) => {
-          const setButton = find('[class^="Button-wrap"]:nth-of-type(1)');
+          const setButton = find('[class^="Button-wrap"]:nth-child(2)');
 
           action.click(setButton);
         });
