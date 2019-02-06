1fdb314ce4c6423c6aaf8764382912a5dced96f9
KONTUR\sivukhin <sivukhin@skbkontur.ru>
2019-02-06 19:42:08 +0500

    test(Autocomplete): fix methods in control for Autocomplete component

--- packages/react-ui-testing/SeleniumTesting/Controls/Autocomplete.cs
+++ packages/react-ui-testing/SeleniumTesting/Controls/Autocomplete.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-
 using JetBrains.Annotations;
-
+using Kontur.Selone.Extensions;
+using Kontur.Selone.Properties;
 using OpenQA.Selenium;
+using SKBKontur.SeleniumTesting.Internals;
 
 namespace SKBKontur.SeleniumTesting.Controls
 {
@@ -12,22 +14,41 @@ namespace SKBKontur.SeleniumTesting.Controls
         public Autocomplete(ISearchContainer container, ISelector selector)
             : base(container, selector)
         {
-        }
-
-        public void InputText([NotNull] string text)
-        {
-            ExecuteAction(x => x.FindElement(By.CssSelector("label > input")).SendKeys(text), $"InputText({text})");
+            portal = this.Find<Portal>().By("noscript");
+            menu = new Menu(portal, new UniversalSelector("Menu"));
         }
 
         [NotNull]
-        public List<string> GetSuggestions()
+        public IProp<string> Text => ValueFromElement(x =>
         {
-            return GetValueFromElement(x => x.FindElements(By.CssSelector("div > div > *"))).Select(x => x.Text).ToList();
+            return x.FindElement(By.CssSelector("label > span > input")).Value().Get();
+        });
+
+        public void InputText([NotNull] string text)
+        {
+            ExecuteAction(x => x.FindElement(By.CssSelector("label > span > input")).SendKeys(text),
+                $"InputText({text})");
+        }
+
+        [NotNull]
+        public List<MenuItem> GetSuggestions()
+        {
+            try
+            {
+                return menu.AsEnumerable().ToList();
+            }
+            catch (ElementNotFoundException)
+            {
+                return new List<MenuItem>();
+            }
         }
 
         public void SelectByIndex(int index)
         {
-            GetValueFromElement(x => x.FindElements(By.CssSelector("div > div > *"))).Skip(index).FirstOrDefault().Click();
+            menu[index].Click();
         }
+
+        private readonly Menu menu;
+        private readonly Portal portal;
     }
 }
