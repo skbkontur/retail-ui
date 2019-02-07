1e0c260c012637a7e7b7dfee7726025f2fe312cc
KONTUR\sivukhin <sivukhin@skbkontur.ru>
2019-02-08 02:44:45 +0500

    test(Autocomplete): simplify code for Autocomplete test page

--- packages/react-ui-testing/TestPages/src/components/TestPages/AutocompleteTestPage.jsx
+++ packages/react-ui-testing/TestPages/src/components/TestPages/AutocompleteTestPage.jsx
@@ -2,47 +2,54 @@ import React from 'react';
 import Autocomplete from 'retail-ui/components/Autocomplete'
 import { CaseSuite, Case } from '../Case';
 
+const suggestions = ["Grey Face", "Grey Space", "Kappa", "Keepo", "Resident Sleeper"];
+
+class ListBasedAutocopmlete extends React.Component {
+  state = {
+    value: ""
+  };
+
+  render(): React.Element<*> {
+    return (
+      <Autocomplete
+        source={suggestions}
+        value={this.state.value}
+        onChange={(_, value) => this.setState({value: value})}
+        data-tid='ListBasedAutocomplete'
+      />
+    );
+  }
+}
+
+class FetchBasedAutocopmlete extends React.Component {
+  state = {
+    value: ""
+  };
+
+  render(): React.Element<*> {
+    return (
+      <Autocomplete
+        source={(pattern) => Promise.resolve(suggestions.filter(s => s.startsWith(pattern)))}
+        value={this.state.value}
+        onChange={(_, value) => this.setState({value: value})}
+        data-tid='FetchBasedAutocomplete'
+      />
+    );
+  }
+}
+
 export default class AutocompleteTestPage extends React.Component {
-    state = {
-        listBasedValue: "",
-        fetchBasedValue: "",
-        disabledPortalValue: "",
-        suggestions: ["Grey Face", "Grey Space", "Kappa", "Keepo", "Resident Sleeper"],
-    };
-
-    renderListBasedAutocomplete()  : React.Element<*> {
-      return (
-        <Autocomplete
-          source={this.state.suggestions}
-          value={this.state.listBasedValue}
-          onChange={(_, value) => this.setState({listBasedValue: value})}
-          data-tid='ListBasedAutocomplete'
-        />
-      );
-    }
-
-    renderFetchBasedAutocomplete() : React.Element<*> {
-      return (
-        <Autocomplete
-          source={(pattern) => Promise.resolve(this.state.suggestions.filter(s => s.startsWith(pattern)))}
-          value={this.state.fetchBasedValue}
-          onChange={(_, value) => this.setState({fetchBasedValue: value})}
-          data-tid='FetchBasedAutocomplete'
-        />
-      );
-    }
-
     render(): React.Element<*> {
       return (
             <CaseSuite title='Autocomplete'>
                 <Case title='List Based Autocomplete'>
                     <Case.Body>
-                      {this.renderListBasedAutocomplete()}
+                      <ListBasedAutocopmlete/>
                     </Case.Body>
                 </Case>
                 <Case title='Fetch Based Autocomplete'>
                     <Case.Body>
-                      {this.renderFetchBasedAutocomplete()}
+                      <FetchBasedAutocopmlete/>
                     </Case.Body>
                 </Case>
            </CaseSuite>
