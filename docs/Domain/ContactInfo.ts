578e3046b84612648b102d473d87cb30e8e24c0a
slawwan <slawwan@skobkontur.ru>
2019-02-15 13:29:59 +0500

    types for ContactInfo.ts

--- docs/Domain/ContactInfo.ts
+++ docs/Domain/ContactInfo.ts
@@ -1,23 +1,15 @@
 import { Nullable } from "../../typings/Types";
-import { ValidationResultFor } from "../components/Pages/Examples/Editors/ValidationBuilder";
-import { ValidationInfo } from "src/index";
+import { ValidationResultFor } from "docs/components/Pages/Examples/Editors/ValidationBuilder";
 
-export type ContactInfo = {
-    name: string,
-    email: string,
-    phone: string,
-    sex: "male" | "female" | null,
-};
+export interface ContactInfo {
+    name: string;
+    email: string;
+    phone: string;
+    sex: "male" | "female" | null;
+}
 
-export type ContactInfoValidationInfo = {
-    name?: ValidationInfo,
-    email?: ValidationInfo,
-    phone?: ValidationInfo,
-    sex?: ValidationInfo,
-};
-
-export type FormEditorProps = {
-    data: ContactInfo,
-    validationInfo: Nullable<ValidationResultFor<ContactInfo>>,
-    onChange: (update: Partial<ContactInfo>) => any,
-};
+export interface FormEditorProps {
+    data: ContactInfo;
+    validationInfo?: Nullable<ValidationResultFor<ContactInfo>>;
+    onChange: (update: Partial<ContactInfo>) => void;
+}
