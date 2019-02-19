902e17110619be6deb32f1da1661c2dab552a5a4
aagordienko <aagordienko@skbkontur.ru>
2019-02-19 20:40:26 +0300

    test(ScrollContainer.stories): new story with scroll state

--- packages/retail-ui/components/ScrollContainer/__stories__/ScrollContainer.stories.tsx
+++ packages/retail-ui/components/ScrollContainer/__stories__/ScrollContainer.stories.tsx
@@ -29,30 +29,48 @@ storiesOf('ScrollContainer', module).add('with large content height', () => {
 });
 
 storiesOf('ScrollContainer', module).add('with scroll state', () => {
-  const messageStyles: CSSProperties = {
-    position: 'absolute',
-    width: 75,
-    top: 0,
-    right: 0,
-    padding: 5,
-    paddingRight: 20,
-    textAlign: 'right',
-    background: '#f1f1f1',
-    boxSizing: 'border-box'
+  class Wrapper extends React.Component<{}, {scrollState: ScrollContainerScrollState}> {
+    state = {scrollState: 'top'};
+
+    render(){
+      const commonBlocksStyles: CSSProperties = {
+        padding: '5px 10px',
+        position: 'relative',
+        transition: 'box-shadow 0.2s',
+        zIndex: 1
+      }
+
+      const headerStyles: CSSProperties = {
+        boxShadow: this.state.scrollState !== 'top' ? '0 5px 10px rgba(0, 0, 0, 0.2)' : 'none',
+        ...commonBlocksStyles
+      }
+
+      const footerStyles: CSSProperties = {
+        boxShadow: this.state.scrollState !== 'bottom' ? 'rgba(0, 0, 0, 0.2) 0px -5px 10px' : 'none',
+        background: '#f1f1f1',
+        ...commonBlocksStyles
+      }
+
+      const scrollContainerWrapperStyles: CSSProperties = {
+        ...wrapperStyle,
+        border: 'none',
+        padding: '0px 5px',
+        boxSizing: 'border-box'
+      }
+
+      return (
+        <div style={{margin: 20, boxShadow: 'rgba(0, 0, 0, 0.2) 0px 0px 10px'}}>
+          <div style={headerStyles}>header</div>
+          <div style={scrollContainerWrapperStyles}>
+            <ScrollContainer onScrollStateChange={scrollState => this.setState({scrollState})}>
+              {getItems(20).map(i => <div key={i}>{i}</div>)}
+            </ScrollContainer>
+          </div>
+          <div style={footerStyles}>footer</div>
+        </div>
+      );
+    }
   }
 
-  return (
-    <div style={wrapperStyle}>
-      <ScrollContainer>
-        {(scrollState: ScrollContainerScrollState) => {
-          return (
-            <div>
-              {getItems(20).map(i => <div key={i}>{i}</div>)}
-              <div style={messageStyles}>state:<br />'{scrollState}'</div>
-            </div>
-          )
-        }}
-      </ScrollContainer>
-    </div>
-  );
+  return <Wrapper/>;
 });
