72fb736ec843304496827e420121a03a3267bfe6
kga <kga@skbkontur.ru>
2019-02-27 13:42:03 +0500

    fix(Popup): remove ambient types
    When consumer does not skipLibCheck, typescript compiler has no way to find ambient types of
    dependant @skbkontur/react-ui package, so one must specify path to those types in 'include' section
    of his tsconfig. To overcome this complexity, we replace ambient types (IMosueEvent/IFocusEvent)
    with concrete ones and import them explicitly.

--- packages/retail-ui/typings/opaque-helper-types.d.ts
+++
@@ -1,4 +0,0 @@
-declare type IMouseEvent<T extends HTMLElement = HTMLElement> = import('react').MouseEvent<T> | MouseEvent;
-declare type IFocusEvent<T extends HTMLElement = HTMLElement> = import('react').FocusEvent<T> | FocusEvent;
-declare type IFocusInEvent<T extends HTMLElement = HTMLElement> = import('react').FocusEvent<T> | FocusEvent;
-declare type IBlurEvent<T extends HTMLElement = HTMLElement> = import('react').BlurEvent<T> | BlurEvent;
