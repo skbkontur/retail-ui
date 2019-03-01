e70dff12b346c7afff6db3334dd78c5de9a4a93e
wKich <nflazzer@gmail.com>
2019-03-02 00:07:33 +0500

    style(react-ui-validations): reformat some stories

--- packages/react-ui-validations/stories/SyncStories/LostfocusDependentValidation.tsx
+++ packages/react-ui-validations/stories/SyncStories/LostfocusDependentValidation.tsx
@@ -1,111 +1,107 @@
-import * as React from "react";
-import { text, ValidationInfo } from "../../src";
-import ValidationContainer from "../../src/ValidationContainer";
-import Gapped from "retail-ui/components/Gapped";
-import ValidationWrapperV1 from "../../src/ValidationWrapperV1";
-import Input from "retail-ui/components/Input";
-import Button from "retail-ui/components/Button";
-import { Nullable } from "../../typings/Types";
-import { ValidationState } from "../ValidationHelper";
+import * as React from 'react';
+import { text, ValidationInfo } from '../../src';
+import ValidationContainer from '../../src/ValidationContainer';
+import Gapped from 'retail-ui/components/Gapped';
+import ValidationWrapperV1 from '../../src/ValidationWrapperV1';
+import Input from 'retail-ui/components/Input';
+import Button from 'retail-ui/components/Button';
+import { Nullable } from '../../typings/Types';
+import { ValidationState } from '../ValidationHelper';
 
 interface LostfocusDependentValidationState {
-    sending: boolean;
-    valueA: string;
-    valueB: string;
-    validation: ValidationState;
+  sending: boolean;
+  valueA: string;
+  valueB: string;
+  validation: ValidationState;
 }
 
 export default class LostfocusDependentValidation extends React.Component<{}, LostfocusDependentValidationState> {
-    public state: LostfocusDependentValidationState = {
-        sending: false,
-        valueA: "",
-        valueB: "",
-        validation: "none",
-    };
+  public state: LostfocusDependentValidationState = {
+    sending: false,
+    valueA: '',
+    valueB: '',
+    validation: 'none',
+  };
 
-    private container: ValidationContainer | null = null;
+  private container: ValidationContainer | null = null;
 
-    public validateA(): Nullable<ValidationInfo> {
-        if (this.state.valueA.substr(0, 3) === "bad") {
-            return {
-                message: "incorrect value",
-                type: "lostfocus",
-            };
-        }
-        if (this.state.valueA && this.state.valueB && this.state.valueA === this.state.valueB) {
-            return {
-                message: "duplicate value",
-                type: "lostfocus",
-            };
-        }
-        return null;
+  public validateA(): Nullable<ValidationInfo> {
+    if (this.state.valueA.substr(0, 3) === 'bad') {
+      return {
+        message: 'incorrect value',
+        type: 'lostfocus',
+      };
     }
-
-    public validateB(): Nullable<ValidationInfo> {
-        if (this.state.valueB.substr(0, 3) === "bad") {
-            return {
-                message: "incorrect value",
-                type: "submit",
-            };
-        }
-        if (this.state.valueA && this.state.valueB && this.state.valueA === this.state.valueB) {
-            return {
-                message: "duplicate value",
-                type: "submit",
-            };
-        }
-        return null;
+    if (this.state.valueA && this.state.valueB && this.state.valueA === this.state.valueB) {
+      return {
+        message: 'duplicate value',
+        type: 'lostfocus',
+      };
     }
+    return null;
+  }
 
-    public render() {
-        return (
-            <ValidationContainer ref={this.refContainer}>
-                <div style={{ padding: 30 }}>
-                    <Gapped vertical>
-                        <Gapped>
-                            <b>A</b>
-                            <ValidationWrapperV1 data-tid="InputAValidation" validationInfo={this.validateA()} renderMessage={text()}>
-                                <Input
-                                    data-tid={"InputA"}
-                                    value={this.state.valueA}
-                                    onChange={(_, value) => this.setState({ valueA: value })}
-                                />
-                            </ValidationWrapperV1>
-                        </Gapped>
-                        <Gapped>
-                            <b>B</b>
-                            <ValidationWrapperV1 data-tid="InputBValidation" validationInfo={this.validateB()} renderMessage={text()}>
-                                <Input
-                                    data-tid={"InputB"}
-                                    value={this.state.valueB}
-                                    onChange={(_, value) => this.setState({ valueB: value })}
-                                />
-                            </ValidationWrapperV1>
-                        </Gapped>
-                        <Gapped>
-                            <Button
-                                data-tid={"SubmitButton"}
-                                loading={this.state.sending}
-                                onClick={this.handleSubmit}
-                            >
-                                Submit
-                            </Button>
-                            <span data-tid={"ValidationState"}>{this.state.validation}</span>
-                        </Gapped>
-                    </Gapped>
-                </div>
-            </ValidationContainer>
-        );
+  public validateB(): Nullable<ValidationInfo> {
+    if (this.state.valueB.substr(0, 3) === 'bad') {
+      return {
+        message: 'incorrect value',
+        type: 'submit',
+      };
     }
+    if (this.state.valueA && this.state.valueB && this.state.valueA === this.state.valueB) {
+      return {
+        message: 'duplicate value',
+        type: 'submit',
+      };
+    }
+    return null;
+  }
 
-    public handleSubmit = () => {
-        this.setState({ sending: true, validation: "validating" }, async () => {
-            if (this.container) {
-                const isValid = await this.container.validate();
-                this.setState({ sending: false, validation: isValid ? "valid" : "invalid" });
-            }
-        });
-    };
+  public render() {
+    return (
+      <ValidationContainer ref={this.refContainer}>
+        <div style={{ padding: 30 }}>
+          <Gapped vertical>
+            <Gapped>
+              <b>A</b>
+              <ValidationWrapperV1 data-tid="InputAValidation" validationInfo={this.validateA()} renderMessage={text()}>
+                <Input
+                  data-tid={'InputA'}
+                  value={this.state.valueA}
+                  onChange={(_, value) => this.setState({ valueA: value })}
+                />
+              </ValidationWrapperV1>
+            </Gapped>
+            <Gapped>
+              <b>B</b>
+              <ValidationWrapperV1 data-tid="InputBValidation" validationInfo={this.validateB()} renderMessage={text()}>
+                <Input
+                  data-tid={'InputB'}
+                  value={this.state.valueB}
+                  onChange={(_, value) => this.setState({ valueB: value })}
+                />
+              </ValidationWrapperV1>
+            </Gapped>
+            <Gapped>
+              <Button data-tid={'SubmitButton'} loading={this.state.sending} onClick={this.handleSubmit}>
+                Submit
+              </Button>
+              <span data-tid={'ValidationState'}>{this.state.validation}</span>
+            </Gapped>
+          </Gapped>
+        </div>
+      </ValidationContainer>
+    );
+  }
 
-    private refContainer = (el: ValidationContainer | null) => (this.container = el);
+  public handleSubmit = () => {
+    this.setState({ sending: true, validation: 'validating' }, async () => {
+      if (this.container) {
+        const isValid = await this.container.validate();
+        this.setState({ sending: false, validation: isValid ? 'valid' : 'invalid' });
+      }
+    });
+  };
+
+  private refContainer = (el: ValidationContainer | null) => (this.container = el);
 }
--- packages/react-ui-validations/stories/SyncStories/LostfocusDynamicValidation.tsx
+++ packages/react-ui-validations/stories/SyncStories/LostfocusDynamicValidation.tsx
@@ -1,64 +1,67 @@
-import * as React from "react";
-import { text, ValidationInfo } from "../../src";
-import ValidationContainer from "../../src/ValidationContainer";
-import Gapped from "retail-ui/components/Gapped";
-import ValidationWrapperV1 from "../../src/ValidationWrapperV1";
-import Input from "retail-ui/components/Input";
-import { Nullable } from "../../typings/Types";
+import * as React from 'react';
+import { text, ValidationInfo } from '../../src';
+import ValidationContainer from '../../src/ValidationContainer';
+import Gapped from 'retail-ui/components/Gapped';
+import ValidationWrapperV1 from '../../src/ValidationWrapperV1';
+import Input from 'retail-ui/components/Input';
+import { Nullable } from '../../typings/Types';
 
 interface LostfocusDynamicValidationState {
-    sending: boolean;
-    valueA: string;
-    valueB: string;
+  sending: boolean;
+  valueA: string;
+  valueB: string;
 }
 
 export default class LostfocusDynamicValidation extends React.Component<{}, LostfocusDynamicValidationState> {
-    public state: LostfocusDynamicValidationState = {
-        sending: false,
-        valueA: "",
-        valueB: "",
-    };
+  public state: LostfocusDynamicValidationState = {
+    sending: false,
+    valueA: '',
+    valueB: '',
+  };
 
-    private counter: number = 0;
+  private counter: number = 0;
 
-    public validateA(): Nullable<ValidationInfo> {
-        if (this.state.valueA.substr(0, 3) === "bad") {
-            return {
-                message: "incorrect times: " + ++this.counter,
-                type: "lostfocus",
-            };
-        }
-        return null;
+  public validateA(): Nullable<ValidationInfo> {
+    if (this.state.valueA.substr(0, 3) === 'bad') {
+      return {
+        message: 'incorrect times: ' + ++this.counter,
+        type: 'lostfocus',
+      };
     }
+    return null;
+  }
 
-    public render() {
-        return (
-            <ValidationContainer>
-                <div style={{ padding: 30 }}>
-                    <Gapped vertical>
-                        <Gapped>
-                            <b>A</b>
-                            <ValidationWrapperV1 data-tid="InputAValidation" validationInfo={this.validateA()} renderMessage={text()}>
-                                <Input
-                                    data-tid={"InputA"}
-                                    value={this.state.valueA}
-                                    onChange={(_, value) => this.setState({ valueA: value })}
-                                />
-                            </ValidationWrapperV1>
-                        </Gapped>
-                        <Gapped>
-                            <b>B</b>
-                            <ValidationWrapperV1 data-tid="InputBValidation" validationInfo={null/*it is important to wrap input with ValidationWrapper*/}>
-                                <Input
-                                    data-tid={"InputB"}
-                                    value={this.state.valueB}
-                                    onChange={(_, value) => this.setState({ valueB: value })}
-                                />
-                            </ValidationWrapperV1>
-                        </Gapped>
-                    </Gapped>
-                </div>
-            </ValidationContainer>
-        );
-    }
+  public render() {
+    return (
+      <ValidationContainer>
+        <div style={{ padding: 30 }}>
+          <Gapped vertical>
+            <Gapped>
+              <b>A</b>
+              <ValidationWrapperV1 data-tid="InputAValidation" validationInfo={this.validateA()} renderMessage={text()}>
+                <Input
+                  data-tid={'InputA'}
+                  value={this.state.valueA}
+                  onChange={(_, value) => this.setState({ valueA: value })}
+                />
+              </ValidationWrapperV1>
+            </Gapped>
+            <Gapped>
+              <b>B</b>
+              <ValidationWrapperV1
+                data-tid="InputBValidation"
+                validationInfo={null /*it is important to wrap input with ValidationWrapper*/}
+              >
+                <Input
+                  data-tid={'InputB'}
+                  value={this.state.valueB}
+                  onChange={(_, value) => this.setState({ valueB: value })}
+                />
+              </ValidationWrapperV1>
+            </Gapped>
+          </Gapped>
+        </div>
+      </ValidationContainer>
+    );
+  }
 }
