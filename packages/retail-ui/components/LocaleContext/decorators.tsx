8791334aaf3cb4f4d01406bb30ba0541ab96c760
lossir <m.pakhomov@skbkontur.ru>
2019-03-06 12:45:09 +0500

    refactor(locale-decorators): rename `componentName` -> `controlName`

--- packages/retail-ui/components/LocaleContext/decorators.tsx
+++ packages/retail-ui/components/LocaleContext/decorators.tsx
@@ -3,10 +3,10 @@ import { LocaleContextConsumer, LocaleContextProps } from "./LocaleContext";
 import { LocaleHelper } from "./LocaleHelper";
 import { LocaleControls } from "./types";
 
-export function locale<C>(componentName: keyof LocaleControls, localeHelper: LocaleHelper<C>) {
+export function locale<C>(controlName: keyof LocaleControls, localeHelper: LocaleHelper<C>) {
   return<T extends { new(...args: any[]): React.Component }>(constructor: T) => {
     const LocaleDecorator = class extends constructor {
-      public componentName: keyof LocaleControls = componentName;
+      public controlName: keyof LocaleControls = controlName;
       public localeHelper: LocaleHelper<C> = localeHelper;
 
       public _localeContext: LocaleContextProps = {};
@@ -33,7 +33,7 @@ export function getLocale(target: any, propertyKey: string) {
     // tslint:disable-next-line
     get: function () {
       const langCode = this._localeContext.langCode;
-      const localeFromContext = this._localeContext.locale && this._localeContext.locale[this.componentName];
+      const localeFromContext = this._localeContext.locale && this._localeContext.locale[this.controlName];
       return Object.assign({}, this.localeHelper.get(langCode), localeFromContext);
     },
     set: value => value
