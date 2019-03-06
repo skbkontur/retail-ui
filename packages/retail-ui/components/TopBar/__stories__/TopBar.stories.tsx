7748431c7a8719d0f155a57ac20ca322d7703977
lossir <m.pakhomov@skbkontur.ru>
2019-03-06 10:58:39 +0500

    test(TopBar): fix screenshot-tests - remove LocaleContext from stories

--- packages/retail-ui/components/TopBar/__stories__/TopBar.stories.tsx
+++ packages/retail-ui/components/TopBar/__stories__/TopBar.stories.tsx
@@ -2,8 +2,6 @@ import BabyIcon from '@skbkontur/react-icons/Baby';
 import { storiesOf } from '@storybook/react';
 // tslint:disable:jsx-no-lambda
 import * as React from 'react';
-import { LangCodes } from "../../LocaleContext";
-import LocaleContext from "../../LocaleContext/LocaleContext";
 import Logotype from '../../Logotype';
 
 import TopBar from '../TopBar';
@@ -18,19 +16,17 @@ const Logout = TopBar.Logout;
 
 storiesOf('TopBar', module)
   .add('TopBar Old', () => (
-    <LocaleContext langCode={LangCodes.en_EN}>
-      <TopBar
-        cabinetUrl="https://cabinet.test.ru"
-        userName="Alexander The Great"
-        suffix="ui"
-        onLogout={() => alert('Logout!')}
-        leftItems={[
-          <Item key="left-item-1">
-            <BabyIcon color="#666" />
-          </Item>
-        ]}
-      />
-    </LocaleContext>
+    <TopBar
+      cabinetUrl="https://cabinet.test.ru"
+      userName="Alexander The Great"
+      suffix="ui"
+      onLogout={() => alert('Logout!')}
+      leftItems={[
+        <Item key="left-item-1">
+          <BabyIcon color="#666" />
+        </Item>
+      ]}
+    />
   ))
   .add('TopBar New', () => (
     <TopBar>
