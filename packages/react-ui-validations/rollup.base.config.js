c7a1edeb50723f46649faed2af7b37a51d5eb80e
wKich <nflazzer@gmail.com>
2019-03-13 12:00:40 +0500

    build(react-ui-validations): fix build with `warning` dep
    `warning` module has cjs format, that don't handle rollup without specific plugin

--- packages/react-ui-validations/rollup.base.config.js
+++ packages/react-ui-validations/rollup.base.config.js
@@ -1,3 +1,4 @@
+import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import typescript2 from 'rollup-plugin-typescript2';
@@ -15,6 +16,7 @@ export function buildConfig(outputDir, reactUiPackageName) {
         useTsconfigDeclarationDir: true,
       }),
       resolve(),
+      commonjs(),
       replace({
         REACT_UI_PACKAGE: JSON.stringify(reactUiPackageName),
       }),
