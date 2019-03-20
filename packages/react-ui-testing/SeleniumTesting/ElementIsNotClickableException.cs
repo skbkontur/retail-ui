809a30b9666145e34d0f6e447de5b985ee05ec63
greved <greved@skbkontur.ru>
2019-03-20 10:51:58 +0500

    ElementIsNotClickableException + create common ControlElementException

---
+++ packages/react-ui-testing/SeleniumTesting/ElementIsNotClickableException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SKBKontur.SeleniumTesting
+{
+    public class ElementIsNotClickableException : ControlElementException
+    {
+        public ElementIsNotClickableException(ControlBase control, ISearchContainer container, Type getType, ISelector selector, Exception exception)
+            : base($"Элемент {getType} по правилу {selector} внутри элемента {container.GetType().Name} невозможно кликнуть", control, container, getType, selector, exception)
+        {
+        }
+    }
+}
--- packages/react-ui-testing/SeleniumTesting/ElementNotFoundException.cs
+++ packages/react-ui-testing/SeleniumTesting/ElementNotFoundException.cs
@@ -2,20 +2,11 @@
 
 namespace SKBKontur.SeleniumTesting
 {
-    public class ElementNotFoundException : Exception
+    public class ElementNotFoundException : ControlElementException
     {
         public ElementNotFoundException(ControlBase control, ISearchContainer container, Type getType, ISelector selector, Exception exception)
-            : base($"Элемент {getType} по правилу {selector} не найден внутри элемента {container.GetType().Name}", exception)
+            : base($"Элемент {getType} по правилу {selector} не найден внутри элемента {container.GetType().Name}", control, container, getType, selector, exception)
         {
-            Control = control;
-            Container = container;
-            ControlType = getType;
-            ControlSelector = selector;
         }
-
-        public ControlBase Control { get; set; }
-        public ISearchContainer Container { get; set; }
-        public Type ControlType { get; set; }
-        public ISelector ControlSelector { get; set; }
     }
 }
