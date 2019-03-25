f34e71e0530b0905e68d3df7b7780a8782085536
step-off <nofantasy12@gmail.com>
2019-03-25 13:50:54 +0500

    refactor(ThemeManager): variables object is merging with both themes

--- packages/retail-ui/lib/ThemeManager.tsx
+++ packages/retail-ui/lib/ThemeManager.tsx
@@ -1,48 +1,52 @@
 import defaultThemeVariables from '../components/variables.less';
 import flatThemeVariables from '../components/variables.flat.less';
+import Upgrades from './Upgrades';
 
-interface VariablesObject {
+Upgrades.enableFlatDesign();
+
+interface IndexSignature {
   [key: string]: string;
 }
-type ThemeType = 'default' | 'flat';
+type VariablesObject = typeof defaultThemeVariables & typeof flatThemeVariables & IndexSignature;
 
 export default class ThemeManager {
-  public static getVariables(themeType: ThemeType): VariablesObject {
-    if (themeType === 'default') {
-      return this.defaultThemeVariables;
+  public static getVariables(): VariablesObject {
+    if (!this.variablesConstructed) {
+      this.constructVariablesObject();
     }
-
-    if (themeType === 'flat') {
-      return this.flatThemeVariables;
-    }
-
-    throw new Error(`Incompatible them type in argument: ${themeType}`);
+    return this.variables;
   }
 
-  public static overrideVariables(themeType: ThemeType, themeObject: VariablesObject) {
-    if (themeType === 'default') {
-      this.defaultThemeVariables = { ...this.defaultThemeVariables, ...themeObject };
-    }
-
-    if (themeType === 'flat') {
-      this.flatThemeVariables = { ...this.flatThemeVariables, ...themeObject };
-    }
+  public static overrideVariables(themeObject: VariablesObject): VariablesObject {
+    this.variables = { ...this.variables, ...themeObject } as VariablesObject;
+    return this.variables;
   }
 
-  public static resetVariablesToDefaultValues(themeType: ThemeType): VariablesObject {
-    if (themeType === 'default') {
-      this.defaultThemeVariables = defaultThemeVariables;
-      return this.defaultThemeVariables;
-    }
-
-    if (themeType === 'flat') {
-      this.flatThemeVariables = flatThemeVariables;
-      return this.flatThemeVariables;
-    }
-
-    throw new Error(`Incompatible them type in argument: ${themeType}`);
+  public static resetVariablesToDefaultValues(): VariablesObject {
+    this.constructVariablesObject();
+    return this.variables;
   }
 
-  private static defaultThemeVariables: VariablesObject = defaultThemeVariables;
-  private static flatThemeVariables: VariablesObject = flatThemeVariables;
+  private static isFlatDesign: boolean = Upgrades.isFlatDesignEnabled();
+  private static variablesConstructed: boolean = false;
+  private static variables: VariablesObject = {} as VariablesObject;
+
+  private static constructVariablesObject() {
+    const bothThemesKeys = [...Object.keys(defaultThemeVariables), ...Object.keys(flatThemeVariables)] as Array<
+      keyof VariablesObject
+    >;
+
+    this.variables = bothThemesKeys.reduce(
+      (resultObj: VariablesObject, currentKey: keyof VariablesObject) => {
+        if (this.isFlatDesign) {
+          resultObj[currentKey] = (flatThemeVariables as IndexSignature)[currentKey];
+        } else {
+          resultObj[currentKey] = (defaultThemeVariables as IndexSignature)[currentKey];
+        }
+        return resultObj;
+      },
+      {} as VariablesObject,
+    );
+    this.variablesConstructed = true;
+  }
 }
