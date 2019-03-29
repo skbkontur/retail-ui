587ff52b8dc0af8ab0b0560c7088f105bbd293d8
wKich <nflazzer@gmail.com>
2019-03-29 16:04:38 +0500

    refactor(retail-ui): remove unused file

--- packages/retail-ui/lib/browserNormalizer.tsx
+++
@@ -1,9 +0,0 @@
-import { isSafari, isFirefox } from './utils';
-
-export const hasFocusOnButtonClick = !isSafari() && !isFirefox();
-export const hasFocusOnLinkClick = !isSafari();
-
-export default {
-  hasFocusOnButtonClick,
-  hasFocusOnLinkClick,
-};
