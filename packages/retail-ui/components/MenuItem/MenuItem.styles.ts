13e929fd632b2ed57ce01fba4767f44432335b91
step-off <nofantasy12@gmail.com>
2019-04-09 11:19:59 +0500

    feat(MenuItem): customization with css-in-js

---
+++ packages/retail-ui/components/MenuItem/MenuItem.styles.ts
@@ -0,0 +1,38 @@
+import { css } from 'emotion';
+import { ITheme } from '../../lib/ThemeManager';
+
+const jsStyles = {
+  hover(t: ITheme) {
+    // Color with !important in purpose to override `a:hover`
+    return css`
+      background: ${t.dropdownMenuHoverBg};
+      color: ${t.textColorInvert} !important;
+    `;
+  },
+
+  selected(t: ITheme) {
+    return css`
+      background: ${t.dropdownMenuSelectedBg};
+    `;
+  },
+
+  disabled(t: ITheme) {
+    return css`
+      color: ${t.textColorDisabled};
+    `;
+  },
+
+  link(t: ITheme) {
+    return css`
+      color: ${t.linkColor};
+    `;
+  },
+
+  withIcon(t: ITheme) {
+    return css`
+      padding-left: ${t.menuItemPaddingForIcon};
+    `;
+  },
+};
+
+export default jsStyles;
