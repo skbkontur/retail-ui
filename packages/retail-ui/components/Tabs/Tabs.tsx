3af6b942de27d120fb5cd2d06d2367a3b703cb49
wKich <nflazzer@gmail.com>
2019-04-12 10:29:01 +0500

    refactor(Tabs): rename private members, refactor statics

--- packages/retail-ui/components/Tabs/Tabs.tsx
+++ packages/retail-ui/components/Tabs/Tabs.tsx
@@ -54,8 +54,23 @@ export interface TabsState {
  * contains static property `Tab`
  */
 class Tabs extends React.Component<TabsProps, TabsState> {
-  public static propTypes = {};
-  public static childContextTypes = {};
+  public static propTypes = {
+    children: PropTypes.node,
+    indicatorClassName: PropTypes.string,
+    value: PropTypes.string.isRequired,
+    vertical: PropTypes.bool,
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    onChange: PropTypes.func,
+  };
+  public static childContextTypes = {
+    activeTab: PropTypes.string.isRequired,
+    addTab: PropTypes.func.isRequired,
+    notifyUpdate: PropTypes.func.isRequired,
+    removeTab: PropTypes.func.isRequired,
+    shiftFocus: PropTypes.func.isRequired,
+    switchTab: PropTypes.func.isRequired,
+    vertical: PropTypes.bool.isRequired,
+  };
   public static defaultProps = {
     vertical: false,
   };
@@ -66,26 +81,26 @@ class Tabs extends React.Component<TabsProps, TabsState> {
     tabs: [],
   };
 
-  private _tabUpdates = {
+  private tabUpdates = {
     on: (cb: () => void) => {
-      const index = this._listeners.push(cb);
+      const index = this.listeners.push(cb);
       return () => {
-        this._listeners.splice(index, 1);
+        this.listeners.splice(index, 1);
       };
     },
   };
 
   private getProps = createPropsGetter(Tabs.defaultProps);
-  private _listeners: Array<() => void> = [];
+  private listeners: Array<() => void> = [];
 
   public getChildContext() {
     return {
       activeTab: this.props.value,
-      addTab: this._addTab,
-      notifyUpdate: this._notifyUpdate,
-      removeTab: this._removeTab,
-      shiftFocus: this._shiftFocus,
-      switchTab: this._switchTab,
+      addTab: this.addTab,
+      notifyUpdate: this.notifyUpdate,
+      removeTab: this.removeTab,
+      shiftFocus: this.shiftFocus,
+      switchTab: this.switchTab,
       vertical: this.props.vertical,
     };
   }
@@ -100,14 +115,14 @@ class Tabs extends React.Component<TabsProps, TabsState> {
         <Indicator
           className={this.props.indicatorClassName}
           getAnchorNode={activeTab ? activeTab.getNode : () => null}
-          tabUpdates={this._tabUpdates}
+          tabUpdates={this.tabUpdates}
           vertical={vertical}
         />
       </div>
     );
   }
 
-  private _shiftFocus = (fromTab: string, delta: number) => {
+  private shiftFocus = (fromTab: string, delta: number) => {
     const { tabs } = this.state;
     const index = tabs.findIndex(x => x.id === fromTab);
     const newIndex = Math.max(0, Math.min(index + delta, tabs.length - 1));
@@ -124,54 +139,33 @@ class Tabs extends React.Component<TabsProps, TabsState> {
     }
   };
 
-  private _notifyUpdate = () => {
-    this._listeners.forEach(cb => cb());
+  private notifyUpdate = () => {
+    this.listeners.forEach(cb => cb());
   };
 
-  private _switchTab = (id: string) => {
+  private switchTab = (id: string) => {
     const { onChange, value } = this.props;
     if (id !== value && onChange) {
-      onChange(this._createEvent(id), id);
+      onChange(this.createEvent(id), id);
     }
   };
 
-  private _addTab = (id: string, getNode: () => any) => {
+  private addTab = (id: string, getNode: () => any) => {
     this.setState(({ tabs }: TabsState) => ({
       tabs: tabs.concat({ id, getNode }),
     }));
   };
 
-  private _removeTab = (id: string) => {
+  private removeTab = (id: string) => {
     this.setState((state: TabsState) => {
       const tabs = state.tabs.filter(tab => tab.id !== id);
       return { tabs };
     });
   };
 
-  private _createEvent(value: string) {
+  private createEvent(value: string) {
     return { target: { value } };
   }
 }
-
-const { string, func, bool, node, oneOfType, number } = PropTypes;
-
-Tabs.propTypes = {
-  children: node,
-  indicatorClassName: string,
-  value: string.isRequired,
-  vertical: bool,
-  width: oneOfType([string, number]),
-  onChange: func,
-};
-
-Tabs.childContextTypes = {
-  activeTab: string.isRequired,
-  addTab: func.isRequired,
-  notifyUpdate: func.isRequired,
-  removeTab: func.isRequired,
-  shiftFocus: func.isRequired,
-  switchTab: func.isRequired,
-  vertical: bool.isRequired,
-};
 
 export default Tabs;
