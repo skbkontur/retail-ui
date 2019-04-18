0ee87473a9ec18a3ec8a8fd15eef0a136e2b1b67
kga <kga@skbkontur.ru>
2019-04-18 19:19:05 +0500

    refactor(theming): make context simpler

--- packages/retail-ui/lib/theming/ThemeDecorator.ts
+++ packages/retail-ui/lib/theming/ThemeDecorator.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { IThemeContext, ThemeConsumer } from './ThemeProvider';
+import { ThemeConsumer } from './ThemeProvider';
 import { ITheme } from "../ThemeManager";
 
 type ComponentConstructorType = new (...args: any[]) => React.Component & { readonly theme: ITheme };
@@ -7,8 +7,8 @@ type ComponentConstructorType = new (...args: any[]) => React.Component & { read
 export function injectTheme<T extends ComponentConstructorType>(Component: T) {
   Component.prototype._originalRender = Component.prototype.render;
   Component.prototype.render = function() {
-    return React.createElement(ThemeConsumer, null, (themeContext: IThemeContext) => {
-      this.theme = themeContext.theme;
+    return React.createElement(ThemeConsumer, null, (theme: ITheme) => {
+      this.theme = theme;
       return this._originalRender();
     });
   };
