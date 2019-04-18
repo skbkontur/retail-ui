a1e396ac4ecb55890cbcfb9643f043efe43bd6a0
kga <kga@skbkontur.ru>
2019-04-18 18:11:32 +0500

    refactor(ModalClose): convert SFC to class

--- packages/retail-ui/components/Modal/ModalClose.tsx
+++ packages/retail-ui/components/Modal/ModalClose.tsx
@@ -1,22 +1,20 @@
 import * as React from 'react';
 import { CloseProps } from './ModalContext';
-
 import styles from './Modal.less';
-
 import { cx as classNames } from 'emotion';
 import ThemeManager from '../../lib/ThemeManager';
 import jsStyles from './Modal.styles';
 
-const Close: React.SFC<CloseProps> = (props: CloseProps) => {
-  const theme = ThemeManager.getTheme();
-  return (
-    <button
-      className={classNames(styles.close, jsStyles.close(theme), props.disableClose && styles.disabled)}
-      onClick={props.requestClose}
-    >
-      <span className={styles.closeOutline} />
-    </button>
-  );
-};
-
-export default Close;
+export default class Close extends React.Component<CloseProps> {
+  public render() {
+    const theme = ThemeManager.getTheme();
+    return (
+      <button
+        className={classNames(styles.close, jsStyles.close(theme), this.props.disableClose && styles.disabled)}
+        onClick={this.props.requestClose}
+      >
+        <span className={styles.closeOutline} />
+      </button>
+    );
+  }
+}
