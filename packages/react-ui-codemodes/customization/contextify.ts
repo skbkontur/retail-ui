9bbf15bcc2f2aa4c1228dab4728ea9b60e1affd2
kga <kga@skbkontur.ru>
2019-05-07 13:40:26 +0500

    refactor(contextify): satisfy tslint member-ordering

--- packages/react-ui-codemodes/customization/contextify.ts
+++ packages/react-ui-codemodes/customization/contextify.ts
@@ -107,11 +107,15 @@ function contextify(fileInfo: FileInfo, api: API) {
     renderMainMethod.accessibility = 'private';
     let insertThemePropAt = 0;
     let insertRenderMainAt = 0;
-    classDeclaration.body.body.forEach((n, i) => {
-      if (n.type === 'ClassProperty' && (n.static || (n as any).accessibility === 'public')) {
+    classDeclaration.body.body.forEach((n: any, i) => {
+      const isProperty = n.type === 'ClassProperty' && (!n.value || n.value && n.value.type !== 'ArrowFunctionExpression');
+      if (isProperty && (n.static || n.accessibility === 'public')) {
         insertThemePropAt = i;
       }
-      if (n.type === 'ClassMethod' && (n.static || n.accessibility === 'public')) {
+
+      const isClassicMethod = n.type === 'ClassMethod';
+      const isPropertyLikeMethod = n.type === 'ClassProperty' && n.value && n.value.type === 'ArrowFunctionExpression';
+      if ((isClassicMethod || isPropertyLikeMethod) && (n.static || n.accessibility === 'public')) {
         insertRenderMainAt = i;
       }
     });
