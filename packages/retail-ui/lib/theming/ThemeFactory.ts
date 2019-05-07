37e71d75fde55dc1de39cfd4193a1de5e29b5b6a
kga <kga@skbkontur.ru>
2019-05-07 17:22:52 +0500

    feat(ThemeFactory): override default theme

--- packages/retail-ui/lib/theming/ThemeFactory.ts
+++ packages/retail-ui/lib/theming/ThemeFactory.ts
@@ -2,13 +2,22 @@ import defaultThemeVariables from './themes/DefaultTheme';
 import { ITheme, IThemeIn } from './Theme';
 
 export default class ThemeFactory {
-  public static create(...themes: IThemeIn[]): ITheme {
-    return Object.freeze(Object.assign({}, defaultThemeVariables, ...themes)) as any;
+  public static create(...themes: IThemeIn[]) {
+    return Object.freeze(Object.assign({}, defaultThemeVariables, ...themes)) as ITheme;
   }
 
-  public static getDefaultTheme(): ITheme {
-    return this.defaultTheme as any;
+  public static getDefaultTheme() {
+    return this.defaultTheme;
   }
 
-  private static defaultTheme = Object.freeze(Object.assign({}, defaultThemeVariables));
+  public static overrideDefaultTheme(...themes: IThemeIn[]) {
+    themes.forEach(themePartial => {
+      Object.keys(themePartial).forEach(variableName => {
+        const descriptor = Object.getOwnPropertyDescriptor(themePartial, variableName)!;
+        Object.defineProperty(this.defaultTheme, variableName, descriptor);
+      });
+    });
+  }
+
+  private static defaultTheme = Object.assign({}, defaultThemeVariables) as ITheme;
 }
