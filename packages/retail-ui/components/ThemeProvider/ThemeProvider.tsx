b70bc7703b49c97f41a084cd54410df614c080d3
kga <kga@skbkontur.ru>
2019-05-08 14:37:07 +0500

    refactor(ThemeProvider): can accept partial theme

--- packages/retail-ui/components/ThemeProvider/ThemeProvider.tsx
+++ packages/retail-ui/components/ThemeProvider/ThemeProvider.tsx
@@ -1,3 +1,27 @@
-import { ThemeProvider } from '../internal/ThemeContext';
+import * as React from 'react';
+import { ThemeProvider as ThemeProviderInternal } from '../internal/ThemeContext';
+import { ITheme, IThemeIn } from '../../lib/theming/Theme';
+import ThemeFactory from '../../lib/theming/ThemeFactory';
 
-export default ThemeProvider;
+interface ThemeProviderProps {
+  value: IThemeIn;
+}
+
+export default class ThemeProvider extends React.Component<ThemeProviderProps> {
+  private theme: ITheme;
+
+  constructor(props: ThemeProviderProps) {
+    super(props);
+    this.theme = ThemeFactory.getOrCreate(props.value);
+  }
+
+  public componentWillReceiveProps(nextProps: Readonly<ThemeProviderProps>): void {
+    if (nextProps.value !== this.props.value) {
+      this.theme = ThemeFactory.getOrCreate(nextProps.value);
+    }
+  }
+
+  public render() {
+    return <ThemeProviderInternal value={this.theme}>{this.props.children}</ThemeProviderInternal>;
+  }
+}
