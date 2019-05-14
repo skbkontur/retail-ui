d48d8aaa2c1fbbed7b3d2b53b007325e7b244ab8
step-off <nofantasy12@gmail.com>
2019-05-14 10:55:19 +0500

    revert(Theme): add index signature to theme type

--- packages/retail-ui/lib/theming/Theme.ts
+++ packages/retail-ui/lib/theming/Theme.ts
@@ -1,7 +1,7 @@
 import defaultThemeVariables from './themes/DefaultTheme';
 import flatThemeVariables from './themes/FlatTheme';
 
-type ThemeType = typeof defaultThemeVariables & typeof flatThemeVariables & { [key: string]: string };
+type ThemeType = typeof defaultThemeVariables & typeof flatThemeVariables;
 type ThemeInType = Partial<ThemeType>;
 
 export interface ITheme extends ThemeType {}
