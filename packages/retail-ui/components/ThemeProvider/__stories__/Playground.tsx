4edef1f3c000b9fdcc2972a7a779e9b308306444
step-off <nofantasy12@gmail.com>
2019-05-14 16:55:48 +0500

    refactor(Playground): rewrite Playground as class component

--- packages/retail-ui/components/ThemeProvider/__stories__/Playground.tsx
+++ packages/retail-ui/components/ThemeProvider/__stories__/Playground.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { cx, css } from 'emotion';
 import Button from '../../Button';
 import Tabs from '../../Tabs/Tabs';
 import { ThemeType } from './enums';
@@ -17,6 +18,8 @@ import Radio from '../../Radio';
 import Checkbox from '../../Checkbox';
 import { FxInputPlayground } from './FxInputPlayground';
 import { CurrencyInputPlayground } from './CurrencyInputPlayground';
+import { ThemeConsumer } from '../../internal/ThemeContext';
+import { PlaygroundTheme } from './ThemeProvider.stories';
 
 export interface IComponentsListProps {
   activeThemeType: ThemeType;
@@ -24,140 +27,185 @@ export interface IComponentsListProps {
   onEditLinkClick: () => void;
 }
 
-export const Playground = (props: IComponentsListProps) => {
-  const { activeThemeType, onThemeChange, onEditLinkClick } = props;
+export class Playground extends React.Component<IComponentsListProps, {}> {
+  private theme!: PlaygroundTheme;
 
-  return (
-    <Gapped vertical gap={50}>
-      <TabsGroup activeThemeId={activeThemeType} onEditLinkClick={onEditLinkClick} onThemeChange={onThemeChange} />
-      <ButtonsGroup />
-      <InputsGroup />
-      <TokenInputsGroup />
-      <DifferentInputsGroup />
-      <TogglesGroup />
-      <SwitchersGroup />
-      <RadiosGroup />
-      <CheckboxesGroup />
-    </Gapped>
-  );
-};
+  public render() {
+    return (
+      <ThemeConsumer>
+        {theme => {
+          this.theme = theme as PlaygroundTheme;
+          return this.renderMain();
+        }}
+      </ThemeConsumer>
+    );
+  }
 
-interface ITabsGroupProps {
-  activeThemeId: ThemeType;
-  onThemeChange: ((ev: { target: { value: string } }, value: string) => void);
-  onEditLinkClick: () => void;
-}
+  /* tslint:disable jsx-key */
 
-const TabsGroup = (props: ITabsGroupProps) => (
-  <Gapped vertical={false} verticalAlign={'middle'} gap={80}>
-    <Tabs value={props.activeThemeId} onChange={props.onThemeChange} vertical={false}>
-      <Tabs.Tab id={ThemeType.Default}>Дефолтная тема</Tabs.Tab>
-      <Tabs.Tab id={ThemeType.Flat}>Плоская тема</Tabs.Tab>
-      <Tabs.Tab id={ThemeType.Custom}>Своя тема</Tabs.Tab>
-    </Tabs>
-    <Link onClick={props.onEditLinkClick}>Изменить свою тему</Link>
-  </Gapped>
-);
+  private renderMain() {
+    return (
+      <div
+        className={cx(
+          styles.playground,
+          css`
+            background: ${this.theme.backgroundMain};
+          `,
+        )}
+      >
+        <Gapped vertical gap={50}>
+          {this.renderTabsGroup()}
+          {this.renderButtonsGroup()}
+          {this.renderInputsGroup()}
+          {this.renderTokenInputsGroup()}
+          {this.renderDifferentInputsGroup()}
+          {this.renderTogglesGroup()}
+          {this.renderSwitchersGroup()}
+          {this.renderRadiosGroup()}
+          {this.renderCheckboxesGroup()}
+        </Gapped>
+      </div>
+    );
+  }
 
-/* tslint:disable jsx-key */
-const ButtonsGroup = () => {
-  const components = [
-    <Button size={'small'}>Small</Button>,
-    <Button size={'medium'}>Medium</Button>,
-    <Button size={'large'}>Large</Button>,
-    <Button size={'medium'} loading>
-      Loading
-    </Button>,
-    <Button size={'medium'} disabled>
-      Disabled
-    </Button>,
-    <Button use="default">Default</Button>,
-    <Button use="primary">Primary</Button>,
-    <Button use="success">Success</Button>,
-    <Button use="danger">Danger</Button>,
-    <Button use="pay">Pay</Button>,
-    <Button use="link">Link</Button>,
-    <Button icon={<Spinner type={'mini'} />} />,
-    <Button size={'medium'} arrow>
-      Далее
-    </Button>,
-    <Button size={'medium'} arrow={'left'}>
-      Назад
-    </Button>,
-  ];
-  return renderComponentsGroup('Кнопки', components);
-};
-
-const InputsGroup = () => {
-  const components = [
-    <Input width={400} prefix="https://kontur.ru/search?query=" rightIcon={<SearchIcon />} />,
-    <Input width={150} error />,
-    <Input width={150} warning />,
-    <Input width={150} disabled placeholder={'disabled'} />,
-    <Input width={150} placeholder={'Small'} size={'small'} />,
-    <Input width={150} placeholder={'Medium'} size={'medium'} />,
-    <Input width={150} placeholder={'Large'} size={'large'} />,
-  ];
-  return renderComponentsGroup('Поле ввода', components);
-};
-
-const TokenInputsGroup = () => {
-  const components = [<TokenInputPlayground />];
-  return renderComponentsGroup('Поле с токеном', components);
-};
-
-const DifferentInputsGroup = () => {
-  const components = [<CurrencyInputPlayground />, <FxInputPlayground />, <DatePickerPlayground />];
-  return renderComponentsGroup('Прочие поля', components);
-};
-
-const TogglesGroup = () => {
-  const components = [<TogglePlayground />];
-  return renderComponentsGroup('Тумблеры', components);
-};
-
-const SwitchersGroup = () => {
-  const components = [<SwitcherPlayground />, <SwitcherPlayground error />];
-  return renderComponentsGroup('Переключатели', components);
-};
-
-const RadiosGroup = () => {
-  const value = '';
-  const components = [
-    <Radio value={value} checked>
-      Первый вариант
-    </Radio>,
-    <Radio value={value} error>
-      Ошибка
-    </Radio>,
-    <Radio value={value} warning>
-      Предупреждение
-    </Radio>,
-    <Radio value={value} disabled>
-      Неактивный
-    </Radio>,
-  ];
-  return renderComponentsGroup('Радио', components);
-};
-
-const CheckboxesGroup = () => {
-  const components = [
-    <Checkbox checked>Первый вариант</Checkbox>,
-    <Checkbox error>Ошибка</Checkbox>,
-    <Checkbox warning>Предупреждение</Checkbox>,
-    <Checkbox disabled>Неактивный</Checkbox>,
-    <Checkbox initialIndeterminate>Неопределенный</Checkbox>,
-  ];
-  return renderComponentsGroup('Чекбоксы', components);
-};
-
-const renderComponentsGroup = (title: string, components: Array<React.ReactElement<any>>) => (
-  <Gapped vertical={false} verticalAlign={'top'} gap={10}>
-    <div className={styles.title}>{title}</div>
-    <div className={styles.componentsGroup}>
-      <Gapped verticalAlign={'middle'} gap={20}>
-        {components.map((element, index) => React.cloneElement(element, { key: index }))}
+  private renderTabsGroup = () => {
+    const { activeThemeType, onThemeChange, onEditLinkClick } = this.props;
+    return (
+      <Gapped vertical={false} verticalAlign={'middle'} gap={80}>
+        <Tabs value={activeThemeType} onChange={onThemeChange} vertical={false}>
+          <div
+            className={css`
+              color: ${this.theme.textColorMain};
+            `}
+          >
+            <Tabs.Tab id={ThemeType.Default}>Дефолтная</Tabs.Tab>
+            <Tabs.Tab id={ThemeType.Flat}>Плоская</Tabs.Tab>
+            <Tabs.Tab id={ThemeType.Dark}>Темная</Tabs.Tab>
+            <Tabs.Tab id={ThemeType.Custom}>Своя</Tabs.Tab>
+          </div>
+        </Tabs>
+        <Link onClick={onEditLinkClick}>Изменить свою тему</Link>
       </Gapped>
-    </div>
-  </Gapped>
-);
+    );
+  };
+
+  private renderButtonsGroup = () => {
+    const components = [
+      <Button size={'small'}>Small</Button>,
+      <Button size={'medium'}>Medium</Button>,
+      <Button size={'large'}>Large</Button>,
+      <Button size={'medium'} loading>
+        Loading
+      </Button>,
+      <Button size={'medium'} disabled>
+        Disabled
+      </Button>,
+      <Button use="default">Default</Button>,
+      <Button use="primary">Primary</Button>,
+      <Button use="success">Success</Button>,
+      <Button use="danger">Danger</Button>,
+      <Button use="pay">Pay</Button>,
+      <Button use="link">Link</Button>,
+      <Button icon={<Spinner type={'mini'} />} />,
+      <Button size={'medium'} arrow>
+        Далее
+      </Button>,
+      <Button size={'medium'} arrow={'left'}>
+        Назад
+      </Button>,
+    ];
+    return this.renderComponentsGroup('Кнопки', components);
+  };
+
+  private renderInputsGroup = () => {
+    const components = [
+      <Input width={400} prefix="https://kontur.ru/search?query=" rightIcon={<SearchIcon />} />,
+      <Input width={150} error />,
+      <Input width={150} warning />,
+      <Input width={150} disabled placeholder={'disabled'} />,
+      <Input width={150} placeholder={'Small'} size={'small'} />,
+      <Input width={150} placeholder={'Medium'} size={'medium'} />,
+      <Input width={150} placeholder={'Large'} size={'large'} />,
+    ];
+    return this.renderComponentsGroup('Поле ввода', components);
+  };
+
+  private renderTokenInputsGroup = () => {
+    const components = [<TokenInputPlayground />];
+    return this.renderComponentsGroup('Поле с токеном', components);
+  };
+
+  private renderDifferentInputsGroup = () => {
+    const components = [<CurrencyInputPlayground />, <FxInputPlayground />, <DatePickerPlayground />];
+    return this.renderComponentsGroup('Прочие поля', components);
+  };
+
+  private renderTogglesGroup = () => {
+    const components = [<TogglePlayground />];
+    return this.renderComponentsGroup('Тумблеры', components);
+  };
+
+  private renderSwitchersGroup = () => {
+    const components = [<SwitcherPlayground />, <SwitcherPlayground error />];
+    return this.renderComponentsGroup('Переключатели', components);
+  };
+
+  private renderRadiosGroup = () => {
+    const value = '';
+    const components = [
+      <Radio value={value} checked>
+        Первый вариант
+      </Radio>,
+      <Radio value={value} error>
+        Ошибка
+      </Radio>,
+      <Radio value={value} warning>
+        Предупреждение
+      </Radio>,
+      <Radio value={value} disabled>
+        Неактивный
+      </Radio>,
+    ];
+    return this.renderComponentsGroup('Радио', components);
+  };
+
+  private renderCheckboxesGroup = () => {
+    const components = [
+      <Checkbox checked>Первый вариант</Checkbox>,
+      <Checkbox error>Ошибка</Checkbox>,
+      <Checkbox warning>Предупреждение</Checkbox>,
+      <Checkbox disabled>Неактивный</Checkbox>,
+      <Checkbox initialIndeterminate>Неопределенный</Checkbox>,
+    ];
+    return this.renderComponentsGroup('Чекбоксы', components);
+  };
+
+  private renderComponentsGroup = (title: string, components: Array<React.ReactElement<any>>) => {
+    return (
+      <Gapped vertical={false} verticalAlign={'top'} gap={10}>
+        <div
+          className={cx(
+            styles.title,
+            css`
+              color: ${this.theme.textColorMain};
+            `,
+          )}
+        >
+          {title}
+        </div>
+        <div
+          className={cx(
+            styles.componentsGroup,
+            css`
+              color: ${this.theme.textColorMain};
+            `,
+          )}
+        >
+          <Gapped verticalAlign={'middle'} gap={20}>
+            {components.map((element, index) => React.cloneElement(element, { key: index }))}
+          </Gapped>
+        </div>
+      </Gapped>
+    );
+  };
+}
