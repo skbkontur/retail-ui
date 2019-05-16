6c9df865a4e28ef55afe7846b3403c31c060e361
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2019-05-16 17:04:08 +0500

    fix(tabListener): reset isTabPressed flag on focus

--- packages/retail-ui/lib/events/tabListener.ts
+++ packages/retail-ui/lib/events/tabListener.ts
@@ -3,7 +3,14 @@ const KEYCODE_TAB = 9;
 class TabListener {
   public isTabPressed: boolean = false;
   constructor() {
-    window.addEventListener('keydown', event => (this.isTabPressed = event.keyCode === KEYCODE_TAB));
+    window.addEventListener('keydown', event => {
+      if (event.keyCode === KEYCODE_TAB) {
+        setTimeout(() => {
+          this.isTabPressed = true;
+        }, 0);
+      }
+    });
+    window.addEventListener('focus', () => (this.isTabPressed = false), true);
   }
 }
 
