0b6ef3cb307543147ddbaaea1251f9ac9bad620e
lossir <m.pakhomov@skbkontur.ru>
2019-05-20 11:24:47 +0500

    refactor(TabListener): debug (10)

--- packages/retail-ui/lib/events/tabListener.ts
+++ packages/retail-ui/lib/events/tabListener.ts
@@ -3,7 +3,7 @@ class TabListener {
   public isTabPressed: boolean = false;
   constructor() {
     window.addEventListener('keydown', event => (this.isTabPressed = event.keyCode === TAB));
-    window.addEventListener('keyup', event => (this.isTabPressed = this.isTabPressed && event.keyCode !== TAB), true);
+    window.addEventListener('keyup', () => (this.isTabPressed = false), true);
   }
 }
 
