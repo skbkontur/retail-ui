e25c96e0cb611a8d96d86397b2409387cf2288f3
lossir <m.pakhomov@skbkontur.ru>
2019-05-20 12:37:54 +0500

    refactor(tabListener): return `event.keyCode` instead `event.key`

--- packages/retail-ui/lib/events/tabListener.ts
+++ packages/retail-ui/lib/events/tabListener.ts
@@ -1,8 +1,8 @@
+const TAB = 9;
 class TabListener {
   public isTabPressed: boolean = false;
   constructor() {
-    window.addEventListener('keydown', event => (this.isTabPressed = event.key === 'Tab'));
-    window.addEventListener('keyup', event => (this.isTabPressed = this.isTabPressed && event.key !== 'Tab'));
+    window.addEventListener('keydown', event => (this.isTabPressed = event.keyCode === TAB));
     window.addEventListener('mousedown', () => (this.isTabPressed = false));
   }
 }
