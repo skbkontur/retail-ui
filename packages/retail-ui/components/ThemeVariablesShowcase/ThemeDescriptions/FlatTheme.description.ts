605a5ebe918d86201b191385832080673d094832
kga <kga@skbkontur.ru>
2019-05-22 20:40:16 +0500

    chore(ThemeVariablesShowcase): recollect variables

--- packages/retail-ui/components/ThemeVariablesShowcase/ThemeDescriptions/FlatTheme.description.ts
+++ packages/retail-ui/components/ThemeVariablesShowcase/ThemeDescriptions/FlatTheme.description.ts
@@ -1,9 +1,109 @@
 export default {
+  btnCheckedShadowArrow: [
+    "btnCheckedBg"
+  ],
+  btnCheckedShadowArrowLeft: [
+    "btnCheckedBg"
+  ],
+  btnFocusBorder: [
+    "borderColorFocus"
+  ],
   btnCheckedHoverBorderColor: [
     "btnCheckedBg"
   ],
   btnDefaultShadowArrowLeft: [
     "btnDefaultShadowArrow"
+  ],
+  btnDefaultCheckedShadowArrow: [
+    "btnCheckedBg"
+  ],
+  btnDefaultHoverShadowArrow: [
+    "btnDefaultHoverBorderColor",
+    "btnDefaultHoverBg"
+  ],
+  btnDefaultHoverShadowArrowLeft: [
+    "btnDefaultHoverBorderColor",
+    "btnDefaultHoverBg"
+  ],
+  btnDefaultActiveShadowArrow: [
+    "btnDefaultActiveBorderColor",
+    "btnDefaultActiveBg"
+  ],
+  btnDefaultActiveShadowArrowLeft: [
+    "btnDefaultActiveBorderColor",
+    "btnDefaultActiveBg"
+  ],
+  btnPrimaryShadowArrow: [
+    "btnPrimaryDisabledBg"
+  ],
+  btnPrimaryShadowArrowLeft: [
+    "btnPrimaryDisabledBg"
+  ],
+  btnPrimaryHoverShadowArrow: [
+    "btnPrimaryHoverBg"
+  ],
+  btnPrimaryHoverShadowArrowLeft: [
+    "btnPrimaryHoverBg"
+  ],
+  btnPrimaryActiveShadowArrow: [
+    "btnPrimaryActiveBg"
+  ],
+  btnPrimaryActiveShadowArrowLeft: [
+    "btnPrimaryActiveBg"
+  ],
+  btnSuccessShadowArrow: [
+    "btnSuccessDisabledBg"
+  ],
+  btnSuccessShadowArrowLeft: [
+    "btnSuccessDisabledBg"
+  ],
+  btnSuccessHoverShadowArrow: [
+    "btnSuccessHoverBg"
+  ],
+  btnSuccessHoverShadowArrowLeft: [
+    "btnSuccessHoverBg"
+  ],
+  btnSuccessActiveShadowArrow: [
+    "btnSuccessActiveBg"
+  ],
+  btnSuccessActiveShadowArrowLeft: [
+    "btnSuccessActiveBg"
+  ],
+  btnDangerShadowArrow: [
+    "btnDangerDisabledBg"
+  ],
+  btnDangerShadowArrowLeft: [
+    "btnDangerDisabledBg"
+  ],
+  btnDangerHoverShadowArrow: [
+    "btnDangerHoverBg"
+  ],
+  btnDangerHoverShadowArrowLeft: [
+    "btnDangerHoverBg"
+  ],
+  btnDangerActiveShadowArrow: [
+    "btnDangerActiveBg"
+  ],
+  btnDangerActiveShadowArrowLeft: [
+    "btnDangerActiveBg"
+  ],
+  btnPayShadowArrow: [
+    "btnPayDisabledBg"
+  ],
+  btnPayShadowArrowLeft: [
+    "btnPayDisabledBg"
+  ],
+  btnPayHoverShadowArrow: [
+    "btnPayHoverBg"
+  ],
+  btnPayHoverShadowArrowLeft: [
+    "btnPayHoverBg"
+  ],
+  btnPayActiveShadowArrow: [
+    "btnPayActiveBg"
+  ],
+  btnPayActiveShadowArrowLeft: [
+    "btnPayActiveBg"
   ],
   chbBorder: [
     "btnDefaultBorder"
@@ -28,6 +128,9 @@ export default {
   ],
   radioActiveBg: [
     "chbActiveBg"
+  ],
+  radioFocusShadow: [
+    "outlineColorFocus"
   ],
   radioCheckedBgColor: [
     "chbCheckedBg"
