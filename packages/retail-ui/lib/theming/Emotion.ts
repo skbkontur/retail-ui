0a8091b88b9f6f768ec5eff46707e184398b1bad
kga <kga@skbkontur.ru>
2019-05-22 18:42:34 +0500

    refactor(theming): take less specificityLevel into consideration

--- packages/retail-ui/lib/theming/Emotion.ts
+++ packages/retail-ui/lib/theming/Emotion.ts
@@ -1,6 +1,13 @@
 import createEmotion from 'create-emotion';
 import extraScopePlugin from 'stylis-plugin-extra-scope';
 import Upgrade from '../Upgrades';
+import LESS_VARIABLES from '../../components/variables.less';
+
+// NOTE: for ones who overrides specificityLevel at custom less
+const specificityLevel = parseInt(LESS_VARIABLES.specificityLevel, 10) || 0;
+if (specificityLevel) {
+  Upgrade.setSpecificityLevel(specificityLevel);
+}
 
 const scope = new Array(Upgrade.getSpecificityLevel()).fill('.retail-ui').join('');
 
