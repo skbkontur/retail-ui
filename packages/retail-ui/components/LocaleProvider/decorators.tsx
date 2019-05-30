6f1bd5fb25183ab75979e5173882ddade61af429
lossir <m.pakhomov@skbkontur.ru>
2019-05-30 17:23:20 +0500

    refactor(LocaleProvider): add method `componentLocaleDidUpdate()`, etc

--- packages/retail-ui/components/LocaleProvider/decorators.tsx
+++ packages/retail-ui/components/LocaleProvider/decorators.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
+import { defaultLangCode } from './constants';
 import { LocaleConsumer, LocaleProviderProps } from './LocaleProvider';
 import { LocaleHelper } from './LocaleHelper';
-import { LocaleControls } from './types';
+import { LangCodes, LocaleControls } from './types';
+import isEqual from 'lodash.isequal'
+
+interface LocaleInstance {
+  componentLocaleDidUpdate?: () => void;
+}
 
 export function locale<C>(controlName: keyof LocaleControls, localeHelper: LocaleHelper<C>) {
-  return <T extends { new (...args: any[]): React.Component }>(constructor: T) => {
+  return <T extends { new (...args: any[]): React.Component & LocaleInstance }>(constructor: T) => {
     const LocaleDecorator = class extends constructor {
       public controlName: keyof LocaleControls = controlName;
       public localeHelper: LocaleHelper<C> = localeHelper;
@@ -15,7 +21,11 @@ export function locale<C>(controlName: keyof LocaleControls, localeHelper: Local
         return (
           <LocaleConsumer>
             {localeContext => {
+              const prev = this._localeContext;
               this._localeContext = localeContext;
+              if (!isEqual({}, prev) && !isEqual(prev, localeContext)) {
+                setTimeout(() => this.componentLocaleDidUpdate && this.componentLocaleDidUpdate(), 0);
+              }
               return super.render();
             }}
           </LocaleConsumer>
@@ -26,6 +36,10 @@ export function locale<C>(controlName: keyof LocaleControls, localeHelper: Local
         const langCode = this._localeContext.langCode;
         const localeFromContext = this._localeContext.locale && this._localeContext.locale[this.controlName];
         return Object.assign({}, this.localeHelper.get(langCode), localeFromContext);
+      }
+
+      public get langCode(): LangCodes {
+        return this._localeContext.langCode || defaultLangCode;
       }
     };
     Object.defineProperty(LocaleDecorator, 'name', { value: constructor.name });
