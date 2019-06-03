17f711f749c2d4426f3fbf3866cb98e5133d034b
lossir <m.pakhomov@skbkontur.ru>
2019-06-03 14:24:07 +0500

    refactor(LocaleProvider): change date component order for 'en_GB'

--- packages/retail-ui/lib/date/localeSets.ts
+++ packages/retail-ui/lib/date/localeSets.ts
@@ -9,8 +9,8 @@ const DateCustomSet_DMY_Dot_Monday_6_7: InternalDateLocaleSet = {
   notWorkingDays: [InternalDateDayWeek.Saturday, InternalDateDayWeek.Sunday],
 };
 
-const DateCustomSet_MDY_Slash_Sunday_6_7: InternalDateLocaleSet = {
-  order: InternalDateOrder.MDY,
+const DateCustomSet_DMY_Slash_Sunday_6_7: InternalDateLocaleSet = {
+  order: InternalDateOrder.DMY,
   separator: InternalDateSeparator.Slash,
   firstDayWeek: InternalDateFirstDayWeek.Sunday,
   notWorkingDays: [InternalDateDayWeek.Saturday, InternalDateDayWeek.Sunday],
@@ -20,5 +20,5 @@ export const internalDateLocale: {
   [key in LangCodes]: InternalDateLocaleSet
 } = {
   [LangCodes.ru_RU]: DateCustomSet_DMY_Dot_Monday_6_7,
-  [LangCodes.en_GB]: DateCustomSet_MDY_Slash_Sunday_6_7,
+  [LangCodes.en_GB]: DateCustomSet_DMY_Slash_Sunday_6_7,
 };
