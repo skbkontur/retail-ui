1f921190657d42552b59a999550faade11642b55
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2019-06-06 15:26:26 +0500

    build(styleguide): build styleguide into the dir based on the dist-tag

--- packages/retail-ui/.styleguide/build.js
+++ packages/retail-ui/.styleguide/build.js
@@ -4,6 +4,7 @@ const { copySync, removeSync, emptyDirSync } = require('fs-extra');
 const { getPackageInfo, TAGS } = require('../scripts/package');
 const { distTag, publishVersion } = getPackageInfo();
 const { LATEST, LTS, UNSTABLE, OLD } = TAGS;
+const { ROOT_DIR, VERSION_DIR } = require('./helpers');
 
 const ROOT_CONFIG = path.join(__dirname, 'config/root.config.js');
 const VERSION_CONFIG = path.join(__dirname, 'config/version.config.js');
@@ -20,8 +21,34 @@ const buildConfig = config => {
   console.log(stdout.toString());
 };
 
-buildConfig(VERSION_CONFIG);
+const buildStyleguideRoot = () => {
+  buildConfig(ROOT_CONFIG);
+};
+
+const buildStyleguideVersion = tag => {
+  const BUILD_DIR = VERSION_DIR;
+  const FINAL_DIR = {
+    [LATEST]: path.join(ROOT_DIR, publishVersion),
+    [LTS]: path.join(ROOT_DIR, 'lts'),
+    [UNSTABLE]: path.join(ROOT_DIR, 'unstable', publishVersion),
+    [OLD]: path.join(ROOT_DIR, publishVersion),
+  };
+
+  buildConfig(VERSION_CONFIG);
+
+  if (BUILD_DIR !== FINAL_DIR[tag]) {
+    copySync(BUILD_DIR, FINAL_DIR[tag], { overwrite: true });
+    console.log(`Style guide copied to: ${FINAL_DIR[tag]}`);
+  }
+
+  if (tag === UNSTABLE) {
+    removeSync(BUILD_DIR);
+    console.log(`Style guide removed from: ${BUILD_DIR}`);
+  }
+};
+
+buildStyleguideVersion(distTag);
 
 if (distTag === LATEST) {
-  buildConfig(ROOT_CONFIG);
+  buildStyleguideRoot();
 }
