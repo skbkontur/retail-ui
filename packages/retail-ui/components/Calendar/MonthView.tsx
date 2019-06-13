593d6a8bc7e1456be3d09600273fe3454b3590fc
kga <kga@skbkontur.ru>
2019-06-13 19:29:41 +0500

    feat(Calendar): customize with css-in-js

--- packages/retail-ui/components/Calendar/MonthView.tsx
+++ packages/retail-ui/components/Calendar/MonthView.tsx
@@ -1,12 +1,12 @@
-import classNames from 'classnames';
 import * as React from 'react';
 import * as CDS from './CalendarDateShape';
-
 import config from './config';
-
 import styles from './MonthView.less';
-
 import DateSelect from '../DateSelect';
+import { cx as classNames } from '../../lib/theming/Emotion';
+import { ITheme } from '../../lib/theming/Theme';
+import jsStyles from './MonthView.styles';
+import ThemeConsumer from '../ThemeConsumer';
 
 interface MonthViewProps {
   children: React.ReactNode;
@@ -25,97 +25,109 @@ interface MonthViewProps {
   yearSelectRef: (select: DateSelect | null) => void;
 }
 
-export const MonthView = ({
-  children,
-  firstDayOffset,
-  height,
-  isFirstInYear,
-  isLastInYear,
-  maxDate,
-  minDate,
-  month,
-  top,
-  year,
-  onMonthSelect,
-  onYearSelect,
-  monthSelectRef,
-  yearSelectRef,
-}: MonthViewProps) => {
-  const isTopNegative = top <= 0;
-  const isHeaderSticked = isTopNegative && height >= -top;
+export class MonthView extends React.Component<MonthViewProps> {
+  private theme!: ITheme;
 
-  const headerTop = isHeaderSticked ? Math.min(-top, height - config.MONTH_TITLE_HEIGHT) : 0;
+  public render() {
+    return (
+      <ThemeConsumer>
+        {theme => {
+          this.theme = theme;
+          return this.renderMain();
+        }}
+      </ThemeConsumer>
+    );
+  }
 
-  const alpha = isHeaderSticked ? (height + top - config.MONTH_TITLE_HEIGHT) / 10 : 1;
+  private renderMain() {
+    const {
+      children,
+      height,
+      isFirstInYear,
+      isLastInYear,
+      maxDate,
+      minDate,
+      month,
+      top,
+      year,
+      onMonthSelect,
+      onYearSelect,
+      monthSelectRef,
+      yearSelectRef,
+    } = this.props;
 
-  const borderBottomColor = `rgba(223, 222, 222, ${alpha})`;
+    const isTopNegative = top <= 0;
+    const isHeaderSticky = isTopNegative && height >= -top;
+    const headerTop = isHeaderSticky ? Math.min(-top, height - config.MONTH_TITLE_HEIGHT) : 0;
+    const alpha = isHeaderSticky ? (height + top - config.MONTH_TITLE_HEIGHT) / 10 : 1;
+    const borderBottomColor = `rgba(223, 222, 222, ${alpha})`;
+    const isYearVisible = isFirstInYear || isHeaderSticky;
+    const yearTop = isHeaderSticky && !isLastInYear ? -headerTop - top : 0;
+    const monthSelectDisabled = top > 40 || headerTop < 0 || headerTop >= height - config.MONTH_TITLE_HEIGHT;
+    const yearSelectDisabled = top > 40 || (isLastInYear && top < -height + config.MONTH_TITLE_HEIGHT);
 
-  const isYearVisible = isFirstInYear || isHeaderSticked;
-  const yearTop = isHeaderSticked && !isLastInYear ? -headerTop - top : 0;
-
-  const monthSelectDisabled = top > 40 || headerTop < 0 || headerTop >= height - config.MONTH_TITLE_HEIGHT;
-
-  const yearSelectDisabled = top > 40 || (isLastInYear && top < -height + config.MONTH_TITLE_HEIGHT);
-
-  const getMinMonth = (value: number) => {
-    let min = 0;
-    for (let i = 0; i < 12; ++i) {
-      if (minDate && CDS.isGreaterOrEqual({ date: 31, month: i, year: value }, minDate)) {
-        min = i;
-        break;
+    const getMinMonth = (value: number) => {
+      let min = 0;
+      for (let i = 0; i < 12; ++i) {
+        if (minDate && CDS.isGreaterOrEqual({ date: 31, month: i, year: value }, minDate)) {
+          min = i;
+          break;
+        }
       }
-    }
-    return min;
-  };
+      return min;
+    };
 
-  const getMaxMonth = (value: number) => {
-    let max = 11;
-    for (let i = 11; i >= 0; --i) {
-      if (maxDate && CDS.isLessOrEqual({ date: 1, month: i, year: value }, maxDate)) {
-        max = i;
-        break;
+    const getMaxMonth = (value: number) => {
+      let max = 11;
+      for (let i = 11; i >= 0; --i) {
+        if (maxDate && CDS.isLessOrEqual({ date: 1, month: i, year: value }, maxDate)) {
+          max = i;
+          break;
+        }
       }
-    }
-    return max;
-  };
+      return max;
+    };
 
-  return (
-    <div className={styles.month} style={{ top }} key={month + '-' + year}>
-      <div
-        style={{ lineHeight: `${config.MONTH_TITLE_HEIGHT}px`, top: headerTop, borderBottomColor }}
-        className={classNames({
-          [styles.monthTitle]: true,
-          [styles.headerSticked]: isHeaderSticked,
-        })}
-      >
-        <div className={styles.headerMonth}>
-          <DateSelect
-            disabled={monthSelectDisabled}
-            width={85}
-            type="month"
-            value={month}
-            onChange={onMonthSelect}
-            ref={!monthSelectDisabled ? monthSelectRef : undefined}
-            minValue={getMinMonth(year)}
-            maxValue={getMaxMonth(year)}
-          />
-        </div>
-        {isYearVisible && (
-          <div className={styles.headerYear} style={{ top: yearTop }}>
+    return (
+      <div className={styles.month} style={{ top }} key={month + '-' + year}>
+        <div
+          style={{ lineHeight: `${config.MONTH_TITLE_HEIGHT}px`, top: headerTop, borderBottomColor }}
+          className={classNames({
+            [styles.monthTitle]: true,
+            [jsStyles.monthTitle(this.theme)]: true,
+            [styles.headerSticky]: isHeaderSticky,
+            [jsStyles.headerSticky(this.theme)]: isHeaderSticky,
+          })}
+        >
+          <div className={styles.headerMonth}>
             <DateSelect
-              disabled={yearSelectDisabled}
-              width={50}
-              type="year"
-              value={year}
-              minValue={minDate ? minDate.year : undefined}
-              maxValue={maxDate ? maxDate.year : undefined}
-              onChange={onYearSelect}
-              ref={!yearSelectDisabled ? yearSelectRef : undefined}
+              disabled={monthSelectDisabled}
+              width={85}
+              type="month"
+              value={month}
+              onChange={onMonthSelect}
+              ref={!monthSelectDisabled ? monthSelectRef : undefined}
+              minValue={getMinMonth(year)}
+              maxValue={getMaxMonth(year)}
             />
           </div>
-        )}
+          {isYearVisible && (
+            <div className={styles.headerYear} style={{ top: yearTop }}>
+              <DateSelect
+                disabled={yearSelectDisabled}
+                width={50}
+                type="year"
+                value={year}
+                minValue={minDate ? minDate.year : undefined}
+                maxValue={maxDate ? maxDate.year : undefined}
+                onChange={onYearSelect}
+                ref={!yearSelectDisabled ? yearSelectRef : undefined}
+              />
+            </div>
+          )}
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
-  );
-};
+    );
+  }
+}
