c4538ebf5434632391abbaa6a81c5c1226b0771e
kga <kga@skbkontur.ru>
2019-06-13 17:19:28 +0500

    chore(storybook): use ThemeProvider instead of overrideDefaultTheme

--- packages/retail-ui/.storybook/config.js
+++ packages/retail-ui/.storybook/config.js
@@ -2,12 +2,8 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { configure, addDecorator, storiesOf, getStorybook } from '@storybook/react';
-import ThemeFactory from "../lib/theming/ThemeFactory";
-import FlatTheme from "../lib/theming/themes/FlatTheme";
-
-if (process.env.STORYBOOK_FLAT_UI) {
-  ThemeFactory.overrideDefaultTheme(FlatTheme)
-}
+import FlatTheme from '../lib/theming/themes/FlatTheme';
+import ThemeProvider from '../components/ThemeProvider';
 
 let stories = null;
 
@@ -38,6 +34,10 @@ addDecorator(story => (
     {story()}
   </div>
 ));
+
+if (process.env.STORYBOOK_FLAT_UI) {
+  addDecorator(story => <ThemeProvider value={FlatTheme}>{story()}</ThemeProvider>);
+}
 
 const req = require.context('../components', true, /.stories.tsx?$/);
 
