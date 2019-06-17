316cd5154780f15d64e9e24b92f8f6b5de9d30f6
kga <kga@skbkontur.ru>
2019-06-17 12:37:40 +0500

    refactor(TokenInput): simplify styles

--- packages/retail-ui/components/TokenInput/TokenInput.styles.ts
+++ packages/retail-ui/components/TokenInput/TokenInput.styles.ts
@@ -3,40 +3,32 @@ import styles from './TokenInput.less';
 import { ITheme } from '../../lib/theming/Theme';
 
 const jsStyles = {
+  label(t: ITheme) {
+    return css`
+      background: ${t.bgDefault};
+      border: 1px solid ${t.borderColorGrayLight};
+      border-top-color: ${t.borderColorGrayDark};
+    `;
+  },
+
   warning(t: ITheme) {
     return css`
-      .${styles.root} .${styles.label}& {
-        border: 1px solid ${t.borderColorWarning};
-        box-shadow: 0 0 0 1px ${t.borderColorWarning};
-      }
+      border: 1px solid ${t.borderColorWarning};
+      box-shadow: 0 0 0 1px ${t.borderColorWarning};
     `;
   },
 
   error(t: ITheme) {
     return css`
-      .${styles.root} .${styles.label}& {
-        border: 1px solid ${t.borderColorError};
-        box-shadow: 0 0 0 1px ${t.borderColorError};
-      }
-    `;
-  },
-
-  label(t: ITheme) {
-    return css`
-      .${styles.root} & {
-        background: ${t.bgDefault};
-        border: 1px solid ${t.borderColorGrayLight};
-        border-top-color: ${t.borderColorGrayDark};
-      }
+      border: 1px solid ${t.borderColorError};
+      box-shadow: 0 0 0 1px ${t.borderColorError};
     `;
   },
 
   labelFocused(t: ITheme) {
     return css`
-      .${styles.root} & {
-        border: 1px solid ${t.borderColorFocus};
-        box-shadow: 0 0 0 1px ${t.borderColorFocus};
-      }
+      border: 1px solid ${t.borderColorFocus};
+      box-shadow: 0 0 0 1px ${t.borderColorFocus};
     `;
   },
 
