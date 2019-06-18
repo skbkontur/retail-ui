e04ae80af618de0ba476edfac8a075db29843dbd
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2019-06-18 10:36:39 +0500

    refactor: switch to execSync

--- packages/retail-ui/.styleguide/build.js
+++ packages/retail-ui/.styleguide/build.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { spawnSync } = require('child_process');
+const { execSync } = require('child_process');
 const { copySync, removeSync, emptyDirSync } = require('fs-extra');
 const { getPackageInfo, TAGS } = require('../scripts/package');
 const { distTag, publishVersion } = getPackageInfo();
@@ -10,15 +10,9 @@ const ROOT_CONFIG = path.join(__dirname, 'config/root.config.js');
 const VERSION_CONFIG = path.join(__dirname, 'config/version.config.js');
 
 const buildConfig = config => {
-  const { stdout, stderr } = spawnSync('yarn styleguidist build', ['--config', config], {
+  execSync(`yarn styleguidist build --config ${config}`, {
     shell: true,
   });
-
-  if (stderr) {
-    console.log(stderr.toString());
-  }
-
-  console.log(stdout.toString());
 };
 
 const buildStyleguideRoot = () => {
