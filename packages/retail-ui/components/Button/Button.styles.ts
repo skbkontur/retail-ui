ee5507ebeb01ababf6cbaceff0a62629839b9c4e
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2019-08-22 11:54:09 +0500

    refactor(Button): fix arrow error/warning outline

--- packages/retail-ui/components/Button/Button.styles.ts
+++ packages/retail-ui/components/Button/Button.styles.ts
@@ -176,11 +176,11 @@ const jsClasses = {
           background: ${t.btnDisabledBg};
           box-shadow: 1px -1px 0 0 ${t.btnDisabledShadowColor};
 
-          &.${classes.arrow_warning} {
+          &.${jsClasses.arrow_warning(t)} {
             box-shadow: 2px -2px 0 0 ${t.borderColorWarning};
           }
 
-          &.${classes.arrow_error} {
+          &.${jsClasses.arrow_error(t)} {
             box-shadow: 2px -2px 0 0 ${t.borderColorError};
           }
         }
