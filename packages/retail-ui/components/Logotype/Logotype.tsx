94fc0276b00011618f36cf4bad4a821a4c6092c6
ppecheguzov <ppecheguzov@skbkontur.ru>
2019-08-23 13:41:33 +0700

    feat(Logotype): add props size and onArrowClick

--- packages/retail-ui/components/Logotype/Logotype.tsx
+++ packages/retail-ui/components/Logotype/Logotype.tsx
@@ -12,8 +12,12 @@ import jsStyles from './Logotype.styles';
 import { ThemeConsumer } from '../internal/ThemeContext';
 import { ITheme } from '../../lib/theming/Theme';
 
-const createCloud = (color: string) => (
-  <svg width="24" height="17" viewBox="0 0 24 17" className={styles.cloud}>
+const createCloud = (size: number, color: string) => (
+  <svg
+    viewBox={`0 0 23 17`}
+    className={styles.cloud}
+    style={{ width: size * 1.0455, height: size * 0.773, verticalAlign: -(size * 0.0455) }}
+  >
     <path
       fill={color}
       d="M10.7947842,0 C7.67880353,0 5.01508082,2.01193926 3.95917347,4.8416599 C1.64805866,5.73629303 0,8.0359337 0,10.7178625 C0,14.1880144 2.7403766,17 6.10931511,17 L16.1509748,17 C19.926179,17 23,13.8471079 23,9.9670801 C23,6.23681465 20.163806,3.18047989 16.5974838,2.93810131 C15.2464589,1.11730613 13.1365606,0 10.7947842,0 M10.8198444,2 C12.5405642,2 14.1189689,2.79038796 15.1502918,4.16792125 L15.5920233,4.75130284 L16.3109922,4.80399537 C18.9410506,4.97148234 21,7.20903301 21,9.89635206 C21,12.7059931 18.7525292,15 15.9857004,15 L6.30087549,15 C3.93142023,15 2,13.0315576 2,10.6133468 C2,8.80298205 3.12373541,7.16010423 4.78715953,6.51650261 L5.45807393,6.25868558 L5.71682879,5.57368269 C6.52081712,3.43587145 8.57052529,2 10.8198444,2"
@@ -41,6 +45,11 @@ export interface LogotypeProps {
    */
   suffix?: string;
   /**
+   * Размер шрифта логотипа в пикселях.
+   * @default 22
+   */
+  size?: number;
+  /**
    * Цвет логотипа Контура в rgb, rgba, hex
    */
   textColor: string;
@@ -48,6 +57,10 @@ export interface LogotypeProps {
    * Наличие виджета с продуктами
    */
   withWidget?: boolean;
+  /**
+   * нажатие по кнопке открытия виджета
+   */
+  onArrowClick?: React.MouseEventHandler<HTMLButtonElement>;
   /**
    * Словарь текстовых констант
    * @default { prefix: 'к', suffix: 'нтур' }
@@ -61,6 +74,7 @@ class Logotype extends React.Component<LogotypeProps> {
     color: PropTypes.string,
     href: PropTypes.string,
     suffix: PropTypes.string,
+    size: PropTypes.number,
     textColor: PropTypes.string,
     withWidget: PropTypes.bool,
     locale: PropTypes.shape({
@@ -71,6 +85,7 @@ class Logotype extends React.Component<LogotypeProps> {
 
   public static defaultProps = {
     color: '#D92932',
+    size: 22,
     textColor: '#000',
     component: 'a',
     href: '/',
@@ -110,6 +125,7 @@ class Logotype extends React.Component<LogotypeProps> {
       textColor,
       component: Component,
       suffix,
+      size,
       href,
       withWidget,
       locale: propLocale = this.locale,
@@ -121,9 +137,14 @@ class Logotype extends React.Component<LogotypeProps> {
     return (
       <div id="spwDropdown" className={dropdownClassName}>
         <span ref={this.refLogoWrapper} className={styles.widgetWrapper}>
-          <Component href={href} tabIndex="-1" className={cx(styles.root, jsStyles.root(this.theme))}>
+          <Component
+            href={href}
+            tabIndex="-1"
+            className={cx(styles.root, jsStyles.root(this.theme))}
+            style={{ fontSize: `${size || 22}px` }}
+          >
             <span style={{ color: textColor }}>{propLocale.prefix}</span>
-            <span style={{ color }}>{createCloud(color)}</span>
+            <span style={{ color }}>{createCloud(size || 22, color)}</span>
             <span style={{ color: textColor }}>
               {propLocale.suffix}
               {suffix && '.'}
@@ -133,7 +154,7 @@ class Logotype extends React.Component<LogotypeProps> {
           {withWidget && <span className={cx(styles.divider, jsStyles.divider(this.theme))} />}
         </span>
         {withWidget && (
-          <button className={styles.button}>
+          <button className={styles.button} onClick={this.props.onArrowClick}>
             <ArrowChevronDownIcon color="#aaa" size={20} />
           </button>
         )}
