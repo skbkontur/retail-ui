c4dcf3e854292728c26b7bcaf3301e1c53c9d91e
wKich <nflazzer@gmail.com>
2019-08-30 13:15:20 +0500

    build(react-ui-selenium): update creevey, improve scripts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -371,7 +371,7 @@ object ReactUI_ScreenshotTests : BuildType({
         step {
             name = "Test UI"
             type = "jonnyzzz.yarn"
-            param("yarn_commands", "workspace react-ui-selenium test:ci")
+            param("yarn_commands", "workspace react-ui-selenium test")
         }
     }
 
