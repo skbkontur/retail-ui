1bd68b44aa1fd353afa59709d2a0836e2ff377c3
dmitry.lazarev <dmitry.lazarev@TeamCity>
2019-09-02 10:22:22 +0500

    TeamCity change in 'Frontend Infrastructure / Packages / ReactUI' project: build features of 'Screenshot tests' build configuration were updated

--- .teamcity/patches/buildTypes/ReactUI_ScreenshotTests.kts
+++ .teamcity/patches/buildTypes/ReactUI_ScreenshotTests.kts
@@ -1,6 +1,8 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -15,5 +17,29 @@ changeBuildType(RelativeId("ReactUI_ScreenshotTests")) {
             "Unexpected option value: executionTimeoutMin = $executionTimeoutMin"
         }
         executionTimeoutMin = 60
+    }
+
+    features {
+        val feature1 = find<PullRequests> {
+            pullRequests {
+                provider = github {
+                    authType = token {
+                        token = "credentialsJSON:e85896f8-074d-433d-af0c-704bc784121e"
+                    }
+                    filterTargetBranch = "refs/heads/master"
+                    filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+                }
+            }
+        }
+        feature1.apply {
+            provider = github {
+                serverUrl = ""
+                authType = token {
+                    token = "credentialsJSON:37119025-2749-4abf-8ed8-ff4221b59d50"
+                }
+                filterTargetBranch = "refs/heads/master"
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+            }
+        }
     }
 }
