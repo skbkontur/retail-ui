3c23035e7ee85b66007b25a3361d84c7f2cdf017
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2019-10-07 18:55:53 +0500

    refactor(ThemeContext): cast original Context type

--- packages/retail-ui/lib/theming/ThemeContext.ts
+++ packages/retail-ui/lib/theming/ThemeContext.ts
@@ -1,9 +1,11 @@
-import React, { Context } from 'react';
+import * as React from 'react';
 import createReactContext from 'create-react-context';
 
 import { ITheme } from './Theme';
 import ThemeFactory from './ThemeFactory';
 
-export const ThemeContext = (React.createContext || createReactContext)(ThemeFactory.getDefaultTheme()) as Context<ITheme>;
+// @ts-ignore: cast original React.Context<T> type
+// to get it compatible with the useContext hook
+export const ThemeContext = createReactContext(ThemeFactory.getDefaultTheme()) as React.Context<ITheme>;
 
 export default ThemeContext
