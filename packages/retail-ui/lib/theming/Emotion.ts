6043228604703e8715eb9b5095d9f5faf1d02f47
lossir <m.pakhomov@skbkontur.ru>
2019-10-25 12:34:14 +0500

    feat(Emotion): add 'prefixer' for classes

--- packages/retail-ui/lib/theming/Emotion.ts
+++ packages/retail-ui/lib/theming/Emotion.ts
@@ -3,6 +3,8 @@ import extraScopePlugin from 'stylis-plugin-extra-scope';
 import Upgrade from '../Upgrades';
 import LESS_VARIABLES from '../../components/variables.module.less';
 
+const PREFIX = 'react-ui';
+
 // NOTE: for ones who overrides specificityLevel at custom less
 const specificityLevel = parseInt(LESS_VARIABLES.specificityLevel, 10) || 0;
 if (specificityLevel) {
@@ -23,6 +25,13 @@ export const {
   sheet,
   cache,
 } = createEmotion({
-  key: 'react-ui',
+  key: PREFIX,
   stylisPlugins: scope ? [extraScopePlugin(scope)] : undefined,
 });
+
+export function prefixer<T = { [key: string]: string }>(classes: T): T {
+  return Object.entries(classes).reduce<T>(
+    (pc, [key, value]) => ({ ...pc, [key]: `${PREFIX}-${value}-${(~~(Math.random() * 1e8)).toString(16)}` }),
+    {} as T,
+  );
+}
