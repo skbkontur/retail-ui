58aaf7c804b44da3b63cdd30d579ca816fe4dcfc
wKich <nflazzer@gmail.com>
2019-11-01 11:29:55 +0500

    test(Sticky): remove unnecessary horizontal scroll

--- packages/react-ui-selenium/tests/Sticky.ts
+++ packages/react-ui-selenium/tests/Sticky.ts
@@ -88,15 +88,6 @@ describe('Sticky', function() {
       await this.browser.executeScript(function() {
         // tslint:disable
         var stickyContent = window.document.querySelector('[data-tid="stickyContent"]');
-        // @ts-ignore
-        var scrollOffset = stickyContent.getBoundingClientRect().height / 2;
-
-        window.scrollTo(0, scrollOffset);
-        // tslint:enable
-      });
-      await this.browser.executeScript(function() {
-        // tslint:disable
-        var stickyContent = window.document.querySelector('[data-tid="stickyContent"]');
         var nonStickyText = window.document.querySelector('[data-tid="nonStickyText"]');
         // @ts-ignore
         var scrollXOffset = nonStickyText.getBoundingClientRect().width / 2;
