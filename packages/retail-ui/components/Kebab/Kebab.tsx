8d68387e7e3ca511ac7ec84e2cd1e60fed14d0f5
wKich <nflazzer@gmail.com>
2019-12-09 13:30:41 +0500

    refactor(Icon20px): move font icons into internal/icons directory

--- packages/retail-ui/components/Kebab/Kebab.tsx
+++ packages/retail-ui/components/Kebab/Kebab.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import MenuKebabIcon from '@skbkontur/react-icons/MenuKebab';
 import { isKeyArrowVertical, isKeyEnter, isKeySpace, someKeys } from '../../lib/events/keyboard/identifiers';
-import Icon20 from '../Icon/20px';
+import Icon20 from '../internal/icons/20px';
 import LayoutEvents from '../../lib/LayoutEvents';
 import tabListener from '../../lib/events/tabListener';
 import PopupMenu from '../internal/PopupMenu';
