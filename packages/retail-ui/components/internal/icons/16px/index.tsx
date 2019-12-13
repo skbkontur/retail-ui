917e9828406eb2d7059907f587f7e28b3b0641d6
wKich <nflazzer@gmail.com>
2019-12-13 09:52:32 +0500

    refactor: fix icon styles typings

--- packages/retail-ui/components/internal/icons/16px/index.tsx
+++ packages/retail-ui/components/internal/icons/16px/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './icon.module.css';
+import styles from './icon.module.less';
 
 // NOTE Icons copy-pasted from @skbkontur/react-icons package, because it's not fully opensource
 
