aeb61c50a01f18289786c5e0425adba09526fea0
lossir <lossir1@gmail.com>
2019-12-14 19:04:49 +0500

    test(DateInput): add tests: 'blur always after change'

--- packages/retail-ui/.creevey/tests/DateInput.ts
+++ packages/retail-ui/.creevey/tests/DateInput.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { By } from 'selenium-webdriver';
+import { By, Key } from 'selenium-webdriver';
 
 async function focus($: any) {
   await $.browser.executeScript(() => {
@@ -46,6 +46,48 @@ describe('DateInput', function() {
     it('idle', async function() {
       const element = await this.browser.findElement(By.css('#test-element'));
       await expect(await element.takeScreenshot()).to.matchImage('idle');
+    });
+  });
+  describe('blur always after change', function() {
+    it('value not changed', async function() {
+      const element = await this.browser.findElement(By.css('#test-element'));
+      await focus(this);
+      await this.browser
+        .actions({
+          bridge: true,
+        })
+        .click(this.browser.findElement(By.css('body')))
+        .perform();
+      await expect(await element.takeScreenshot()).to.matchImage('value not changed');
+    });
+    it('value changed', async function() {
+      const element = await this.browser.findElement(By.css('#test-element'));
+      await focus(this);
+      await this.browser
+        .actions({
+          bridge: true,
+        })
+        .sendKeys('12')
+        .click(this.browser.findElement(By.css('body')))
+        .perform();
+      await expect(await element.takeScreenshot()).to.matchImage('value changed');
+    });
+    it('value restored', async function() {
+      this.browser.executeScript(() => {
+        const oldDate = Date;
+        // @ts-ignore
+        window.Date = function(){return new oldDate(2000,0,1)}
+      });
+      const element = await this.browser.findElement(By.css('#test-element'));
+      await focus(this);
+      await this.browser
+        .actions({
+          bridge: true,
+        })
+        .sendKeys(Key.DELETE)
+        .click(this.browser.findElement(By.css('body')))
+        .perform();
+      await expect(await element.takeScreenshot()).to.matchImage('value restored');
     });
   });
 });
