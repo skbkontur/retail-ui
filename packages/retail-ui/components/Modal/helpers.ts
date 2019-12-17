a58705cabd44a5a8abd7bdca19d0c9c89ac0f95a
Mikhail <dasdgool@gmail.com>
2019-12-17 12:16:27 +0500

    docs(Components): fix Components for right dock

--- packages/retail-ui/components/Modal/helpers.ts
+++ packages/retail-ui/components/Modal/helpers.ts
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { FooterProps } from './ModalFooter';
 import { HeaderProps } from './ModalHeader';
 
-export function isFooter(child: React.ReactChild): child is React.ReactElement<FooterProps> {
+export function isFooter(child: React.ReactNode): child is React.ReactElement<FooterProps> {
   return React.isValidElement<FooterProps>(child) && child.type.hasOwnProperty('__MODAL_FOOTER__');
 }
-export function isHeader(child: React.ReactChild): child is React.ReactElement<HeaderProps> {
+export function isHeader(child: React.ReactNode): child is React.ReactElement<HeaderProps> {
   return React.isValidElement<HeaderProps>(child) && child.type.hasOwnProperty('__MODAL_HEADER__');
 }
