5cae026a5690070bd1ba4b67ed5f03e5c29363eb
dmitry.lazarev <dmitry.lazarev@TeamCity>
2019-12-18 11:40:25 +0500

    TeamCity change in 'Frontend Infrastructure / Packages / SeleniumTesting' project: build features of 'Test' build configuration were updated

--- .teamcity/patches/buildTypes/SeleniumTesting_Test.kts
+++ .teamcity/patches/buildTypes/SeleniumTesting_Test.kts
@@ -1,7 +1,6 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.CommitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
@@ -21,7 +20,7 @@ changeBuildType(RelativeId("SeleniumTesting_Test")) {
     }
 
     features {
-        val feature1 = find<CommitStatusPublisher> {
+        remove {
             commitStatusPublisher {
                 publisher = github {
                     githubUrl = "https://api.github.com"
@@ -31,15 +30,6 @@ changeBuildType(RelativeId("SeleniumTesting_Test")) {
                 }
                 param("github_oauth_user", "wKich")
             }
-        }
-        feature1.apply {
-            publisher = github {
-                githubUrl = "https://api.github.com"
-                authType = personalToken {
-                    token = "credentialsJSON:2e60a13b-65b3-4f80-b342-2cb770ad7a7d"
-                }
-            }
-            param("github_oauth_user", "")
         }
     }
 }
