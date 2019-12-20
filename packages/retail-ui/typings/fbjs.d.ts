e30538555c650bc068cc84499ec7a65e51d5c2c1
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2019-12-20 16:14:06 +0500

    build: remove fbjs

--- packages/retail-ui/typings/fbjs.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'fbjs/lib/ExecutionEnvironment' {
-  export const canUseDOM: boolean;
-  export const canUseWorkers: boolean;
-  export const canUseEventListeners: boolean;
-  export const canUseViewport: boolean;
-  export const isInWorker: boolean;
-}
