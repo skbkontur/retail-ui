62ba452ed72b51671f7319efc1e3189edda17f24
wKich <nflazzer@gmail.com>
2020-01-22 18:05:56 +0500

    build: update babel to 7.8.3

--- packages/retail-ui/.babelrc.js
+++ packages/retail-ui/.babelrc.js
@@ -5,12 +5,12 @@ module.exports = {
     },
     development: {
       presets: [['@babel/preset-env', { loose: true, modules: 'commonjs', targets: { ie: '11' } }]],
-      plugins: [['@babel/plugin-transform-runtime', { version: '7.7.0' }]],
+      plugins: [['@babel/plugin-transform-runtime', { version: '7.8.3' }]],
     },
   },
   presets: [['@babel/preset-env', { loose: true, modules: false }], '@babel/typescript', '@babel/preset-react'],
   plugins: [
-    ['@babel/plugin-transform-runtime', { useESModules: true, version: '7.7.0' }],
+    ['@babel/plugin-transform-runtime', { useESModules: true, version: '7.8.3' }],
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
