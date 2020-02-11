ff974b46879d8f8e27a0e3679699db4863eaecc3
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-02-11 15:35:08 +0500

    refactor(TopBar): rename inner classes
    so we can dynamicaly reexport them from index #1883
    
    BREAKING CHANGE: Item/ButtonItem/Devider/Logout/Organizations/User have been renamed with 'TopBar'-prefix, e.g.: TopBarItem, etc.

--- packages/react-ui/components/TopBar/index.tsx
+++ packages/react-ui/components/TopBar/index.tsx
@@ -1,14 +1,10 @@
 export { TopBar, TopBarProps } from './TopBar';
-export { ButtonItem as TopBarButtonItem, ButtonItemProps as TopBarButtonItemProps } from './TopBarButtonItem';
-export { Divider as TopBarDivider } from './TopBarDivider';
+export { TopBarButtonItem, TopBarButtonItemProps } from './TopBarButtonItem';
+export { TopBarDivider } from './TopBarDivider';
 export { TopBarDropdown, TopBarDropdownProps } from './TopBarDropdown';
 export { TopBarEnd } from './TopBarEnd';
-export { Item as TopBarItem, ItemProps as TopBarItemProps } from './TopBarItem';
-export { Logout as TopBarLogout } from './TopBarLogout';
-export {
-  Organizations as TopBarOrganizations,
-  OrganizationsProps as TopBarOrganizationsProps,
-  OrganizationsState as TopBarOrganizationsState,
-} from './TopBarOrganizations';
+export { TopBarItem, TopBarItemProps } from './TopBarItem';
+export { TopBarLogout } from './TopBarLogout';
+export { TopBarOrganizations, TopBarOrganizationsProps, TopBarOrganizationsState } from './TopBarOrganizations';
 export { TopBarStart } from './TopBarStart';
-export { User as TopBarUser, UserProps as TopBarUserProps } from './TopBarUser';
+export { TopBarUser, TopBarUserProps } from './TopBarUser';
