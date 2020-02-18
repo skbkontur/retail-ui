e9c3d26da07db3d7a0f1dd60344f83d49f1c99e5
lossir <m.pakhomov@skbkontur.ru>
2020-02-18 11:48:52 +0500

    refactor(Paging): move remaining styles into css-in-js

--- packages/react-ui/components/Paging/Paging.styles.ts
+++ packages/react-ui/components/Paging/Paging.styles.ts
@@ -1,60 +1,126 @@
-import { css } from '../../lib/theming/Emotion';
+import { css, cssName } from '../../lib/theming/Emotion';
 import { Theme } from '../../lib/theming/Theme';
 
-import styles from './Paging.module.less';
-
 export const jsStyles = {
+  paging(t: Theme) {
+    return css`
+      user-select: none;
+      outline: 0;
+    `;
+  },
+
   dots(t: Theme) {
     return css`
       color: ${t.pagingDotsColor};
+      display: inline-block;
+      font-size: 16px;
+      padding: 6px 10px 0;
     `;
   },
 
   forwardLink(t: Theme) {
     return css`
       color: ${t.pagingForwardLinkColor};
+      cursor: pointer;
+      display: inline-block;
+      font-size: 16px;
+      margin: 4px 0 2px 10px;
+      outline: none;
+      padding-right: 22px;
+      position: relative;
+      text-decoration: none;
+      user-select: none;
+      vertical-align: top;
+    `;
+  },
+
+  forwardLinkFocused() {
+    return css`
+      text-decoration: underline;
+    `;
+  },
+
+  forwardIcon() {
+    return css`
+      vertical-align: -2px;
+      position: absolute;
+      right: 0;
     `;
   },
 
   disabled(t: Theme) {
     return css`
-      .${styles.forwardLink}& {
+      ${cssName(jsStyles.forwardLink(t))}& {
         color: ${t.pagingForwardLinkDisabledColor};
       }
     `;
   },
 
-  pageLink(t: Theme) {
+  pageLinkWrapper() {
     return css`
-      .${styles.pageLinkWrapper} & {
-        color: ${t.pagingForwardLinkColor};
-      }
-      .${styles.pageLinkWrapper} &:not(.${styles.active}):hover {
-        background: ${t.pagingPageLinkHoverBg};
-      }
+      display: inline-block;
+      font-size: 0;
+      text-align: center;
+      user-select: none;
+      vertical-align: top;
     `;
   },
 
-  active(t: Theme) {
+  pageLink(t: Theme) {
     return css`
-      .${styles.pageLinkWrapper} .${styles.pageLink}& {
+      border-radius: 16px;
+      color: ${t.pagingForwardLinkColor};
+      cursor: pointer;
+      display: block;
+      font-size: 16px;
+      margin: 2px 1px;
+      outline: none;
+      padding: 2px 10px 5px;
+      text-decoration: none;
+
+      &:not(${cssName(jsStyles.active())}):hover {
+        background: ${t.pagingPageLinkHoverBg};
+      }
+
+      ${cssName(jsStyles.active())}& {
         background: ${t.pagingPageLinkActiveBg};
         color: ${t.pagingPageLinkActiveColor};
       }
     `;
   },
 
-  focused(t: Theme) {
+  active() {
     return css`
-      .${styles.pageLinkWrapper} .${styles.pageLink}& {
-        border: solid 2px ${t.borderColorFocus};
-      }
+      cursor: default;
+    `;
+  },
+
+  pageLinkFocused(t: Theme) {
+    return css`
+      margin: 0 -1px !important;
+      border: solid 2px ${t.borderColorFocus};
+    `;
+  },
+
+  transparent() {
+    return css`
+      color: transparent;
+    `;
+  },
+
+  pageLinkHintPlaceHolder() {
+    return css`
+      height: 15px;
     `;
   },
 
   pageLinkHint(t: Theme) {
     return css`
-      .${styles.pageLinkWrapper} & {
+      display: inline-block;
+      margin: 0 -20px;
+      font-size: 11px;
+
+      ${cssName(jsStyles.pageLinkWrapper())} & {
         color: ${t.pagingPageLinkHintColor};
       }
     `;
