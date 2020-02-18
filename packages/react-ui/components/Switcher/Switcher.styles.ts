f93bf4c5674b530237c792a44ba067750626c7db
wKich <nflazzer@gmail.com>
2020-02-18 17:05:18 +0500

    perf(switcher): memoize css-in-js styles

--- packages/react-ui/components/Switcher/Switcher.styles.ts
+++ packages/react-ui/components/Switcher/Switcher.styles.ts
@@ -1,7 +1,7 @@
-import { css } from '../../lib/theming/Emotion';
+import { css, memoizeStyle } from '../../lib/theming/Emotion';
 import { Theme } from '../../lib/theming/Theme';
 
-export const jsStyles = {
+const styles = {
   wrap() {
     return css`
       display: inline-block;
@@ -33,3 +33,5 @@ export const jsStyles = {
     `;
   },
 };
+
+export const jsStyles = memoizeStyle(styles);
