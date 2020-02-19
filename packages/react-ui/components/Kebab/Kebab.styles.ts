b52952171fdda3523085c5138a361367d01debf5
wKich <nflazzer@gmail.com>
2020-02-19 15:21:15 +0500

    refactor(kebab): move remaining styles into css-in-js

--- packages/react-ui/components/Kebab/Kebab.styles.ts
+++ packages/react-ui/components/Kebab/Kebab.styles.ts
@@ -1,19 +1,87 @@
-import { css } from '../../lib/theming/Emotion';
+import { css, cssName } from '../../lib/theming/Emotion';
 import { Theme } from '../../lib/theming/Theme';
 
-import styles from './Kebab.module.less';
-
 export const jsStyles = {
+  kebab() {
+    return css`
+      display: inline-block;
+      text-align: center;
+      border: 2px solid transparent;
+      box-sizing: border-box;
+      border-radius: 50%;
+      user-select: none;
+      cursor: pointer;
+      outline: 0;
+      transition: background 0.12s ease-out;
+      width: 26px;
+      height: 26px;
+
+      &:hover {
+        background: rgba(0, 0, 0, 0.09);
+      }
+    `;
+  },
+
   focused(t: Theme) {
     return css`
-      .${styles.kebab}& {
+      ${cssName(jsStyles.kebab())}& {
+        background: rgba(0, 0, 0, 0.09);
         border-color: ${t.borderColorFocus};
+      }
+    `;
+  },
+
+  opened() {
+    return css`
+      ${cssName(jsStyles.kebab())}& {
+        background: rgba(0, 0, 0, 0.09);
+        cursor: default;
+      }
+    `;
+  },
+
+  disabled() {
+    return css`
+      ${cssName(jsStyles.kebab())}& {
+        cursor: default;
+
+        &:hover {
+          background: none;
+        }
+      }
+    `;
+  },
+
+  iconsmall() {
+    return css`
+      ${cssName(jsStyles.kebab())} & {
+        margin-top: 1px;
+        margin-left: 0.8124px;
+      }
+    `;
+  },
+
+  iconmedium() {
+    return css`
+      ${cssName(jsStyles.kebab())} & {
+        margin-top: -2px;
+        margin-left: 0.5px;
+      }
+    `;
+  },
+
+  iconlarge() {
+    return css`
+      ${cssName(jsStyles.kebab())} & {
+        margin-top: -5px;
+        margin-left: 0.5px;
       }
     `;
   },
 
   menu(t: Theme) {
     return css`
+      overflow: hidden;
       border-radius: ${t.popupBorderRadius};
     `;
   },
