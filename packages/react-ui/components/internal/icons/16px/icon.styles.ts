40fd6396c31f65623447e98bf14ffdc8afd324c1
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-02-20 16:38:43 +0500

    refactor(icon16px): move remaining styles into css-in-js

---
+++ packages/react-ui/components/internal/icons/16px/icon.styles.ts
@@ -0,0 +1,19 @@
+import { css } from '../../../../lib/theming/Emotion';
+
+export const jsStyles = {
+  root() {
+    return css`
+      display: inline-block;
+      margin-left: -0.0714285714285714em;
+      margin-right: -0.0714285714285714em;
+    `;
+  },
+
+  icon() {
+    return css`
+      height: 1.1428571428571428em;
+      width: 1.1428571428571428em;
+      margin-bottom: -0.1428571428571428em;
+    `;
+  },
+};
