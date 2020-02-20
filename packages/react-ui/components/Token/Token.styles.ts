51427a50aaf4e2fd0f4634a568d4bab76337ad5a
lossir <lossir1@gmail.com>
2020-02-20 17:31:11 +0500

    refactor(Token): move remaining styles into css-in-js

--- packages/react-ui/components/Token/Token.styles.ts
+++ packages/react-ui/components/Token/Token.styles.ts
@@ -1,33 +1,85 @@
-import { css } from '../../lib/theming/Emotion';
+import { css, cssName } from '../../lib/theming/Emotion';
 import { Theme } from '../../lib/theming/Theme';
 import * as ColorFunctions from '../../lib/styles/ColorFunctions';
 
-import styles from './Token.module.less';
-
 export const jsStyles = {
+  token() {
+    return css`
+      display: inline-flex;
+      align-items: center;
+      border-radius: 1px;
+      padding: 0 4px;
+      line-height: 1.5;
+      font-size: 14px;
+      margin: 3px;
+      min-width: 0;
+      word-break: break-word;
+      user-select: none;
+
+      &:hover {
+        cursor: pointer;
+      }
+    `;
+  },
+
   disabled(t: Theme) {
     return css`
-      color: ${t.textColorDisabled};
+      & {
+        box-shadow: none !important;
+        margin: 2px;
+        padding: 1px 4px;
+        user-select: text;
+        cursor: text;
+        color: ${t.textColorDisabled} !important;
+      }
+    `;
+  },
+
+  text() {
+    return css`
+      display: inline-block;
+      padding-bottom: 1px;
+    `;
+  },
+
+  removeIcon() {
+    return css`
+      height: 1em;
+      width: 1em;
+      flex-shrink: 0;
+      padding: 2px;
+      box-sizing: border-box;
+      margin-left: 4px;
+      transition: none;
+      fill: currentColor;
+      opacity: 0.5;
+      line-height: 0;
+
+      &:hover {
+        opacity: 1;
+      }
     `;
   },
 };
 
 interface TokenColors {
-  defaultIdle: (t: Theme) => string;
-  defaultActive: (t: Theme) => string;
+  defaultIdle: (t: Theme, v: 'error' | 'warning' | null) => string;
+  defaultActive: (t: Theme, v: 'error' | 'warning' | null) => string;
   defaultDisabled: (t: Theme) => string;
-  grayIdle: (t: Theme) => string;
-  grayActive: (t: Theme) => string;
-  blueIdle: (t: Theme) => string;
-  blueActive: (t: Theme) => string;
-  greenIdle: (t: Theme) => string;
-  greenActive: (t: Theme) => string;
-  yellowIdle: (t: Theme) => string;
-  yellowActive: (t: Theme) => string;
-  redIdle: (t: Theme) => string;
-  redActive: (t: Theme) => string;
-  white: (t: Theme) => string;
-  black: (t: Theme) => string;
+  defaultDisabledWarning: (t: Theme) => string;
+  defaultDisabledError: (t: Theme) => string;
+  grayIdle: (t: Theme, v: 'error' | 'warning' | null) => string;
+  grayActive: (t: Theme, v: 'error' | 'warning' | null) => string;
+  blueIdle: (t: Theme, v: 'error' | 'warning' | null) => string;
+  blueActive: (t: Theme, v: 'error' | 'warning' | null) => string;
+  greenIdle: (t: Theme, v: 'error' | 'warning' | null) => string;
+  greenActive: (t: Theme, v: 'error' | 'warning' | null) => string;
+  yellowIdle: (t: Theme, v: 'error' | 'warning' | null) => string;
+  yellowActive: (t: Theme, v: 'error' | 'warning' | null) => string;
+  redIdle: (t: Theme, v: 'error' | 'warning' | null) => string;
+  redActive: (t: Theme, v: 'error' | 'warning' | null) => string;
+  white: (t: Theme, v: 'error' | 'warning' | null) => string;
+  black: (t: Theme, v: 'error' | 'warning' | null) => string;
 }
 
 export const jsTokenColors = [
@@ -48,22 +100,24 @@ export const jsTokenColors = [
 ].reduce(
   (colors: TokenColors, { name, color }) => ({
     ...colors,
-    [name](t: Theme) {
+    [name](t: Theme, v: 'error' | 'warning') {
+      const warning = css`
+        box-shadow: 0 0 0 2px ${t.borderColorWarning}, inset 0 0 0 1px ${t[color]};
+      `;
+      const error = css`
+        box-shadow: 0 0 0 2px ${t.borderColorError}, inset 0 0 0 1px ${t[color]};
+      `;
+      const vStyle = v === 'error' ? error : v === 'warning' ? warning : '';
+
       return css`
         background-color: ${t[color]};
         color: ${ColorFunctions.contrast(t[color])};
         box-shadow: 0 0 0 1px ${ColorFunctions.darken(t[color], '5%')}, inset 0 0 0 1px ${t[color]};
 
-        & .${styles.removeIcon}:hover {
+        ${vStyle}
+
+        & ${cssName(jsStyles.removeIcon())}:hover {
           color: ${ColorFunctions.contrast(t[color])};
-        }
-
-        &.${styles.warning} {
-          box-shadow: 0 0 0 2px ${t.borderColorWarning}, inset 0 0 0 1px ${t[color]};
-        }
-
-        &.${styles.error} {
-          box-shadow: 0 0 0 2px ${t.borderColorError}, inset 0 0 0 1px ${t[color]};
         }
       `;
     },
@@ -75,18 +129,20 @@ export const jsTokenColors = [
         color: ${ColorFunctions.contrast(t.tokenDisabledBg)};
         box-shadow: 0 0 0 1px ${t.tokenDisabledBg};
 
-        & .${styles.removeIcon} {
+        & ${cssName(jsStyles.removeIcon())} {
           fill: ${t.textColorDisabled};
           opacity: 1;
         }
-
-        &.${styles.warning} {
-          box-shadow: 0 0 0 2px ${t.borderColorWarning}, inset 0 0 0 1px ${t.tokenDisabledBg};
-        }
-
-        &.${styles.error} {
-          box-shadow: 0 0 0 2px ${t.borderColorError}, inset 0 0 0 1px ${t.tokenDisabledBg};
-        }
+      `;
+    },
+    defaultDisabledWarning(t: Theme) {
+      return css`
+        box-shadow: 0 0 0 2px ${t.borderColorWarning}, inset 0 0 0 1px ${t.tokenDisabledBg};
+      `;
+    },
+    defaultDisabledError(t: Theme) {
+      return css`
+        box-shadow: 0 0 0 2px ${t.borderColorError}, inset 0 0 0 1px ${t.tokenDisabledBg};
       `;
     },
   } as TokenColors,
