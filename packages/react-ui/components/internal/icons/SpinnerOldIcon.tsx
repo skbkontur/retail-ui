3fdc846a7cc3f17eb7baa4ab2d3dbde92136d401
lossir <m.pakhomov@skbkontur.ru>
2020-03-02 17:00:44 +0500

    refactor(SpinnerOldIcon): replace `cx` with `cn`

--- packages/react-ui/components/internal/icons/SpinnerOldIcon.tsx
+++ packages/react-ui/components/internal/icons/SpinnerOldIcon.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import { cx } from '../../../lib/theming/Emotion';
+import cn from 'classnames';
 
 import { jsStyles } from './SpinnerIcon.styles';
 
@@ -29,7 +28,7 @@ export const SpinnerOldIcon = ({ size, strokeClassName, className }: SpinnerIcon
     return (
       <span className={jsStyles.root()}>
         <svg
-          className={cx(jsStyles.icon(), className)}
+          className={cn(jsStyles.icon(), className)}
           width="16"
           height="16"
           fill="none"
