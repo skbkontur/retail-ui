b1229b7915aaefa8f2827ea7e1de393b54f490b7
Vladimir Sorokin <v_sorokin@skbkontur.ru>
2020-03-05 13:24:55 +0500

    refactor(ModalClose): replace class-component with functional

--- packages/react-ui/components/Modal/ModalClose.tsx
+++ packages/react-ui/components/Modal/ModalClose.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { cx } from '../../lib/theming/Emotion';
 import { ThemeContext } from '../../lib/theming/ThemeContext';
@@ -7,30 +7,16 @@ import { CloseProps } from './ModalContext';
 import styles from './Modal.module.less';
 import { jsStyles } from './Modal.styles';
 
+export function ModalClose({ disableClose, requestClose }: CloseProps) {
+  const theme = useContext(ThemeContext);
 
-export class ModalClose extends React.Component<CloseProps> {
-  private theme!: Theme;
-
-  public render() {
-    return (
-      <ThemeConsumer>
-        {theme => {
-          this.theme = theme;
-          return this.renderMain();
-        }}
-      </ThemeConsumer>
-    );
-  }
-
-  private renderMain() {
-    return (
-      <button
-        className={cx(styles.close, jsStyles.close(this.theme), this.props.disableClose && styles.disabled)}
-        onClick={this.props.requestClose}
-        data-tid="modal-close"
-      >
-        <span className={styles.closeOutline} />
-      </button>
-    );
-  }
+  return (
+    <button
+      className={cx(styles.close, jsStyles.close(theme), disableClose && styles.disabled)}
+      onClick={requestClose}
+      data-tid="modal-close"
+    >
+      <span className={styles.closeOutline} />
+    </button>
+  );
 }
