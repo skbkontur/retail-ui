c0ecd1d4f8b869b5a1c7b63b496dcec3a71c7bc4
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-03-06 11:55:03 +0500

    test(codemode): add tests for "transformOnChange"

--- packages/react-ui-codemodes/react-ui-2.0/__tests__/transformOnChange.test.ts
+++ packages/react-ui-codemodes/react-ui-2.0/__tests__/transformOnChange.test.ts
@@ -9,13 +9,91 @@ defineInlineTest(
   {},
   `
     <>
-      <TokenInput onChange={(v) => this.handleChange(v)} />
+      <Toggle onChange={v => this.handleChange(v)} />
+      <DateSelect onChange={v => this.handleChange(v)} />
+      <TokenInput onChange={v => this.handleChange(v)} />
+
+      <ComboBox onInputChange={v => this.handleChange(v)} />
+      <CustomComboBox onInputChange={v => this.handleChange(v)} />
+      <FiasSearch onInputChange={v => this.handleChange(v)} />
     </>
 `,
   `
     <>
-      <TokenInput onValueChange={(v) => this.handleChange(v)} />
+      <Toggle onValueChange={v => this.handleChange(v)} />
+      <DateSelect onValueChange={v => this.handleChange(v)} />
+      <TokenInput onValueChange={v => this.handleChange(v)} />
+
+      <ComboBox onInputValueChange={v => this.handleChange(v)} />
+      <CustomComboBox onInputValueChange={v => this.handleChange(v)} />
+      <FiasSearch onInputValueChange={v => this.handleChange(v)} />
     </>
   `,
-  `transforms "onChange" without "event" argument`,
+  `transforms "onChange/onInputChange" with only "value" argument`,
+);
+
+// defineInlineTest(
+//   transform,
+//   {},
+//   `
+//     <>
+//       <Toggle changeEventHandler={e => this.handleChange(e)} />
+//     </>
+// `,
+//   `
+//     <>
+//       <Toggle onChange={e => this.handleChange(e)} />
+//     </>
+//   `,
+//   `transforms custom "onChange" without one "event" arg to native onChange`,
+// );
+
+defineInlineTest(
+  transform,
+  {},
+  `
+    <>
+      <TokenInput onChange={this.handleChange} />
+      <Input onChange={handle} />
+    </>
+`,
+  `
+    <>
+      <TokenInput onValueChange={this.handleChange} />
+      <Input onChange={handle} />
+    </>
+  `,
+  `doesn't transform handlers specified as references, except cases with just one arg`,
+);
+
+defineInlineTest(
+  transform,
+  {},
+  `
+    <>
+      <Input onChange={(e, v) => handle(v)} />
+    </>
+`,
+  `
+    <>
+      <Input onValueChange={v => handle(v)} />
+    </>
+  `,
+  `removes "event" arg if it isn't being used and transforms onChange->onValueChange`,
+);
+
+defineInlineTest(
+  transform,
+  {},
+  `
+    <>
+      <Input onChange={(e, v) => handle(e, v)} />
+    </>
+`,
+  `
+    <>
+      <Input onChange={(e, v) => handle(e, v)} />
+    </>
+  `,
+  `doesn't transform onChange if "event" arg is being used`,
 );
