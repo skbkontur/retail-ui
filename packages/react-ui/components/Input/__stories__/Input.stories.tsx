1e3b66867aa53a7c7af6d534e8bfc3304b3f1beb
Artyom Desyatnikov <10nikov@kontur.ru>
2020-03-14 17:33:40 +0500

    test: placeholder input stories

--- packages/react-ui/components/Input/__stories__/Input.stories.tsx
+++ packages/react-ui/components/Input/__stories__/Input.stories.tsx
@@ -476,6 +476,38 @@ InputWithPhoneMask.story = {
   },
 };
 
+export const InputWithPlaceholder: CSFStory<JSX.Element> = () => <Input placeholder="Hold the place!" />;
+InputWithPlaceholder.story = {
+  name: 'Input with placeholder',
+  parameters: {
+    creevey: {
+      tests: {
+        async Plain() {
+          await this.expect(await this.takeScreenshot()).to.matchImage('Plain');
+        },
+        async Focused() {
+          await this.browser
+            .actions({
+              bridge: true,
+            })
+            .click(this.browser.findElement({ css: 'label' }))
+            .perform();
+          await this.expect(await this.takeScreenshot()).to.matchImage('Focused');
+        },
+        async ["With typed text"]() {
+          await this.browser
+            .actions({
+              bridge: true,
+            })
+            .click(this.browser.findElement({ css: 'input' }))
+            .sendKeys('Test...')
+            .perform();
+          await this.expect(await this.takeScreenshot()).to.matchImage('With typed text');
+        },
+      }
+    }
+  }
+};
 export const SelectAllByProp: CSFStory<JSX.Element> = () => <Input defaultValue="Some value" selectAllOnFocus />;
 SelectAllByProp.story = {
   name: 'Select all by prop',
