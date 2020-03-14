8c0aef6de95cede5c7708fa7418710bea844f98e
Nadya <n.ageeva@skbkontur.ru>
2020-03-14 15:45:25 +0500

    test: bug tooltip doesnt close when click on button inside

--- packages/react-ui/components/example.stories.tsx
+++ packages/react-ui/components/example.stories.tsx
@@ -5,6 +5,9 @@ import { Nullable } from '../typings/utility-types'
 
 import { Button } from './Button';
 import { Toast } from './Toast';
+import { Tooltip } from './Tooltip';
+import { Indeterminate } from './Checkbox/__stories__/Checkbox.stories';
+import { Input } from './Input';
 
 
 export default {
@@ -193,3 +196,64 @@ ToastDissapearWhenNext.story = {
     },
   },
 };
+
+
+// 0. Имеется поле ввода и тултип с кнопкой, по клику на которую поле ввода фокусится
+// 1. Триггерим тултип по клику, сфотографируем
+// 2. Кликаем по кнопке в тултипе
+// 3. Тултип должен закрыться, сфотографируем
+
+export const ToolptipWithLinkInside = () => {
+  let input: Nullable<Input>;
+
+  const renderTooltip = () => {
+    return <div>
+      Text and <Button onClick={handleButtonClick} data-tid={`buttonToClick`} use="link">Button</Button>
+    </div>
+  };
+
+  const handleButtonClick = () => {
+    if (input) {
+      input.focus();
+    }
+  };
+
+  return <div>
+    <Tooltip trigger="click" render={renderTooltip}>
+      <Input data-tid={`inputWithTooltip`}/>
+    </Tooltip>
+    <div><Input ref={el => (input = el)} data-tid={`inputToFocus`}/></div>
+  </div>
+};
+
+ToolptipWithLinkInside.story = {
+  parameters: {
+    creevey: {
+      tests: {
+        async itemSelected(this: {browser: WebDriver}) {
+          const element = await this.browser.findElement({css: `#test-element`});
+          const inputField1 = await this.browser.findElement( {css: `[data-tid~=inputWithTooltip]`});
+
+          await this.browser
+            .actions({bridge: true})
+            .click(inputField1)
+            .perform();
+
+          const tooltip = await element.takeScreenshot();
+
+          const button = await this.browser.findElement({css: `[data-tid~=buttonToClick]`});
+
+          await this.browser
+            .actions({bridge: true})
+            .click(button)
+            .sendKeys(`this is very long text to enter into inputField`)
+            .perform();
+
+          const focusedInputWithText = await element.takeScreenshot();
+
+          await expect({ tooltip, focusedInputWithText }).to.matchImages();
+        }
+      }
+    }
+  }
+}
