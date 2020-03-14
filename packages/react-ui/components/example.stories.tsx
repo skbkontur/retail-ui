d7fa3963cfa1e2966b43b3ae80f842196586ea13
budnikov_km <budnikov_km@skbkontur.ru>
2020-03-14 16:42:05 +0400

    Added tests for Autocomplete component

--- packages/react-ui/components/example.stories.tsx
+++ packages/react-ui/components/example.stories.tsx
@@ -1,5 +1,5 @@
 export default {
-  title: '😌 TestRetreat ',
+  title: '😌 TestRetreat',
 };
 
 export const ButtonWithIcon = () => {
@@ -58,6 +58,10 @@ export const BasicAutocomplete = () => {
   );
 };
 
+BasicAutocomplete.story = {
+  parameters: {
+    creevey: {
+      tests: {
 /**
  *  Autocomplete. Выбор значения по Enter
  *
@@ -74,11 +78,6 @@ export const BasicAutocomplete = () => {
  *
  *  Profit!
  */
-
-BasicAutocomplete.story = {
-  parameters: {
-    creevey: {
-      tests: {
         async itemSelected(this: { browser: WebDriver }) {
           const element = await this.browser.findElement({ css: '#test-element' });
           const input = await this.browser.findElement({ css: '[data-comp-name~=Autocomplete]' });
@@ -113,7 +112,275 @@ BasicAutocomplete.story = {
 
           await expect({ focused, typed, highlighted, selected }).to.matchImages();
         },
+        
+/**
+ *  Autocomplete. Независимость от регистра
+ *
+ *  0. История BasicAutocomplete
+ *  1. Найти элемент на странице
+ *  2. Фокус на поле ввода
+ *  3. Ввести "ONE"
+ *  4. 📸 состояние “введенное слово"
+ *
+ *  Profit!
+ */
+        async shouldSuggestCaseIndependent(this: { browser: WebDriver }) {
+          const element = await this.browser.findElement({ css: '#test-element' });
+          const input = await this.browser.findElement({ css: '[data-comp-name~=Autocomplete]' });
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(input)
+            .perform();
+
+          await this.browser
+            .actions({ bridge: true })
+            .sendKeys('ONE')
+            .perform();
+
+          const upperCaseTyped = await element.takeScreenshot();
+
+          await expect({ typed: upperCaseTyped }).to.matchImages();
+        },
+/**
+ *  Autocomplete. Обрезает пробелы
+ *
+ *  0. История BasicAutocomplete
+ *  1. Найти элемент на странице
+ *  2. Фокус на поле ввода
+ *  3. Ввести "   two   "
+ *  4. 📸 состояние “введенное слово"
+ *
+ *  Profit!
+ */
+        async shouldIgnoreSpacesAfterOrBefore(this: { browser: WebDriver }) {
+          const element = await this.browser.findElement({ css: '#test-element' });
+          const input = await this.browser.findElement({ css: '[data-comp-name~=Autocomplete]' });
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(input)
+            .perform();
+
+          await this.browser
+            .actions({ bridge: true })
+            .sendKeys('   two   ')
+            .perform();
+
+          const typedWithSpaces = await element.takeScreenshot();
+
+          await expect({ typed: typedWithSpaces }).to.matchImages();
+        }
       },
+    },
+  },
+};
+
+export const AutocompleteWithScroll = () => {
+  const [value, updateValue] = React.useState('');
+  const items = [];
+  for (let i = 0; i < 20; i++) {
+    items.push(`Abba ${i}`);
+  }
+  return (
+    <div style={{ padding: '4px 200px 200px 4px' }}>
+      <Autocomplete source={items} value={value} onValueChange={updateValue} />
+    </div>
+  );
+};
+
+AutocompleteWithScroll.story = {
+  parameters: {
+    creevey: {
+      tests: {
+/**
+ *  Autocomplete. При большом количестве вариантов виден скроллбар
+ *
+ *  0. История AutocompleteWithScroll
+ *  1. Найти элемент на странице
+ *  2. Фокус на поле ввода
+ *  3. Ввести "Abba"
+ *  4. 📸 состояние дропдауна со скроллбаром
+ *
+ *  Profit!
+ */
+        async scrollBarShouldBeVisible(this: { browser: WebDriver }) {
+          const element = await this.browser.findElement({ css: '#test-element' });
+          const autocomplete = await this.browser.findElement({ css: '[data-comp-name~=Autocomplete]' });
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(autocomplete)
+            .perform();
+
+          await this.browser
+            .actions({ bridge: true})
+            .sendKeys('Abba')
+            .perform();
+
+          const autocompleteWithScroll = await element.takeScreenshot();
+
+          await expect({ autocompleteWithScroll }).to.matchImages();
+        },
+/**
+ *  Autocomplete. Скроллбар расширяется при наведении
+ *
+ *  0. История AutocompleteWithScroll
+ *  1. Найти элемент на странице
+ *  2. Фокус на поле ввода
+ *  3. Ввести "Abba"
+ *  4. Находим скроллбар
+ *  5. Наводим на него мышь
+ *  6. 📸 состояние скроллбара в дропдауне
+ *
+ *  Profit!
+ */
+        async scrollBarShouldEnlargeOnHover(this: { browser: WebDriver }) {
+          const element = await this.browser.findElement({ css: '#test-element' });
+          const autocomplete = await this.browser.findElement({ css: '[data-comp-name~=Autocomplete]' });
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(autocomplete)
+            .perform();
+
+          await this.browser
+            .actions({ bridge: true})
+            .sendKeys('Abba')
+            .perform();
+
+          const scrollBar = await this.browser.findElement({ className: 'react-ui-ejkfzu'});
+          await this.browser
+            .actions({ bridge: true })
+            .move({origin: scrollBar})
+            .perform(); //Hover не работает на IE11
+          const scrollBarOnHover = await element.takeScreenshot();
+
+          await expect({ scrollBarOnHover }).to.matchImages();
+        },
+      },
+    },
+  },
+};
+
+export const AutocompleteWithSelectAllOnFocus = () => {
+  const [value, updateValue] = React.useState('');
+  return (
+    <div style={{ padding: '4px 200px 200px 4px' }}>
+      <Autocomplete source={['one', 'two', 'three']} value={value} selectAllOnFocus={true} onValueChange={updateValue} />
+    </div>
+  );
+};
+
+/**
+ *  Autocomplete. При введеном тексте фокус на элементе влечет выделение этого текста 
+ *
+ *  0. История AutocompleteWithSelectAllOnFocus
+ *  1. Найти элемент на странице
+ *  2. Фокус на поле ввода
+ *  3. Ввести "shouldSelectThisTextOnFocus"
+ *  4. Нажать TAB для потери фокуса (не проходит на Firefox)
+ *  5. Кликаем вновь по полю ввода
+ *  6. 📸 состояние поля ввода
+ *
+ *  Profit!
+ */
+
+AutocompleteWithSelectAllOnFocus.story = {
+  parameters: {
+    creevey: {
+      tests: {
+        async shouldSelectTextOnFocus(this: { browser: WebDriver }) {
+          const element = await this.browser.findElement({ css: '#test-element' });
+          const autocomplete = await this.browser.findElement({ css: '[data-comp-name~=Autocomplete]' });
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(autocomplete)
+            .perform();
+
+          await this.browser
+            .actions({ bridge: true})
+            .sendKeys('shouldSelectThisTextOnFocus')
+            .sendKeys(Key.TAB) //TODO: Не работает в Firefox, нужно либо по другому терять фокус, либо разобраться в чем проблема Firefox
+            .perform();
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(autocomplete)
+            .perform();
+
+          const selectedOnFocus = await element.takeScreenshot();
+
+          await expect({ selectedOnFocus }).to.matchImages();
+        },
+      },
+    },
+  },
+};
+
+/**
+ *  Autocomplete. Текст вводится с выравниванием по правому краю / по центру 
+ *
+ *  0. История AutocompleteWithRightTextAlignment
+ *  1. Найти элемент на странице
+ *  2. Фокус на поле ввода
+ *  3. Ввести "text"
+ *  4. 📸 состояние поля ввода
+ *
+ *  Profit!
+ */
+
+export const AutocompleteWithRightTextAlignment = () => {
+  const [value, updateValue] = React.useState('');
+  return (
+    <div style={{ padding: '4px 200px 200px 4px' }}>
+      <Autocomplete align={"right"}  source={['one', 'two', 'three']} value={value} onValueChange={updateValue} />
+    </div>
+  );
+};
+
+const textAlignmentTest = {
+  async textShouldBeAligned(this: { browser: WebDriver }) {
+    const element = await this.browser.findElement({ css: '#test-element' });
+    const input = await this.browser.findElement({ css: '[data-comp-name~=Autocomplete]' });
+
+    await this.browser
+      .actions({ bridge: true })
+      .click(input)
+      .perform();
+
+    await this.browser
+      .actions({ bridge: true })
+      .sendKeys('text')
+      .perform();
+
+    const typed = await element.takeScreenshot();
+    await expect({typed}).to.matchImages();
+  },
+};
+
+AutocompleteWithRightTextAlignment.story = {
+  parameters: {
+    creevey: {
+      tests: textAlignmentTest 
+    },
+  },
+};
+
+export const AutocompleteWithCenterTextAlignment = () => {
+  const [value, updateValue] = React.useState('');
+  return (
+    <div style={{ padding: '4px 200px 200px 4px' }}>
+      <Autocomplete align={"center"}  source={['one', 'two', 'three']} value={value} onValueChange={updateValue} />
+    </div>
+  );
+};
+
+AutocompleteWithCenterTextAlignment.story = {
+  parameters: {
+    creevey: {
+      tests: textAlignmentTest 
     },
   },
 };
