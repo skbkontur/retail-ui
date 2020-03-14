1b024dc7d39670c5b1d1d9cdcb919870d035abd3
ghspbravo <gmspbravo@gmail.com>
2020-03-14 15:03:59 +0500

    chore(simpletabs): add story

--- packages/react-ui/components/example.stories.tsx
+++ packages/react-ui/components/example.stories.tsx
@@ -184,3 +184,112 @@ BasicToggle.story = {
     },
   },
 };
+
+export const SimpleTabs = () => {
+  const initialState = 'first';
+  const [current, currentSet] = React.useState(initialState);
+  const changeHandler = (value: string) => currentSet(value);
+
+  return (
+    <Tabs value={current} onValueChange={changeHandler}>
+      <Tabs.Tab id="first">first</Tabs.Tab>
+      <Tabs.Tab disabled id="second">
+        second (disabled)
+      </Tabs.Tab>
+      <Tabs.Tab id="third">third</Tabs.Tab>
+    </Tabs>
+  );
+};
+
+/**
+ * Tabs. Выбор нужного таба
+ *
+ *  0. История BasicToggle
+ *  1. Найти элемент на странице
+ *  2. 📸 дефолтное состояние
+ *  3. Навести мышь на таб
+ *  4. 📸 состояние “hover”
+ *  5. Фокус на табе
+ *  6. 📸 состояние "в фокусе"
+ *  7. Кликнуть на таб
+ *  8. 📸 состояние "включен"
+ *
+ *  Profit!
+ */
+
+SimpleTabs.story = {
+  parameters: {
+    creevey: {
+      tests: {
+        async selectTab(this: { browser: WebDriver }) {
+          const element = await this.browser.findElement({ css: '#test-element' });
+          const thirdTab = await this.browser.findElement({ css: 'a[data-prop-id~=third]' });
+
+          const defaultToggle = await element.takeScreenshot();
+
+          await this.browser
+            .actions({ bridge: true })
+            .move({ origin: thirdTab })
+            .perform();
+
+          const hoverTab = await element.takeScreenshot();
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(thirdTab)
+            .perform();
+
+          const activeTabChanged = await element.takeScreenshot();
+
+          await expect({ defaultToggle, hoverTab, activeTabChanged }).to.matchImages();
+        },
+
+        async selectDisabledTab(this: { browser: WebDriver }) {
+          const element = await this.browser.findElement({ css: '#test-element' });
+          const secondTab = await this.browser.findElement({ css: 'a[data-prop-id~=second]' });
+
+          const defaultToggle = await element.takeScreenshot();
+
+          await this.browser
+            .actions({ bridge: true })
+            .move({ origin: secondTab })
+            .perform();
+
+          const hoverTab = await element.takeScreenshot();
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(secondTab)
+            .perform();
+
+          const activeTabChanged = await element.takeScreenshot();
+
+          await expect({ defaultToggle, hoverTab, activeTabChanged }).to.matchImages();
+        },
+
+        async selectActiveTab(this: { browser: WebDriver }) {
+          const element = await this.browser.findElement({ css: '#test-element' });
+          const firstTab = await this.browser.findElement({ css: 'a[data-prop-id~=first]' });
+
+          const defaultToggle = await element.takeScreenshot();
+
+          await this.browser
+            .actions({ bridge: true })
+            .move({ origin: firstTab })
+            .perform();
+
+          const hoverTab = await element.takeScreenshot();
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(firstTab)
+            .perform();
+
+          const activeTabChanged = await element.takeScreenshot();
+
+          await expect({ defaultToggle, hoverTab, activeTabChanged }).to.matchImages();
+        },
+      },
+    },
+  },
+};
