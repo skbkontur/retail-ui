4bf6564424792bb1c97861b6c3d7851dbd98f46a
Govoruhin Dmitry <govorukhin@skbkontur.ru>
2020-03-14 14:34:47 +0500

    test(Hint): add hint text test

--- packages/react-ui/components/example.stories.tsx
+++ packages/react-ui/components/example.stories.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react';
+import { Hint } from './Hint';
 
 import { Checkbox } from './Checkbox';
 
@@ -195,7 +196,6 @@ CheckBoxDefault.story = {
             .click(button)
             .perform();
           const setInitialIndeterminate = await element.takeScreenshot();
-
           await this.browser
             .actions({ bridge: true })
             .click(button)
@@ -287,6 +287,83 @@ InputWithError.story = {
           const disabled = await element.takeScreenshot();
 
           await expect({ focused, typed, withError, disabled }).to.matchImages();
+        },
+      },
+    },
+  },
+};
+
+const changingTextTimeout = 0;
+
+export const HintTest = () => {
+  let timeout: number;
+  const [value, updateValue] = React.useState('short');
+  const [trigger, updateTrigger] = React.useState(1);
+  const [isOpened, updateIsOpened] = React.useState(false);
+
+  const onClick = () => {
+    updateIsOpened(!isOpened);
+    updateValue('short');
+
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      updateTrigger(trigger + 1);
+      updateValue('new long value after programmatically changing');
+    }, changingTextTimeout);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          padding: '5px 5px 5px 5px',
+          position: 'absolute',
+          border: '1px solid black',
+          right: '50%',
+          top: '100px',
+        }}
+      >
+        <Hint text={value} pos="top" disableAnimations manual opened>
+          hint here
+        </Hint>
+      </div>
+      <div
+        id="hint-wrapper"
+        style={{
+          padding: '80px 5px 5px 160px',
+          position: 'absolute',
+          border: '1px solid black',
+          right: '0',
+          top: '100px',
+        }}
+      >
+        <Hint text={value} pos="top" disableAnimations manual opened={isOpened}>
+          <div id="hint-trigger" onClick={onClick} key={trigger} style={{ border: '1px solid black' }}>
+            hint here
+          </div>
+        </Hint>
+      </div>
+    </>
+  );
+};
+
+HintTest.story = {
+  parameters: {
+    creevey: {
+      tests: {
+        async hintNearWindowBorderAfterTextChanging(this: { browser: WebDriver }) {
+          const hintTrigger = await this.browser.findElement({ css: '#hint-trigger' });
+          const hintWrapper = await this.browser.findElement({ css: '#hint-wrapper' });
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(hintTrigger)
+            .perform();
+          await new Promise(r => setTimeout(r, changingTextTimeout));
+
+          const hintedElement = await hintWrapper.takeScreenshot();
+
+          await expect({ hintedElement }).to.matchImages();
         },
       },
     },
