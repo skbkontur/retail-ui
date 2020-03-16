8aaefaaad047183e92f855d8182c11d534846254
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-03-16 11:46:06 +0500

    Revert "build(react-ui-codemodes): add bin script"
    This reverts commit c278af4e74ebef2630cb27fc92e8ba6095cf8062.

--- packages/react-ui-codemodes/bin.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-const spawn = require('child_process').spawn;
-const path = require('path');
-
-const transform_path = path.join(__dirname, process.argv[2]);
-
-spawn('yarn', ['jscodeshift', ...process.argv.slice(3), `--transform=${transform_path}`], { stdio: 'inherit' });
