34ab221a9c92096fd4cc2bb8b2b8e4cac471d4b3
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-03-17 15:39:41 +0500

    refactor(react-ui-codemodes): run jscodeshift directly
    fix "jscodeshift not found" while running via npx without having local jscodeshift

--- packages/react-ui-codemod/bin.js
+++ packages/react-ui-codemod/bin.js
@@ -2,10 +2,11 @@
 
 const spawn = require('child_process').spawn;
 const path = require('path');
+const jscodeshift = require.resolve('.bin/jscodeshift');
 
-const transform_path = path.join(__dirname, process.argv[2]);
+const transform = path.join(__dirname, process.argv[2]);
 
-spawn('yarn', ['jscodeshift', ...process.argv.slice(3), `--transform=${transform_path}`], {
+spawn(jscodeshift, [...process.argv.slice(3), `--transform=${transform}`], {
   stdio: 'inherit',
   shell: true,
 });
