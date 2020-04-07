ce13722f3ccede2e949acb130ceacad4b6536edd
Ivan Shestakov <ivan.shostik@gmail.com>
2020-04-07 06:58:33 +0300

    feat(TokenInput): new public api method 'reset'

--- packages/retail-ui/components/TokenInput/TokenInput.tsx
+++ packages/retail-ui/components/TokenInput/TokenInput.tsx
@@ -85,6 +85,16 @@ export interface TokenInputState<T> {
   loading?: boolean;
 }
 
+export const DefaultState = {
+  inputValue: '',
+  autocompleteItems: undefined,
+  activeTokens: [],
+  inFocus: false,
+  loading: false,
+  preventBlur: false,
+  inputValueWidth: 20,
+};
+
 const defaultToKey = <T extends any>(item: T): string => item.toString();
 const identity = <T extends any>(item: T): T => item;
 const defaultRenderToken = <T extends any>(
@@ -115,11 +125,7 @@ export default class TokenInput<T = string> extends React.PureComponent<TokenInp
     onMouseLeave: emptyHandler,
   };
 
-  public state: TokenInputState<T> = {
-    inputValue: '',
-    inputValueWidth: 20,
-    activeTokens: [],
-  };
+  public state: TokenInputState<T> = DefaultState;
 
   private readonly locale!: TokenInputLocale;
   private input: HTMLInputElement | null = null;
@@ -242,6 +248,14 @@ export default class TokenInput<T = string> extends React.PureComponent<TokenInp
         </label>
       </div>
     );
+  }
+
+  /**
+   * Сбрасывает введенное пользователем значение
+   * @public
+   */
+  public reset() {
+    this.dispatch({ type: 'RESET' });
   }
 
   private hasValueInItems = (items: T[], value: T) => {
