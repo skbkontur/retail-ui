732c34eaa7a5d8be74eb98be7b0d62120e4c913a
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-05-12 23:20:39 +0500

    test(Link): test "rel" attribute

--- packages/react-ui/components/Link/__tests__/Link-test.tsx
+++ packages/react-ui/components/Link/__tests__/Link-test.tsx
@@ -2,7 +2,6 @@ import { mount } from 'enzyme';
 import React from 'react';
 
 import { Link } from '../Link';
-
 describe('', () => {
   it('calls `onClick`', () => {
     const onClick = jest.fn();
@@ -20,5 +19,40 @@ describe('', () => {
     wrapper.find('a').simulate('click');
 
     expect(onClick.mock.calls.length).toBe(0);
+  });
+
+  describe('"rel" attribute', () => {
+    it("doesn't change if defined in props", () => {
+      const wrapper = mount(<Link href="https://example.com" rel="nofollow" />);
+      expect(wrapper.find('a').prop('rel')).toBe('nofollow');
+    });
+
+    it("doesn't get filled if there is no href", () => {
+      const wrapper = mount(<Link />);
+      expect(wrapper.find('a').prop('rel')).toBe(undefined);
+    });
+
+    describe('external hrefs', () => {
+      it.each([['https://example.com:8080/home'], ['http://example.com'], ['//example.com']])('%s', href => {
+        const wrapper = mount(<Link href={href} />);
+        expect(wrapper.find('a').prop('rel')).toBe('noopener noreferrer');
+      });
+    });
+
+    describe('internal hrefs', () => {
+      it.each([
+        [`https://${location.host}/home`],
+        [`http://${location.host}`],
+        [`//${location.host}`],
+        ['/home'],
+        ['/home?redirect=http://example.com'],
+        ['../home'],
+        ['page.html'],
+        ['#anchor'],
+      ])('%s', href => {
+        const wrapper = mount(<Link href={href} />);
+        expect(wrapper.find('a').prop('rel')).toBe('noopener');
+      });
+    });
   });
 });
