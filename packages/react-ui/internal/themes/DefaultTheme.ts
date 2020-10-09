skbkontur/retail-ui

commit 1c5d078db9f7c6c0b775091cdb58f72fb0017b14

chore(textarea): add height, width, borderwidth,borderwidthfocus

packages/react-ui/internal/themes/DefaultTheme.ts
@@ -44,6 +44,7 @@ export class DefaultTheme {
   public static placeholderColorLight = '#cdcdcd';
   public static blinkColor = 'rgba(0, 136, 255, 0.2)';
   public static controlBorderWidth = '1px';
+  public static controlBorderWidthFocus = '2px';
   public static controlBorderRadiusSmall = '1px';
   public static controlBorderRadiusMedium = '1px';
   public static controlBorderRadiusLarge = '1px';
@@ -962,9 +963,19 @@ export class DefaultTheme {
         return `${parseInt(fontSize, 10) * 1.5}px`;
     }
   }
-  public static get textareaHeightSmall() {
-    return this.controlHeightSmall;
+  public static get textareaBorderWidth() {
+    return this.controlBorderWidth;
   }
+  public static get textareaBorderWidthFocus() {
+    return this.controlBorderWidth;
+  }
+  public static get textareaHeight() {
+    const textareaLineHeight = parseInt(this.textareaLineHeight, 10) * 3;
+    const textareaPaddingY = parseInt(this.textareaPaddingY, 10) * 2;
+    const textareaBorderWidth = parseInt(this.textareaBorderWidth, 10) * 2;
+    return '68px' || `${textareaLineHeight + textareaPaddingY + textareaBorderWidth}px`;
+  }
+  public static textareaWidth = '250px';
   public static get textareaPaddingX() {
     return '10px';
   }

packages/react-ui/components/Textarea/Textarea.styles.ts
@@ -16,7 +16,7 @@ const styles = {
       -webkit-appearance: none;
       background-clip: padding-box;
       background: ${t.textareaBg};
-      border: 1px solid ${t.textareaBorderColor};
+      border: ${t.textareaBorderWidth} solid ${t.textareaBorderColor};
       border-top-color: ${t.textareaBorderTopColor};
       box-shadow: ${t.textareaShadow};
       box-sizing: border-box;
@@ -25,7 +25,8 @@ const styles = {
       font-size: inherit;
       line-height: inherit;
       max-width: 100%;
-      min-height: ${t.textareaHeightSmall};
+      min-width: ${t.textareaWidth};
+      min-height: ${t.textareaHeight};
       outline: none;
       padding: ${t.textareaPaddingY} ${t.textareaPaddingX};
       transition: height 0.2s ease-out;
@@ -34,7 +35,7 @@ const styles = {
 
       &:focus {
         border-color: ${t.textareaBorderColorFocus};
-        box-shadow: 0 0 0 1px ${t.textareaBorderColorFocus};
+        box-shadow: 0 0 0 ${t.textareaBorderWidthFocus} ${t.textareaBorderColorFocus};
         position: relative;
         z-index: 2;
 
@@ -67,11 +68,11 @@ const styles = {
     return css`
       ${cssName(styles.textarea(t))}& {
         border-color: ${t.textareaBorderColorError};
-        box-shadow: 0 0 0 1px ${t.textareaBorderColorError};
+        box-shadow: 0 0 0 ${t.textareaBorderWidth} ${t.textareaBorderColorError};
       }
       ${cssName(styles.textarea(t))}&:focus {
         border-color: ${t.textareaBorderColorError};
-        box-shadow: 0 0 0 1px ${t.textareaBorderColorError};
+        box-shadow: 0 0 0 ${t.textareaBorderWidthFocus} ${t.textareaBorderColorError};
       }
     `;
   },
@@ -80,11 +81,11 @@ const styles = {
     return css`
       ${cssName(styles.textarea(t))}& {
         border-color: ${t.textareaBorderColorWarning};
-        box-shadow: 0 0 0 1px ${t.textareaBorderColorWarning};
+        box-shadow: 0 0 0 ${t.textareaBorderWidthFocus} ${t.textareaBorderColorWarning};
       }
       ${cssName(styles.textarea(t))}&:focus {
         border-color: ${t.textareaBorderColorWarning};
-        box-shadow: 0 0 0 1px ${t.textareaBorderColorWarning};
+        box-shadow: 0 0 0 ${t.textareaBorderWidthFocus} ${t.textareaBorderColorWarning};
       }
     `;
   },

commit 4ba295122668c8d15dc44e384f13e8a95d6ce5d2

fix(Textarea): return correct min height
it was broken by 1c5d078db

#2160

packages/react-ui/internal/themes/DefaultTheme.ts
@@ -1136,10 +1136,10 @@ export class DefaultTheme {
     return this.controlBorderWidth;
   }
   public static get textareaHeight() {
-    const textareaLineHeight = parseInt(this.textareaLineHeight, 10) * 3;
+    const textareaLineHeight = parseInt(this.textareaLineHeight, 10) * 2;
     const textareaPaddingY = parseInt(this.textareaPaddingY, 10) * 2;
     const textareaBorderWidth = parseInt(this.textareaBorderWidth, 10) * 2;
-    return '68px' || `${textareaLineHeight + textareaPaddingY + textareaBorderWidth}px`;
+    return `${textareaLineHeight + textareaPaddingY + textareaBorderWidth}px`;
   }
   public static textareaWidth = '250px';
   public static get textareaPaddingX() {