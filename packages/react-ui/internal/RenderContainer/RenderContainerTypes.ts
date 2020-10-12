5faffefa15e70b6d2222df512c030bcd531cd4c7
Погадаев Егор Александрович <pogadaev.e@skbkontur.ru>
2020-10-12 15:35:09 +0500

    chore(RenderContainer): don't render render-container without its portal
    it leads to unexisted container-ids after hydration

--- packages/react-ui/internal/RenderContainer/RenderContainerTypes.ts
+++ packages/react-ui/internal/RenderContainer/RenderContainerTypes.ts
@@ -2,6 +2,7 @@ import React from 'react';
 
 export interface PortalProps {
   rt_rootID: string;
+  container: HTMLElement;
 }
 
 export interface RenderContainerProps {
--- packages/react-ui/internal/RenderContainer/RenderInnerContainer.tsx
+++ packages/react-ui/internal/RenderContainer/RenderInnerContainer.tsx
@@ -10,9 +10,14 @@ interface RenderInnerContainerProps extends RenderContainerProps {
   rootId: string;
 }
 
-export function Portal(props: PortalProps) {
-  return <noscript data-render-container-id={props.rt_rootID} />;
-}
+export const Portal: React.FunctionComponent<PortalProps> = ({ container, rt_rootID, children }) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(children, container)}
+      <noscript data-render-container-id={rt_rootID} />
+    </React.Fragment>
+  );
+};
 
 export class RenderInnerContainer extends React.Component<RenderInnerContainerProps> {
   public static __KONTUR_REACT_UI__ = 'RenderInnerContainer';
@@ -25,8 +30,11 @@ export class RenderInnerContainer extends React.Component<RenderInnerContainerPr
       inner = (
         <React.Fragment>
           {anchor}
-          {domContainer && ReactDOM.createPortal(children, domContainer)}
-          <Portal key="portal-ref" rt_rootID={rootId} />
+          {domContainer ? (
+            <Portal key="portal-ref" rt_rootID={rootId} container={domContainer}>
+              {children}
+            </Portal>
+          ) : null}
         </React.Fragment>
       );
     }
