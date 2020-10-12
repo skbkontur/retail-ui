61560fa7394b67e4a2ba27634317f4e7fd1e179b
Погадаев Егор Александрович <pogadaev.e@skbkontur.ru>
2020-10-12 09:39:34 +0500

    chore(Textarea): get border width in a cross-browser way
    getComputedStyle().borderWidth doesn't work in IE11 and FF

--- packages/react-ui/components/Textarea/TextareaHelpers.tsx
+++ packages/react-ui/components/Textarea/TextareaHelpers.tsx
@@ -1,12 +1,13 @@
 export function getTextAreaHeight(node: HTMLTextAreaElement, minRows: number, maxRows: number) {
   const style: CSSStyleDeclaration = getComputedStyle(node);
-  const borderWidth = style.borderWidth ? parseInt(style.borderWidth, 10) : 0;
+  const borderTop = style.borderTopWidth ? parseInt(style.borderTopWidth, 10) : 0;
+  const borderBottom = style.borderBottomWidth ? parseInt(style.borderBottomWidth, 10) : 0;
   const lineHeight = style.lineHeight ? parseInt(style.lineHeight, 10) : 0;
   const paddingTop = style.paddingTop ? parseInt(style.paddingTop, 10) : 0;
   const paddingBottom = style.paddingBottom ? parseInt(style.paddingBottom, 10) : 0;
-  const minHeight = borderWidth * 2 + paddingTop + paddingBottom + lineHeight * minRows;
-  const maxHeight = borderWidth * 2 + paddingTop + paddingBottom + lineHeight * maxRows;
-  const expectedHeight = node.scrollHeight + borderWidth * 2 + lineHeight;
+  const minHeight = borderTop + borderBottom + paddingTop + paddingBottom + lineHeight * minRows;
+  const maxHeight = borderTop + borderBottom + paddingTop + paddingBottom + lineHeight * maxRows;
+  const expectedHeight = node.scrollHeight + borderTop + borderBottom + lineHeight;
   return {
     height: Math.min(Math.max(expectedHeight, minHeight), maxHeight),
     exceededMaxHeight: expectedHeight > maxHeight + lineHeight,
