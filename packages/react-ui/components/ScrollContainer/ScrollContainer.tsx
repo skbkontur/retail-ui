4eb0142844ca9145da1aae4c5a9a1b3cd5045310
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-11-02 08:32:37 +0500

    fix(ScrollContainer): fix scroll state calculation
    don't count horizontal scrollbar width
    
    #2190

--- packages/react-ui/components/ScrollContainer/ScrollContainer.tsx
+++ packages/react-ui/components/ScrollContainer/ScrollContainer.tsx
@@ -346,7 +346,7 @@ export class ScrollContainer extends React.Component<ScrollContainerProps, Scrol
   private getImmediateScrollState(): ScrollContainerScrollState {
     if (!this.inner || this.inner.scrollTop === 0) {
       return 'top';
-    } else if (this.inner.scrollTop === this.inner.scrollHeight - this.inner.offsetHeight) {
+    } else if (this.inner.scrollTop === this.inner.scrollHeight - this.inner.clientHeight) {
       return 'bottom';
     } else {
       return 'scroll';
