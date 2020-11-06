09f2a297c0bb419fc2288ea78188b02647307a21
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-11-06 17:03:58 +0500

    refactor(TokenInput): set defaultProps for delimiters

--- packages/react-ui/components/TokenInput/TokenInput.tsx
+++ packages/react-ui/components/TokenInput/TokenInput.tsx
@@ -59,7 +59,7 @@ export interface TokenInputProps<T> {
   valueToItem: (item: string) => T;
   toKey: (item: T) => string | number | undefined;
   placeholder?: string;
-  delimiters?: string[];
+  delimiters: string[];
   error?: boolean;
   warning?: boolean;
   disabled?: boolean;
@@ -110,6 +110,7 @@ export class TokenInput<T = string> extends React.PureComponent<TokenInputProps<
 
   public static defaultProps: Partial<TokenInputProps<any>> = {
     selectedItems: [],
+    delimiters: [',', ' '],
     renderItem: identity,
     renderValue: identity,
     valueToString: identity,
@@ -305,10 +306,6 @@ export class TokenInput<T = string> extends React.PureComponent<TokenInputProps<
     return this.props.type ? this.props.type : TokenInputType.WithReference;
   }
 
-  private get delimiters() {
-    return this.props.delimiters ? this.props.delimiters : [',', ' '];
-  }
-
   private get menuRef(): Menu | null {
     return this.tokensInputMenu && this.tokensInputMenu.getMenuRef();
   }
@@ -397,7 +394,7 @@ export class TokenInput<T = string> extends React.PureComponent<TokenInputProps<
       .sort()
       .map(index => this.props.selectedItems[index])
       .map(item => this.props.valueToString(item));
-    event.clipboardData.setData('text/plain', tokens.join(this.delimiters[0]));
+    event.clipboardData.setData('text/plain', tokens.join(this.props.delimiters[0]));
   };
 
   private handleInputPaste = (event: React.ClipboardEvent<HTMLElement>) => {
@@ -405,7 +402,7 @@ export class TokenInput<T = string> extends React.PureComponent<TokenInputProps<
       return;
     }
     let paste = event.clipboardData.getData('text');
-    const delimiters = this.delimiters;
+    const { delimiters } = this.props;
     if (delimiters.some(delimiter => paste.includes(delimiter))) {
       event.preventDefault();
       event.stopPropagation();
@@ -459,7 +456,7 @@ export class TokenInput<T = string> extends React.PureComponent<TokenInputProps<
   private handleInputKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     e.stopPropagation();
 
-    if (this.type !== TokenInputType.WithReference && this.delimiters.includes(e.key)) {
+    if (this.type !== TokenInputType.WithReference && this.props.delimiters.includes(e.key)) {
       e.preventDefault();
       const newValue = this.state.inputValue;
       if (newValue !== '') {
