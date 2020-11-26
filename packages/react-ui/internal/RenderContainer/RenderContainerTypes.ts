19a909b771c5867b516a022aec7d900901c47db6
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-11-26 09:32:46 +0500

    chore(RenderContainer): fix SSR compatibility

--- packages/react-ui/internal/RenderContainer/RenderContainerTypes.ts
+++ packages/react-ui/internal/RenderContainer/RenderContainerTypes.ts
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import { Nullable } from '../../typings/utility-types';
+
 export interface PortalProps {
   rt_rootID: string;
-  container: HTMLElement;
+  container: Nullable<HTMLElement>;
 }
 
 export interface RenderContainerProps {
--- packages/react-ui/internal/RenderContainer/RenderInnerContainer.tsx
+++ packages/react-ui/internal/RenderContainer/RenderInnerContainer.tsx
@@ -10,11 +10,32 @@ interface RenderInnerContainerProps extends RenderContainerProps {
   rootId: string;
 }
 
+// Заглушка нужна для корректной гидрации порталов после SSR,
+// которую реакт сам пока не поддерживает.
+// @see https://github.com/facebook/react/issues/13097
+// А также для вставки актуального render-container-id на клиенте.
+//
+// Дело в том, что во время гидрации, структура HTML на сервере
+// и на клиенте должна совпадать, иначе возможны артефакты.
+// Алгоритм там примерно такой. Клиент во время гидрации идет
+// по этим двум деревьям и сравнивает узлы. Элементы разных типов
+// он подменяет на свои. А те, что совпадают, он оставляет как есть
+// вместе со всеми атрибутами, навесив только обработчики событий.
+//
+// Поэтому, для портала, который рендерится только на клиенте,
+// нужно использовать серверную заглушку, чтобы при гидрации
+// он не испортил какой-то другой элемент. Null не подходит,
+// т.к. на сервере он тоже не рендерится.
+// А элемент с render-container-id нужно отрендерить с нуля.
+
+const SSRPlaceholder = () => <script data-id="ssr-placeholder" />;
+
 export const Portal: React.FunctionComponent<PortalProps> = ({ container, rt_rootID, children }) => {
+  // container exists only in browser
   return (
     <React.Fragment>
-      {ReactDOM.createPortal(children, container)}
-      <noscript data-render-container-id={rt_rootID} />
+      {container ? ReactDOM.createPortal(children, container) : <SSRPlaceholder />}
+      {container ? <noscript data-render-container-id={rt_rootID} /> : <SSRPlaceholder />}
     </React.Fragment>
   );
 };
@@ -30,11 +51,9 @@ export class RenderInnerContainer extends React.Component<RenderInnerContainerPr
       inner = (
         <React.Fragment>
           {anchor}
-          {domContainer ? (
-            <Portal key="portal-ref" rt_rootID={rootId} container={domContainer}>
-              {children}
-            </Portal>
-          ) : null}
+          <Portal key="portal-ref" rt_rootID={rootId} container={domContainer}>
+            {children}
+          </Portal>
         </React.Fragment>
       );
     }
