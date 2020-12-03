e02d14033079184b5e3ca5e96d32732b21ad1552
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-12-03 08:50:47 +0500

    build(react-ui): remove file-loader from ssr test app

--- packages/react-ui-smoke-test/react-ui-ssr/webpack.config.js
+++ packages/react-ui-smoke-test/react-ui-ssr/webpack.config.js
@@ -17,17 +17,6 @@ module.exports = {
         },
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-            },
-          },
-        ],
-      },
-      {
         loader: 'string-replace-loader',
         options: {
           search: /__REACT_UI_PACKAGE__/g,
