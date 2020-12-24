a00740f61059e9770d334df51251fc32b38d7c67
lossir <lossir1@gmail.com>
2020-12-24 11:46:06 +0500

    chore: 1

--- packages/react-ui/.babelrc.js
+++ packages/react-ui/.babelrc.js
@@ -2,12 +2,12 @@ module.exports = {
   env: {
     cjs: {
       presets: [['@babel/preset-env', { loose: true, modules: 'commonjs', targets: { ie: '11' } }]],
-      plugins: [['@babel/plugin-transform-runtime', { version: '7.8.3' }]],
+      plugins: [['@babel/plugin-transform-runtime', { version: '7.12.5' }]],
     },
   },
   presets: [['@babel/preset-env', { loose: true, modules: false }], '@babel/typescript', '@babel/preset-react'],
   plugins: [
-    ['@babel/plugin-transform-runtime', { useESModules: true, version: '7.8.3' }],
+    ['@babel/plugin-transform-runtime', { useESModules: true, version: '7.12.5' }],
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
