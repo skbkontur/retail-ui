7bac82feefd11c74e332d051edb88ee2ffff599a
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-12-25 14:13:09 +0500

    chore(CurrencyLabel): forward common props

--- packages/react-ui/components/CurrencyLabel/CurrencyLabel.tsx
+++ packages/react-ui/components/CurrencyLabel/CurrencyLabel.tsx
@@ -2,35 +2,41 @@ import React from 'react';
 
 import { MAX_SAFE_DIGITS } from '../CurrencyInput/constants';
 import { CurrencyHelper } from '../CurrencyInput/CurrencyHelper';
+import { CommonProps } from '../../typings/common';
+import { extractCommonProps } from '../../lib/filterProps';
 
-export type CurrencyLabelProps = {
+export interface CurrencyLabelProps extends CommonProps {
   /**
    * Минимальное количество отображаемых знаков после запятой
    * @default 2
    */
-  fractionDigits: number;
+  fractionDigits?: number;
   value: number;
   currencySymbol?: React.ReactNode;
-} & typeof defaultProps;
+}
 
 export const defaultProps = {
   fractionDigits: 2,
 };
 
-export const CurrencyLabel = ({ value, fractionDigits, currencySymbol }: CurrencyLabelProps): JSX.Element => (
-  <span>
-    {CurrencyHelper.format(value, { fractionDigits })}
-    {currencySymbol && String.fromCharCode(0xa0) /* &nbsp; */}
-    {currencySymbol}
-  </span>
-);
+export const CurrencyLabel = (props: CurrencyLabelProps): JSX.Element => {
+  const [commonProps] = extractCommonProps(props);
+  const { value, fractionDigits, currencySymbol } = props;
+  return (
+    <span {...commonProps}>
+      {CurrencyHelper.format(value, { fractionDigits })}
+      {currencySymbol && String.fromCharCode(0xa0) /* &nbsp; */}
+      {currencySymbol}
+    </span>
+  );
+};
 
 CurrencyLabel.__KONTUR_REACT_UI__ = 'CurrencyLabel';
 
 CurrencyLabel.defaultProps = defaultProps;
 
 CurrencyLabel.propTypes = {
-  fractionDigits: (props: CurrencyLabelProps) => {
+  fractionDigits: (props: CurrencyLabelProps & typeof defaultProps) => {
     if (props.fractionDigits > MAX_SAFE_DIGITS) {
       return new Error(
         `[CurrencyLabel]: Prop 'fractionDigits' exceeds ${MAX_SAFE_DIGITS}.` +
