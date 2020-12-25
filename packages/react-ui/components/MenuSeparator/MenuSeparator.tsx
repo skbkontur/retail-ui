b0bdf33e89c8c19a6f8d66e4e87aa60706c8cb02
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-12-25 14:19:31 +0500

    chore(MenuSeparator): forward common props

--- packages/react-ui/components/MenuSeparator/MenuSeparator.tsx
+++ packages/react-ui/components/MenuSeparator/MenuSeparator.tsx
@@ -1,16 +1,19 @@
 import React, { useContext } from 'react';
+import cn from 'classnames';
 
 import { ThemeContext } from '../../lib/theming/ThemeContext';
+import { CommonProps } from '../../typings/common';
 
 import { jsStyles } from './MenuSeparator.styles';
 
+export type MenuSeparatorProps = CommonProps;
 /**
  * Разделитель в меню.
  */
-function MenuSeparator() {
+function MenuSeparator({ className, ...rest }: MenuSeparatorProps) {
   const theme = useContext(ThemeContext);
 
-  return <div className={jsStyles.root(theme)} />;
+  return <div {...rest} className={cn(className, jsStyles.root(theme))} />;
 }
 
 MenuSeparator.__KONTUR_REACT_UI__ = 'MenuSeparator';
