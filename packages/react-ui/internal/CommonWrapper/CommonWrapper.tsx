dba2c62b6ae0e30e901d4ed7328aff1ca150a0e0
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2021-01-21 12:18:49 +0500

    chore: introduce CommonWrapper component

---
+++ packages/react-ui/internal/CommonWrapper/CommonWrapper.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import cn from 'classnames';
+
+import { isFunction } from '../../lib/utils';
+
+export interface CommonProps {
+  className?: React.HTMLAttributes<HTMLElement>['className'];
+  style?: React.HTMLAttributes<HTMLElement>['style'];
+  /** На равне с data-tid транслируются любые data-атрибуты */
+  'data-tid'?: string;
+}
+
+export type NotCommonProps<P> = Omit<P, keyof CommonProps>;
+
+export type CommonWrapperProps<P> = P & {
+  children: React.ReactNode | ((rest: CommonWrapperRestProps<P>) => React.ReactNode);
+};
+export type CommonWrapperRestProps<P> = Omit<NotCommonProps<P>, 'children'>;
+
+export class CommonWrapper<P extends CommonProps> extends React.Component<CommonWrapperProps<P>> {
+  render() {
+    const [{ className, style, ...dataProps }, { children, ...rest }] = extractCommonProps(this.props);
+    const child = isFunction(children) ? children(rest) : children;
+
+    return React.isValidElement<CommonProps>(child)
+      ? React.cloneElement(child, {
+          className: cn(child.props.className, className),
+          style: {
+            ...child.props.style,
+            ...style,
+          },
+          ...dataProps,
+        })
+      : child;
+  }
+}
+
+const extractCommonProps = <P extends CommonProps>(props: P): [CommonProps, NotCommonProps<P>] => {
+  const common = {} as CommonProps;
+  const rest = {} as NotCommonProps<P>;
+
+  for (const key in props) {
+    if (isCommonProp(key)) {
+      // @ts-ignore
+      common[key] = props[key];
+    } else {
+      // @ts-ignore
+      rest[key] = props[key];
+    }
+  }
+
+  return [common, rest];
+};
+
+const isCommonProp = (name: string) => {
+  switch (true) {
+    case name == 'className':
+    case name == 'style':
+    case name.indexOf('data-') === 0: // все data-атрибуты
+      return true;
+    default:
+      return false;
+  }
+};
