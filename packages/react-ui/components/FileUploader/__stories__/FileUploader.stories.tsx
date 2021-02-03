a1aa555bb76753742b20b9ec983174134c0a3dd9
mozalov <mozalov@skbkontur.ru>
2021-02-03 18:51:46 +0500

    add default story

--- packages/react-ui/components/FileUploader/__stories__/FileUploader.stories.tsx
+++ packages/react-ui/components/FileUploader/__stories__/FileUploader.stories.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import { FileUploader } from '../FileUploader';
 
 export default {
-  title: 'FileUploader',
-  decorators: [
-    (storyFn: () => JSX.Element) => (
-      <div style={{ padding: '5px', width: '300px' }}>{storyFn()}</div>
-    ),
-  ],
+  title: 'FileUploader'
 };
 
-export const SyncUploader = () => (
+export const AttachFileUploader = () => (
   <FileUploader />
 );
