d8ad75566786e2fa3564aaa4ce8a7eceab9d2f71
mozalov <mozalov@skbkontur.ru>
2021-02-05 12:27:33 +0500

    added focus view

--- packages/react-ui/components/FileUploader/FileUploader.styles.ts
+++ packages/react-ui/components/FileUploader/FileUploader.styles.ts
@@ -1,26 +1,1 @@
-import { css, memoizeStyle } from '../../lib/theming/Emotion';
-
-const styles = {
-  root() {
-    return css`
-      display: inline-flex;
-      border: 1px dashed rgba(0, 0, 0, 0.37);
-      padding: 5px 6px 7px;
-      cursor: pointer;
-    `;
-  },
-
-  fileInput() {
-    return css`
-      display: none;
-    `;
-  },
-
-  link() {
-    return css`
-      font-size: 14px;
-    `;
-  }
-};
-
-export const jsStyles = memoizeStyle(styles);
+import { css, memoizeStyle } from '../../lib/theming/Emotion';const styles = {  root() {    return css`      display: inline-flex;      border: 1px dashed rgba(0, 0, 0, 0.37);      padding: 5px 6px 7px;      cursor: pointer;      border-radius: 1px;      outline: none;      &:focus {        border: 2px solid #1D85D0;      }    `;  },  fileInput() {    return css`      display: none;    `;  },  link() {    return css`      font-size: 14px;    `;  }};export const jsStyles = memoizeStyle(styles);
