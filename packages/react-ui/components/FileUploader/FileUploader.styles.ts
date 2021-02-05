e77275b0be3c7d872612b7ed0bd3e25a29db5853
mozalov <mozalov@skbkontur.ru>
2021-02-05 15:26:50 +0500

    add drop files reaction

--- packages/react-ui/components/FileUploader/FileUploader.styles.ts
+++ packages/react-ui/components/FileUploader/FileUploader.styles.ts
@@ -1,1 +1,43 @@
-import { css, memoizeStyle } from '../../lib/theming/Emotion';const styles = {  root() {    return css`      display: inline-flex;      border: 1px dashed rgba(0, 0, 0, 0.37);      padding: 5px 6px 7px;      cursor: pointer;      border-radius: 1px;      outline: none;      &:focus {        border: 2px solid #1D85D0;      }    `;  },  fileInput() {    return css`      display: none;    `;  },  link() {    return css`      font-size: 14px;    `;  }};export const jsStyles = memoizeStyle(styles);
+import { css, memoizeStyle } from '../../lib/theming/Emotion';
+
+const styles = {
+  root() {
+    return css`
+      display: inline-flex;
+      align-items: center;
+      border: 1px dashed rgba(0, 0, 0, 0.37);
+      box-sizing: padding-box;
+      border-radius: 1px;
+      outline: none;
+      height: 32px;
+      cursor: pointer;
+      padding: 0 6px;
+
+      &:focus {
+        border: 2px solid #1D85D0;
+      }
+    `;
+  },
+
+  dragOver() {
+    return css`
+      border: 4px solid #2DA4F9;
+      border-radius: 2px;
+      box-shadow: 0px 0px 1px 4px rgba(45,164,249,0.35);
+    `;
+  },
+
+  fileInput() {
+    return css`
+      display: none;
+    `;
+  },
+
+  link() {
+    return css`
+      font-size: 14px;
+    `;
+  }
+};
+
+export const jsStyles = memoizeStyle(styles);
--- packages/react-ui/components/FileUploader/FileUploader.tsx
+++ packages/react-ui/components/FileUploader/FileUploader.tsx
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { jsStyles } from './FileUploader.styles';
 import { stopPropagation } from '../../lib/events/stopPropagation';
 import UploadIcon from '@skbkontur/react-icons/Upload';
 import { Link } from '../Link';
 import { Gapped } from '../Gapped';
+import cn from 'classnames';
 
 // FIXME @mozalov: написать комменты для каждого пропса
 // FIXME @mozalov: локализация
@@ -25,18 +26,76 @@ export interface FileUploaderProps {
 export const FileUploader = (props: FileUploaderProps) => {
   const {name, multiple, accept} = props;
 
+  const rootRef = useRef<HTMLDivElement>(null);
+  const enterCounter = useRef<number>(0);
   const inputRef = useRef<HTMLInputElement>(null);
+  const [isDraggable, setIsDraggable] = useState<boolean>(false);
 
-  const handleClick = React.useCallback(() => {
+  const handleClick = useCallback(() => {
     inputRef.current?.click();
   }, []);
 
-  const handleChange = React.useCallback(() => {
+  const preventDefault = useCallback(event => {
+    event.preventDefault();
+    event.stopPropagation();
+  }, []);
+
+  const handleDragOver = useCallback(event => {
+    event.preventDefault();
+  }, []);
+
+  const handleDragEnter = useCallback(event => {
+    preventDefault(event);
+    enterCounter.current++;
+    setIsDraggable(true);
+  }, [preventDefault]);
+
+  const handleDragLeave = useCallback(event => {
+    enterCounter.current--;
+    if (enterCounter.current === 0) {
+      setIsDraggable(false);
+    }
+  }, []);
+
+  const handleDrop = useCallback(event => {
+    preventDefault(event);
+    setIsDraggable(false);
+    enterCounter.current = 0;
+    if (event.dataTransfer.files && event.dataTransfer.files.length > 0) {
+      console.log({files: event.dataTransfer.files.length});
+      event.dataTransfer.clearData();
+    }
+  }, [preventDefault]);
+
+  useEffect(() => {
+    rootRef.current?.addEventListener("dragenter", handleDragEnter);
+    rootRef.current?.addEventListener("dragleave", handleDragLeave);
+    rootRef.current?.addEventListener("drop", handleDrop);
+    rootRef.current?.addEventListener("dragover", handleDragOver);
+
+    return () => {
+      rootRef.current?.removeEventListener("dragenter", handleDragEnter);
+      rootRef.current?.removeEventListener("dragleave", handleDragLeave);
+      rootRef.current?.removeEventListener("drop", handleDrop);
+      rootRef.current?.removeEventListener("dragover", handleDragOver);
+    };
+  }, []);
+
+  const handleChange = useCallback(() => {
 
   }, []);
 
+  const rootClassNames = cn(jsStyles.root(), {
+    [jsStyles.dragOver()]: isDraggable
+  })
+
   return (
-    <div className={jsStyles.root()} onClick={handleClick} tabIndex={0}>
+    <div
+      className={rootClassNames}
+      onClick={handleClick}
+      tabIndex={0}
+      ref={rootRef}
+    >
       <Gapped gap={5}>
         <div>
           <Link className={jsStyles.link()} tabIndex={-1}>
