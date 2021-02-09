70341474b8f5f1acc4f9f94e947121d3bceac70c
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2021-02-09 22:53:23 +0500

    chore: fix tslint error

--- packages/retail-ui/lib/styles/HoldSelectionColor.ts
+++ packages/retail-ui/lib/styles/HoldSelectionColor.ts
@@ -4,6 +4,7 @@ import { isChrome, isFirefox, isIE11 } from '../utils';
 // This is for screenshot tests
 const Highlight = isIE11 || isFirefox ? '#3399ff' : isChrome ? '#3390ff' : 'Highlight';
 
+// tslint:disable-next-line:no-unused-expression
 injectGlobal`
   *::selection {
     background-color: ${Highlight};
