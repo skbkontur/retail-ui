732532acc2947905ef0cbdb66f589ab9dc8cd5df
lossir <lossir1@gmail.com>
2021-02-12 15:45:52 +0500

    refactor: wip

--- packages/react-ui-validations/.storybook/main.js
+++ packages/react-ui-validations/.storybook/main.js
@@ -1,3 +1,38 @@
+const webpack = require('webpack');
+
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
+  webpackFinal: config => {
+    config.resolve.extensions.unshift('.ts', '.tsx');
+
+    config.entry.push('react-ui-testing/react-selenium-testing');
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      exclude: /node_moduels/,
+      use: [
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+        {
+          loader: 'string-replace-loader',
+          options: {
+            search: /__REACT_UI_PACKAGE__/g,
+            replace: '@skbkontur/react-ui',
+          },
+        },
+      ],
+    });
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.enableReactTesting': JSON.stringify(true),
+      }),
+    );
+
+    return config;
+  },
 };
---
+++ packages/react-ui/.storybook/main.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const isTestEnv = process.env.NODE_ENV === 'test';
+
+module.exports = {
+  addons: ['@storybook/addon-actions/register', '@storybook/addon-links/register', 'creevey'],
+  stories: ['../components/**/*.stories.tsx', '../internal/**/*.stories.tsx'],
+  typescript: {
+    reactDocgenTypescriptOptions: {
+      tsconfigPath: '../prod.tsconfig.json',
+      setDisplayName: false,
+    },
+  },
+  webpackFinal: config => {
+    config.module.rules = [
+      {
+        test: /\.(j|t)sx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          babelrc: false,
+          envName: 'cjs',
+          extends: path.join(__dirname, '../.babelrc.js'),
+        },
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'global',
+                localIdentName: '[name]-[local]-[hash:base64:4]',
+              },
+            },
+          },
+        ],
+      },
+      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url-loader' },
+    ];
+
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.enableReactTesting': JSON.stringify(isTestEnv),
+      }),
+      new ForkTsCheckerWebpackPlugin({
+        tsconfig: path.resolve(__dirname, '../prod.tsconfig.json'),
+      }),
+    );
+
+    return config;
+  },
+};
