0ab031afca7a763051db1ac9f8b3489e6ab8609e
a.bandura <bae1@skbkontur.ru>
2021-02-17 17:53:53 +0500

    test(Hint): add one story

--- packages/react-ui/components/Hint/__stories__/Hint.stories.tsx
+++ packages/react-ui/components/Hint/__stories__/Hint.stories.tsx
@@ -126,3 +126,80 @@ HintsWithoutWrapperAroundInlineBlockWith50Width.story = {
   name: 'Hints without wrapper around inline-block with 50% width',
   parameters: { creevey: { delay: 500 } },
 };
+
+const changingTextTimeout = 0;
+
+export const HintWithDynamicValueShortToLong: CSFStory<JSX.Element> = () => {
+  let timeout: number;
+  const [value, updateValue] = React.useState('short');
+  const [trigger, updateTrigger] = React.useState(1);
+  const [isOpened, updateIsOpened] = React.useState(false);
+
+  const onClick = () => {
+    updateIsOpened(!isOpened);
+    updateValue('short');
+
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      updateTrigger(trigger + 1);
+      updateValue('new long value after programmatically changing');
+    }, changingTextTimeout);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          padding: '5px 5px 5px 5px',
+          position: 'absolute',
+          border: '1px solid black',
+          right: '50%',
+          top: '100px',
+        }}
+      >
+        <Hint text={value} pos="top" disableAnimations manual opened>
+          hint here
+        </Hint>
+      </div>
+      <div
+        id="hint-wrapper"
+        style={{
+          padding: '80px 5px 5px 160px',
+          position: 'absolute',
+          border: '1px solid black',
+          right: '0',
+          top: '100px',
+        }}
+      >
+        <Hint text={value} pos="top" disableAnimations manual opened={isOpened}>
+          <div id="hint-trigger" onClick={onClick} key={trigger} style={{ border: '1px solid black' }}>
+            hint here
+          </div>
+        </Hint>
+      </div>
+    </>
+  );
+};
+
+HintWithDynamicValueShortToLong.story = {
+  parameters: {
+    creevey: {
+      tests: {
+        async hintNearWindowBorderAfterTextChanging() {
+          const hintTrigger = await this.browser.findElement({ css: '#hint-trigger' });
+          const hintWrapper = await this.browser.findElement({ css: '#hint-wrapper' });
+
+          await this.browser
+            .actions({ bridge: true })
+            .click(hintTrigger)
+            .perform();
+          await new Promise(r => setTimeout(r, changingTextTimeout));
+
+          const hintedElement = await hintWrapper.takeScreenshot();
+
+          await this.expect({ hintedElement }).to.matchImages();
+        },
+      },
+    },
+  },
+};
