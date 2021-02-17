e611a7062215b214c8143de618844f99efdd0c9a
a.bandura <bae1@skbkontur.ru>
2021-02-18 02:47:38 +0500

    test(Toast): add cases and screenshots

--- packages/react-ui/components/Toast/__stories__/ToastView.stories.tsx
+++ packages/react-ui/components/Toast/__stories__/ToastView.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-
+import { CSFStory } from 'creevey';
 import { ToastView as Toast } from '../ToastView';
 
 export default { title: 'ToastView', parameters: { creevey: { captureElement: "[data-tid='ToastView__root']" } } };
@@ -14,3 +14,18 @@ export const WithAction = () => (
   </Toast>
 );
 WithAction.story = { name: 'with action' };
+
+// Фичареквест:
+// В компоненте текст не может быть набран в две строки (по гайдам).
+// Тем не менее, он переносится в случае, если в длинной строке есть пробелы
+// Мы предлагаем обдумать это место в гайдах и в случае, когда текст не помещается в одну строку,
+// обрезать его с троеточием в конце
+
+export const LongName: CSFStory<JSX.Element> = () => (
+  <Toast>
+    Loremipsumdolorsitametconsecteturadipisicingelit.Doloresoptioillomagnam!
+    Teneturnumquameosdistinctiodolor,nonbeataedignissimosvoluptatesquos!
+    Delenititemporaofficiamagnidelectusadipisciconsequunturfacilis?
+  </Toast>
+);
+LongName.story = { name: 'toast with long name' };
