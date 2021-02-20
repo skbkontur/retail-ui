087a0edef5da5dd04e67e2796180c8722972cf5f
mozalov <mozalov@skbkontur.ru>
2021-02-20 17:20:05 +0500

    add control validation

--- packages/react-ui/internal/FileAttacherBase/UploadFilesContext.ts
+++ packages/react-ui/internal/FileAttacherBase/UploadFilesContext.ts
@@ -1,11 +1,14 @@
 import * as React from "react";
 import { IUploadFile, UploadFileStatus } from '../../lib/fileUtils';
+import { ValidationResult } from './ValidationResult';
+import { FileAttacherBaseValidationError } from './FileAttacherBase';
 
 export interface UploadFilesContextProps {
   setFileStatus: (fileId: string, status: UploadFileStatus) => void;
   files: IUploadFile[];
   setFiles: (files: IUploadFile[]) => void;
   removeFile: (fileId: string) => void;
+  validationResult: ValidationResult<FileAttacherBaseValidationError>;
 }
 
 export const UploadFilesContext = React.createContext({} as UploadFilesContextProps);
