dace95b1385b5ad14198b20ff13505b24ace40c0
as_kondratyev <as_kondratyev@skbkontur.ru>
2021-03-22 13:56:53 +0500

    refactor(react-ui): check process.env is exists before use

--- packages/react-ui/lib/currentEnvironment.ts
+++ packages/react-ui/lib/currentEnvironment.ts
@@ -1,3 +1,4 @@
-export const isTestEnv: boolean = process.env.NODE_ENV === 'test' || Boolean(process.env.enableReactTesting);
+const enableReactTesting = process?.env?.enableReactTesting != null ? Boolean(process.env.enableReactTesting) : false;
+export const isTestEnv: boolean = process.env.NODE_ENV === 'test' || enableReactTesting;
 export const isProductionEnv: boolean = process.env.NODE_ENV === 'production';
 export const isDevelopmentEnv: boolean = process.env.NODE_ENV === 'development';
