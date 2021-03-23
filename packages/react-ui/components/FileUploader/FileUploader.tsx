245a62ae08d095bfa41d83dab0110ff6e42b9829
mozalov <mozalov@skbkontur.ru>
2021-03-23 13:06:13 +0500

    add file validation to async control

--- packages/react-ui/components/FileUploader/FileUploader.tsx
+++ packages/react-ui/components/FileUploader/FileUploader.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useContext } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import { withUploadFilesProvider } from '../../internal/FileAttacherBase/UploadFilesProvider';
 import { IUploadFile, UploadFileStatus } from '../../lib/fileUtils';
-import { FileAttacherBase, FileAttacherBaseProps } from '../../internal/FileAttacherBase';
+import { FileAttacherBase, FileAttacherBaseProps, FileError } from '../../internal/FileAttacherBase';
 import { UploadFilesContext } from '../../internal/FileAttacherBase/UploadFilesContext';
 
 // FIXME @mozalov: подумать как делать abort запроса по крестику
@@ -13,13 +13,16 @@ export type RequestFunction = (
   onError: (error: object) => void,
 ) => void;
 
-export interface FileUploaderProps extends FileAttacherBaseProps {
+export interface FileUploaderProps extends Omit<FileAttacherBaseProps, 'fileError'> {
   request: RequestFunction;
+  fileValidation: (file: IUploadFile) => Promise<string>;
 }
 
 export const FileUploader = withUploadFilesProvider((props: FileUploaderProps) => {
-  const {request, controlError} = props;
+  const {request, controlError, fileValidation} = props;
   const {setFileStatus} = useContext(UploadFilesContext);
+
+  const [fileError, setFileError] = useState<FileError[]>([]);
 
   const handleStart = useCallback((fileId: string) => {
     setFileStatus(fileId, UploadFileStatus.Loading);
@@ -40,12 +43,23 @@ export const FileUploader = withUploadFilesProvider((props: FileUploaderProps) =
   }, [request, handleSuccess, handleError, handleStart])
 
   const handleChange = useCallback((files: IUploadFile[]) => {
-    !controlError && files.forEach(file => upload(file));
-  }, [upload, controlError]);
+    if (!controlError) {
+      files.forEach(async file => {
+        const validationMessage = await fileValidation(file);
+
+        if (!validationMessage) {
+          upload(file);
+        } else {
+          setFileError([{fileId: file.id, message: validationMessage}]);
+        }
+      });
+    }
+  }, [upload, controlError, fileValidation]);
 
   return (
     <FileAttacherBase
       {...props}
+      fileError={fileError}
       onChange={handleChange}
     />
   );
