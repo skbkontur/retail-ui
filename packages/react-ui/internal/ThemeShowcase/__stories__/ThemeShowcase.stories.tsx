615026168ee3ae206b414f00bedede518878ae9a
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2021-05-13 21:27:24 +0500

    test: remove ThemeShowcase story
    It affects screenshot testing by inserting all the styles on the page at once. That hides bugs.

--- packages/react-ui/internal/ThemeShowcase/__stories__/ThemeShowcase.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { ThemeShowcase } from '../ThemeShowcase';
-
-export default { title: 'ThemeShowcase', parameters: { creevey: { skip: [true] } } };
-
-export const Playground = () => <ThemeShowcase isDebugMode={true} />;
