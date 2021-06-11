3b20af0a0b9b9455fd7f7594b27f6bf41c09eb80
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2021-06-11 11:20:37 +0500

    feat(react-ui): support webpack 5
    safe usage of process.env

--- packages/react-ui/lib/currentEnvironment.ts
+++ packages/react-ui/lib/currentEnvironment.ts
@@ -1,4 +1,7 @@
-const enableReactTesting = process?.env?.enableReactTesting != null ? Boolean(process.env.enableReactTesting) : false;
-export const isTestEnv: boolean = process.env.NODE_ENV === 'test' || enableReactTesting;
-export const isProductionEnv: boolean = process.env.NODE_ENV === 'production';
-export const isDevelopmentEnv: boolean = process.env.NODE_ENV === 'development';
+const env: NodeJS.ProcessEnv = (typeof process === 'object' && process && process.env) || {};
+const { enableReactTesting, NODE_ENV } = env;
+
+const isReactTesting = enableReactTesting != null ? Boolean(enableReactTesting) : false;
+export const isTestEnv: boolean = NODE_ENV === 'test' || isReactTesting;
+export const isProductionEnv: boolean = NODE_ENV === 'production';
+export const isDevelopmentEnv: boolean = NODE_ENV === 'development';
