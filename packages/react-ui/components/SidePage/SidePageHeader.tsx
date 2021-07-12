99f7ea19f2f7160eeb844d3727517dd7426b2150
ishmatova <ishmatova@skbkontur.ru>
2021-07-12 12:35:04 +0500

    chore(SidePage): delete tab index to close cross icon

--- packages/react-ui/components/SidePage/SidePageHeader.tsx
+++ packages/react-ui/components/SidePage/SidePageHeader.tsx
@@ -105,7 +105,6 @@ export class SidePageHeader extends React.Component<SidePageHeaderProps, SidePag
           })}
           onClick={requestClose}
           data-tid="SidePage__close"
-          tabIndex={0}
         >
           <span
             className={cn(jsStyles.closeIcon(this.theme), {
