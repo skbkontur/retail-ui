c274d8b014e259722960d63ec4c1eb331fed0324
Илья Балабуев <balabuev09@gmail.com>
2021-07-15 11:23:04 +0700

    test(ScrollContainer): fix unit test

--- packages/react-ui/components/ScrollContainer/__tests__/ScrollContainer-test.tsx
+++ packages/react-ui/components/ScrollContainer/__tests__/ScrollContainer-test.tsx
@@ -14,6 +14,6 @@ describe('ScrollContainer', () => {
       </ScrollContainer>,
     );
 
-    expect(wrapper.state('scrollY')).toMatchObject({ scrollState: 'top' });
+    expect(wrapper.state('y')).toMatchObject({ scrollState: 'top' });
   });
 });
