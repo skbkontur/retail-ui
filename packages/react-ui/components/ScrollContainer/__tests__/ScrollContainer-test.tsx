2df97cf552ee65a22b25b46c6c15b4c3146f57a6
Илья Балабуев <balabuev09@gmail.com>
2021-07-28 12:00:35 +0700

    test(ScrollContainer): unit tests for convertors

--- packages/react-ui/components/ScrollContainer/__tests__/ScrollContainer-test.tsx
+++ packages/react-ui/components/ScrollContainer/__tests__/ScrollContainer-test.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 
 import { MenuItem } from '../../MenuItem';
 import { ScrollContainer } from '../ScrollContainer';
+import { convertScrollbarXScrollState, convertScrollbarYScrollState } from '../ScrollContainer.helpers';
 
 describe('ScrollContainer', () => {
-  test('renders with proper scroll state', () => {
+  test('rendering with correct vertical scroll state', () => {
     const wrapper = mount(
       <ScrollContainer>
         {new Array(50).fill('').map((i, index) => (
@@ -14,6 +15,48 @@ describe('ScrollContainer', () => {
       </ScrollContainer>,
     );
 
-    expect(wrapper.state('y')).toMatchObject({ scrollState: 'top' });
+    expect(wrapper.state('y')).toMatchObject({ state: 'begin' });
+  });
+
+  test('rendering with correct horizontal scroll state', () => {
+    const wrapper = mount(
+      <ScrollContainer maxWidth={200}>
+        {new Array(50).fill('').map((i, index) => (
+          <div key={index} style={{ width: 350 }}>
+            {'test'}
+          </div>
+        ))}
+      </ScrollContainer>,
+    );
+
+    expect(wrapper.state('x')).toMatchObject({ state: 'begin' });
+  });
+});
+
+describe('convertScrollbarXScrollState', () => {
+  test('begin position left', () => {
+    expect(convertScrollbarXScrollState('begin')).toBe('left');
+  });
+
+  test('middle position scroll', () => {
+    expect(convertScrollbarXScrollState('middle')).toBe('scroll');
+  });
+
+  test('end position right', () => {
+    expect(convertScrollbarXScrollState('end')).toBe('right');
+  });
+});
+
+describe('convertScrollbarYScrollState', () => {
+  test('begin position top', () => {
+    expect(convertScrollbarYScrollState('begin')).toBe('top');
+  });
+
+  test('middle position scroll', () => {
+    expect(convertScrollbarYScrollState('middle')).toBe('scroll');
+  });
+
+  test('end position bottom', () => {
+    expect(convertScrollbarYScrollState('end')).toBe('bottom');
   });
 });
