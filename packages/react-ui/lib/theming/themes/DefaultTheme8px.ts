acc8bd836b5d4aeae3757fbd1981cff892e30b9a
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2021-08-03 15:50:03 +0500

    fix(react-ui): add compatibility with addons@2.x
    see #2339

--- packages/react-ui/lib/theming/themes/DefaultTheme8px.ts
+++ packages/react-ui/lib/theming/themes/DefaultTheme8px.ts
@@ -1,4 +1,6 @@
+import { DefaultThemeInternal } from '../../../internal/themes/DefaultTheme';
 import { Theme8pxInternal } from '../../../internal/themes/Theme8px';
 import { ThemeFactory } from '../ThemeFactory';
+import { markAs8pxTheme } from '../ThemeHelpers';
 
-export const DEFAULT_THEME_8PX = ThemeFactory.create(Theme8pxInternal);
+export const DEFAULT_THEME_8PX = ThemeFactory.create(Theme8pxInternal, markAs8pxTheme(DefaultThemeInternal));
