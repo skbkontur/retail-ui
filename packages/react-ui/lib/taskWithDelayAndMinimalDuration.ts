2ffa70b165af3d31f2e6da06214d91b07c031563
Khlutkova Anna <82796282+khlutkova@users.noreply.github.com>
2021-08-26 17:57:04 +0500

    feat(Loader): add props for show loader without spinner (#2445)
    Resolve #2266

---
+++ packages/react-ui/lib/taskWithDelayAndMinimalDuration.ts
@@ -0,0 +1,81 @@
+import { Nullable } from '../typings/utility-types';
+
+export interface TaskWithDelayAndMinimalDurationProps {
+  delayBeforeTaskStart: number;
+  durationOfTask: number;
+  taskStartCallback: () => void;
+  taskStopCallback: () => void;
+}
+
+export class TaskWithDelayAndMinimalDuration {
+  private timeoutBeforeTaskStart: Nullable<NodeJS.Timeout>;
+  private timeoutBeforeTaskStop: Nullable<NodeJS.Timeout>;
+  private taskParams: TaskWithDelayAndMinimalDurationProps;
+  private isTaskActive = false;
+
+  constructor(initialValues: TaskWithDelayAndMinimalDurationProps) {
+    this.taskParams = initialValues;
+  }
+
+  private setTimeoutBeforeTaskStart = () => {
+    this.timeoutBeforeTaskStart = setTimeout(() => {
+      this.isTaskActive && this.taskParams.taskStartCallback();
+      this.clearTimeoutBeforeTaskStart();
+      this.setTimeoutBeforeTaskStop();
+    }, this.taskParams.delayBeforeTaskStart);
+  };
+
+  private setTimeoutBeforeTaskStop = () => {
+    this.timeoutBeforeTaskStop = setTimeout(() => {
+      !this.isTaskActive && this.taskParams.taskStopCallback();
+      this.clearTimeoutBeforeTaskStop();
+    }, this.taskParams.durationOfTask);
+  };
+
+  private clearTimeoutBeforeTaskStart = () => {
+    this.timeoutBeforeTaskStart && clearTimeout(this.timeoutBeforeTaskStart);
+    this.timeoutBeforeTaskStart = null;
+  };
+
+  private clearTimeoutBeforeTaskStop = () => {
+    this.timeoutBeforeTaskStop && clearTimeout(this.timeoutBeforeTaskStop);
+    this.timeoutBeforeTaskStop = null;
+  };
+
+  public start = () => {
+    if (this.isTaskActive) {
+      return;
+    }
+
+    this.isTaskActive = true;
+
+    if (this.timeoutBeforeTaskStart) {
+      this.clearTimeoutBeforeTaskStart();
+    }
+
+    this.setTimeoutBeforeTaskStart();
+  };
+
+  public update = (updateData: Partial<TaskWithDelayAndMinimalDurationProps>) => {
+    this.taskParams = { ...this.taskParams, ...updateData };
+  };
+
+  public stop = () => {
+    if (!this.isTaskActive) return;
+
+    this.isTaskActive = false;
+
+    if (!this.timeoutBeforeTaskStart && !this.timeoutBeforeTaskStop) {
+      this.taskParams.taskStopCallback();
+    }
+
+    if (this.timeoutBeforeTaskStart) {
+      this.clearTimeoutBeforeTaskStart();
+    }
+  };
+
+  public clearTask = () => {
+    this.clearTimeoutBeforeTaskStart();
+    this.clearTimeoutBeforeTaskStop();
+  };
+}
