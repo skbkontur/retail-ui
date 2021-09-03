aa914ccfe195acd407362990c0c40b44693e1674
mozalov <mozalov@skbkontur.ru>
2021-09-03 14:11:11 +0500

    fix single attach control's styles and add another color on delete icon by hover

--- packages/react-ui/internal/FileAttacherBase/UploadFileList/UploadFileList.tsx
+++ packages/react-ui/internal/FileAttacherBase/UploadFileList/UploadFileList.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { UploadFile } from '../UploadFile/UploadFile';
 import { UploadFilesContext } from '../UploadFilesContext';
+import { jsStyles } from './UploadFileList.styles';
 
 export const UploadFileList = () => {
   const {files} = useContext(UploadFilesContext);
@@ -8,7 +9,9 @@ export const UploadFileList = () => {
   return (
     <div>
       {files.map((file) => (
-          <UploadFile key={file.id} file={file} showSize />
+        <div key={file.id} className={jsStyles.fileWrapper()}>
+          <UploadFile file={file} showSize />
+        </div>
       ))}
     </div>
   );
