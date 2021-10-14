4c91e5c2d5c8eed81d9ef9853b9253fc02796787
ishmatova <ishmatova@skbkontur.ru>
2021-10-14 10:28:54 +0500

    chore(GlobalLoader): implement stories

---
+++ packages/react-ui/components/GlobalLoader/__stories__/GlobalLoader.stories.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+
+import { Story } from '../../../typings/stories';
+import { GlobalLoader } from '../GlobalLoader';
+import { Button } from '../../Button';
+import { Modal } from '../../Modal';
+
+function GlobalLoaderWithProps() {
+  const [error, setError] = React.useState(false);
+  const [active, setActive] = React.useState(false);
+
+  return (
+    <div>
+      <Button onClick={showGlobalLoaderWithProps}>Show Global Loader</Button>
+      <GlobalLoader expectedResponseTime={2000} delayBeforeShow={0} active={active} rejected={error} />
+    </div>
+  );
+
+  function showGlobalLoaderWithProps() {
+    setTimeout(() => {
+      setActive(true);
+    }, 1000);
+
+    setTimeout(() => {
+      setError(true);
+    }, 10000);
+
+    setTimeout(() => {
+      setActive(false);
+    }, 30000);
+  }
+}
+
+function GlobalLoaderWithStaticMethods() {
+  return (
+    <div>
+      <Button onClick={showGlobalLoader}>Start</Button>
+      <Button onClick={sendSuccess}>Success</Button>
+      <Button onClick={sendError}>Error</Button>
+      <GlobalLoader expectedResponseTime={2000} />
+    </div>
+  );
+
+  function showGlobalLoader() {
+    GlobalLoader.start();
+  }
+
+  function sendSuccess() {
+    GlobalLoader.done();
+  }
+
+  function sendError() {
+    GlobalLoader.reject();
+  }
+}
+
+function GlobalLoaderInsideModalBody() {
+  const [opened, setOpened] = React.useState(false);
+
+  return (
+    <div>
+      {opened && renderModal()}
+      <Button onClick={open}>Open</Button>
+    </div>
+  );
+
+  function showGlobalLoader() {
+    GlobalLoader.start();
+  }
+
+  function sendSuccess() {
+    GlobalLoader.done();
+  }
+
+  function sendError() {
+    GlobalLoader.reject();
+  }
+
+  function renderModal() {
+    return (
+      <Modal onClose={close}>
+        <Modal.Header>Title</Modal.Header>
+        <Modal.Body style={{ position: 'relative' }}>
+          <GlobalLoader expectedResponseTime={2000} />
+          <p>Use rxjs operators with react hooks</p>
+          <Button onClick={showGlobalLoader}>Start</Button>
+          <Button onClick={sendSuccess}>Success</Button>
+          <Button onClick={sendError}>Error</Button>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={close}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  function open() {
+    setOpened(true);
+  }
+
+  function close() {
+    setOpened(false);
+  }
+}
+
+export default { title: 'GlobalLoader' };
+
+export const GlobalLoaderWithPropsContent: Story = () => <GlobalLoaderWithProps />;
+GlobalLoaderWithPropsContent.storyName = 'with props';
+
+export const GlobalLoaderWithStaticMethodsContent: Story = () => <GlobalLoaderWithStaticMethods />;
+GlobalLoaderWithStaticMethodsContent.storyName = 'with static methods';
+
+export const GlobalLoaderInsideModalBodyContent: Story = () => <GlobalLoaderInsideModalBody />;
+GlobalLoaderInsideModalBodyContent.storyName = 'inside parent';
