80e5606a43d3d8b3f1cc1d9c4bee86a3f549c7cc
mozalov <mozalov@skbkontur.ru>
2021-10-19 19:56:42 +0500

    fix handleSelect for sync control and add fixme

--- packages/react-ui/components/FileUploader/FileUploader.tsx
+++ packages/react-ui/components/FileUploader/FileUploader.tsx
@@ -33,6 +33,8 @@ const _FileUploader = React.forwardRef<FileUploaderControlRef, IFileUploaderProp
 
   const [fileErrors, setFileErrors] = useState<IFileUploaderFileError[]>([]);
 
+  const isAsync = !!request;
+
   const switchToLoading = useCallback(
     (fileId: string) => {
       setFileStatus(fileId, UploadFileStatus.Loading);
@@ -68,12 +70,22 @@ const _FileUploader = React.forwardRef<FileUploaderControlRef, IFileUploaderProp
         switchToError(id);
       }
     },
-    [request, switchToSuccess, switchToLoading],
+    [request, switchToSuccess, switchToLoading, switchToError],
   );
+
+  // FIXME @mozalov: подумать, мб стоит некоторый код перегруппировать
+
+  // FIXME @mozalov: кажется handleSelect все же handleSelect и верхние методы лучше оставить тут.
+  //  чтобы можно было валидировать дропзону по-другому
+
+  // FIXME @mozalov: подумать, а как валидировать дропзону и уже от этого отталкиваться в плане выноса общих механизмов валидации
+  // FIXME @mozalov: для дропзоны нуно поддержать getFileValidationText
 
   const handleSelect = useCallback(
     (files: IUploadFile[]) => {
       onSelect?.(files);
+
+      // FIXME @mozalov: норм, что сейчас не валидируем файлы при error?
 
       if (error) {
         return;
@@ -83,14 +95,13 @@ const _FileUploader = React.forwardRef<FileUploaderControlRef, IFileUploaderProp
         const validationMessage = getFileValidationText && (await getFileValidationText(file));
 
         if (!validationMessage) {
-          // FIXME @mozalov: обработеть случай для отсутствия request (синхронный контрол)
-          upload(file);
+          isAsync && upload(file);
         } else {
           setFileErrors((state) => [...state, { fileId: file.id, message: validationMessage }]);
         }
       });
     },
-    [upload, error, getFileValidationText, onSelect],
+    [upload, error, getFileValidationText, onSelect, isAsync],
   );
 
   useValidationSetter(fileErrors);
