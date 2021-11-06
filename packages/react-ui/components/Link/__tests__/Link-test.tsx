97cbc2d762d9bf52ea00ba47d7ebe7f2ace720a4
JackUait <jackuait@bk.ru>
2021-11-07 01:42:13 +0300

    Refactor

--- packages/react-ui/components/Link/__tests__/Link-test.tsx
+++ packages/react-ui/components/Link/__tests__/Link-test.tsx
@@ -4,51 +4,58 @@ import React from 'react';
 import { LinkProps } from '..';
 import { Link } from '../Link';
 
+const render = (props?: LinkProps) => mount(<Link {...props} />);
+
 describe('Link', () => {
-  describe('event `onClick`', () => {
-    const setUp = (props?: LinkProps) => mount(<Link onClick={onClick} {...props} />);
+  it('calls `onClick` when link clicked', () => {
+    const onClick = jest.fn();
 
-    let onClick: jest.Mock<any, any>;
-    beforeEach(() => {
-      onClick = jest.fn();
+    const wrapper = render({ onClick });
+    wrapper.find('a').simulate('click');
+
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  describe('disabled link', () => {
+    it('does not call `onClick` when link clicked', () => {
+      const onClick = jest.fn();
+
+      const wrapper = render({ onClick, disabled: true });
+      wrapper.find('a').simulate('click');
+
+      expect(onClick).toHaveBeenCalledTimes(0);
     });
 
-    it('calls `onClick` when link clicked', () => {
-      const wrapper = setUp().find('a');
-      wrapper.simulate('click');
+    it('does not call `onClick` when Enter pressed', () => {
+      const onClick = jest.fn();
 
-      expect(onClick.mock.calls.length).toBe(1);
-    });
+      const wrapper = render({ onClick, disabled: true });
+      wrapper.find('a').simulate('focus').simulate('keydown', { key: 'Enter' });
 
-    describe('prop `disabled`', () => {
-      it('does not call `onClick` when disabled', () => {
-        const wrapper = setUp({ disabled: true }).find('a');
-        wrapper.simulate('click');
-
-        expect(onClick.mock.calls.length).toBe(0);
-      });
+      expect(onClick).toHaveBeenCalledTimes(0);
     });
   });
 
   describe('"rel" attribute', () => {
-    const setUp = (props?: LinkProps) => mount(<Link {...props} />);
-
     it("doesn't change if defined in props", () => {
-      const wrapper = setUp({ href: 'https://example.com', rel: 'nofollow' }).find('a');
-      expect(wrapper.prop('rel')).toBe('nofollow');
+      const wrapper = render({ href: 'https://example.com', rel: 'nofollow' });
+
+      expect(wrapper.find('a').prop('rel')).toBe('nofollow');
     });
 
     it("doesn't get filled if there is no href", () => {
-      const wrapper = setUp().find('a');
-      expect(wrapper.prop('rel')).toBe(undefined);
+      const wrapper = render();
+
+      expect(wrapper.find('a').prop('rel')).toBe(undefined);
     });
 
     describe('external hrefs', () => {
       it.each([['https://example.com:8080/home'], ['http://example.com'], ['//example.com/'], ['HTTP://EXAMPLE.COM']])(
         '%s',
         (href) => {
-          const wrapper = setUp({ href: href }).find('a');
-          expect(wrapper.prop('rel')).toBe('noopener noreferrer');
+          const wrapper = render({ href: href });
+
+          expect(wrapper.find('a').prop('rel')).toBe('noopener noreferrer');
         },
       );
     });
@@ -64,8 +71,9 @@ describe('Link', () => {
         ['page.html'],
         ['#anchor'],
       ])('%s', (href) => {
-        const wrapper = setUp({ href: href }).find('a');
-        expect(wrapper.prop('rel')).toBe('noopener');
+        const wrapper = render({ href: href });
+
+        expect(wrapper.find('a').prop('rel')).toBe('noopener');
       });
     });
   });
