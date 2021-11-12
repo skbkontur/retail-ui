a632bcd34a578cebbd57374fdbc48eae290fe7b6
JackUait <jackuait@bk.ru>
2021-11-12 08:58:58 +0500

    refactor(Link): convert Link to function component

--- packages/react-ui/components/Link/Link.tsx
+++ packages/react-ui/components/Link/Link.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { forwardRef, useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Override } from '../../typings/utility-types';
 import { keyListener } from '../../lib/events/keyListener';
-import { Theme } from '../../lib/theming/Theme';
 import { ThemeContext } from '../../lib/theming/ThemeContext';
 import { isExternalLink } from '../../lib/utils';
 import { Spinner } from '../Spinner';
-import { CommonWrapper, CommonProps, CommonWrapperRestProps } from '../../internal/CommonWrapper';
+import { CommonWrapper, CommonProps } from '../../internal/CommonWrapper';
 import { cx } from '../../lib/theming/Emotion';
 
 import { styles } from './Link.styles';
@@ -56,127 +55,116 @@ export interface LinkProps
       }
     > {}
 
-export interface LinkState {
-  focusedByTab: boolean;
-}
-
 /**
  * Элемент ссылки из HTML.
  */
-export class Link extends React.Component<LinkProps, LinkState> {
-  public static __KONTUR_REACT_UI__ = 'Link';
+const Link = forwardRef<HTMLAnchorElement, React.PropsWithChildren<LinkProps>>((props, ref) => {
+  const {
+    disabled = false,
+    href = '',
+    use = 'default',
+    icon,
+    loading,
+    _button,
+    _buttonOpened,
+    rel: relOrigin,
+    tabIndex,
+    children,
+    onClick,
+    ...rest
+  } = props;
 
-  public static propTypes = {
-    disabled: PropTypes.bool,
+  const theme = useContext(ThemeContext);
 
-    href: PropTypes.string,
+  const [focusedByTab, setFocusedByTab] = useState(false);
+  const focused = !disabled && focusedByTab;
 
-    icon: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-
-    use: PropTypes.oneOf(['default', 'success', 'danger', 'grayed']),
-  };
-
-  public static defaultProps = {
-    href: '',
-    use: 'default',
-  };
-
-  public state = {
-    focusedByTab: false,
-  };
-
-  private theme!: Theme;
-  public render(): JSX.Element {
-    return (
-      <ThemeContext.Consumer>
-        {(theme) => {
-          this.theme = theme;
-          return <CommonWrapper {...this.props}>{this.renderMain}</CommonWrapper>;
-        }}
-      </ThemeContext.Consumer>
+  let iconElement: React.ReactNode = null;
+  if (icon) {
+    iconElement = (
+      <span className={styles.icon(theme)}>{loading ? <Spinner caption={null} dimmed type="mini" /> : icon}</span>
     );
   }
 
-  private renderMain = (props: CommonWrapperRestProps<LinkProps>) => {
-    const { disabled, href, icon, use, loading, _button, _buttonOpened, rel: relOrigin, ...rest } = props;
+  let arrow: React.ReactNode = null;
+  if (_button) {
+    arrow = <span className={styles.arrow()} />;
+  }
 
-    let iconElement = null;
-    if (icon) {
-      iconElement = (
-        <span className={styles.icon(this.theme)}>
-          {loading ? <Spinner caption={null} dimmed type="mini" /> : icon}
-        </span>
-      );
+  let rel = relOrigin;
+  if (typeof rel === 'undefined' && href) {
+    rel = `noopener${isExternalLink(href) ? ' noreferrer' : ''}`;
+  }
+
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    if (!href) {
+      event.preventDefault();
     }
-
-    let arrow = null;
-    if (_button) {
-      arrow = <span className={styles.arrow()} />;
-    }
-
-    let rel = relOrigin;
-    if (typeof rel === 'undefined' && href) {
-      rel = `noopener${isExternalLink(href) ? ' noreferrer' : ''}`;
-    }
-
-    const focused = !disabled && this.state.focusedByTab;
-
-    const linkProps = {
-      className: cx({
-        [styles.root(this.theme)]: true,
-        [styles.button(this.theme)]: !!_button,
-        [styles.buttonOpened()]: !!_buttonOpened,
-        [styles.useDefault(this.theme)]: use === 'default',
-        [styles.useSuccess(this.theme)]: use === 'success',
-        [styles.useDanger(this.theme)]: use === 'danger',
-        [styles.useGrayed(this.theme)]: use === 'grayed',
-        [styles.useGrayedFocus(this.theme)]: use === 'grayed' && focused,
-        [styles.focus(this.theme)]: focused,
-        [styles.disabled(this.theme)]: !!disabled || !!loading,
-      }),
-      href,
-      rel,
-      onClick: this._handleClick,
-      onFocus: this._handleFocus,
-      onBlur: this._handleBlur,
-      tabIndex: this.props.tabIndex,
-    };
-    if (disabled) {
-      props.tabIndex = -1;
-    }
-
-    return (
-      <a {...rest} {...linkProps}>
-        {iconElement}
-        {this.props.children}
-        {arrow}
-      </a>
-    );
+    onClick?.(event);
   };
 
-  private _handleFocus = (event: React.FocusEvent<HTMLAnchorElement>) => {
-    if (!this.props.disabled) {
+  const handleFocus = () => {
+    if (!disabled) {
       // focus event fires before keyDown eventlistener
       // so we should check tabPressed in async way
       requestAnimationFrame(() => {
         if (keyListener.isTabPressed) {
-          this.setState({ focusedByTab: true });
+          setFocusedByTab(true);
         }
       });
     }
   };
 
-  private _handleBlur = () => {
-    this.setState({ focusedByTab: false });
+  const handleBlur = () => {
+    setFocusedByTab(false);
   };
 
-  private _handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
-    const { href, onClick, disabled } = this.props;
-    if (!href) {
-      event.preventDefault();
-    }
-    if (onClick && !disabled) {
-      onClick(event);
-    }
+  const linkProps = {
+    className: cx({
+      [styles.root(theme)]: true,
+      [styles.button(theme)]: !!_button,
+      [styles.buttonOpened()]: !!_buttonOpened,
+      [styles.useDefault(theme)]: use === 'default',
+      [styles.useSuccess(theme)]: use === 'success',
+      [styles.useDanger(theme)]: use === 'danger',
+      [styles.useGrayed(theme)]: use === 'grayed',
+      [styles.useGrayedFocus(theme)]: use === 'grayed' && focused,
+      [styles.focus(theme)]: focused,
+      [styles.disabled(theme)]: !!disabled || !!loading,
+    }),
+    href,
+    rel,
+    onClick: handleClick,
+    onFocus: handleFocus,
+    onBlur: handleBlur,
+    tabIndex: disabled || loading ? -1 : tabIndex,
   };
-}
+
+  return (
+    <CommonWrapper {...props}>
+      <a ref={ref} {...rest} {...linkProps}>
+        {iconElement}
+        {children}
+        {arrow}
+      </a>
+    </CommonWrapper>
+  );
+});
+
+Link.propTypes = {
+  disabled: PropTypes.bool,
+  href: PropTypes.string,
+  icon: PropTypes.element,
+  use: PropTypes.oneOf(['default', 'success', 'danger', 'grayed']),
+  _button: PropTypes.bool,
+  _buttonOpened: PropTypes.bool,
+  tabIndex: PropTypes.number,
+  loading: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+const componentName = 'Link';
+Link.displayName = componentName;
+
+const LinkWithName = Object.assign(Link, { __KONTUR_REACT_UI__: componentName });
+export { LinkWithName as Link };
