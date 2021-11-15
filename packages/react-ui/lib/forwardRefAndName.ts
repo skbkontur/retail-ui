6f20272e36293b10f5f342a3a6aa239ae12dce06
JackUait <jackuait@bk.ru>
2021-11-16 02:00:01 +0500

    Put forwardName logic in a separate function

--- packages/react-ui/lib/forwardRefAndName.ts
+++ packages/react-ui/lib/forwardRefAndName.ts
@@ -1,18 +1,23 @@
 import { forwardRef } from 'react';
 
 export interface ReactUIComponentWithRef<T, P>
-  extends React.NamedExoticComponent<React.PropsWithoutRef<P> & React.RefAttributes<T>> {
+  extends React.NamedExoticComponent<React.PropsWithoutRef<P> & React.RefAttributes<T>>,
+    Pick<React.ForwardRefExoticComponent<P>, 'propTypes'> {
   __KONTUR_REACT_UI__: string;
-  propTypes?: React.WeakValidationMap<P>;
+}
+
+function forwardName<ElementType, Props>(
+  name: string,
+  render: ReactUIComponentWithRef<ElementType, Props>,
+): ReactUIComponentWithRef<ElementType, Props> {
+  render.displayName = name;
+  render.__KONTUR_REACT_UI__ = name;
+  return render;
 }
 
 export function forwardRefAndName<ElementType, Props>(
   name: string,
   render: React.ForwardRefRenderFunction<ElementType, Props>,
 ): ReactUIComponentWithRef<ElementType, Props> {
-  const renderWithRef = forwardRef(render) as ReactUIComponentWithRef<ElementType, Props>;
-  renderWithRef.displayName = name;
-  renderWithRef.__KONTUR_REACT_UI__ = name;
-
-  return renderWithRef;
+  return forwardName<ElementType, Props>(name, forwardRef(render) as ReactUIComponentWithRef<ElementType, Props>);
 }
