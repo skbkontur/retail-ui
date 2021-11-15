735264edac81173039107e3be166c487d468b54b
ishmatova <ishmatova@skbkontur.ru>
2021-11-15 09:54:26 +0500

    chore(GlobalLoader): merge animation to one file

--- packages/react-ui/components/GlobalLoader/GlabalLoaderView.tsx
+++ packages/react-ui/components/GlobalLoader/GlabalLoaderView.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import { AnimationKeyframes } from '../../lib/theming/AnimationKeyframes';
 import { Theme } from '../../lib/theming/Theme';
 import { cx } from '../../lib/theming/Emotion';
 import { ZIndex } from '../../internal/ZIndex';
 import { ThemeContext } from '../../lib/theming/ThemeContext';
 
-import { styles } from './GlobalLoaderView.styles';
+import { animations, styles } from './GlobalLoaderView.styles';
 
 interface GlobalLoaderViewProps {
   expectedResponseTime: number;
@@ -32,34 +31,12 @@ export class GlobalLoaderView extends React.Component<GlobalLoaderViewProps> {
       <ZIndex priority="GlobalLoader" className={styles.outer(this.theme)}>
         <div
           className={cx(styles.inner(this.theme), {
-            [styles.fullWidth()]: this.props.status === 'success',
+            [animations.successAnimation()]: this.props.status === 'success',
+            [animations.errorAnimation(this.theme)]: this.props.status === 'error',
+            [animations.standardAnimation(this.theme, this.props.expectedResponseTime)]: !this.props.status,
           })}
-          style={{
-            animation: this.getAnimation(),
-          }}
         />
       </ZIndex>
     );
-  }
-  private getAnimation(): string {
-    const waitingFactor = parseInt(this.theme.globalLoaderWaitingFactor);
-    const transitionDuration = parseInt(this.theme.globalLoaderTransitionDuration);
-    const spinnerAnimationDuration = parseInt(this.theme.globalLoaderSpinnerAnimationDuration);
-
-    if (this.props.status === 'success') {
-      return 'none';
-    }
-    if (this.props.status === 'error') {
-      return `${AnimationKeyframes.globalLoaderMoveToRight()} ${transitionDuration}ms linear, ${spinnerAnimationDuration}ms ${AnimationKeyframes.globalLoaderSpinner()} ${transitionDuration}ms infinite alternate`;
-    }
-    return `${AnimationKeyframes.globalLoaderProgress()} ${this.props.expectedResponseTime}ms linear, ${
-      this.props.expectedResponseTime! * waitingFactor
-    }ms ${AnimationKeyframes.globalLoaderSlowProgress()} ${
-      this.props.expectedResponseTime
-    }ms ease-out, ${transitionDuration}ms ${AnimationKeyframes.globalLoaderMoveToRight()} ${
-      this.props.expectedResponseTime! * (waitingFactor + 1)
-    }ms linear, ${spinnerAnimationDuration}ms ${AnimationKeyframes.globalLoaderSpinner()} ${
-      this.props.expectedResponseTime! * (waitingFactor + 1) + transitionDuration
-    }ms infinite alternate`;
   }
 }
--- packages/react-ui/components/GlobalLoader/GlobalLoaderView.styles.ts
+++ packages/react-ui/components/GlobalLoader/GlobalLoaderView.styles.ts
@@ -1,3 +1,5 @@
+import { keyframes } from '@emotion/css';
+
 import { css, memoizeStyle } from '../../lib/theming/Emotion';
 import { Theme } from '../../lib/theming/Theme';
 
@@ -23,12 +25,77 @@ export const styles = memoizeStyle({
       position: absolute;
       left: 0;
       overflow: hidden;
-      transition: all 1s ease-in-out;
-    `;
-  },
-  fullWidth() {
-    return css`
-      width: 100%;
     `;
   },
 });
+
+const moveToRightAnimation = keyframes`
+      0% {
+        left: 0;
+        width: 100%;
+      }
+      50% {
+        left: 50%;
+        width: 50%;
+      }
+      100% {
+        left: 99%;
+        width: 1%
+      }
+    `;
+const spinnerAnimation = keyframes`
+      0% {
+        left: 0;
+        width: 100%;
+        transform: translateX(50%) scaleX(0.005);
+        animation-timing-function: cubic-bezier(0.895, 0.03, 0.685, 0.22);
+      }
+      50% {
+        left: 0;
+        width: 100%;
+        transform: translateX(0%) scaleX(0.35);
+        animation-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1);
+      }
+      100% {
+        left: 0;
+        width: 100%;
+        transform: translateX(-50%) scaleX(0.005);
+      }
+    `;
+const linearProgressAnimation = keyframes`from { width: 0; }
+        to { width: 80% }`;
+const slowProgressAnimation = keyframes`
+      from { width: 80% }
+      to { width: 100% }
+    `;
+
+export const animations = {
+  successAnimation() {
+    return css`
+      width: 100%;
+      animation: none;
+    `;
+  },
+  errorAnimation(t: Theme) {
+    const transitionDuration = parseInt(t.globalLoaderTransitionDuration);
+    const spinnerAnimationDuration = parseInt(t.globalLoaderSpinnerAnimationDuration);
+
+    return css`
+      animation: ${moveToRightAnimation} ${transitionDuration}ms linear,
+        ${spinnerAnimationDuration}ms ${spinnerAnimation} ${transitionDuration}ms infinite alternate;
+    `;
+  },
+  standardAnimation(t: Theme, expectedTime: number) {
+    const transitionDuration = parseInt(t.globalLoaderTransitionDuration);
+    const spinnerAnimationDuration = parseInt(t.globalLoaderSpinnerAnimationDuration);
+    const waitingFactor = parseInt(t.globalLoaderWaitingFactor);
+
+    return css`
+      animation: ${linearProgressAnimation} ${expectedTime}ms linear,
+        ${expectedTime! * waitingFactor}ms ${slowProgressAnimation} ${expectedTime}ms ease-out,
+        ${transitionDuration}ms ${moveToRightAnimation} ${expectedTime * (waitingFactor + 1)}ms linear,
+        ${spinnerAnimationDuration}ms ${spinnerAnimation} ${expectedTime * (waitingFactor + 1) + transitionDuration}ms
+          infinite alternate;
+    `;
+  },
+};
