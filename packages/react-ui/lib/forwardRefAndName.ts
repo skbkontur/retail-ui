279929917516c4984d064ec88c37d9bdfdd01c57
JackUait <jackuait@bk.ru>
2021-11-15 22:46:33 +0500

    Fix type & optimize import

--- packages/react-ui/lib/forwardRefAndName.ts
+++ packages/react-ui/lib/forwardRefAndName.ts
@@ -1,16 +1,18 @@
-import React from 'react';
+import { forwardRef } from 'react';
 
-export interface CompoundedComponent<T> extends React.ForwardRefExoticComponent<T> {
+export interface ReactUIComponentWithRef<T, P>
+  extends React.NamedExoticComponent<React.PropsWithoutRef<P> & React.RefAttributes<T>> {
   __KONTUR_REACT_UI__: string;
+  propTypes?: React.WeakValidationMap<P>;
 }
 
 export function forwardRefAndName<ElementType, Props>(
   name: string,
   render: React.ForwardRefRenderFunction<ElementType, Props>,
-) {
-  const renderWithRef = React.forwardRef(render) as CompoundedComponent<Props>;
-
+): ReactUIComponentWithRef<ElementType, Props> {
+  const renderWithRef = forwardRef(render) as ReactUIComponentWithRef<ElementType, Props>;
   renderWithRef.displayName = name;
   renderWithRef.__KONTUR_REACT_UI__ = name;
+
   return renderWithRef;
 }
