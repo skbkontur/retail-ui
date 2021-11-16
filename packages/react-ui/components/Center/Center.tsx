7cee0fa321c49aa555333d00ff0a7c9b5a4386e6
JackUait <jackuait@bk.ru>
2021-11-16 23:55:00 +0500

    Add class wrapper

--- packages/react-ui/components/Center/Center.tsx
+++ packages/react-ui/components/Center/Center.tsx
@@ -1,10 +1,11 @@
-import React, { forwardRef } from 'react';
+import React from 'react';
 
 import { Override } from '../../typings/utility-types';
 import { CommonProps, CommonWrapper } from '../../internal/CommonWrapper';
 import { cx } from '../../lib/theming/Emotion';
 
 import { styles } from './Center.styles';
+import { forwardRefAndName } from '../../lib/forwardRefAndName';
 
 export type HorizontalAlign = 'left' | 'center' | 'right';
 
@@ -22,30 +23,37 @@ export interface CenterProps
       }
     > {}
 
+const CenterFuture = forwardRefAndName<HTMLDivElement, React.PropsWithChildren<CenterProps>>(
+  'CenterFuture',
+  (props, ref) => {
+    return (
+      <CommonWrapper {...props}>
+        <div
+          {...props}
+          ref={ref}
+          className={cx({
+            [styles.root()]: true,
+            [styles.rootAlignLeft()]: props.align === 'left',
+            [styles.rootAlignRight()]: props.align === 'right',
+          })}
+        >
+          <span className={styles.spring()} />
+          <span className={styles.container()}>{props.children}</span>
+        </div>
+      </CommonWrapper>
+    );
+  },
+);
+
 /**
  * Контейнер, который центрирует элементы внутри себя.
  */
-const Center = forwardRef<HTMLDivElement, React.PropsWithChildren<CenterProps>>(({ align, children, ...rest }, ref) => {
-  return (
-    <CommonWrapper {...rest}>
-      <div
-        ref={ref}
-        {...rest}
-        className={cx({
-          [styles.root()]: true,
-          [styles.rootAlignLeft()]: align === 'left',
-          [styles.rootAlignRight()]: align === 'right',
-        })}
-      >
-        <span className={styles.spring()} />
-        <span className={styles.container()}>{children}</span>
-      </div>
-    </CommonWrapper>
-  );
-});
+export class Center extends React.Component<React.PropsWithChildren<CenterProps>> {
+  constructor(props: React.PropsWithChildren<CenterProps>) {
+    super(props);
+  }
 
-const componentName = 'Center';
-Center.displayName = componentName;
-
-const CenterWithName = Object.assign(Center, { __KONTUR_REACT_UI__: 'Center' });
-export { CenterWithName as Center };
+  render() {
+    return <CenterFuture {...this.props} />;
+  }
+}
