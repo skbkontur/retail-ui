7bd5b05e223ff3271f2fbc6a822c9e4afe02084c
ishmatova <ishmatova@skbkontur.ru>
2021-11-16 14:33:57 +0500

    chore(select): add handleKey to search div

--- packages/react-ui/components/Select/Select.tsx
+++ packages/react-ui/components/Select/Select.tsx
@@ -389,7 +389,7 @@ export class Select<TValue = {}, TItem = {}> extends React.Component<SelectProps
 
   private renderMenu(): React.ReactNode {
     const search = this.props.search ? (
-      <div className={styles.search()}>
+      <div className={styles.search()} onKeyDown={this.handleKey}>
         <Input ref={this.focusInput} onValueChange={this.handleSearch} width="100%" />
       </div>
     ) : null;
