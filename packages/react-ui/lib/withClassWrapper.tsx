af4ce682a84483ff2a8a9526e274974e3fd001bb
JackUait <jackuait@bk.ru>
2021-11-17 14:59:49 +0500

    Change name Wrapper to ClassWrapper

--- packages/react-ui/lib/withClassWrapper.tsx
+++ packages/react-ui/lib/withClassWrapper.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export function withClassWrapper<P>(name: string, RFC: React.FunctionComponent<P>) {
-  return class Wrapper extends React.Component<P> {
+  return class ClassWrapper extends React.Component<P> {
     public static __KONTUR_REACT_UI__ = name;
     public static displayName = name;
 
     public static FC = RFC;
 
     render() {
-      return <Wrapper.FC {...this.props} />;
+      return <ClassWrapper.FC {...this.props} />;
     }
   };
 }
