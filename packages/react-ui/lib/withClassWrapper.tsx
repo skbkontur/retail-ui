afaac9d41730d3980108b167ad597ad127b8b958
JackUait <jackuait@bk.ru>
2021-11-19 17:52:23 +0500

    Add documentation

--- packages/react-ui/lib/withClassWrapper.tsx
+++ packages/react-ui/lib/withClassWrapper.tsx
@@ -27,6 +27,14 @@ class ClassWrapper<P> extends React.Component<P> {
   }
 }
 
+/**
+ * HOC for moving from Class to Functional components.
+ *
+ * Should be used to remove distinctions between Class and Functional refs.
+ *
+ * @param RFC Functional component wrapped in forwardRefAndName decorator.
+ * @returns Class component that wraps Functional component.
+ */
 export function withClassWrapper<T, P>(RFC: ReactUIComponentWithRef<T, P>) {
   const fullName = getDisplayName(RFC);
   const nameWithoutPostfix = removePostfix(fullName, /FC$/);
