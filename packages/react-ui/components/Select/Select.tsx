cb8ccc80b5ec475c12dd850705325c8ae8b9d44f
HelenaIsh <elena-ishmatova@mail.ru>
2021-11-25 12:44:18 +0500

    chore(select): add highlighting first element on search

--- packages/react-ui/components/Select/Select.tsx
+++ packages/react-ui/components/Select/Select.tsx
@@ -503,6 +503,7 @@ export class Select<TValue = {}, TItem = {}> extends React.Component<SelectProps
 
   private handleSearch = (value: string) => {
     this.setState({ searchPattern: value });
+    this.menu?.highlightItem(1);
   };
 
   private select(value: TValue) {
