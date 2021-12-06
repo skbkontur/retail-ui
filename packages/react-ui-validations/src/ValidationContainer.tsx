9d16981b762eb254a66f549b914aef8a7e62bafc
lossir <m.pakhomov@skbkontur.ru>
2021-12-06 15:56:03 +0500

    refactor(validations): remove argument `withoutIndependent` (2)

--- packages/react-ui-validations/src/ValidationContainer.tsx
+++ packages/react-ui-validations/src/ValidationContainer.tsx
@@ -37,18 +37,18 @@ export class ValidationContainer extends React.Component<ValidationContainerProp
 
   private childContext: ValidationContext | null = null;
 
-  public async submit(withoutFocus = false, withoutIndependent = false): Promise<void> {
+  public async submit(withoutFocus = false): Promise<void> {
     if (!this.childContext) {
       throw new Error('childContext is not defined');
     }
-    await this.childContext.validate(withoutFocus, withoutIndependent);
+    await this.childContext.validate(withoutFocus);
   }
 
-  public validate(withoutFocus = false, withoutIndependent = false): Promise<boolean> {
+  public validate(withoutFocus = false): Promise<boolean> {
     if (!this.childContext) {
       throw new Error('childContext is not defined');
     }
-    return this.childContext.validate(withoutFocus, withoutIndependent);
+    return this.childContext.validate(withoutFocus);
   }
 
   public render() {
