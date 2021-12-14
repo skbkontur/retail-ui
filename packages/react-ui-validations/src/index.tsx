3c1114dec23fbabd25b2cdbff2f1c299f9c94d17
Anna Khlutkova <khlutkova@skbkontur.ru>
2021-12-14 12:45:43 +0500

    fix: rewrite legasy context for validations

--- packages/react-ui-validations/src/index.tsx
+++ packages/react-ui-validations/src/index.tsx
@@ -1,15 +1,15 @@
 import { text, tooltip } from './ErrorRenderer';
 import { ValidationContainer, ValidationContainerProps } from './ValidationContainer';
-import { ValidationContext, ValidationContextProps } from './ValidationContext';
 import { TooltipPosition, ValidationTooltip, ValidationTooltipProps } from './ValidationTooltip';
 import { RenderErrorMessage, Validation, ValidationBehaviour } from './ValidationWrapperInternal';
 import { ValidationWrapper, ValidationInfo, ValidationWrapperProps } from './ValidationWrapper';
+import { ValidationContext, ValidationContextType } from './ValidationContextWrapper';
 
 export {
   ValidationContainer,
   ValidationContainerProps,
   ValidationContext,
-  ValidationContextProps,
+  ValidationContextType,
   ValidationWrapper as ValidationWrapperV1,
   ValidationWrapperProps as ValidationWrapperV1Props,
   RenderErrorMessage,
