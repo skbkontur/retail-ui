feda016946620a73095fe26289a1560a28bfde35
Anna Khlutkova <khlutkova@skbkontur.ru>
2021-12-17 17:26:49 +0500

    chore

--- packages/react-ui/lib/rootNode/getRootNode.ts
+++ packages/react-ui/lib/rootNode/getRootNode.ts
@@ -4,16 +4,19 @@ import React from 'react';
 import { Nullable } from '../../typings/utility-types';
 import { isHTMLElement } from '../SSRSafe';
 import { canUseDOM } from '../client';
+import { isFunction } from '../utils';
 
 export const getRootNode = (instance: Nullable<React.ReactInstance>): Nullable<HTMLElement> => {
+  if (!canUseDOM) return null;
   if (isHTMLElement(instance) || instance === null) {
     return instance;
   }
 
   const instanceAsAny = instance as any;
-  if (instanceAsAny && instanceAsAny.getRootNode && typeof instanceAsAny.getRootNode === 'function') {
+  if (instanceAsAny && isFunction(instanceAsAny.getRootNode)) {
     return instanceAsAny.getRootNode();
   }
-  const node = canUseDOM && findDOMNode(instance);
+
+  const node = findDOMNode(instance);
   return node instanceof HTMLElement ? node : null;
 };
