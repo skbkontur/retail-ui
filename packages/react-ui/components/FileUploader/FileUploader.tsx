00b090347dce724b4fb08be44b78a62751126f8c
mozalov <mozalov@skbkontur.ru>
2021-12-17 17:45:17 +0500

    fix drag over for error and warning

--- packages/react-ui/components/FileUploader/FileUploader.tsx
+++ packages/react-ui/components/FileUploader/FileUploader.tsx
@@ -193,10 +193,10 @@ const _FileUploader = React.forwardRef<FileUploaderRef, _FileUploaderProps>((pro
 
   const uploadButtonClassNames = cx(jsStyles.uploadButton(theme), {
     [jsStyles.uploadButtonFocus(theme)]: focusedByTab,
-    [jsStyles.dragOver()]: isDraggable && !disabled,
     [jsStyles.disabled(theme)]: disabled,
     [jsStyles.warning(theme)]: !!warning,
     [jsStyles.error(theme)]: !!error,
+    [jsStyles.dragOver()]: isDraggable && !disabled,
   });
 
   const uploadButtonWrapperClassNames = cx({
