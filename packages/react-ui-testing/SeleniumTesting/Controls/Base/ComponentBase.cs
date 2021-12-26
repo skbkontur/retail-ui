2408f5094fbe3aecc8b2b26f1733b6bce4e17bd0
Артем Кузнецов <askuznetsov@skbkontur.ru>
2021-12-26 12:09:11 +0500

    сделал базовый класс для контролов

---
+++ packages/react-ui-testing/SeleniumTesting/Controls/Base/ComponentBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using Kontur.Selone.Extensions;
+using Kontur.Selone.Properties;
+using OpenQA.Selenium;
+
+namespace SKBKontur.SeleniumTesting.Controls.Base
+{
+    public class ComponentBase : ISearchContext
+    {
+        protected ComponentBase(ISearchContext searchContext, By @by)
+        {
+            By = @by;
+            Container = searchContext.SearchElement(@by);
+        }
+
+        public IWebElement Container { get; }
+        private By By { get; }
+
+        public IProp<bool> IsPresent => Container.Present();
+        public IProp<string> Text => Container.Text();
+
+        public IWebElement FindElement(By @by)
+        {
+            return Container.FindElement(by);
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(By @by)
+        {
+            return Container.FindElements(by);
+        }
+
+        public void Click()
+        {
+            this.Container.Click();
+        }
+    }
+}
