ecefada396f4056f6734b6bfd6dac14bc7d2894e
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-01-11 18:33:04 +0500

    chore(DarkTheme): ability to determinate dark theme

--- packages/react-ui/lib/theming/ThemeHelpers.ts
+++ packages/react-ui/lib/theming/ThemeHelpers.ts
@@ -1,3 +1,5 @@
+import { Theme, ThemeIn } from './Theme';
+
 export const exposeGetters = <T extends object>(theme: T): T => {
   const descriptors = Object.getOwnPropertyDescriptors(theme);
   Object.keys(descriptors).forEach((key) => {
@@ -8,4 +10,22 @@ export const exposeGetters = <T extends object>(theme: T): T => {
     }
   });
   return theme;
+};
+
+export const REACT_UI_DARK_THEME_KEY = '__IS_REACT_UI_DARK_THEME__';
+
+export const isDarkTheme = (theme: Theme | ThemeIn): boolean => {
+  //@ts-ignore
+  return theme[REACT_UI_DARK_THEME_KEY] === true;
+};
+
+export const markAsDarkTheme = <T extends object>(theme: T): T => {
+  return Object.create(theme, {
+    [REACT_UI_DARK_THEME_KEY]: {
+      value: true,
+      writable: false,
+      enumerable: false,
+      configurable: false,
+    },
+  });
 };
