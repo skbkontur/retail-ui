fc71454b6e4efbb5c62f364d5612bf4005d544e5
ishmatova <ishmatova@skbkontur.ru>
2022-01-12 15:24:39 +0500

    chore(GlobalLoader): add story with timer

--- packages/react-ui/components/GlobalLoader/__stories__/GlobalLoader.stories.tsx
+++ packages/react-ui/components/GlobalLoader/__stories__/GlobalLoader.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ThemeContext, ThemeFactory } from '@skbkontur/react-ui';
+import { Select, ThemeContext, ThemeFactory, Toast } from '@skbkontur/react-ui';
 
 import { Story } from '../../../typings/stories';
 import { GlobalLoader } from '../GlobalLoader';
@@ -114,6 +114,50 @@ function GlobalLoaderInsideModalBody() {
   }
 }
 
+function GlobalLoaderWithTimer() {
+  const [active, setActive] = React.useState(false);
+  const [time, setTime] = React.useState(1);
+  const [timerTime, setTimerTime] = React.useState(0);
+  const items = [0.5, 1, 2, 4, 8, 16];
+  let timer: ReturnType<typeof setInterval> | null = null;
+  return (
+    <div>
+      <div>
+        Выбирите время, через которое придет ответ от сервера:{' '}
+        <Select items={items} value={time} onValueChange={setTime} />
+        Прошло: {timerTime / 1000} секунд
+      </div>
+      <Button onClick={startGlobalLoader}> Запустить Глобальный лоадер </Button>
+      <GlobalLoader expectedResponseTime={2000} delayBeforeShow={0} active={active} disableAnimations={false} />
+      <Toast />
+    </div>
+  );
+  function startGlobalLoader() {
+    setActive(true);
+    setTimerTime(0);
+    startTimer();
+    setTimeout(() => {
+      setActive(false);
+      Toast.push('Загрузка завершена');
+    }, time * 1000);
+  }
+  function startTimer() {
+    if (!timer) {
+      let currentTime = 0;
+      timer = setInterval(() => {
+        setTimerTime(currentTime);
+        currentTime += 100;
+        if (currentTime > time * 1000) {
+          if (timer) {
+            clearInterval(timer);
+            timer = null;
+          }
+        }
+      }, 100);
+    }
+  }
+}
+
 export default { title: 'GlobalLoader' };
 
 export const GlobalLoaderWithPropsContent: Story = () => <GlobalLoaderWithProps />;
@@ -127,3 +171,7 @@ GlobalLoaderWithStaticMethodsContent.parameters = { creevey: { skip: true } };
 export const GlobalLoaderInsideModalBodyContent: Story = () => <GlobalLoaderInsideModalBody />;
 GlobalLoaderInsideModalBodyContent.storyName = 'inside parent';
 GlobalLoaderInsideModalBodyContent.parameters = { creevey: { skip: true } };
+
+export const GlobalLoaderWithTimerContent: Story = () => <GlobalLoaderWithTimer />;
+GlobalLoaderWithTimerContent.storyName = 'with timer';
+GlobalLoaderWithTimerContent.parameters = { creevey: { skip: true } };
