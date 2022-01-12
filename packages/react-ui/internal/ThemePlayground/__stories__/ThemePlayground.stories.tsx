b583f321fe85da48e6080e95fd990f4bd4849d37
HelenaIsh <elena-ishmatova@mail.ru>
2022-01-12 09:34:12 +0500

    feat(theme): add screenshots for theme playground

--- packages/react-ui/internal/ThemePlayground/__stories__/ThemePlayground.stories.tsx
+++ packages/react-ui/internal/ThemePlayground/__stories__/ThemePlayground.stories.tsx
@@ -13,9 +13,29 @@ Playground.parameters = {
   creevey: {
     skip: [
       {
-        tests: ['default theme top', 'flat theme top', 'dark theme top'],
-        in: ['ie11', 'ie118px', 'ie11Flat8px', 'ie11Dark'],
-        reason: 'flacky input outlines',
+        tests: [
+          'default theme top',
+          'default theme bottom',
+          'dark theme top',
+          'dark theme bottom',
+          'default old theme top',
+          'default old theme bottom',
+          'flat old theme top',
+          'flat old theme bottom',
+        ],
+        in: [
+          'ie11',
+          'ie118px',
+          'ie11Flat8px',
+          'ie11Dark',
+          'chrome8px',
+          'chromeFlat8px',
+          'chromeDark',
+          'firefox8px',
+          'firefoxFlat8px',
+          'firefoxDark',
+        ],
+        reason: 'repeating tests',
       },
     ],
     tests: {
@@ -81,24 +101,6 @@ Playground.parameters = {
           document.documentElement.scrollTop = document.documentElement.offsetHeight;
         });
         await this.expect(await this.browser.takeScreenshot()).to.matchImage('flat old theme bottom');
-      },
-      async ['dark old theme top']() {
-        await this.browser
-          .actions({ bridge: true })
-          .click(this.browser.findElement({ css: '[data-prop-id="darkOld"]' }))
-          .perform();
-        await delay(500);
-        await this.expect(await this.browser.takeScreenshot()).to.matchImage('dark old theme top');
-      },
-      async ['dark old theme bottom']() {
-        await this.browser
-          .actions({ bridge: true })
-          .click(this.browser.findElement({ css: '[data-prop-id="darkOld"]' }))
-          .perform();
-        await this.browser.executeScript(function () {
-          document.documentElement.scrollTop = document.documentElement.offsetHeight;
-        });
-        await this.expect(await this.browser.takeScreenshot()).to.matchImage('dark old theme bottom');
       },
     },
   },
