344dc20d4381af5fdd51b52a62ba75f9daf8aa1f
Евгений Пятков <jackuait@bk.ru>
2022-01-17 18:46:13 +0300

    feat: enable addon-essentials

--- packages/react-ui/.storybook/main.ts
+++ packages/react-ui/.storybook/main.ts
@@ -1,19 +1,5 @@
 module.exports = {
-  addons: [
-    'creevey',
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        docs: false,
-        controls: false,
-        backgrounds: false,
-        measure: false,
-        outline: false,
-      },
-    },
-  ],
+  addons: ['creevey', '@storybook/addon-links', '@storybook/addon-a11y', '@storybook/addon-essentials'],
   stories: ['../components/**/*.stories.tsx', '../internal/**/*.stories.tsx'],
   typescript: {
     reactDocgen: 'none',
