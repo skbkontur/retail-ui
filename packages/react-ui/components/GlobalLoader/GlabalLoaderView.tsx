2bc70cd759401183ec94e44e1200995607f2a7b1
HelenaIsh <ishmatova@skbkontur.ru>
2022-01-20 16:19:54 +0500

    chore(GlobalLoader): make GlobalLoaderView to be a functional component

--- packages/react-ui/components/GlobalLoader/GlabalLoaderView.tsx
+++ packages/react-ui/components/GlobalLoader/GlabalLoaderView.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { Theme } from '../../lib/theming/Theme';
 import { cx } from '../../lib/theming/Emotion';
 import { ZIndex } from '../../internal/ZIndex';
 import { ThemeContext } from '../../lib/theming/ThemeContext';
@@ -15,44 +14,36 @@ interface GlobalLoaderViewProps {
   overtime: number;
 }
 
-export class GlobalLoaderView extends React.Component<GlobalLoaderViewProps> {
-  private theme!: Theme;
-  private readonly myRef: React.RefObject<HTMLDivElement>;
-  constructor(props: GlobalLoaderViewProps) {
-    super(props);
-    this.myRef = React.createRef();
+export const GlobalLoaderView = ({
+  expectedResponseTime,
+  delayBeforeHide,
+  status,
+  disableAnimations,
+  overtime,
+}: GlobalLoaderViewProps) => {
+  const globalLoaderViewRef: React.RefObject<HTMLDivElement> | null = React.useRef(null);
+  const theme = React.useContext(ThemeContext);
+  let currentWidth = 0;
+  let currentLeftPosition = 0;
+  if (globalLoaderViewRef) {
+    currentWidth = globalLoaderViewRef.current?.getBoundingClientRect().width || 0;
+    currentLeftPosition = globalLoaderViewRef.current?.getBoundingClientRect().left || 0;
   }
-
-  public render() {
-    return (
-      <ThemeContext.Consumer>
-        {(theme) => {
-          this.theme = theme;
-          return this.renderMain();
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-
-  private renderMain() {
-    const currentWidth = this.myRef.current?.getBoundingClientRect().width || 0;
-    const currentLeftPosition = this.myRef.current?.getBoundingClientRect().left || 0;
-    return (
-      <ZIndex priority="GlobalLoader" className={styles.outer(this.theme)}>
-        <div
-          ref={this.myRef}
-          className={cx(styles.inner(this.theme), {
-            [styles.error()]: this.props.status === 'error',
-            [animations.successAnimation(this.props.delayBeforeHide, currentWidth, currentLeftPosition)]:
-              !this.props.disableAnimations && this.props.status === 'success',
-            [styles.successWithoutAnimation()]: this.props.disableAnimations && this.props.status === 'success',
-            [animations.errorAnimation(this.theme)]: !this.props.disableAnimations && this.props.status === 'error',
-            [styles.errorWithoutAnimation()]: this.props.disableAnimations && this.props.status === 'error',
-            [animations.standardAnimation(this.theme, this.props.expectedResponseTime, this.props.overtime)]:
-              !this.props.disableAnimations && this.props.status === 'standard',
-          })}
-        />
-      </ZIndex>
-    );
-  }
-}
+  return (
+    <ZIndex priority="GlobalLoader" className={styles.outer(theme)}>
+      <div
+        ref={globalLoaderViewRef}
+        className={cx(styles.inner(theme), {
+          [styles.error()]: status === 'error',
+          [animations.successAnimation(delayBeforeHide, currentWidth, currentLeftPosition)]:
+            !disableAnimations && status === 'success',
+          [styles.successWithoutAnimation()]: disableAnimations && status === 'success',
+          [animations.errorAnimation(theme)]: !disableAnimations && status === 'error',
+          [styles.errorWithoutAnimation()]: disableAnimations && status === 'error',
+          [animations.standardAnimation(theme, expectedResponseTime, overtime)]:
+            !disableAnimations && status === 'standard',
+        })}
+      />
+    </ZIndex>
+  );
+};
