f50cd2db6ef2fe17d6e6ca89ff0eec91d92c963e
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-01-27 17:44:14 +0500

    chore(react-ui): add the dark theme mark (#2746)

--- packages/react-ui/lib/theming/themes/DarkTheme.ts
+++ packages/react-ui/lib/theming/themes/DarkTheme.ts
@@ -1,4 +1,5 @@
 import { DarkThemeInternal } from '../../../internal/themes/DarkTheme';
 import { ThemeFactory } from '../ThemeFactory';
+import { markAsDarkTheme } from '../ThemeHelpers';
 
-export const DARK_THEME = ThemeFactory.create({}, DarkThemeInternal);
+export const DARK_THEME = ThemeFactory.create({}, markAsDarkTheme(DarkThemeInternal));
