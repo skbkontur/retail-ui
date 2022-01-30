ffe744a6e7605bd773ca39caa07adbc190bdaa22
Евгений Пятков <jackuait@bk.ru>
2022-01-30 17:35:15 +0300

    refactor(RadioGroupContext): change type definition keyword

--- packages/react-ui/components/RadioGroup/RadioGroupContext.tsx
+++ packages/react-ui/components/RadioGroup/RadioGroupContext.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export interface RadioGroupContextType<T> {
+export type RadioGroupContextType<T> = {
   activeItem: T | undefined;
   onSelect: (value: T) => void;
   name: string;
   disabled: boolean | undefined;
   error: boolean | undefined;
   warning: boolean | undefined;
-}
+};
 
 export const RadioGroupContext = React.createContext<RadioGroupContextType<any>>({
   activeItem: undefined,
