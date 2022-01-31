0a4bd6ab3f84cc14d2fce94697f5b6b22c22f73d
Евгений Пятков <jackuait@bk.ru>
2022-01-31 17:10:58 +0300

    test(test-setup): make jest-dom part of test-setup

--- packages/react-ui/test-setup.js
+++ packages/react-ui/test-setup.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len,react/no-deprecated */
 import 'core-js/stable';
+import '@testing-library/jest-dom/extend-expect';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
