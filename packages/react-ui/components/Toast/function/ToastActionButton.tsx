b2ccd4b596cb2aca1e093b4ef4b0c2b37a15c346
Евгений Пятков <jackuait@bk.ru>
2022-02-02 00:40:07 +0300

    test: fix screenshot tests

--- packages/react-ui/components/Toast/function/ToastActionButton.tsx
+++ packages/react-ui/components/Toast/function/ToastActionButton.tsx
@@ -1,10 +1,8 @@
 import React, { useContext } from 'react';
 
-import { cx } from '../../../lib/theming/Emotion';
 import { ThemeContext } from '../../../lib/theming/ThemeContext';
 import { styles } from '../ToastView.styles';
 
-import { resetStyles } from './reset.styles';
 import { ToastProps } from './Toast';
 
 export type ToastActionButtonProps = Pick<ToastProps, 'action'>;
@@ -13,12 +11,8 @@ export const ToastActionButton = ({ action }: ToastActionButtonProps) => {
   const theme = useContext(ThemeContext);
 
   return (
-    <button
-      data-tid="ToastView__action"
-      className={cx(resetStyles.button(), styles.link(theme))}
-      onClick={action?.handler}
-    >
+    <span data-tid="ToastView__action" className={styles.link(theme)} onClick={action?.handler}>
       {action?.label}
-    </button>
+    </span>
   );
 };
