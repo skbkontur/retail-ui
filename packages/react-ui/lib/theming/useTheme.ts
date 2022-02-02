aa8023bd789de0446e0eae8eb16ae7b1b79f619c
Евгений Пятков <jackuait@bk.ru>
2022-02-02 10:20:51 +0300

    refactor: return theme

--- packages/react-ui/lib/theming/useTheme.ts
+++ packages/react-ui/lib/theming/useTheme.ts
@@ -3,7 +3,5 @@ import { useContext } from 'react';
 import { ThemeContext } from './ThemeContext';
 
 export const useTheme = () => {
-  const theme = useContext(ThemeContext);
-
-  return { theme };
+  return useContext(ThemeContext);
 };
