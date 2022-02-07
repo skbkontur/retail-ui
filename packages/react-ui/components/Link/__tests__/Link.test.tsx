65a2b76a649ee8965748061cd6cfbc9535ae7430
Евгений Пятков <jackuait@bk.ru>
2022-02-08 02:13:17 +0300

    refactor: tests

--- packages/react-ui/components/Link/__tests__/Link.test.tsx
+++ packages/react-ui/components/Link/__tests__/Link.test.tsx
@@ -1,37 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { Link } from '../Link';
 
 describe('Link', () => {
   it('has an interactive link', () => {
-    const { getByRole } = render(<Link href="https://tech.skbkontur.ru/react-ui">children</Link>);
+    render(<Link href="https://tech.skbkontur.ru/react-ui">children</Link>);
 
-    getByRole('link');
+    expect(screen.getByRole('link')).toBeInTheDocument();
   });
 
   it('calls callback when an interactive link is clicked', () => {
     const callback = jest.fn();
-    const { getByRole } = render(
+
+    render(
       <Link onClick={callback} href="https://tech.skbkontur.ru/react-ui">
         children
       </Link>,
     );
 
-    const link = getByRole('link');
+    const link = screen.getByRole('link');
     userEvent.click(link);
 
-    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalled();
   });
 
-  it('calls callback when an non-interactive link is clicked', () => {
+  it('calls callback when a non-interactive link is clicked', () => {
     const callback = jest.fn();
-    const { getByText } = render(<Link onClick={callback}>children</Link>);
+    render(<Link onClick={callback}>children</Link>);
 
-    const link = getByText('children');
+    const link = screen.getByText('children');
     userEvent.click(link);
 
-    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalled();
   });
 });
