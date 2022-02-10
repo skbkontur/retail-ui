dc4e6fe6eaaa1222e9bdecb423a43c8ea60e15e5
Евгений Пятков <jackuait@bk.ru>
2022-02-11 02:47:15 +0300

    chore(eslint): add array-callback-return rule

--- packages/react-ui/lib/utils.ts
+++ packages/react-ui/lib/utils.ts
@@ -144,10 +144,10 @@ export const extractDataProps = <T>(props: T) => {
 
   Object.entries(props).map(([name, value]) => {
     if (name.startsWith('data-')) {
-      dataProps[name] = value;
-    } else {
-      restWithoutDataProps[name] = value;
+      return (dataProps[name] = value);
     }
+
+    return (restWithoutDataProps[name] = value);
   });
 
   return { dataProps, restWithoutDataProps };
