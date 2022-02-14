713b7c403b0e6b6f3d5d49178400f766da386ac2
HelenaIsh <ishmatova@skbkontur.ru>
2022-02-14 10:26:23 +0500

    chore(GlobalLoader): delete usePrevious

--- packages/react-ui/components/GlobalLoader/useWidthAndPosition.ts
+++ packages/react-ui/components/GlobalLoader/useWidthAndPosition.ts
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 
-import { usePrevious } from './usePrevious';
 import { GlobalLoaderViewProps, GlobalLoaderViewRef } from './GlobalLoaderView';
 
 /**
@@ -14,10 +13,9 @@ import { GlobalLoaderViewProps, GlobalLoaderViewRef } from './GlobalLoaderView';
 export const useWidthAndPosition = (status: GlobalLoaderViewProps['status'], ref: GlobalLoaderViewRef['refObject']) => {
   const { width = 0, left = 0 } = ref.current?.getBoundingClientRect() || {};
   const [startWidth, setStartWidth] = useState(0);
-  const prevStatus = usePrevious(status);
   useEffect(() => {
-    const hasStatusChangedToError = (prevStatus === 'standard' || prevStatus === 'accept') && status === 'error';
-    const hasStatusChangedToAccept = prevStatus === 'error' && status === 'accept';
+    const hasStatusChangedToError = status === 'error';
+    const hasStatusChangedToAccept = status === 'accept';
     if (hasStatusChangedToError) {
       setStartWidth(width);
     } else if (hasStatusChangedToAccept) {
