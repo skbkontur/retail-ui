55d8de7ef0b2c2781052d0d25dd10afab07fab33
HelenaIsh <ishmatova@skbkontur.ru>
2022-02-14 10:50:19 +0500

    chore(GlobalLoader): small refactor

--- packages/react-ui/components/GlobalLoader/useWidthAndPosition.ts
+++ packages/react-ui/components/GlobalLoader/useWidthAndPosition.ts
@@ -7,18 +7,16 @@ import { GlobalLoaderViewProps, GlobalLoaderViewRef } from './GlobalLoaderView';
  *
  * @param status - статус `GlobalLoader`
  * @param ref - ref `GlobalLoader`
- * @returns - возвращает ширину, левое положение и ширину полоски `GlobalLoader`
+ * @returns - возвращает ширину и положение полоски `GlobalLoader`
  */
 
 export const useWidthAndPosition = (status: GlobalLoaderViewProps['status'], ref: GlobalLoaderViewRef['refObject']) => {
   const { width = 0, left = 0 } = ref.current?.getBoundingClientRect() || {};
   const [startWidth, setStartWidth] = useState(0);
   useEffect(() => {
-    const hasStatusChangedToError = status === 'error';
-    const hasStatusChangedToAccept = status === 'accept';
-    if (hasStatusChangedToError) {
+    if (status === 'error') {
       setStartWidth(width);
-    } else if (hasStatusChangedToAccept) {
+    } else if (status === 'accept') {
       setStartWidth(startWidth);
     } else {
       setStartWidth(0);
