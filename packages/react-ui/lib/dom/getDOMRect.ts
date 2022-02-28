76c718e1d88e33a64a9d0ffb29fbf3bec545f337
HelenaIsh <ishmatova@skbkontur.ru>
2022-02-28 14:13:27 +0500

    chore: refactor getDOMRect

--- packages/react-ui/lib/dom/getDOMRect.ts
+++ packages/react-ui/lib/dom/getDOMRect.ts
@@ -1,14 +1,13 @@
+import { Nullable } from '../../typings/utility-types';
+type DOMRectDefaultValues = Omit<DOMRect, 'toJSON'>;
 /**
  * Возвращает размер элемента и его позицию относительно viewport
  *
  * @param element - ref элемента или сам элемент
  * @returns - возвращает размер элемента и его позицию относительно viewport
  */
-
-import { Nullable } from '../../typings/utility-types';
-
-export const getDOMRect = <T extends Element>(element: Nullable<T> | React.RefObject<T>): Omit<DOMRect, 'toJSON'> => {
-  const defaultValues = {
+export const getDOMRect = <T extends Element>(element: Nullable<T> | React.RefObject<T>): DOMRectDefaultValues => {
+  const defaultValues: DOMRectDefaultValues = {
     width: 0,
     height: 0,
     x: 0,
@@ -18,17 +17,20 @@ export const getDOMRect = <T extends Element>(element: Nullable<T> | React.RefOb
     right: 0,
     top: 0,
   };
-  if (element) {
-    if ('current' in element) {
-      if (element.current) {
-        return element.current.getBoundingClientRect();
-      } else {
-        return defaultValues;
-      }
-    } else {
-      return element.getBoundingClientRect();
-    }
-  } else {
-    return defaultValues;
+  if (element && 'current' in element) {
+    return getRefRect(defaultValues, element);
   }
+  return getElementRect(defaultValues, element);
+};
+const getElementRect = (defaultValues: DOMRectDefaultValues, element: Nullable<Element>) => {
+  if (element) {
+    return element.getBoundingClientRect();
+  }
+  return defaultValues;
+};
+const getRefRect = <T extends Element>(defaultValues: DOMRectDefaultValues, ref: React.RefObject<T>) => {
+  if (ref?.current) {
+    return ref.current.getBoundingClientRect();
+  }
+  return defaultValues;
 };
