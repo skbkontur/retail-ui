9b187bc84910eaf9c66cf25ca015c6723f9b94ad
Anna Khlutkova <anna.khlutkova@mail.ru>
2022-03-18 11:34:51 +0500

    chore: delete relative class after test

--- packages/react-ui/components/DatePicker/__stories__/DatePicker.stories.tsx
+++ packages/react-ui/components/DatePicker/__stories__/DatePicker.stories.tsx
@@ -351,6 +351,11 @@ DatePickerInRelativeBody.parameters = {
           .click(this.browser.findElement({ css: '[data-comp-name~="DatePicker"]' }))
           .perform();
         await this.expect(await this.takeScreenshot()).to.matchImage('opened');
+        await this.browser
+          .actions({
+            bridge: true,
+          })
+          .click(this.browser.findElement({ css: 'button' }));
       },
     },
   },
