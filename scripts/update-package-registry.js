59b45c89210839f2fccb4e6bd4abbc019750978c
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-03-21 17:53:17 +0500

    chore: review fixes

--- scripts/update-package-registry.js
+++ scripts/update-package-registry.js
@@ -3,7 +3,6 @@ const { existsSync, readFileSync, writeFileSync } = require('fs');
 const { execSync } = require('child_process');
 
 const LOCK_FILE_PATH = path.join(__dirname, '../yarn.lock');
-const ENV_FILE_PATH = path.join(__dirname, '../.env');
 const ENV_PACKAGE_REGISTRY = getRegistryFromEnv();
 
 if (ENV_PACKAGE_REGISTRY) {
@@ -11,12 +10,9 @@ if (ENV_PACKAGE_REGISTRY) {
   if (/^http(s?):\/\//.test(CONFIG_PACKAGE_REGISTRY) && CONFIG_PACKAGE_REGISTRY !== ENV_PACKAGE_REGISTRY) {
     const lockFile = readFileSync(LOCK_FILE_PATH, 'utf-8');
 
-    let modifiedLockFile;
-    if (process.argv.includes('back')) {
-      modifiedLockFile = lockFile.replaceAll(ENV_PACKAGE_REGISTRY, CONFIG_PACKAGE_REGISTRY);
-    } else {
-      modifiedLockFile = lockFile.replaceAll(CONFIG_PACKAGE_REGISTRY, ENV_PACKAGE_REGISTRY);
-    }
+    const modifiedLockFile = process.argv.includes('back')
+      ? lockFile.replaceAll(ENV_PACKAGE_REGISTRY, CONFIG_PACKAGE_REGISTRY)
+      : lockFile.replaceAll(CONFIG_PACKAGE_REGISTRY, ENV_PACKAGE_REGISTRY);
 
     writeFileSync(LOCK_FILE_PATH, modifiedLockFile);
   }
@@ -24,13 +20,14 @@ if (ENV_PACKAGE_REGISTRY) {
 
 function getRegistryFromEnv() {
   const ENV_VAR_NAME = 'PACKAGE_REGISTRY';
+  const ENV_FILE_PATH = path.join(__dirname, '../.env');
+
   if (process.env[ENV_VAR_NAME]) return process.env[ENV_VAR_NAME];
 
   if (!existsSync(ENV_FILE_PATH)) return undefined;
 
-  const envFile = readFileSync(ENV_FILE_PATH, 'utf-8');
-  return envFile.split('\n').reduce((acc, line) => {
-    const [name, value] = line.split('=');
-    return name === ENV_VAR_NAME ? value : acc;
-  }, undefined);
+  return readFileSync(ENV_FILE_PATH, 'utf-8')
+    .split('\n')
+    .find((line) => line.startsWith(`${ENV_VAR_NAME}=`))
+    ?.split('=')[1];
 }
