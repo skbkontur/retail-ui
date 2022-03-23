4bb39d29d23596512336aa22a997fff28306241b
Olga Lazareva <lazareva@skbkontur.ru>
2022-03-23 14:24:47 +0500

    docs: fix a few typos

--- packages/react-ui/components/Token/Token.tsx
+++ packages/react-ui/components/Token/Token.tsx
@@ -21,11 +21,11 @@ export interface TokenProps extends CommonProps {
   colors?: TokenColors;
   isActive?: boolean;
   /**
-   * Cостояние валидации при ошибке.
+   * Состояние валидации при ошибке.
    */
   error?: boolean;
   /**
-   * Cостояние валидации при предупреждении.
+   * Состояние валидации при предупреждении.
    */
   warning?: boolean;
   disabled?: boolean;
