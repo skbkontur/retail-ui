skbkontur/retail-ui

commit cb5254f10d92456018b1ead3790f8bbade0f34e0

fix(DatePicker): width for different sizes with empty value (#2614)

packages/react-ui/components/DateInput/DateInput.styles.ts
@@ -33,4 +33,16 @@ export const styles = {
       color: ${t.textColorDisabled};
     `;
   },
+
+  value() {
+    return css`
+      opacity: 0;
+    `;
+  },
+
+  valueVisible() {
+    return css`
+      opacity: 1;
+    `;
+  },
 };

packages/react-ui/.creevey/images/DateInput/With No Value/focused/chrome.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5a791c5b40783e9bfc4f9678417b140335b70d160648db43494ac7b826a7e677
+size 401

packages/react-ui/.creevey/images/DateInput/With No Value/focused/chrome8px.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:540c6717556e4e4fafa05a0400e363cabd386e24ceb7db06a2525c79ba942ff6
+size 396

packages/react-ui/.creevey/images/DateInput/With No Value/focused/firefox.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:de9d2fd1172d9f12f13ce554588e38053e909470cb62480e7620d7c65e0f985f
+size 255

packages/react-ui/.creevey/images/DateInput/With No Value/focused/firefox8px.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:955925c346bd25e5eda69fd4ec5d365885079e9cd9adf191b8c8365522cab637
+size 250

packages/react-ui/.creevey/images/DateInput/With No Value/focused/ie11.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:14a5f2524277e3d8baea0c4c4153d4e95f3572bbe251948ddb63edfd10bdeff5
+size 321

packages/react-ui/.creevey/images/DateInput/With No Value/focused/ie118px.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b46bc69fdd8a99935dcef364a06ddbb8a3f565ec008d9b74cf57bc96b4863710
+size 317

packages/react-ui/.creevey/images/DateInput/With No Value/idle/chrome.png
@@ -1,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5b2bc2a2bb62ab94057b37844e6a0be0f74be0021f836461499d6d1ccc44bb66
+size 264

commit 8339c75a54ae30ae8814551907e1df4089084dc0

fix(DatePicker): fix focus and fragments selection
broke in #2614

packages/react-ui/components/DateInput/DateInput.styles.ts
@@ -36,13 +36,13 @@ export const styles = {
 
   value() {
     return css`
-      opacity: 0;
+      visibility: hidden;
     `;
   },
 
   valueVisible() {
     return css`
-      opacity: 1;
+      visibility: visible;
     `;
   },
 };