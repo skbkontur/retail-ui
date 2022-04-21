30c55b1c871ffa74f7fd17160188dbc5757d390f
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-04-21 13:29:50 +0500

    chore(CommonWrapper): watch rootNodes changes
    they may happen not only during mount/unmout (i.e. Tooltip/Popup) which previous solution via refs was based on

--- packages/react-ui/lib/rootNode/rootNodeDecorator.tsx
+++ packages/react-ui/lib/rootNode/rootNodeDecorator.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import EventEmitter from 'eventemitter3';
 
 import { Nullable } from '../../typings/utility-types';
 
@@ -6,25 +7,45 @@ import { getRootNode } from './getRootNode';
 
 export type TSetRootNode = (e: Nullable<React.ReactNode>) => void;
 
+export type TRootNodeSubscription = {
+  remove: () => void;
+};
+
 export interface InstanceWithRootNode {
   rootNode: Nullable<HTMLElement>;
+  rootNodeEmitter: EventEmitter;
   setRootNode: (instance: Nullable<React.ReactInstance>) => void;
   getRootNode: () => Nullable<HTMLElement>;
+  addRootNodeChangeListener: (callback: (node: Nullable<HTMLElement>) => void) => TRootNodeSubscription;
 }
 
 export function rootNode<T extends new (...args: any[]) => React.Component>(Component: T) {
   const rootNode = class extends Component implements InstanceWithRootNode {
     public rootNode: Nullable<HTMLElement> = null;
+    public rootNodeEmitter = new EventEmitter();
     public constructor(...args: any[]) {
       super(args[0]);
     }
 
     public setRootNode = (instance: Nullable<React.ReactInstance>) => {
-      this.rootNode = getRootNode(instance);
+      const rootNode = getRootNode(instance);
+      if (rootNode !== this.rootNode) {
+        this.rootNode = rootNode;
+        this.rootNodeEmitter.emit('change', rootNode);
+      }
     };
 
     public getRootNode = (): Nullable<HTMLElement> => {
       return this.rootNode;
+    };
+
+    public addRootNodeChangeListener = (callback: (node: Nullable<HTMLElement>) => void): TRootNodeSubscription => {
+      this.rootNodeEmitter.addListener('change', callback);
+      return {
+        remove: () => {
+          this.rootNodeEmitter.removeListener('change', callback);
+        },
+      };
     };
   };
 
