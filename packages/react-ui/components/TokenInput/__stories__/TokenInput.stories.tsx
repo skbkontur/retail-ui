cbab88f6198ee6cf8273165ded017162b034b439
Olga Lazareva <lazareva@skbkontur.ru>
2022-05-05 11:49:13 +0500

    feat(tokeninput): add renderTotalCount and totalCount props
    https://guides.kontur.ru/components/tokens/#13.
    
    fix #2314

--- packages/react-ui/components/TokenInput/__stories__/TokenInput.stories.tsx
+++ packages/react-ui/components/TokenInput/__stories__/TokenInput.stories.tsx
@@ -704,3 +704,32 @@ FullWidthMenu.parameters = {
     },
   },
 };
+
+export const CustomRenderTotalCount: Story = () => {
+  return (
+    <TokenInput
+      type={TokenInputType.Combined}
+      getItems={getItems}
+      renderTotalCount={(found, total) => `Показано ${found} из ${total}`}
+      totalCount={100}
+    />
+  );
+};
+
+CustomRenderTotalCount.storyName = 'custom render total count';
+
+CustomRenderTotalCount.parameters = {
+  creevey: {
+    tests: {
+      async renderTotalCount() {
+        await this.browser
+          .actions({
+            bridge: true,
+          })
+          .click(this.browser.findElement({ css: '[data-comp-name~="TokenInput"]' }))
+          .perform();
+        await this.expect(await this.takeScreenshot()).to.matchImage();
+      },
+    },
+  },
+};
