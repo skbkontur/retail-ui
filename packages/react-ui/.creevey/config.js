667c0acee9954335834421b67a0da3471cf5318c
Anna Khlutkova <anna.khlutkova@mail.ru>
2022-05-26 10:47:13 +0500

    chore

--- packages/react-ui/.creevey/config.js
+++ packages/react-ui/.creevey/config.js
@@ -139,43 +139,43 @@ const config = {
     //   'ie.browserCommandLineSwitches': '-private',
     //   'ie.ensureCleanSession': true,
     // },
-    ie118px: {
-      browserName: 'internet explorer',
-      viewport: { width: 1024, height: 720 },
-      _storybookGlobals: {
-        theme: 'DEFAULT_THEME_8PX_OLD',
-      },
-      name: 'infrafront/ie118px',
-      ...capabilities,
-    },
-    ie11Flat8px: {
-      browserName: 'internet explorer',
-      viewport: { width: 1024, height: 720 },
-      _storybookGlobals: {
-        theme: 'FLAT_THEME_8PX_OLD',
-      },
-      name: 'infrafront/ie11Flat8px',
-      ...capabilities,
-    },
-    ie11: {
-      browserName: 'internet explorer',
-      viewport: { width: 1024, height: 720 },
-      _storybookGlobals: {
-        theme: 'DEFAULT_THEME',
-      },
-      name: 'infrafront/ie11',
-      ...capabilities,
-    },
-    ie11Dark: {
-      browserName: 'internet explorer',
-      viewport: { width: 1024, height: 720 },
-      _storybookGlobals: {
-        theme: 'DARK_THEME',
-      },
-      backgrounds: { default: 'dark' },
-      name: 'infrafront/ie11Dark',
-      ...capabilities,
-    },
+    // ie118px: {
+    //   browserName: 'internet explorer',
+    //   viewport: { width: 1024, height: 720 },
+    //   _storybookGlobals: {
+    //     theme: 'DEFAULT_THEME_8PX_OLD',
+    //   },
+    //   name: 'infrafront/ie118px',
+    //   ...capabilities,
+    // },
+    // ie11Flat8px: {
+    //   browserName: 'internet explorer',
+    //   viewport: { width: 1024, height: 720 },
+    //   _storybookGlobals: {
+    //     theme: 'FLAT_THEME_8PX_OLD',
+    //   },
+    //   name: 'infrafront/ie11Flat8px',
+    //   ...capabilities,
+    // },
+    // ie11: {
+    //   browserName: 'internet explorer',
+    //   viewport: { width: 1024, height: 720 },
+    //   _storybookGlobals: {
+    //     theme: 'DEFAULT_THEME',
+    //   },
+    //   name: 'infrafront/ie11',
+    //   ...capabilities,
+    // },
+    // ie11Dark: {
+    //   browserName: 'internet explorer',
+    //   viewport: { width: 1024, height: 720 },
+    //   _storybookGlobals: {
+    //     theme: 'DARK_THEME',
+    //   },
+    //   backgrounds: { default: 'dark' },
+    //   name: 'infrafront/ie11Dark',
+    //   ...capabilities,
+    // },
     chromeMobile: {
       browserName: 'chrome',
       viewport: { width: 400, height: 720 },
