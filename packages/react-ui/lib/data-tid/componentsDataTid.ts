00b93c3c76fd9e7360038e936da72134b76763c2
HelenaIsh <ishmatova@skbkontur.ru>
2022-06-01 11:03:35 +0300

    chore: add internal components to data-tids list

--- packages/react-ui/lib/data-tid/componentsDataTid.ts
+++ packages/react-ui/lib/data-tid/componentsDataTid.ts
@@ -1,6 +1,36 @@
 import * as Components from '../../index';
+import * as Calendar from '../../internal/Calendar';
+import * as CalendarMonthView from '../../internal/Calendar/MonthView';
+import * as CustomComboBox from '../../internal/CustomComboBox';
+import * as CustomComboBoxComboBoxView from '../../internal/CustomComboBox/ComboBoxView';
+import * as DateSelect from '../../internal/DateSelect';
+import * as InputLikeText from '../../internal/InputLikeText';
+import * as InternalMenu from '../../internal/InternalMenu';
+import * as MaskedInput from '../../internal/MaskedInput';
+import * as Menu from '../../internal/Menu';
+import * as Popup from '../../internal/Popup';
+import * as PopupPin from '../../internal/Popup/PopupPin';
+import * as MobilePopupDataTid from '../../internal/MobilePopup';
+import * as PopupMenu from '../../internal/PopupMenu';
 
-const tids = Object.keys(Components).filter((componentName) => {
+const allComponents = {
+  ...Components,
+  ...Calendar,
+  ...CalendarMonthView,
+  ...CustomComboBox,
+  ...CustomComboBoxComboBoxView,
+  ...DateSelect,
+  ...InputLikeText,
+  ...InternalMenu,
+  ...MaskedInput,
+  ...Menu,
+  ...Popup,
+  ...PopupPin,
+  ...MobilePopupDataTid,
+  ...PopupMenu,
+};
+
+const tids = Object.keys(allComponents).filter((componentName) => {
   if (componentName.match(/datatid/i)) {
     return componentName;
   }
@@ -9,8 +39,8 @@ const tids = Object.keys(Components).filter((componentName) => {
 export const componentsDataTid: { [key: string]: { [key: string]: string } } = tids.reduce((accum, currTid) => {
   return {
     ...accum,
-    [Components[currTid].origin]: {
-      [currTid]: Object.entries(Components[currTid])
+    [allComponents[currTid].origin]: {
+      [currTid]: Object.entries(allComponents[currTid])
         .map((x) => x.join(':'))
         .join(', \n'),
     },
