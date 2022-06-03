8fd1b9a66c144be8b3d2a36827c7bd1359c7b1c0
HelenaIsh <ishmatova@skbkontur.ru>
2022-06-03 12:11:58 +0300

    chore: delete internal components from data-tid table

--- packages/react-ui/lib/data-tid/componentsDataTids.ts
+++ packages/react-ui/lib/data-tid/componentsDataTids.ts
@@ -1,30 +1,6 @@
 import * as Components from '../../index';
-import * as Calendar from '../../internal/Calendar';
-import * as CustomComboBox from '../../internal/CustomComboBox';
-import * as DateSelect from '../../internal/DateSelect';
-import * as InputLikeText from '../../internal/InputLikeText';
-import * as InternalMenu from '../../internal/InternalMenu';
-import * as MaskedInput from '../../internal/MaskedInput';
-import * as Menu from '../../internal/Menu';
-import * as Popup from '../../internal/Popup';
-import * as MobilePopupDataTid from '../../internal/MobilePopup';
-import * as PopupMenu from '../../internal/PopupMenu';
 
-const allComponents: any = {
-  ...Components,
-  ...Calendar,
-  ...CustomComboBox,
-  ...DateSelect,
-  ...InputLikeText,
-  ...InternalMenu,
-  ...MaskedInput,
-  ...Menu,
-  ...Popup,
-  ...MobilePopupDataTid,
-  ...PopupMenu,
-};
-
-const tids = Object.keys(allComponents).filter((componentName) => {
+const tids = Object.keys(Components).filter((componentName) => {
   if (componentName.match(/datatids/i)) {
     return componentName;
   }
@@ -35,7 +11,7 @@ export const componentsDataTids: { [key: string]: { [key: string]: string } } = 
   return {
     ...accum,
     [componentName]: {
-      [currTid]: Object.entries(allComponents[currTid])
+      [currTid]: Object.entries(Components[currTid])
         .map((x) => x.join(': '))
         .join(', \n'),
     },
