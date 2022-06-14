d3b16fb3c59aebd3ceba4305dce9358685a0df2c
Anna Khlutkova <anna.khlutkova@mail.ru>
2022-06-14 11:16:37 +0500

    if-366: fix count limit

--- packages/react-ui/lib/memo.ts
+++ packages/react-ui/lib/memo.ts
@@ -1,33 +1,31 @@
-export function memo<T>(fn: T): T {
+const RESULT_KEY = Symbol('RESULT_KEY');
+
+export function memo<T>(fn: T): any {
   let cache = new Map();
-  let keysCount = 0;
   const limit = 1e4;
 
-  // @ts-ignore
-  return (...args) => {
+  return (...args: any[]) => {
     try {
       let currentLevelOfCache = cache;
       args.forEach((currentArg) => {
         const temp = currentLevelOfCache.get(currentArg);
-        if (!temp?.size) {
+        if (!temp.size) {
           currentLevelOfCache.set(currentArg, new Map());
         }
         currentLevelOfCache = currentLevelOfCache.get(currentArg);
       });
 
-      const resultFromCache = currentLevelOfCache.get('result');
+      const resultFromCache = currentLevelOfCache.get(RESULT_KEY);
       if (resultFromCache) {
         return resultFromCache;
       }
 
-      // @ts-ignore
       const result = fn(...args);
-      currentLevelOfCache.set('result', result);
+      currentLevelOfCache.set(RESULT_KEY, result);
       return result;
     } finally {
-      if (keysCount > limit) {
+      if (cache.size > limit) {
         cache = new Map();
-        keysCount = 0;
       }
     }
   };
