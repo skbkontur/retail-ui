29e3243f388588e9945da3b1079f4aa2f8a2b0fc
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-07-18 12:57:16 +0500

    fix(ResponsiveLayout): fix memory leak (#2948)

---
+++ packages/react-ui/components/ResponsiveLayout/__tests__/useResponsiveLayout-test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { eventListenersMap } from '../ResponsiveLayoutEvents';
+import { useResponsiveLayout } from '../useResponsiveLayout';
+
+describe('useResponsiveLayout', () => {
+  it('removes listeners after unmount', () => {
+    const WithResponsiveLayoutListener = () => {
+      const { isMobile } = useResponsiveLayout();
+      return <i>{isMobile}</i>;
+    };
+    eventListenersMap.clear();
+
+    const { unmount } = render(<WithResponsiveLayoutListener />);
+    expect(eventListenersMap.size).toBe(1);
+
+    unmount();
+    expect(eventListenersMap.size).toBe(0);
+  });
+});
