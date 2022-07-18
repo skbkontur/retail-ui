5a9f89552f71bb3908f13c8d218a9c397522fbf1
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-07-18 15:40:57 +0500

    build(validations): replace ts-jest vs babel-jest
    so we are able to use babel-plugins
    (eg.: babel-plugin-search-and-replace)

---
+++ packages/react-ui-validations/babel.config.js
@@ -0,0 +1,25 @@
+// using babel.config.js here as a workaround
+// to enable processing files from react-ui
+// that are outside of validations package
+// @see https://stackoverflow.com/a/67235617
+
+module.exports = {
+  presets: [['@babel/preset-env', { modules: 'auto' }], '@babel/typescript', '@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    'react-hot-loader/babel',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      'search-and-replace',
+      {
+        rules: [
+          {
+            // @see ReactUIDetection.ts
+            search: /__REACT_UI_PACKAGE__/,
+            replace: '@skbkontur/react-ui',
+          },
+        ],
+      },
+    ],
+  ],
+};
