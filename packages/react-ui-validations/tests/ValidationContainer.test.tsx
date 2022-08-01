9630282d72c371470863a10d323d01b64409c50f
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-08-01 15:22:39 +0500

    test(ValidationContainer): test scroll into view on submit

--- packages/react-ui-validations/tests/ValidationContainer.test.tsx
+++ packages/react-ui-validations/tests/ValidationContainer.test.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import {
+  ComboBox,
+  DatePicker,
+  FileUploader,
+  Input,
+  RadioGroup,
+  Switcher,
+  TokenInput,
+  TokenInputType,
+} from '@skbkontur/react-ui';
 
-import { ValidationContainer } from '../src';
+import { ValidationContainer, ValidationContainerProps, ValidationWrapper } from '../src';
+import { smoothScrollIntoView } from '../src/smoothScrollIntoView';
 
 describe('ValidationContainer', () => {
   it('renders passed children', () => {
@@ -12,5 +23,49 @@ describe('ValidationContainer', () => {
     );
 
     expect(screen.getByTestId('passed-div')).toBeInTheDocument();
+  });
+
+  describe('scroll into view on submit', () => {
+    beforeEach(() => {
+      (smoothScrollIntoView as jest.Mock).mockReset();
+    });
+
+    const renderValidationContainer = (
+      input: React.ReactElement,
+      props?: ValidationContainerProps,
+    ): React.RefObject<ValidationContainer> => {
+      const containerRef = React.createRef<ValidationContainer>();
+      render(
+        <ValidationContainer ref={containerRef} {...props}>
+          <ValidationWrapper validationInfo={{ message: 'message', level: 'error' }}>{input}</ValidationWrapper>
+        </ValidationContainer>,
+      );
+      return containerRef;
+    };
+    it('doesn`t get called if disabled', async () => {
+      const containerRef = renderValidationContainer(<Input />, { disableSmoothScroll: true });
+
+      await containerRef.current?.submit();
+
+      expect(smoothScrollIntoView).not.toBeCalled();
+    });
+
+    describe('gets called if not disabled for', () => {
+      it.each([
+        ['Input', <Input key="Input" />],
+        ['TokenInput', <TokenInput type={TokenInputType.WithoutReference} key="TokenInput" />],
+        ['FileUploader', <FileUploader key="FileUploader" />],
+        ['Switcher', <Switcher items={[]} key="Switcher" />],
+        ['DatePicker', <DatePicker onValueChange={() => null} key="DatePicker" />],
+        ['ComboBox', <ComboBox getItems={() => Promise.resolve([])} key="ComboBox" />],
+        ['RadioGroup', <RadioGroup items={[]} key="RadioGroup" />],
+      ])('%s', async (_, input) => {
+        const containerRef = renderValidationContainer(input, { disableSmoothScroll: false });
+
+        await containerRef.current?.submit();
+
+        expect(smoothScrollIntoView).toBeCalled();
+      });
+    });
   });
 });
--- packages/react-ui-validations/tests/test-setup.js
+++ packages/react-ui-validations/tests/test-setup.js
@@ -4,3 +4,11 @@ import '@testing-library/jest-dom';
 configure({
   testIdAttribute: 'data-tid',
 });
+
+jest.mock('../src/smoothScrollIntoView', () => {
+  const originalModule = jest.requireActual('../src/smoothScrollIntoView');
+  return {
+    ...originalModule,
+    smoothScrollIntoView: jest.fn(originalModule.smoothScrollIntoView),
+  };
+});
