3ae1cf5fc9d42942869cccb85b3d774340d8870b
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2022-08-12 16:27:27 +0500

    test(Validations): mock matchMedia API

--- packages/react-ui-validations/tests/test-setup.js
+++ packages/react-ui-validations/tests/test-setup.js
@@ -12,3 +12,15 @@ jest.mock('../src/smoothScrollIntoView', () => {
     smoothScrollIntoView: jest.fn(originalModule.smoothScrollIntoView),
   };
 });
+
+window.matchMedia = jest.fn().mockImplementation((query) => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+  };
+});
