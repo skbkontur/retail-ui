9bc3f9c6a8b3c5aa7f166008b9b1191537e648d6
Евгений Пятков <48599460+JackUait@users.noreply.github.com>
2022-08-22 18:27:34 +0400

    fix(react-ui): add inputFocusedBg and inputFocusedIconColor variables (#2981)

--- packages/react-ui/components/Input/Input.tsx
+++ packages/react-ui/components/Input/Input.tsx
@@ -404,6 +404,7 @@ export class Input extends React.Component<InputProps, InputState> {
     return (
       <span
         className={cx(styles.icon(), sizeClassName, styles.useDefaultColor(this.theme), {
+          [styles.iconFocus(this.theme)]: this.state.focused,
           [styles.iconDisabled()]: disabled,
         })}
       >
