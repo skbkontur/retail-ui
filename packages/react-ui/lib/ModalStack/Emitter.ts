c322843deaa5f64e817380963b216f1f1972a593
lossir <lossir1@gmail.com>
2022-09-01 20:13:48 +0500

    refactor: wip

--- packages/react-ui/lib/ModalStack/Emitter.ts
+++ packages/react-ui/lib/ModalStack/Emitter.ts
@@ -1,16 +1,15 @@
 import EventEmitter from 'eventemitter3';
 
-// there are cases when users use several versions of `@skbkontur/react-ui` in one app.
-// later versions use the `fbemitter` package, which does not have a `removeListener()` method
-// meanwhile, `eventemitter3` does not have a `remove()` method.
-// we didn't take this into account when we replaced `fbemitter` with `eventemitter3`.
+// There are cases when users use several versions of `@skbkontur/react-ui` in one app.
+// Older versions use `fbemitter` package, which doesn't have a `removeListener()` method.
+// Meanwhile, latest versions moved to `eventemitter3`, which doesn't have a `remove()` method.
 
-// when this problem was found, we released fixes for more popular versions.
+// When this problem was found, we released fixes for more popular versions,
 // but not all teams can move to fixed versions, that may cause app crashing.
-// see all details https://github.com/skbkontur/retail-ui/issues/2197
+// See all details https://github.com/skbkontur/retail-ui/issues/2197
 
 /**
- * EventEmitter wrapper with compatibility with all later versions of `@skbkontur/react-ui`.
+ * EventEmitter wrapper with compatibility with all older versions of `@skbkontur/react-ui`
  */
 export class Emitter extends EventEmitter {
   // @ts-expect-error: Type 'FallbackFBEmitter' is not assignable to type 'this'
@@ -24,11 +23,7 @@ export class Emitter extends EventEmitter {
     return new FallbackFBEmitter(() => this.removeListener?.(event, fn));
   };
 
-  public emit = <T extends EventEmitter.EventNames<string | symbol>>(event: T, ...args: unknown[]): boolean => {
-    return super.emit(event, ...args);
-  };
-
-  // @ts-expect-error Method is optional because it is not present in `fbemitter`
+  // @ts-expect-error: Method is optional because it is not present in `fbemitter`
   public removeListener? = <T extends EventEmitter.EventNames<string | symbol>>(
     event: T,
     fn?: EventEmitter.EventListener<string | symbol, T>,
@@ -39,7 +34,7 @@ export class Emitter extends EventEmitter {
   };
 }
 
-// Backward compatible with versions using the `fbemitter` package.
+// Backward compatible with older versions which use the `fbemitter` package
 export class FallbackFBEmitter extends Emitter {
   // Method is optional because it is not present in `eventemitter3`
   constructor(public readonly remove?: () => void) {
