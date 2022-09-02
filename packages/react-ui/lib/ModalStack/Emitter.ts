5f1843f29f0d2ca3987de4d631a47b5bff35b724
lossir <lossir1@gmail.com>
2022-09-02 11:16:11 +0500

    refactor: wip

--- packages/react-ui/lib/ModalStack/Emitter.ts
+++ packages/react-ui/lib/ModalStack/Emitter.ts
@@ -2,14 +2,15 @@ import EventEmitter from 'eventemitter3';
 
 // There are cases when users use several versions of `@skbkontur/react-ui` in one app.
 // Older versions use `fbemitter` package, which doesn't have a `removeListener()` method.
-// Meanwhile, latest versions moved to `eventemitter3`, which doesn't have a `remove()` method.
+// Meanwhile, latest versions use `eventemitter3`, which doesn't have a `remove()` method
+// among methods returned by the `addListener` method, as `fbemitter` did.
 
 // When this problem was found, we released fixes for more popular versions,
 // but not all teams can move to fixed versions, that may cause app crashing.
 // See all details https://github.com/skbkontur/retail-ui/issues/2197
 
 /**
- * EventEmitter wrapper with compatibility with all older versions of `@skbkontur/react-ui`
+ * EventEmitter wrapper behaves like `eventemitter3` and almost like `fbemitter` at the same time.
  */
 export class Emitter extends EventEmitter {
   // @ts-expect-error: Type 'FallbackFBEmitter' is not assignable to type 'this'
