d05c5b4c7a583dc4c08ac78a7df8ffdc484d7736
Евгений Пятков <jackuait@bk.ru>
2022-09-15 03:39:13 +0300

    test: match the whole word in .toHaveTextContent

--- packages/react-ui/components/CurrencyLabel/__tests__/CurrencyLabel.test.tsx
+++ packages/react-ui/components/CurrencyLabel/__tests__/CurrencyLabel.test.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import { THIN_SPACE } from '../../../components/CurrencyInput/constants';
 import { CurrencyLabel, CurrencyLabelDataTids } from '../CurrencyLabel';
-
-const SPACE_SYMBOL = String.fromCharCode(THIN_SPACE);
 
 describe('CurrencyLabel', () => {
   it('should correctly format value', () => {
     render(<CurrencyLabel value={12345} fractionDigits={0} />);
 
     const node = screen.getByTestId(CurrencyLabelDataTids.root);
-    expect(node).toHaveTextContent('12 345');
+    expect(node).toHaveTextContent(/^12 345$/);
   });
 
   it('should render trailing zeros', () => {
@@ -19,9 +16,7 @@ describe('CurrencyLabel', () => {
 
     const node = screen.getByTestId(CurrencyLabelDataTids.root);
 
-    // .toHaveTextContent normalizes input value in the way that makes test always pass: https://github.com/testing-library/jest-dom/blob/948d90f32cc79339bdeebea0454599db74c5d071/src/to-have-text-content.js#L10-L12
-    // eslint-disable-next-line jest-dom/prefer-to-have-text-content
-    expect(node.textContent).toBe(`12${SPACE_SYMBOL}356,14000`);
+    expect(node).toHaveTextContent(/^12 356,14000$/);
   });
 
   it('should render exactly five zeros after comma', () => {
@@ -29,9 +24,7 @@ describe('CurrencyLabel', () => {
 
     const node = screen.getByTestId(CurrencyLabelDataTids.root);
 
-    // .toHaveTextContent normalizes input value in the way that makes test always pass: https://github.com/testing-library/jest-dom/blob/948d90f32cc79339bdeebea0454599db74c5d071/src/to-have-text-content.js#L10-L12
-    // eslint-disable-next-line jest-dom/prefer-to-have-text-content
-    expect(node.textContent).toBe(`12${SPACE_SYMBOL}356,00000`);
+    expect(node).toHaveTextContent(/^12 356,00000$/);
   });
 
   it('should hide trailing zeros', () => {
@@ -39,8 +32,6 @@ describe('CurrencyLabel', () => {
 
     const node = screen.getByTestId(CurrencyLabelDataTids.root);
 
-    // .toHaveTextContent normalizes input value in the way that makes test always pass: https://github.com/testing-library/jest-dom/blob/948d90f32cc79339bdeebea0454599db74c5d071/src/to-have-text-content.js#L10-L12
-    // eslint-disable-next-line jest-dom/prefer-to-have-text-content
-    expect(node.textContent).toBe(`12${SPACE_SYMBOL}356,14`);
+    expect(node).toHaveTextContent(/^12 356,14$/);
   });
 });
