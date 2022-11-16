82883d2aea11f474ee5eec7521b4fdbb5b6afee7
Kamilia Taranova <taranova.kn@skbkontur.ru>
2022-11-16 14:23:52 +0300

    Refactor old tests, added new tests

--- packages/react-ui/components/Button/__tests__/Button-test.tsx
+++ packages/react-ui/components/Button/__tests__/Button-test.tsx
@@ -1,28 +1,63 @@
-import { mount } from 'enzyme';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent, { specialChars } from '@testing-library/user-event';
 import React from 'react';
 
 import { Button, ButtonType } from '../Button';
 
 describe('Button', () => {
-  it('has correct label', () => {
-    const wrapper = mount(<Button>Foo</Button>);
-
-    expect(wrapper.text()).toBe('Foo');
-  });
-
-  it('handles click event', () => {
-    const onClick = jest.fn();
-    const wrapper = mount(<Button onClick={onClick} />);
-
-    wrapper.find('button').simulate('click');
-
-    expect(onClick.mock.calls).toHaveLength(1);
+  it('Has correct label', () => {
+    render(<Button>Foo</Button>);
+    expect(screen.getByRole('button')).toHaveTextContent('Foo');
   });
 
   (['submit', 'button', 'reset'] as ButtonType[]).forEach((type) => {
     it(`sets type ${type} when type=${type} specified`, () => {
-      const wrapper = mount(<Button type={type} />);
-      expect(wrapper.find('button').prop('type')).toBe(type);
+      render(<Button type={type} />);
+      expect(screen.getByRole('button')).toHaveProperty('type', type);
     });
   });
+
+  it('Handels click event', async () => {
+    const onClick = jest.fn();
+
+    render(<Button onClick={onClick} />);
+    userEvent.click(screen.getByRole('button'));
+
+    expect(onClick.mock.calls).toHaveLength(1);
+  });
+
+  it('Handels onBlur event', () => {
+    const onBlur = jest.fn();
+    render(<Button onBlur={onBlur} />);
+
+    userEvent.click(screen.getByRole('button'));
+    screen.getByRole('button').blur();
+
+    expect(onBlur).toHaveBeenCalledTimes(1);
+  })
+
+  it('Handels onFocus event', () => {
+    const onFocus = jest.fn();
+    render(<Button onFocus={onFocus} />);
+
+    userEvent.click(screen.getByRole('button'));
+
+    expect(onFocus).toHaveBeenCalledTimes(1);
+  })
+
+  it('Handels onKeyDown event', () => {
+    const onKeyDown = jest.fn();
+    render(<Button onKeyDown={onKeyDown} />);
+    userEvent.type(screen.getByRole('button'), '{enter}')
+
+    expect(onKeyDown).toHaveBeenCalledTimes(1);
+  })
 });
+
+
+// title	string | undefined
+// className	string | undefined
+// data-tid	string | undefined
+// onMouseEnter	MouseEventHandler<HTMLButtonElement> | undefined
+// onMouseLeave	MouseEventHandler<HTMLButtonElement> | undefined
+// onMouseOver	MouseEventHandler<HTMLButtonElement> | undefined
