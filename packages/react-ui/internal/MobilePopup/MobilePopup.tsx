95dc4246b63aec293e88cd6d14682d61e3dcb041
Евгений Пятков <jackuait@bk.ru>
2022-11-17 11:59:56 +0300

    chore(MobilePopup): remove logic for tracking scroll

--- packages/react-ui/internal/MobilePopup/MobilePopup.tsx
+++ packages/react-ui/internal/MobilePopup/MobilePopup.tsx
@@ -3,7 +3,6 @@ import { Transition } from 'react-transition-group';
 
 import { Theme } from '../../lib/theming/Theme';
 import { ThemeContext } from '../../lib/theming/ThemeContext';
-import { Nullable } from '../../typings/utility-types';
 import { RenderContainer } from '../RenderContainer';
 import { HideBodyVerticalScroll } from '../HideBodyVerticalScroll';
 import { ZIndex } from '../ZIndex';
@@ -30,24 +29,15 @@ interface MobilePopupProps {
   opened: boolean;
 }
 
-interface MobilePopupState {
-  isScrolled: boolean;
-}
-
 export const MobilePopupDataTids = {
   root: 'MobilePopup__root',
   container: 'MobilePopup__container',
 } as const;
 
-export class MobilePopup extends React.Component<MobilePopupProps, MobilePopupState> {
+export class MobilePopup extends React.Component<MobilePopupProps> {
   public static __KONTUR_REACT_UI__ = 'MobileMenuHeader';
 
-  private contentDiv: Nullable<HTMLDivElement>;
   private theme!: Theme;
-
-  public state: MobilePopupState = {
-    isScrolled: false,
-  };
 
   public render() {
     return (
@@ -69,14 +59,7 @@ export class MobilePopup extends React.Component<MobilePopupProps, MobilePopupSt
               <div data-tid={MobilePopupDataTids.container} className={jsStyles.container(this.theme)}>
                 <div data-tid={MobilePopupDataTids.root} className={jsStyles.root(this.theme)}>
                   <MobilePopupHeader caption={this.props.caption}>{this.props.headerChildComponent}</MobilePopupHeader>
-                  <div
-                    onClick={(e) => e.stopPropagation()}
-                    className={jsStyles.content(this.theme)}
-                    onScroll={this.handleScrollMenu}
-                    ref={this.refContent}
-                  >
-                    {this.props.children}
-                  </div>
+                  <div className={jsStyles.content(this.theme)}>{this.props.children}</div>
                 </div>
                 <div onClick={this.close} className={jsStyles.bottomIndent()} />
               </div>
@@ -98,20 +81,6 @@ export class MobilePopup extends React.Component<MobilePopupProps, MobilePopupSt
   public close = () => {
     if (this.props.onCloseRequest) {
       this.props.onCloseRequest();
-    }
-  };
-
-  private refContent = (contentDiv: HTMLDivElement) => {
-    this.contentDiv = contentDiv;
-  };
-
-  private handleScrollMenu = () => {
-    if (this.contentDiv) {
-      const isScrolled = this.contentDiv.scrollTop > 0;
-
-      if (isScrolled !== this.state.isScrolled) {
-        this.setState({ isScrolled });
-      }
     }
   };
 }
