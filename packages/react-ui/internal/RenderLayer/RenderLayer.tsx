335217cf1ef45793bc6193cca3e768889ca54f3e
Евгений Пятков <jackuait@bk.ru>
2022-11-28 08:56:05 +0300

    chore(RenderLayer): replace ontouchend with ontouchstart to reduce confusion

--- packages/react-ui/internal/RenderLayer/RenderLayer.tsx
+++ packages/react-ui/internal/RenderLayer/RenderLayer.tsx
@@ -86,7 +86,9 @@ export class RenderLayer extends React.Component<RenderLayerProps> {
     this.focusOutsideListenerToken = listenFocusOutside(() => [node], this.handleFocusOutside);
     window.addEventListener('blur', this.handleFocusOutside);
     document.addEventListener(
-      'ontouchend' in document.documentElement && 'onpointerup' in document.documentElement ? 'pointerup' : 'mousedown',
+      'ontouchstart' in document.documentElement && 'onpointerup' in document.documentElement
+        ? 'pointerup'
+        : 'mousedown',
       this.handleNativeDocClick,
     );
   }
@@ -99,7 +101,9 @@ export class RenderLayer extends React.Component<RenderLayerProps> {
 
     window.removeEventListener('blur', this.handleFocusOutside);
     document.removeEventListener(
-      'ontouchend' in document.documentElement && 'onpointerup' in document.documentElement ? 'pointerup' : 'mousedown',
+      'ontouchstart' in document.documentElement && 'onpointerup' in document.documentElement
+        ? 'pointerup'
+        : 'mousedown',
       this.handleNativeDocClick,
     );
   }
