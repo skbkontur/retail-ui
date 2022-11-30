30e8cc6748aa876efc0d8edff5e44891b08ef2a4
lossir <m.pakhomov@skbkontur.ru>
2022-11-30 15:37:12 +0500

    refactor(icons2022): add icon `ArrowCDownIcon` for `Select`

---
+++ packages/react-ui/internal/icons2022/ArrowCDownIcon.tsx
@@ -0,0 +1,13 @@
+import { ArrowCDownIcon16Regular } from './ArrowCDownIcon16Regular';
+import { ArrowCDownIcon20Regular } from './ArrowCDownIcon20Regular';
+import { ArrowCDownIcon24Regular } from './ArrowCDownIcon24Regular';
+import { iconSizer } from './iconSizer';
+
+export const ArrowCDownIcon = iconSizer(
+  {
+    small: ArrowCDownIcon16Regular,
+    medium: ArrowCDownIcon20Regular,
+    large: ArrowCDownIcon24Regular,
+  },
+  'ArrowCDownIcon',
+);
