fb176848e02de0aa5e13bc6ad73f8eb6962bc088
HelenaIsh <ishmatova@skbkontur.ru>
2022-12-16 09:07:41 +0300

    chore: add warnings and todo

--- packages/react-ui/lib/Upgrades.ts
+++ packages/react-ui/lib/Upgrades.ts
@@ -6,12 +6,21 @@ let canModifySpecificityLevel = true;
 let specificityClassName = 'react-ui';
 let canModifySpecificityClassName = true;
 
+//TODO Delete Upgrade in major release if it still doesn't work (https://tech.skbkontur.ru/react-ui/#/Migration)
 export const Upgrade = {
   getSpecificityLevel() {
+    warning(
+      false,
+      "getSpecificityLevel doesn't work. For more information please visit https://tech.skbkontur.ru/react-ui/#/Migration",
+    );
     canModifySpecificityLevel = false;
     return specificityLevel;
   },
   setSpecificityLevel(level: number) {
+    warning(
+      true,
+      "setSpecificityLevel doesn't work. For more information please visit https://tech.skbkontur.ru/react-ui/#/Migration",
+    );
     if (canModifySpecificityLevel) {
       specificityLevel = level;
     } else {
@@ -19,10 +28,18 @@ export const Upgrade = {
     }
   },
   getSpecificityClassName() {
+    warning(
+      false,
+      "getSpecificityClassName doesn't work. For more information please visit https://tech.skbkontur.ru/react-ui/#/Migration",
+    );
     canModifySpecificityClassName = false;
     return specificityClassName;
   },
   setSpecificityClassName(className: string) {
+    warning(
+      false,
+      "setSpecificityClassName doesn't work. For more information please visit https://tech.skbkontur.ru/react-ui/#/Migration",
+    );
     if (canModifySpecificityClassName) {
       specificityClassName = className;
     } else {
