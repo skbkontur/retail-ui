41e89c8b4931206e8cfbf68dbe1f0637c2aa3362
Евгений Пятков <jackuait@bk.ru>
2022-12-21 03:42:45 +0300

    chore(Autocomplete): pass prop pos to DropdownContainer

--- packages/react-ui/components/Autocomplete/Autocomplete.tsx
+++ packages/react-ui/components/Autocomplete/Autocomplete.tsx
@@ -7,7 +7,7 @@ import { ThemeContext } from '../../lib/theming/ThemeContext';
 import { Theme } from '../../lib/theming/Theme';
 import { isKeyArrowDown, isKeyArrowUp, isKeyEnter, isKeyEscape } from '../../lib/events/keyboard/identifiers';
 import { Input, InputProps } from '../Input';
-import { DropdownContainer } from '../../internal/DropdownContainer';
+import { DropdownContainer, DropdownContainerProps } from '../../internal/DropdownContainer';
 import { Menu } from '../../internal/Menu';
 import { MenuItem } from '../MenuItem';
 import { RenderLayer } from '../../internal/RenderLayer';
@@ -41,6 +41,7 @@ function renderItem(item: any) {
 
 export interface AutocompleteProps
   extends CommonProps,
+    Pick<DropdownContainerProps, 'pos'>,
     Override<
       InputProps,
       {
@@ -240,7 +241,7 @@ export class Autocomplete extends React.Component<AutocompleteProps, Autocomplet
 
   private renderMenu(): React.ReactNode {
     const items = this.state.items;
-    const { menuMaxHeight, hasShadow, menuWidth, width, preventWindowScroll, menuAlign, disablePortal } =
+    const { menuMaxHeight, hasShadow, menuWidth, width, preventWindowScroll, menuAlign, disablePortal, pos } =
       this.getProps();
     const menuProps = {
       ref: this.refMenu,
@@ -254,7 +255,13 @@ export class Autocomplete extends React.Component<AutocompleteProps, Autocomplet
     }
 
     return (
-      <DropdownContainer offsetY={1} getParent={this.getAnchor} align={menuAlign} disablePortal={disablePortal}>
+      <DropdownContainer
+        offsetY={1}
+        getParent={this.getAnchor}
+        align={menuAlign}
+        disablePortal={disablePortal}
+        pos={pos}
+      >
         <Menu {...menuProps}>{this.getItems()}</Menu>
       </DropdownContainer>
     );
