2ed5290f174323634e2003b8a812d765c8f72dac
Евгений Пятков <jackuait@bk.ru>
2023-01-12 13:32:59 +0300

    test(ErrorRenderer): tooltip and text should not render anything when validationInfo is not provided

--- packages/react-ui-validations/tests/ErrorRenderer.test.tsx
+++ packages/react-ui-validations/tests/ErrorRenderer.test.tsx
@@ -37,4 +37,29 @@ describe('ErrorRenderer', () => {
 
     expect(screen.getByTestId(dataTid)).toBeInTheDocument();
   });
+
+  it('should not render text error message when validationInfo is not provided', () => {
+    const input = render(
+      <ValidationWrapper validationInfo={null} renderMessage={text('right')}>
+        <Input />
+      </ValidationWrapper>,
+    );
+
+    userEvent.click(screen.getByRole('textbox'));
+
+    expect(input.container.querySelector('[data-validation-message="text"]')).toBeEmptyDOMElement();
+  });
+
+  it('should not render tooltip error message when validationInfo is not provided', () => {
+    const dataTid = 'data-tid';
+    render(
+      <ValidationWrapper validationInfo={null} renderMessage={tooltip('left bottom', { dataTid })}>
+        <Input />
+      </ValidationWrapper>,
+    );
+
+    userEvent.click(screen.getByRole('textbox'));
+
+    expect(screen.queryByTestId(dataTid)).not.toBeInTheDocument();
+  });
 });
