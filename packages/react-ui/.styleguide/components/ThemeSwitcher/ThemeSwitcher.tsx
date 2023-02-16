7ed8095872a7286c3efb87bfe7f8ebb08b1ea9fb
lossir <m.pakhomov@skbkontur.ru>
2023-02-16 15:45:42 +0500

    docs: change theme of theme switcher to THEME_2022_DARK

--- packages/react-ui/.styleguide/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ packages/react-ui/.styleguide/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -2,14 +2,14 @@ import React, { useContext } from 'react';
 import Context from 'react-styleguidist/lib/client/rsg-components/Context';
 
 import { Select } from '../../../components/Select';
-import { DARK_THEME } from '../../../lib/theming/themes/DarkTheme';
+import { THEME_2022_DARK } from '../../../lib/theming/themes/Theme2022Dark';
 import { ThemeContext } from '../../../lib/theming/ThemeContext';
 import { THEMES } from './constants';
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useContext(Context);
   return (
-    <ThemeContext.Provider value={DARK_THEME}>
+    <ThemeContext.Provider value={THEME_2022_DARK}>
       <Select
         value={theme}
         items={Object.keys(THEMES)}
