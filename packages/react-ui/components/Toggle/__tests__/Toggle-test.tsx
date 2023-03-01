31ed44c0fcc6c8923f5ef24cfee81c3f4158d6fd
Kamilia Taranova <taranova.kn@skbkontur.ru>
2023-03-01 10:29:14 +0300

    Add tests

--- packages/react-ui/components/Toggle/__tests__/Toggle-test.tsx
+++ packages/react-ui/components/Toggle/__tests__/Toggle-test.tsx
@@ -1,5 +1,7 @@
 ﻿import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { red } from 'react-ui/lib/styles/ColorFunctions';
+import userEvent from '@testing-library/user-event';
 
 import { Toggle } from '../Toggle';
 
@@ -21,5 +23,57 @@ describe('Toggle', () => {
     const toggle = screen.getByRole('switch');
     expect(toggle).toHaveAttribute('aria-describedby', 'elementId');
     expect(toggle).toHaveAccessibleDescription('Description');
+  });
+
+  it('focuses by autofocus', () => {
+    render(<Toggle autoFocus>Toggle</Toggle>);
+
+    const toggle = screen.getByRole('switch', { name: 'Toggle' });
+
+    expect(toggle).toHaveFocus();
+  });
+
+  it('handels onValueChange', () => {
+    const onValueChange = jest.fn();
+
+    render(<Toggle onValueChange={onValueChange}>Toggle</Toggle>);
+
+    const toggle = screen.getByRole('switch', { name: 'Toggle' });
+    userEvent.click(toggle);
+    expect(onValueChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('handels onChange', () => {
+    const onChange = jest.fn();
+
+    render(<Toggle onChange={onChange}>Toggle</Toggle>);
+
+    const toggle = screen.getByRole('switch', { name: 'Toggle' });
+    userEvent.click(toggle);
+    expect(onChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('handels onFocus', () => {
+    const onFocus = jest.fn();
+
+    render(<Toggle onFocus={onFocus}>Toggle</Toggle>);
+
+    const toggle = screen.getByRole('switch', { name: 'Toggle' });
+    userEvent.click(toggle);
+    expect(onFocus).toHaveBeenCalledTimes(1);
+  });
+
+  it('handels onBlur', () => {
+    const onBlur = jest.fn();
+
+    render(<Toggle onBlur={onBlur}>Toggle</Toggle>);
+
+    const toggle = screen.getByRole('switch', { name: 'Toggle' });
+    userEvent.click(toggle);
+
+    expect(toggle).toHaveFocus();
+
+    toggle.blur();
+    expect(onBlur).toHaveBeenCalledTimes(1);
   });
 });
