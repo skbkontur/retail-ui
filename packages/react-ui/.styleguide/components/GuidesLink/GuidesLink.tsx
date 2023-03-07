60262bcafa70ba72dca3b739dd184926f7524bd5
lossir <m.pakhomov@skbkontur.ru>
2023-03-07 14:36:07 +0500

    docs(GuidesLink): remove `Styled` due to its uselessness

--- packages/react-ui/.styleguide/components/GuidesLink/GuidesLink.tsx
+++ packages/react-ui/.styleguide/components/GuidesLink/GuidesLink.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import Styled from 'react-styleguidist/lib/client/rsg-components/Styled';
 import { Link } from '../../../components/Link';
 import { fetch } from '../../../lib/net/fetch';
 import Context from 'react-styleguidist/lib/client/rsg-components/Context';
 import { ThemeContext } from '../../../lib/theming/ThemeContext';
 import { THEMES } from '../ThemeSwitcher/constants';
 
-interface Classes {
-  root: React.CSSProperties;
-}
-
 interface GuidesLinkProps {
-  classes: { [name: string]: string };
   componentName: string;
 }
 
-const styles = ({ baseBackground }: any): Classes => ({
-  root: {
-    backgroundColor: baseBackground,
-  },
-});
-
 const GUIDES_LINK = 'https://guides.kontur.ru/components/';
 
-const GuidesLink: React.FunctionComponent<GuidesLinkProps> = ({ componentName, classes }) => {
+const GuidesLink: React.FunctionComponent<GuidesLinkProps> = ({ componentName }) => {
   const [hasGuide, setHasGuide] = React.useState(false);
   const context = React.useContext<{ theme: keyof typeof THEMES }>(Context);
 
@@ -35,13 +23,11 @@ const GuidesLink: React.FunctionComponent<GuidesLinkProps> = ({ componentName, c
 
   return (
     <ThemeContext.Provider value={THEMES[context.theme]}>
-      <div className={classes.root}>
-        <Link loading={!hasGuide} target="_blank" href={componentLink}>
-          Компонент в гайдах
-        </Link>
-      </div>
+      <Link loading={!hasGuide} target="_blank" href={componentLink}>
+        Компонент в гайдах
+      </Link>
     </ThemeContext.Provider>
   );
 };
 
-export default Styled(styles)(GuidesLink);
+export default GuidesLink;
