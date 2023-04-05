6f72242390b20754bfa806ea2b4b08a7dc457873
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2023-04-05 12:32:50 +0500

    chore: skips as regexp

--- packages/react-ui/components/Button/__tests__/Button.hermione.js
+++ packages/react-ui/components/Button/__tests__/Button.hermione.js
@@ -4,29 +4,26 @@ describe('Button', () => {
       await this.browser.assertView('idle', '#test-element');
     });
 
-    hermione.skip.in(['ie11', 'ie118px', 'ie11Flat8px', 'ie11Dark'], 'hover');
-    hermione.skip.in(['chrome8px', 'chromeFlat8px', 'chrome', 'chromeDark'], 'flacky');
+    hermione.skip.in(/^ie/, 'hover');
+    hermione.skip.in(/^chrome/, 'flacky');
     it('hover', async function () {
       await this.browser.$('[data-tid~="test-button"]').moveTo();
       await this.browser.assertView('hover', '#test-element');
     });
 
-    hermione.skip.in(['chrome8px', 'chromeFlat8px', 'chrome', 'chromeDark'], 'flacky');
+    hermione.skip.in(/^chrome/, 'flacky');
     it('pressed', async function () {
       await this.browser.$('[data-tid~="test-button"]').click({ skipRelease: true });
       await this.browser.assertView('pressed', '#test-element');
     });
 
-    hermione.skip.in(['chrome8px', 'chromeFlat8px', 'chrome', 'chromeDark'], 'flacky');
+    hermione.skip.in(/^chrome/, 'flacky');
     it('clicked', async function () {
       await this.browser.$('[data-tid~="test-button"]').click();
       await this.browser.assertView('clicked', '#test-element');
     });
 
-    hermione.skip.in(
-      ['firefox', 'firefox8px', 'firefoxFlat8px', 'firefoxDark'],
-      'focus goes out of page and breaks other tests',
-    );
+    hermione.skip.in(/^firefox/, 'focus goes out of page and breaks other tests');
     it('tabPress', async function () {
       await this.browser.keys('Tab');
       await this.browser.assertView('tabPress', '#test-element');
