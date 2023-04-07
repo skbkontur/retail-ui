53b8eb39b399b995d68424813d109bdd218764d7
Kamilia Taranova <taranova.kn@skbkontur.ru>
2023-04-07 11:38:17 +0300

    Refactor tests

--- packages/react-ui/components/Gapped/__tests__/Gapped-test.tsx
+++ packages/react-ui/components/Gapped/__tests__/Gapped-test.tsx
@@ -1,19 +1,64 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 
-import { Gapped } from '../';
+import { Gapped, GappedDataTids } from '../';
 
 describe('Gapped', () => {
+  const defaultGap = 8;
   it("First child inside Gapped shouldn't contain margin-left", () => {
-    const wrapper = render(
+    render(
       <Gapped>
         {null}
         <div id="test-div-1" />
         <div id="test-div-2" />
       </Gapped>,
     );
+    expect(document.getElementById('test-div-1')?.parentElement).not.toHaveStyle(`margin-left: ${defaultGap}px`);
+    expect(document.getElementById('test-div-2')?.parentElement).toHaveStyle(`margin-left: ${defaultGap}px`);
+  });
 
-    expect(wrapper.find('#test-div-1').parent().prop('style')).not.toContain('margin-left');
-    expect(wrapper.find('#test-div-2').parent().prop('style')).toContain('margin-left');
+  it('Renders gapped with vertical prop', () => {
+    render(
+      <Gapped vertical>
+        <div id="test-div-1" />
+        <div id="test-div-2" />
+      </Gapped>,
+    );
+
+    expect(screen.getByTestId(GappedDataTids.vertical)).toBeInTheDocument();
+  });
+  it('Renders gapped with horizontal layout by default', () => {
+    render(
+      <Gapped>
+        <div id="test-div-1" />
+        <div id="test-div-2" />
+      </Gapped>,
+    );
+
+    expect(screen.getByTestId(GappedDataTids.horizontal)).toBeInTheDocument();
+  });
+
+  it('Renders horizontal orientated gap with given value', () => {
+    const gap = 20;
+
+    render(
+      <Gapped gap={gap}>
+        <div id="test-div-1" />
+        <div id="test-div-2" />
+      </Gapped>,
+    );
+    expect(document.getElementById('test-div-2')?.parentElement).toHaveStyle(`margin-left: ${gap}px`);
+  });
+
+  it('Renders vertical orientated gap with given value', () => {
+    const gap = 20;
+
+    render(
+      <Gapped gap={gap} vertical>
+        <div id="test-div-1" />
+        <div id="test-div-2" />
+      </Gapped>,
+    );
+    expect(document.getElementById('test-div-2')?.parentElement).toHaveStyle(`padding-top: ${gap}px`);
   });
 });
