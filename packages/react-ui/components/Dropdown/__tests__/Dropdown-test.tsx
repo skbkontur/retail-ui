2bf89f727f5308d3a6667adbb24c0546a256aa04
Kamilia Taranova <taranova.kn@skbkontur.ru>
2023-04-07 12:39:49 +0300

    Refactor tests

--- packages/react-ui/components/Dropdown/__tests__/Dropdown-test.tsx
+++ packages/react-ui/components/Dropdown/__tests__/Dropdown-test.tsx
@@ -1,49 +1,51 @@
-import { mount } from 'enzyme';
+//import { mount } from 'enzyme';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-import { Dropdown } from '../Dropdown';
-import { MenuItem } from '../../MenuItem';
+import { Dropdown, DropdownDataTids } from '../Dropdown';
+import { MenuItem, MenuItemDataTids } from '../../MenuItem';
 import { Select, SelectState } from '../../Select';
 
 describe('Dropdown', () => {
+  const caption = <span id="test-caption">Open</span>;
+  const menuItem = <MenuItem>Menu item</MenuItem>;
+
   it('renders', () => {
-    const wrapper = mount(
+    render(
       <Dropdown caption="button">
         <MenuItem>Menu item</MenuItem>
       </Dropdown>,
     );
 
-    expect(wrapper.exists()).toBe(true);
+    expect(screen.getByTestId(DropdownDataTids.root)).toBeInTheDocument();
   });
 
   it('opens and closes', () => {
-    const caption = <span id="test-caption">Open</span>;
-    const menuItem = <MenuItem>Menu item</MenuItem>;
-    const wrapper = mount(<Dropdown caption={caption}>{menuItem}</Dropdown>);
+    render(<Dropdown caption={caption}>{menuItem}</Dropdown>);
+    //is menu open check
+    expect(screen.queryByTestId(MenuItemDataTids.root)).not.toBeInTheDocument();
 
-    wrapper.find('#test-caption').simulate('click');
+    document.getElementById('test-caption')?.click();
 
-    const selectInstance = wrapper.find(Select).instance();
+    expect(screen.getByTestId(MenuItemDataTids.root)).toBeInTheDocument();
 
-    expect((selectInstance.state as SelectState<JSX.Element>).opened).toBeTruthy();
-
-    wrapper.find('#test-caption').simulate('click');
-    expect((selectInstance.state as SelectState<JSX.Element>).opened).toBeFalsy();
+    document.getElementById('test-caption')?.click();
+    expect(screen.queryByTestId(MenuItemDataTids.root)).not.toBeInTheDocument();
   });
 
-  it('Pass props to select', () => {
-    const caption = <span id="test-caption">Open</span>;
-    const menuItem = <MenuItem>Menu item</MenuItem>;
-    const wrapper = mount(<Dropdown caption={caption}>{menuItem}</Dropdown>);
+  // it('Pass props to select', () => {
+  //   // const caption = <span id="test-caption">Open</span>;
+  //   // const menuItem = <MenuItem>Menu item</MenuItem>;
+  //   const wrapper = mount(<Dropdown caption={caption}>{menuItem}</Dropdown>);
 
-    const select = wrapper.find(Select);
+  //   const select = wrapper.find(Select);
 
-    expect(select.prop('value')).toEqual(caption);
-    expect(select.prop('items')).toHaveLength(1);
+  //   expect(select.prop('value')).toEqual(caption);
+  //   expect(select.prop('items')).toHaveLength(1);
 
-    expect(React.isValidElement(select.prop<React.ReactChild[]>('items')[0])).toBeTruthy();
-  });
+  //   expect(React.isValidElement(select.prop<React.ReactChild[]>('items')[0])).toBeTruthy();
+  // });
 
   it('props aria-describedby applied correctly', () => {
     render(
@@ -55,5 +57,30 @@ describe('Dropdown', () => {
     const dropdown = screen.getByRole('button');
     expect(dropdown).toHaveAttribute('aria-describedby', 'elementId');
     expect(dropdown).toHaveAccessibleDescription('Description');
+  });
+
+  it('method open works', () => {
+    const dropdownRef = React.createRef<Dropdown>();
+
+    render(<Dropdown caption={caption} ref={dropdownRef}>{menuItem}</Dropdown>);
+    //is menu open check
+    expect(screen.queryByTestId(MenuItemDataTids.root)).not.toBeInTheDocument();
+
+    dropdownRef.current?.open();
+    expect(screen.getByTestId(MenuItemDataTids.root)).toBeInTheDocument();
+  });
+
+  it('method close works', () => {
+    const dropdownRef = React.createRef<Dropdown>();
+
+    render(<Dropdown caption={caption} ref={dropdownRef}>{menuItem}</Dropdown>);
+    //is menu open check
+    expect(screen.queryByTestId(MenuItemDataTids.root)).not.toBeInTheDocument();
+
+    dropdownRef.current?.open();
+    expect(screen.getByTestId(MenuItemDataTids.root)).toBeInTheDocument();
+
+    dropdownRef.current?.close();
+    expect(screen.queryByTestId(MenuItemDataTids.root)).not.toBeInTheDocument();
   });
 });
