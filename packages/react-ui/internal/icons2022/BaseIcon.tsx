cfe8dc755834200066e55154acb00116865adf74
lossir <lossir1@gmail.com>
2023-04-27 13:25:57 +0600

    test(BaseIcon): fix `ref` usage

--- packages/react-ui/internal/icons2022/BaseIcon.tsx
+++ packages/react-ui/internal/icons2022/BaseIcon.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import { ReactUIComponentWithRef } from '../../lib/forwardRefAndName';
+import { forwardRefAndName, ReactUIComponentWithRef } from '../../lib/forwardRefAndName';
 import { SvgIconProps } from '../icons/16px';
 
 export type IconProps = {
-  ref?: React.ForwardedRef<SVGSVGElement>;
   size?: number;
   viewBoxSize?: number;
   color?: string;
@@ -15,34 +14,28 @@ export type BothIconType =
   | ReactUIComponentWithRef<HTMLElement, SvgIconProps>
   | ReactUIComponentWithRef<SVGSVGElement, IconProps>;
 
-export const BaseIcon = ({
-  ref,
-  color,
-  size,
-  viewBoxSize = 16,
-  style,
-  disableCompensation = true,
-  children,
-  ...rest
-}: IconProps) => {
-  return (
-    <svg
-      ref={ref}
-      width={size || viewBoxSize}
-      height={size || viewBoxSize}
-      style={{
-        fill: color ?? 'currentColor',
-        color,
-        marginBottom: disableCompensation ? 0 : '-0.1875em',
-        flexShrink: 0,
-        ...style,
-      }}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox={`0 0 ${viewBoxSize} ${viewBoxSize}`}
-      aria-hidden
-      {...rest}
-    >
-      {children}
-    </svg>
-  );
-};
+export const BaseIcon = forwardRefAndName<SVGSVGElement, IconProps>(
+  'BaseIcon',
+  ({ color, size, viewBoxSize = 16, style, disableCompensation = true, children, ...rest }: IconProps, ref) => {
+    return (
+      <svg
+        ref={ref}
+        width={size || viewBoxSize}
+        height={size || viewBoxSize}
+        style={{
+          fill: color ?? 'currentColor',
+          color,
+          marginBottom: disableCompensation ? 0 : '-0.1875em',
+          flexShrink: 0,
+          ...style,
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={`0 0 ${viewBoxSize} ${viewBoxSize}`}
+        aria-hidden
+        {...rest}
+      >
+        {children}
+      </svg>
+    );
+  },
+);
