60d0b65f7f8c8cc4cafb3bfc7f32b703d1a9b05d
Kamilia Taranova <taranova.kn@skbkontur.ru>
2023-05-16 15:30:50 +0300

    Add tests

--- packages/react-ui/components/CurrencyLabel/__tests__/CurrencyLabel.test.tsx
+++ packages/react-ui/components/CurrencyLabel/__tests__/CurrencyLabel.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import { CurrencyLabel, CurrencyLabelDataTids } from '../CurrencyLabel';
+import { MAX_SAFE_DIGITS } from '../../CurrencyInput/constants';
 
 describe('CurrencyLabel', () => {
   it('should correctly format value', () => {
@@ -33,5 +34,35 @@ describe('CurrencyLabel', () => {
     const node = screen.getByTestId(CurrencyLabelDataTids.root);
 
     expect(node).toHaveTextContent(/^12 356,14$/);
+  });
+
+  it('should throw error if fractionDigits exceed MAX_SAFE_DIGITS', () => {
+    const fractionDigits = MAX_SAFE_DIGITS + 1;
+    const props = { value: 123.45, fractionDigits };
+    render(<CurrencyLabel {...props} />);
+
+    expect(() => {
+      screen.getByText(`[CurrencyLabel]: Prop 'fractionDigits' exceeds ${MAX_SAFE_DIGITS}`);
+    }).toThrow();
+  });
+
+  it('should throw error if fractionDigits is not integer', () => {
+    const props = { value: 123.45, fractionDigits: 2.5 };
+    render(<CurrencyLabel {...props} />);
+
+    expect(() => {
+      screen.getByText(`[CurrencyLabel]: Prop 'fractionDigits' is not integer`);
+    }).toThrow();
+  });
+
+  it('should throw error if fractionDigits is less than the fractional part of the value', () => {
+    const props = { value: 123.4567, fractionDigits: 2 };
+    render(<CurrencyLabel {...props} />);
+
+    expect(() => {
+      screen.getByText(
+        `[CurrencyLabel]: Prop 'fractionDigits' less than fractional part of the 'value' property, 'value' will not be cutted`,
+      );
+    }).toThrow();
   });
 });
