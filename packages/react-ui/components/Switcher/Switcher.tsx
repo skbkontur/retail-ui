c2ce568f677f6f27db3e32de79765e3ae079341c
Kamilia Taranova <taranova.kn@skbkontur.ru>
2023-05-26 09:54:39 +0300

    Add tests

--- packages/react-ui/components/Switcher/Switcher.tsx
+++ packages/react-ui/components/Switcher/Switcher.tsx
@@ -20,6 +20,7 @@ type SwitcherItems = string | SwitcherItem;
 
 export const SwitcherDataTids = {
   root: 'Switcher__root',
+  selectedItem: 'Switcher__selectedItem'
 } as const;
 
 export interface SwitcherProps extends CommonProps {
@@ -236,6 +237,10 @@ export class Switcher extends React.Component<SwitcherProps, SwitcherState> {
         ...commonButtonProps,
         ...customButtonProps,
       };
+
+      if (value === itemValue) {
+        buttonProps['data-tid'] = SwitcherDataTids.selectedItem
+      }
 
       const renderDefault = () => this.renderDefaultItem(label, itemValue, buttonProps);
 
--- packages/react-ui/components/Switcher/__tests__/Switcher-test.tsx
+++ packages/react-ui/components/Switcher/__tests__/Switcher-test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { Switcher, SwitcherDataTids } from '../Switcher';
-import { ButtonDataTids } from '../../Button';
+
 
 describe('Switcher', () => {
   const items = [
@@ -17,18 +18,79 @@ describe('Switcher', () => {
     {
       label: 'Three',
       value: '333',
+      buttonProps: {
+        disabled: true,
+      }
+    },
+    {
+      label: 'Four',
+      value: '444',
     }
   ];
 
-  const Comp = () => {
+  const Comp = (props = {}) => {
     const [value, setValue] = React.useState<string>();
     return (
-      <Switcher items={items} value={value} onValueChange={setValue} />
+      <Switcher items={items} value={value} onValueChange={setValue} {...props} />
     );
   };
 
   it('should render default Switcher', () => {
     const renderSwitcher = () => render(<Switcher items={['One', 'Two', 'Three']} />);
     expect(renderSwitcher).not.toThrow();
+  });
+
+  it('should select item', () => {
+    render(<Comp />);
+
+    const buttons = screen.getAllByRole('button');
+    expect(screen.queryByTestId(SwitcherDataTids.selectedItem)).not.toBeInTheDocument();
+
+    userEvent.click(buttons[1]);
+
+    expect(screen.getByTestId(SwitcherDataTids.selectedItem)).toBeInTheDocument();
+    // eslint-disable-next-line jest-dom/prefer-to-have-text-content
+    expect(buttons[1].textContent).toEqual(screen.getByTestId(SwitcherDataTids.selectedItem).textContent)
+  });
+
+  it('should select item if enter key is pressed and not buttonProps.disabled', () => {
+    render(<Comp />);
+    const buttons = screen.getAllByRole('button');
+
+    expect(screen.queryByTestId(SwitcherDataTids.selectedItem)).not.toBeInTheDocument();
+
+    buttons[0].focus();
+    userEvent.keyboard('{enter}');
+
+    // eslint-disable-next-line jest-dom/prefer-to-have-text-content
+    expect(buttons[0].textContent).toEqual(screen.getByTestId(SwitcherDataTids.selectedItem).textContent)
+  });
+
+  it('should not select item if enter key is pressed and buttonProps.disabled', () => {
+    render(<Comp />);
+    const buttons = screen.getAllByRole('button');
+
+    expect(screen.queryByTestId(SwitcherDataTids.selectedItem)).not.toBeInTheDocument();
+
+    buttons[2].focus();
+    userEvent.keyboard('{enter}');
+
+    expect(screen.queryByTestId(SwitcherDataTids.selectedItem)).not.toBeInTheDocument();
+  });
+
+  it('should handle focus', () => {
+    render(<Comp />);
+    const buttons = screen.getAllByRole('button');
+    buttons[0].focus();
+    expect(buttons[0]).toHaveFocus();
+  });
+
+  it('should handle blur', () => {
+    render(<Comp />);
+    const buttons = screen.getAllByRole('button');
+    buttons[0].focus();
+    expect(buttons[0]).toHaveFocus();
+    buttons[0].blur();
+    expect(buttons[0]).not.toHaveFocus();
   });
 });
