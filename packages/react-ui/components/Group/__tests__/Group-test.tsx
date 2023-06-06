024adc31f60e760f278d733b849368967d2ed09a
Kamilia Taranova <taranova.kn@skbkontur.ru>
2023-06-06 17:00:44 +0300

    Add tests

--- packages/react-ui/components/Group/__tests__/Group-test.tsx
+++ packages/react-ui/components/Group/__tests__/Group-test.tsx
@@ -1,52 +1,50 @@
-import { mount, render } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 
-import { Group } from '../Group';
+import { Group, GroupDataTids } from '../Group';
 import { Input } from '../../Input';
 import { Button } from '../../Button';
 import { styles } from '../Group.styles';
 
+
 describe('Group', () => {
   it('renders', () => {
-    const wrapper = mount(
+    render(
       <Group>
         <Input />
         <Button>Test</Button>
       </Group>,
     );
 
-    expect(wrapper.exists()).toBe(true);
+    expect(screen.getByTestId(GroupDataTids.root)).toBeInTheDocument();
   });
 
   it('main element in group gets "stretch" class', () => {
-    const wrapper = render(
+    render(
       <Group>
-        <Input width="100%" id="test-input" />
-        <Button>Test</Button>
+        <Input width="100%" data-tid="test-input" />
+        <Button data-tid="test-button">Test</Button>
       </Group>,
     );
-
-    const wrappingElements = wrapper.find(`.${styles.root()} > *`);
-
-    expect(wrappingElements.eq(0).prop('class')).toContain(styles.stretch());
-    expect(wrappingElements.eq(1).prop('class')).toContain(styles.fixed());
+    const wrappingElements = screen.getByTestId(GroupDataTids.root).childNodes;
+    expect(wrappingElements[0]).toHaveClass(styles.stretch());
+    expect(wrappingElements[1]).toHaveClass(styles.fixed());
   });
 
   it('group does not render wrong child', () => {
-    const wrapper = render(<Group>{undefined}</Group>);
+    render(<Group>{undefined}</Group>);
 
-    const wrappingElements = wrapper.find(`.${styles.root()} *`);
-
+    const wrappingElements = screen.getByTestId(GroupDataTids.root).childNodes;
+    // eslint-disable-next-line jest-dom/prefer-in-document
     expect(wrappingElements).toHaveLength(0);
   });
 
   it('width is applied to the root from props', () => {
-    const wrapper = render(
+    render(
       <Group width="100px">
         <div />
       </Group>,
     );
-
-    expect(wrapper.find(`.${styles.root()} > *`).parent().prop('style').width).toBe('100px');
+    expect(screen.getByTestId(GroupDataTids.root)).toHaveStyle('width: 100px');
   });
 });
