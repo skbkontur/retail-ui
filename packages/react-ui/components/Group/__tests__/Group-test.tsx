840505017414b5c4a1ac05bd2a0032cc03e56c70
Taranova Kamilia <68959081+taranovakn@users.noreply.github.com>
2023-06-09 07:21:09 +0300

    test(Group): remove using enzyme (#3180)

--- packages/react-ui/components/Group/__tests__/Group-test.tsx
+++ packages/react-ui/components/Group/__tests__/Group-test.tsx
@@ -1,52 +1,36 @@
-import { mount, render } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 
-import { Group } from '../Group';
+import { Group, GroupDataTids } from '../Group';
 import { Input } from '../../Input';
 import { Button } from '../../Button';
-import { styles } from '../Group.styles';
 
 describe('Group', () => {
   it('renders', () => {
-    const wrapper = mount(
+    render(
       <Group>
         <Input />
         <Button>Test</Button>
       </Group>,
     );
 
-    expect(wrapper.exists()).toBe(true);
-  });
-
-  it('main element in group gets "stretch" class', () => {
-    const wrapper = render(
-      <Group>
-        <Input width="100%" id="test-input" />
-        <Button>Test</Button>
-      </Group>,
-    );
-
-    const wrappingElements = wrapper.find(`.${styles.root()} > *`);
-
-    expect(wrappingElements.eq(0).prop('class')).toContain(styles.stretch());
-    expect(wrappingElements.eq(1).prop('class')).toContain(styles.fixed());
+    expect(screen.getByTestId(GroupDataTids.root)).toBeInTheDocument();
   });
 
   it('group does not render wrong child', () => {
-    const wrapper = render(<Group>{undefined}</Group>);
+    render(<Group>{undefined}</Group>);
 
-    const wrappingElements = wrapper.find(`.${styles.root()} *`);
-
+    const wrappingElements = screen.getByTestId(GroupDataTids.root).childNodes;
+    // eslint-disable-next-line jest-dom/prefer-in-document
     expect(wrappingElements).toHaveLength(0);
   });
 
   it('width is applied to the root from props', () => {
-    const wrapper = render(
+    render(
       <Group width="100px">
         <div />
       </Group>,
     );
-
-    expect(wrapper.find(`.${styles.root()} > *`).parent().prop('style').width).toBe('100px');
+    expect(screen.getByTestId(GroupDataTids.root)).toHaveStyle('width: 100px');
   });
 });
