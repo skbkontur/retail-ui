9795006c799b7266fd88bb9dcf5760148c3ad12d
Max <m.pakhomov@skbkontur.ru>
2023-07-17 12:14:12 +0500

    fix(ComboBox): use `flushSync` only in React@18 (#3202)

--- packages/react-ui/internal/CustomComboBox/CustomComboBox.tsx
+++ packages/react-ui/internal/CustomComboBox/CustomComboBox.tsx
@@ -346,7 +346,10 @@ export class CustomComboBox<T> extends React.PureComponent<CustomComboBoxProps<T
       );
     };
 
-    if (sync) {
+    // Auto-batching React@18 creates problems that are fixed with flushSync
+    // https://github.com/skbkontur/retail-ui/pull/3144#issuecomment-1535235366
+    const isReact18 = React.version.search('18') === 0;
+    if (sync && isReact18) {
       return ReactDOM.flushSync(() => {
         updateState(action);
       });
