e49b3b06ee085982a90ade9c538b417986b037d5
Funkicide <knel@skbkontur.ru>
2023-08-07 13:04:44 +0500

    refactor(Menu): add variables
    This ensures compatibility with Menu.

--- packages/react-ui/components/DropdownMenu/getDropdownMenuTheme.ts
+++ packages/react-ui/components/DropdownMenu/getDropdownMenuTheme.ts
@@ -5,6 +5,9 @@ export const getDropdownMenuTheme = (theme: Theme): Theme => {
   return ThemeFactory.create(
     {
       popupMargin: theme.dropdownMenuMenuOffsetY,
+      menuBoxSizing: theme.dropdownMenuMenuBoxSizing,
+      menuOffsetY: theme.dropdownMenuMenuMarginY,
+      menuPaddingY: theme.menuLegacyPaddingY,
     },
     theme,
   );
