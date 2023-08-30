71cd487ddf43ee07c722aba4afec63aca607dd97
SchwJ <juliachirkova@e1.ru>
2023-08-30 18:42:51 +0500

    chore(Link): add comment

--- packages/react-ui/components/Link/Link.styles.ts
+++ packages/react-ui/components/Link/Link.styles.ts
@@ -50,6 +50,9 @@ export const styles = memoizeStyle({
     // При hover'е подчеркивание из прозрачного переходит в currentColor.
     // За счет наложения этого цвета на подчеркивание lineText (currentColor с половинной прозрачностью)
     // достигается эффект перехода currentColor с половинной прозрачностью до currentColor.
+
+    // Планировалось добавить transition и color-mix(in srgb, currentColor 50%, transparent) в lineText.
+    // Однако, в chrome и edge сочетание transition, color-mix и currentColor вызывает моргание при transition.
     return css`
       @supports (border-bottom-color: ${linkLineBorderBottomColor}) {
         transition: border-bottom-color ${t.transitionDuration} ${t.transitionTimingFunction};
