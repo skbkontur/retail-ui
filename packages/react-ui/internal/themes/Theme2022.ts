86d4101ec5535cc518ff61865b03ee29bfe5d0c6
Funkicide <knel@skbkontur.ru>
2023-09-19 19:05:33 +0500

    fix: sync with figma layouts

--- packages/react-ui/internal/themes/Theme2022.ts
+++ packages/react-ui/internal/themes/Theme2022.ts
@@ -78,9 +78,14 @@ export class Theme2022 extends (class {} as typeof DefaultThemeInternal) {
 
   public static btnDisabledBg = 'rgba(0, 0, 0, 0.06)';
   public static get btnDisabledTextColor() {
-    return this.textColorDisabledContrast;
+    return this.textColorDisabled;
   }
   public static btnDisabledBorderColor = 'rgba(0, 0, 0, 0.10)';
+
+  public static btnBacklessHoverBg = 'rgba(0, 0, 0, 0.06) !important';
+
+  public static btnTextBg = 'transparent !important';
+  public static btnTextHoverBg = 'rgba(0, 0, 0, 0.06) !important';
 
   public static btnBorderRadiusSmall = '8px';
   public static btnBorderRadiusMedium = '8px';
