a25baed0d3071d231b94ef59f69e4764f1c6f3c1
Elena Ishmatova <ishmatova@skbkontur.ru>
2023-10-13 13:44:13 +0600

    chore: add screenshots

--- packages/react-ui/components/Link/Link.styles.ts
+++ packages/react-ui/components/Link/Link.styles.ts
@@ -52,12 +52,14 @@ export const styles = memoizeStyle({
     // Планировалось добавить transition и color-mix(in srgb, currentColor 50%, transparent) в lineText.
     // Однако, в chrome и edge сочетание transition, color-mix и currentColor вызывает моргание при transition.
     return css`
-      transition: border-bottom-color ${t.transitionDuration} ${t.transitionTimingFunction};
-      border-bottom-style: ${t.linkLineBorderBottomStyle};
-      border-bottom-width: ${t.linkLineBorderBottomWidth};
-      border-bottom-color: transparent;
-      &:hover {
-        border-bottom-color: currentColor;
+      @supports (border-bottom-color: ${t.linkLineBorderBottomColor}) {
+        transition: border-bottom-color ${t.transitionDuration} ${t.transitionTimingFunction};
+        border-bottom-style: ${t.linkLineBorderBottomStyle};
+        border-bottom-width: ${t.linkLineBorderBottomWidth};
+        border-bottom-color: transparent;
+        &:hover {
+          border-bottom-color: currentColor;
+        }
       }
     `;
   },
@@ -81,9 +83,13 @@ export const styles = memoizeStyle({
     `;
   },
 
-  lineTextWithUnderlineOnHover() {
+  lineTextWithUnderlineOnHover(t: Theme) {
     return css`
-      border-bottom-color: transparent !important; //override
+      border-bottom-color: transparent !important;
+      transition: border-bottom-color ${t.transitionDuration} ${t.transitionTimingFunction};
+      &:hover {
+        border-bottom-color: currentColor !important;
+      }
     `;
   },
 
