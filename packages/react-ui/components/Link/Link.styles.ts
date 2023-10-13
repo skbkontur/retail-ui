146460b6c6d4d3677cc18993bcf7f7005ad84588
Elena Ishmatova <ishmatova@skbkontur.ru>
2023-10-13 13:36:11 +0600

    chore: fix

--- packages/react-ui/components/Link/Link.styles.ts
+++ packages/react-ui/components/Link/Link.styles.ts
@@ -52,14 +52,12 @@ export const styles = memoizeStyle({
     // Планировалось добавить transition и color-mix(in srgb, currentColor 50%, transparent) в lineText.
     // Однако, в chrome и edge сочетание transition, color-mix и currentColor вызывает моргание при transition.
     return css`
-      @supports (border-bottom-color: ${t.linkLineBorderBottomColor}) {
-        transition: border-bottom-color ${t.transitionDuration} ${t.transitionTimingFunction};
-        border-bottom-style: ${t.linkLineBorderBottomStyle};
-        border-bottom-width: ${t.linkLineBorderBottomWidth};
-        border-bottom-color: transparent;
-        &:hover {
-          border-bottom-color: currentColor;
-        }
+      transition: border-bottom-color ${t.transitionDuration} ${t.transitionTimingFunction};
+      border-bottom-style: ${t.linkLineBorderBottomStyle};
+      border-bottom-width: ${t.linkLineBorderBottomWidth};
+      border-bottom-color: transparent;
+      &:hover {
+        border-bottom-color: currentColor;
       }
     `;
   },
