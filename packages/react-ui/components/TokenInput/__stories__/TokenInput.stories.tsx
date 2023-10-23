ebc4d9274e19bb7134c2eb577d0f6ba4066faa93
SchwJ <juliachirkova@e1.ru>
2023-10-23 15:26:31 +0500

    chore(): increase delay in TokenInput tests

--- packages/react-ui/components/TokenInput/__stories__/TokenInput.stories.tsx
+++ packages/react-ui/components/TokenInput/__stories__/TokenInput.stories.tsx
@@ -218,7 +218,7 @@ EmptyWithReference.parameters = {
           })
           .click(this.browser.findElement({ css: '[data-comp-name~="TokenInput"]' }))
           .perform();
-        await delay(1000);
+        await delay(10000);
         await this.expect(await this.takeScreenshot()).to.matchImage('clicked');
       },
       async withMenu() {
@@ -479,14 +479,14 @@ CustomAddButton.parameters = {
           })
           .click(this.browser.findElement({ css: '[data-comp-name~="TokenInput"]' }))
           .perform();
-        await delay(1000);
+        await delay(10000);
         this.browser
           .actions({
             bridge: true,
           })
           .sendKeys('zzz')
           .perform();
-        await delay(1000);
+        await delay(10000);
 
         await this.expect(await this.takeScreenshot()).to.matchImage();
       },
