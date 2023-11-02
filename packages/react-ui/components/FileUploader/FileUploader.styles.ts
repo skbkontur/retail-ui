3454e500cd8b0ac434f7417bbf9fbbb973d251f6
Евгений Пятков <48599460+JackUait@users.noreply.github.com>
2023-11-02 19:28:20 +0300

    fix(FileUploader): hide native input in Safari (#3309)

--- packages/react-ui/components/FileUploader/FileUploader.styles.ts
+++ packages/react-ui/components/FileUploader/FileUploader.styles.ts
@@ -136,10 +136,18 @@ const styles = {
     `;
   },
 
-  fileInput() {
+  visuallyHidden() {
     return css`
-      width: 0;
-      height: 0;
+      position: absolute;
+      width: 1px;
+      height: 1px;
+      margin: -1px;
+      border: 0;
+      padding: 0;
+      white-space: nowrap;
+      clip-path: inset(100%);
+      clip: rect(0 0 0 0);
+      overflow: hidden;
     `;
   },
 
