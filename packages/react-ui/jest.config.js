c721ee8e5a1060d0530b5eb984a2568de2b0db42
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2023-11-16 09:58:57 +0500

    chore: configure jest to transform feature-flags package

--- packages/react-ui/jest.config.js
+++ packages/react-ui/jest.config.js
@@ -1,13 +1,15 @@
+const path = require('path');
+
 module.exports = {
   testResultsProcessor: 'jest-teamcity-reporter',
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
   },
   transform: {
-    '\\.[jt]sx?$': 'babel-jest',
+    '\\.[jt]sx?$': ['babel-jest', { babelrc: false, envName: 'cjs', extends: path.join(__dirname, './.babelrc.js') }],
   },
   testRegex: '__tests__(\\\\|/).*(\\.|-)test\\.(j|t)sx?$',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/test-setup.js'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/../react-ui-feature-flags/'],
 };
