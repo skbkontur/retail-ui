8374dc5615a3c5ebbc704c62bbd0298c7f600dc5
Roman Knel <knel@skbkontur.ru>
2024-01-13 16:24:31 +0300

    feat(Spinner): hide default caption under a feature flag (#3334)

--- packages/react-ui/lib/featureFlagsContext/ReactUIFeatureFlagsContext.tsx
+++ packages/react-ui/lib/featureFlagsContext/ReactUIFeatureFlagsContext.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 export interface ReactUIFeatureFlags {
   tokenInputRemoveWhitespaceFromDefaultDelimiters?: boolean;
+  spinnerLoaderRemoveDefaultCaption?: boolean;
 }
 
 export const reactUIFeatureFlagsDefault: ReactUIFeatureFlags = {
   tokenInputRemoveWhitespaceFromDefaultDelimiters: false,
+  spinnerLoaderRemoveDefaultCaption: false,
 };
 
 export const ReactUIFeatureFlagsContext = React.createContext<ReactUIFeatureFlags>(reactUIFeatureFlagsDefault);
