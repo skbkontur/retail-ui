e40c88bd8b0a47015842d8125348dc066ef30e0e
Max <m.pakhomov@skbkontur.ru>
2024-01-25 16:51:11 +0600

    feat(Textarea): add feature flag `textareaUseSafari17Workaround` (#3352)

--- packages/react-ui/lib/featureFlagsContext/ReactUIFeatureFlagsContext.tsx
+++ packages/react-ui/lib/featureFlagsContext/ReactUIFeatureFlagsContext.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 export interface ReactUIFeatureFlags {
   tokenInputRemoveWhitespaceFromDefaultDelimiters?: boolean;
+  textareaUseSafari17Workaround?: boolean;
 }
 
 export const reactUIFeatureFlagsDefault: ReactUIFeatureFlags = {
   tokenInputRemoveWhitespaceFromDefaultDelimiters: false,
+  textareaUseSafari17Workaround: false,
 };
 
 export const ReactUIFeatureFlagsContext = React.createContext<ReactUIFeatureFlags>(reactUIFeatureFlagsDefault);
