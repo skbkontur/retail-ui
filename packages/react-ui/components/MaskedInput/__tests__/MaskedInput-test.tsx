2a2d6ca906740d9adc3385ad9d2d1ec814fcd0d6
butusov <butusov@skbkontur.ru>
2024-02-05 20:16:44 +0600

    fix(maskedinput): remove InputElementProps extra callbacks
    IF-1495

--- packages/react-ui/components/MaskedInput/__tests__/MaskedInput-test.tsx
+++ packages/react-ui/components/MaskedInput/__tests__/MaskedInput-test.tsx
@@ -106,4 +106,24 @@ describe('MaskedInput', () => {
 
     expect(input).toHaveValue(expectedValue);
   });
+
+  it('onValueChange do not fire on focus', () => {
+    const valueChangeEvent = jest.fn();
+    render(<MaskedInput mask="+7 (999) 999 99 99" onValueChange={valueChangeEvent} />);
+
+    const input = screen.getByRole('textbox');
+    input.focus();
+
+    expect(valueChangeEvent).not.toHaveBeenCalled();
+  });
+
+  it('masked input calls onUnexpectedInput', () => {
+    const handleUnexpectedInput = jest.fn();
+    render(<MaskedInput mask="+7 (999) 999 99 99" onUnexpectedInput={handleUnexpectedInput} />);
+
+    const input = screen.getByRole('textbox');
+    fireEvent.input(input, { target: { value: 'A' } });
+
+    expect(handleUnexpectedInput).not.toHaveBeenCalled();
+  });
 });
