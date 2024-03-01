3401fad9dca0fcb4ec48774cdb474568bb01d5b5
Elena Ishmatova <ishmatova@skbkontur.ru>
2024-03-01 13:21:28 +0500

    chore: add test for InternalTextareaWithLayout

---
+++ packages/react-ui/internal/InternalTextareaWithLayout/__stories__/InternalTextareaWithLayout.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import OkIcon from '@skbkontur/react-icons/Ok';
+
+import { Story } from '../../../typings/stories';
+import { Gapped } from '../../../components/Gapped';
+import { InternalTextareaWithLayout } from '../InternalTextareaWithLayout';
+
+export default { title: 'InternalTextareaWithLayout' };
+export const DifferentSizes: Story = () => {
+  return (
+    <Gapped vertical>
+      <InternalTextareaWithLayout size={'small'} value={'WithLayout Size: small'} autoResize rows={1} />
+      <InternalTextareaWithLayout size={'medium'} value={'WithLayout Size: large'} autoResize rows={1} />
+      <InternalTextareaWithLayout size={'large'} value={'WithLayout Size: large'} autoResize rows={1} />
+      <InternalTextareaWithLayout
+        size={'small'}
+        value={'WithLayout Size: small'}
+        autoResize
+        rows={1}
+        leftIcon={<OkIcon />}
+        rightIcon={<OkIcon />}
+      />
+      <InternalTextareaWithLayout
+        size={'medium'}
+        value={'WithLayout Size: large'}
+        autoResize
+        rows={1}
+        leftIcon={<OkIcon />}
+        rightIcon={<OkIcon />}
+      />
+      <InternalTextareaWithLayout
+        size={'large'}
+        value={'WithLayout Size: large'}
+        autoResize
+        rows={1}
+        leftIcon={<OkIcon />}
+        rightIcon={<OkIcon />}
+      />
+    </Gapped>
+  );
+};
