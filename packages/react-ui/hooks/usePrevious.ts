a7e2c1b96f23b226e6edcb73211a148c35026d22
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2024-03-07 15:21:10 +0500

    Revert "feat(Calendar): added period (#3258)"
    This reverts commit 0fce96495b36d8e06f985f02d25cdef0c38e2608.

--- packages/react-ui/hooks/usePrevious.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const usePrevious = (primitive: string | number | undefined | null | boolean) => {
-  const prevRef = React.useRef<typeof primitive>();
-  React.useEffect(() => {
-    prevRef.current = primitive;
-  }, [primitive]);
-  return prevRef.current;
-};
