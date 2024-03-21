6b08c7e4829490694270f85c3cf473d26cfc3b88
SchwJ <juliachirkova@e1.ru>
2024-03-21 10:12:55 +0500

    fix(): split creevey tests and add screenshots

--- packages/react-ui/components/__stories__/focus.stories.tsx
+++ packages/react-ui/components/__stories__/focus.stories.tsx
@@ -1,13 +1,10 @@
-import React, { FormEvent, useCallback, useRef, useState } from 'react';
+import React, { FormEvent, useCallback, useState } from 'react';
 import { globalObject } from '@skbkontur/global-object';
 
 import { Story } from '../../typings/stories';
 import { Input } from '../Input';
-import { ThemeContext } from '../../lib/theming/ThemeContext';
-import { THEME_2022 } from '../../lib/theming/themes/Theme2022';
 import { Gapped } from '../Gapped';
 import { Autocomplete } from '../Autocomplete';
-import { Switcher } from '../Switcher';
 import { CurrencyInput } from '../CurrencyInput';
 import { Checkbox } from '../Checkbox';
 import { DateInput } from '../DateInput';
@@ -21,9 +18,10 @@ import { delay } from '../../lib/utils';
 
 export default { title: 'Focus' };
 
-export const RadioFocus: Story = () => {
+const DELAY = 1500;
+
+export const InputElementsSequentialFocus: Story = () => {
   const [isDisabled, setIsDisabled] = useState(false);
-  const inputRef = useRef<Input>(null);
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
     e.stopPropagation();
@@ -37,7 +35,6 @@ export const RadioFocus: Story = () => {
 
   const handleKeyDown = (e: KeyboardEvent) => {
     if (e.key === 'Enter') {
-      inputRef.current?.blur();
       new Promise((resolve) => {
         setIsDisabled(true);
         setTimeout(resolve, 1000);
@@ -54,101 +51,207 @@ export const RadioFocus: Story = () => {
 
   return (
     <>
-      <ThemeContext.Provider value={THEME_2022}>
-        <h1 style={{ margin: '10px' }}>Форма</h1>
-        <Gapped gap={5} style={{ border: 'black solid 1px', margin: '10px', padding: '10px' }}>
-          <form onSubmit={onSubmit}>
-            <div>
-              <Input disabled={isDisabled} value={'Input'}></Input>
-            </div>
-            <br />
-            <div>
-              <Autocomplete onValueChange={emptyFunction} value="Autocomplete" disabled={isDisabled} />
-            </div>
-            <br />
-            <div>
-              <Switcher onValueChange={emptyFunction} disabled={isDisabled} items={['1', '2', '3']} />
-            </div>
-            <br />
-            <div>
-              <CurrencyInput onValueChange={emptyFunction} disabled={isDisabled} />
-            </div>
-            <br />
-            <div>
-              <Checkbox disabled={isDisabled} />
-            </div>
-            <br />
-            <div>
-              <DateInput disabled={isDisabled} />
-            </div>
-            <br />
-            <div>
-              <DatePicker onValueChange={emptyFunction} disabled={isDisabled} />
-            </div>
-            <br />
-            <div>
-              <FileUploader disabled={isDisabled} />
-            </div>
-            <br />
-            <div>
-              <FxInput onValueChange={emptyFunction} disabled={isDisabled} />
-            </div>
-            <br />
-            <div>
-              <Radio value={'Radio'} disabled={isDisabled} />
-            </div>
-            <br />
-            <div>
-              <Toggle disabled={isDisabled} />
-            </div>
-            <br />
-            <Button use="primary" type="submit">
-              Submit
-            </Button>
-          </form>
-        </Gapped>
-      </ThemeContext.Provider>
+      <h1 style={{ margin: '10px' }}>Форма</h1>
+      <Gapped gap={5} style={{ border: 'black solid 1px', margin: '10px', padding: '10px' }}>
+        <form onSubmit={onSubmit}>
+          <div>
+            <Input disabled={isDisabled} value={'Input'}></Input>
+          </div>
+          <br />
+          <div>
+            <Autocomplete onValueChange={emptyFunction} value="Autocomplete" disabled={isDisabled} />
+          </div>
+          <br />
+          <div>
+            <CurrencyInput onValueChange={emptyFunction} disabled={isDisabled} />
+          </div>
+          <br />
+          <div>
+            <FxInput onValueChange={emptyFunction} disabled={isDisabled} />
+          </div>
+          <br />
+          <Button use="primary" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Gapped>
     </>
   );
 };
-RadioFocus.storyName = 'Radio focus';
-RadioFocus.parameters = {
+InputElementsSequentialFocus.parameters = {
   creevey: {
     tests: {
       async 'focus elements one by one'() {
-        await this.browser
-          .actions({
-            bridge: true,
-          })
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .sendKeys(this.keys.TAB)
-          .pause(500)
-          .perform();
-        await delay(500);
-        await this.expect(await this.takeScreenshot()).to.matchImage('02 - focus');
+        await this.browser.actions({
+          bridge: true,
+        });
+
+        const repeatAmount = 6;
+        for (let i = 0; i < repeatAmount; i++) {
+          await this.browser
+            .actions({
+              bridge: true,
+            })
+            .sendKeys(this.keys.TAB)
+            .pause(DELAY)
+            .sendKeys(this.keys.ENTER)
+            .pause(DELAY)
+            .perform();
+        }
+
+        await delay(DELAY);
+        await this.expect(await this.takeScreenshot()).to.matchImage('focus elements one by one');
+      },
+    },
+  },
+};
+
+export const ChoiceElementsSequentialFocus: Story = () => {
+  const [isDisabled, setIsDisabled] = useState(false);
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    new Promise((resolve) => {
+      setIsDisabled(true);
+      setTimeout(resolve, 1000);
+    }).then(() => {
+      setIsDisabled(false);
+    });
+  };
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      new Promise((resolve) => {
+        setIsDisabled(true);
+        setTimeout(resolve, 1000);
+      }).then(() => {
+        setIsDisabled(false);
+      });
+    }
+  };
+  globalObject.document?.addEventListener('keydown', handleKeyDown);
+
+  return (
+    <>
+      <h1 style={{ margin: '10px' }}>Форма</h1>
+      <Gapped gap={5} style={{ border: 'black solid 1px', margin: '10px', padding: '10px' }}>
+        <form onSubmit={onSubmit}>
+          <div>
+            <Radio value={'Radio'} disabled={isDisabled} />
+          </div>
+          <br />
+          <div>
+            <Toggle disabled={isDisabled} />
+          </div>
+          <br />
+          <div>
+            <Checkbox disabled={isDisabled} />
+          </div>
+          <br />
+          <Button use="primary" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Gapped>
+    </>
+  );
+};
+ChoiceElementsSequentialFocus.parameters = {
+  creevey: {
+    tests: {
+      async 'focus elements one by one'() {
+        const repeatAmount = 4;
+        for (let i = 0; i < repeatAmount; i++) {
+          await this.browser
+            .actions({
+              bridge: true,
+            })
+            .sendKeys(this.keys.TAB)
+            .pause(DELAY)
+            .sendKeys(this.keys.ENTER)
+            .pause(DELAY)
+            .perform();
+        }
+        await delay(DELAY);
+        await this.expect(await this.takeScreenshot()).to.matchImage('focus elements one by one');
+      },
+    },
+  },
+};
+
+export const OtherElementsSequentialFocus: Story = () => {
+  const [isDisabled, setIsDisabled] = useState(false);
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    new Promise((resolve) => {
+      setIsDisabled(true);
+      setTimeout(resolve, 1000);
+    }).then(() => {
+      setIsDisabled(false);
+    });
+  };
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      new Promise((resolve) => {
+        setIsDisabled(true);
+        setTimeout(resolve, 1000);
+      }).then(() => {
+        setIsDisabled(false);
+      });
+    }
+  };
+  globalObject.document?.addEventListener('keydown', handleKeyDown);
+
+  return (
+    <>
+      <h1 style={{ margin: '10px' }}>Форма</h1>
+      <Gapped gap={5} style={{ border: 'black solid 1px', margin: '10px', padding: '10px' }}>
+        <form onSubmit={onSubmit}>
+          <div>
+            <DateInput disabled={isDisabled} />
+          </div>
+          <br />
+          <div>
+            <DatePicker
+              onValueChange={useCallback(() => {
+                return;
+              }, [])}
+              disabled={isDisabled}
+            />
+          </div>
+          <br />
+          <div>
+            <FileUploader disabled={isDisabled} />
+          </div>
+          <br />
+          <Button use="primary" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Gapped>
+    </>
+  );
+};
+OtherElementsSequentialFocus.parameters = {
+  creevey: {
+    tests: {
+      async 'focus elements one by one'() {
+        const repeatAmount = 4;
+        for (let i = 0; i < repeatAmount; i++) {
+          await this.browser
+            .actions({
+              bridge: true,
+            })
+            .sendKeys(this.keys.TAB)
+            .pause(DELAY)
+            .sendKeys(this.keys.ENTER)
+            .pause(DELAY)
+            .perform();
+        }
+        await delay(1000);
+        await this.expect(await this.takeScreenshot()).to.matchImage('focus elements one by one');
       },
     },
   },
