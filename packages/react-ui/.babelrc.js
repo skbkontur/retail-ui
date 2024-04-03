1ff6306be4f0cc26defcab23f124a9404892e45e
Mikhail Shatikhin <mshatikhin@gmail.com>
2024-04-03 13:02:44 +0500

    fix: settings babel plugin for tests
    https://babeljs.io/docs/babel-plugin-transform-runtime#useesmodules

--- packages/react-ui/.babelrc.js
+++ packages/react-ui/.babelrc.js
@@ -4,13 +4,13 @@ module.exports = {
   },
   env: {
     cjs: {
-      presets: [['@babel/preset-env', { loose: true, modules: 'commonjs', targets: { ie: '11' } }]],
-      plugins: [['@babel/plugin-transform-runtime', { version: '7.8.3' }]],
+      presets: [['@babel/preset-env', { loose: true, modules: 'auto', targets: { ie: '11' } }]],
+      plugins: [['@babel/plugin-transform-runtime', { version: '7.16.5' }]],
     },
   },
-  presets: [['@babel/preset-env', { loose: true, modules: false }], '@babel/typescript', '@babel/preset-react'],
+  presets: [['@babel/preset-env', { loose: true, modules: 'auto' }], '@babel/typescript', '@babel/preset-react'],
   plugins: [
-    ['@babel/plugin-transform-runtime', { useESModules: true, version: '7.8.3' }],
+    ['@babel/plugin-transform-runtime', { version: '7.16.5' }],
     ['@babel/plugin-proposal-decorators', { legacy: true }],
   ],
 };
