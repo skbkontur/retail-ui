05eb09e7fc6b53fc4d71408781b6c68c81a92e28
Mikhail Shatikhin <mshatikhin@gmail.com>
2024-04-22 16:20:12 +0500

    chore: some refactor signatures

--- packages/react-ui/internal/CommonWrapper/utils/getVisualStateDataAttributes.ts
+++ packages/react-ui/internal/CommonWrapper/utils/getVisualStateDataAttributes.ts
@@ -1,20 +1,19 @@
 import { toKebabCase } from '../../../lib/toKebabCase';
-
-import { tryGetBoolean } from './tryGetBoolean';
+import { Nullable } from '../../../typings/utility-types';
 
 export type VisualStateDataAttributesResultType = Record<string, string>;
 
 const prefix = `data-visual-state-`;
 
-function fillResult(result: VisualStateDataAttributesResultType, key: string, value: unknown) {
-  if (tryGetBoolean(value)) {
+function fillResult(result: VisualStateDataAttributesResultType, key: string, value: Nullable<boolean>) {
+  if (value) {
     result[`${prefix}${toKebabCase(key)}`] = '';
   }
 
   return result;
 }
 
-export function getVisualStateDataAttributes<T extends Record<string, unknown>>(
+export function getVisualStateDataAttributes<T extends Record<string, Nullable<boolean>>>(
   attributes: T,
 ): VisualStateDataAttributesResultType {
   return Object.entries(attributes).reduce(
