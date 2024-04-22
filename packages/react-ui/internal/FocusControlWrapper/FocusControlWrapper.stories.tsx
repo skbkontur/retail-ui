47d3086b294eb0513c82cca244505a0aec154af5
Mikhail Shatikhin <mshatikhin@gmail.com>
2024-04-22 09:38:50 +0500

    chore: test for wrapper

---
+++ packages/react-ui/internal/FocusControlWrapper/FocusControlWrapper.stories.tsx
@@ -0,0 +1,94 @@
+import React, { CSSProperties, FC, FormEvent, PropsWithChildren, useCallback, useState } from 'react';
+import { globalObject } from '@skbkontur/global-object';
+import type { Meta } from '@storybook/react';
+import type { SkipOptions } from 'creevey';
+
+import type { Story } from '../../typings/stories';
+
+import { FocusControlWrapper } from './FocusControlWrapper';
+
+export default { title: 'FocusControlWrapper' } as Meta;
+
+const skip: SkipOptions = { 'chrome only': { in: /^(?!\bchrome2022\b)/ } };
+
+const Header: FC<PropsWithChildren<any>> = ({ children }) => <h1 style={{ margin: 10 }}>{children}</h1>;
+const Wrapper: FC<PropsWithChildren<any>> = ({ children }) => (
+  <div style={{ border: 'black solid 1px', margin: 10, padding: 10 }}>{children}</div>
+);
+
+const useFormHandlers = () => {
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleSubmit = useCallback((e: FormEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    new Promise((resolve) => {
+      setIsDisabled(true);
+      setTimeout(resolve, 100);
+    }).then(() => setIsDisabled(false));
+  }, []);
+
+  const handleKeyDown = useCallback((e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      new Promise((resolve) => {
+        setIsDisabled(true);
+        setTimeout(resolve, 100);
+      }).then(() => setIsDisabled(false));
+    }
+  }, []);
+
+  globalObject.document?.addEventListener('keydown', handleKeyDown);
+
+  return {
+    isDisabled,
+    handleSubmit,
+  };
+};
+
+const BrokenInput = ({ disabled }: { disabled?: boolean }) => {
+  const [focus, setFocus] = useState<boolean>(false);
+
+  const styles: CSSProperties = focus ? { border: '1px solid blue' } : {};
+
+  const handleBlur = (event: React.FocusEvent) => setFocus(false);
+  const handleFocus = (event: React.FocusEvent) => setFocus(true);
+  const handleResetFocus = () => setFocus(false);
+
+  return (
+    <FocusControlWrapper onBlurWhenDisabled={handleResetFocus}>
+      <input type="text" disabled={disabled} style={styles} onBlur={handleBlur} onFocus={handleFocus} />
+    </FocusControlWrapper>
+  );
+};
+
+export const Input: Story = () => {
+  const { isDisabled, handleSubmit } = useFormHandlers();
+  return (
+    <>
+      <Header>Форма</Header>
+      <Wrapper>
+        <form onSubmit={handleSubmit}>
+          <BrokenInput disabled={isDisabled} />
+          <button type="submit">Submit</button>
+        </form>
+      </Wrapper>
+    </>
+  );
+};
+
+Input.parameters = {
+  creevey: {
+    skip,
+    tests: {
+      async 'focus ring out'() {
+        await this.browser
+          .actions({ bridge: true })
+          .sendKeys(this.keys.TAB)
+          .sendKeys(this.keys.ENTER)
+          .pause(1000)
+          .perform();
+        await this.expect(await this.takeScreenshot()).to.matchImage('focus ring out');
+      },
+    },
+  },
+};
