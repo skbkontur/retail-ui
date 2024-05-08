067fb80a6606d86870b811a972ad81bbd052c0e5
Elena Ishmatova <ishmatova@skbkontur.ru>
2024-05-08 11:05:10 +0500

    chore: fix Autocomplete test

---
+++ packages/react-ui/components/Autocomplete/__creevey__/Autocomplete.creevey.ts
@@ -0,0 +1,226 @@
+import { story, kind, test } from 'creevey';
+import { delay } from '../../../lib/utils';
+
+const commonTests = () => {
+  test('focus and type text', async function () {
+    const screenshotElement = this.browser.findElement({
+      css: '#test-element',
+    });
+    const autocompleteElement = this.browser.findElement({
+      css: '[data-comp-name~="Autocomplete"]',
+    });
+    await this.browser.actions({ bridge: true }).click(autocompleteElement).sendKeys('o').perform();
+    await delay(1000);
+    await this.expect(await screenshotElement.takeScreenshot()).to.matchImage();
+  });
+};
+
+const mobileHintsTests = () => {
+  test('noInputValue', async function () {
+    await this.browser
+      .actions({
+        bridge: true,
+      })
+      .click(this.browser.findElement({ css: 'input' }))
+      .perform();
+    await delay(200);
+    await this.expect(await this.browser.takeScreenshot()).to.matchImage('noInputValue');
+  });
+
+  test('nothingWasFound', async function () {
+    await this.browser
+      .actions({
+        bridge: true,
+      })
+      .click(this.browser.findElement({ css: 'input' }))
+      .sendKeys('abc')
+      .perform();
+    await delay(200);
+    await this.expect(await this.browser.takeScreenshot()).to.matchImage('nothingWasFound');
+  });
+
+  test('updateValue', async function () {
+    await this.browser
+      .actions({
+        bridge: true,
+      })
+      .click(this.browser.findElement({ css: 'input' }))
+      .sendKeys('one')
+      .perform();
+    await delay(200);
+    await this.browser
+      .actions({
+        bridge: true,
+      })
+      .click(this.browser.findElement({ css: '[data-comp-name~="MenuItem"]' }))
+      .click(this.browser.findElement({ css: 'input' }))
+      .perform();
+    await delay(200);
+    await this.expect(await this.browser.takeScreenshot()).to.matchImage('updateValue');
+  });
+};
+
+const sizeTests = () => {
+  test('focus and type text small', async function () {
+    const screenshotElement = this.browser.findElement({
+      css: '#test-element',
+    });
+    const autocompleteElements = await this.browser.findElements({
+      css: '[data-comp-name~="Autocomplete"]',
+    });
+    await this.browser.actions({ bridge: true }).click(autocompleteElements[0]).sendKeys('o').perform();
+    await delay(1000);
+    await this.expect(await screenshotElement.takeScreenshot()).to.matchImage();
+  });
+
+  test('focus and type text medium', async function () {
+    const screenshotElement = this.browser.findElement({
+      css: '#test-element',
+    });
+    const autocompleteElements = await this.browser.findElements({
+      css: '[data-comp-name~="Autocomplete"]',
+    });
+    await this.browser.actions({ bridge: true }).click(autocompleteElements[1]).sendKeys('o').perform();
+    await delay(1000);
+    await this.expect(await screenshotElement.takeScreenshot()).to.matchImage();
+  });
+
+  test('focus and type text large', async function () {
+    const screenshotElement = this.browser.findElement({
+      css: '#test-element',
+    });
+    const autocompleteElements = await this.browser.findElements({
+      css: '[data-comp-name~="Autocomplete"]',
+    });
+    await this.browser.actions({ bridge: true }).click(autocompleteElements[2]).sendKeys('o').perform();
+    await delay(1000);
+    await this.expect(await screenshotElement.takeScreenshot()).to.matchImage();
+  });
+};
+
+kind('Autocomplete', () => {
+  story('Simple', ({ setStoryParameters }) => {
+    setStoryParameters({
+      skip: { 'no themes': { in: /Flat8px/ } },
+    });
+    test('idle', async function () {
+      await this.expect(await this.takeScreenshot()).to.matchImage('idle');
+    });
+
+    test('focused', async function () {
+      const autocompleteElement = this.browser.findElement({
+        css: '[data-comp-name~="Autocomplete"]',
+      });
+      await this.browser.actions({ bridge: true }).click(autocompleteElement).perform();
+      await delay(1000);
+      await this.expect(await autocompleteElement.takeScreenshot()).to.matchImage();
+    });
+
+    commonTests();
+  });
+
+  story('WithRenderItem', ({ setStoryParameters }) => {
+    setStoryParameters({
+      skip: { 'no themes': { in: /Flat8px/ } },
+    });
+    commonTests();
+  });
+
+  story('WithBigRenderItemWidth', ({ setStoryParameters }) => {
+    setStoryParameters({
+      skip: { 'no themes': { in: /Flat8px/ } },
+    });
+    commonTests();
+  });
+
+  story('WithFixedMenuSize', ({ setStoryParameters }) => {
+    setStoryParameters({
+      skip: { 'no themes': { in: /Flat8px/ } },
+    });
+    commonTests();
+  });
+
+  story('WithPercentageWidth', ({ setStoryParameters }) => {
+    setStoryParameters({
+      skip: { 'no themes': { in: /Flat8px/ } },
+    });
+    commonTests();
+  });
+
+  story('WithFixedWidth', ({ setStoryParameters }) => {
+    setStoryParameters({
+      skip: { 'no themes': { in: /Flat8px/ } },
+    });
+    commonTests();
+  });
+
+  story('WithZeroWidth', ({ setStoryParameters }) => {
+    setStoryParameters({
+      skip: { 'no themes': { in: /Flat8px/ } },
+    });
+    commonTests();
+  });
+
+  story('MobileHints', () => {
+    mobileHintsTests();
+  });
+
+  story('MobileHintsEN', () => {
+    mobileHintsTests();
+  });
+
+  story('MobileWithTitle', () => {
+    test('opened', async function () {
+      await this.browser
+        .actions({
+          bridge: true,
+        })
+        .click(this.browser.findElement({ css: 'input' }))
+        .perform();
+      await delay(200);
+      await this.expect(await this.browser.takeScreenshot()).to.matchImage('opened');
+    });
+  });
+
+  story('WithManualPosition', ({ setStoryParameters }) => {
+    setStoryParameters({
+      skip: { 'no themes': { in: /^(?!\b(chrome|firefox)\b)/ } },
+    });
+
+    test('opened top with portal', async function () {
+      const screenshotElement = this.browser.findElement({
+        css: '#test-element',
+      });
+      const autocompleteElement = this.browser.findElement({
+        css: '[data-comp-name~="Autocomplete"]',
+      });
+      await this.browser.actions({ bridge: true }).click(autocompleteElement).sendKeys('o').perform();
+      await delay(1000);
+      await this.expect(await screenshotElement.takeScreenshot()).to.matchImage();
+    });
+
+    test('opened bottom with portal', async function () {
+      const screenshotElement = this.browser.findElement({
+        css: '#test-element',
+      });
+      const autocompleteElement = this.browser.findElement({
+        css: '[data-comp-name~="Autocomplete"]',
+      });
+      await this.browser
+        .actions({ bridge: true })
+        .click(this.browser.findElement({ css: '[data-tid~="pos"]' }))
+        .click(autocompleteElement)
+        .sendKeys('o')
+        .perform();
+      await delay(1000);
+      await this.expect(await screenshotElement.takeScreenshot()).to.matchImage();
+    });
+  });
+
+  story('Size', ({ setStoryParameters }) => {
+    setStoryParameters({
+      skip: { 'no themes': { in: /Flat8px/ } },
+    });
+    sizeTests();
+  });
+});
---
+++ packages/react-ui/components/Autocomplete/__stories__/Autocomplete.stories.tsx
@@ -0,0 +1,233 @@
+import React from 'react';
+import { flatten } from 'lodash';
+
+import { Gapped } from '../../Gapped';
+import { Autocomplete } from '../Autocomplete';
+import { Meta, Story } from '../../../typings/stories';
+import { AutocompleteProps } from '..';
+import { LangCodes, LocaleContext } from '../../../lib/locale';
+
+export default {
+  title: 'Autocomplete',
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          padding: '0 200px 200px 0',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta;
+
+export const Simple: Story = () => <UncontrolledAutocomplete source={['One', 'Two', 'Three']} />;
+Simple.storyName = 'simple';
+
+export const WithRenderItem = () => (
+  <UncontrolledAutocomplete
+    source={['One', 'Two', 'Three']}
+    renderItem={(x: string) => <div>Item: {x.toUpperCase()}</div>}
+  />
+);
+WithRenderItem.storyName = 'with renderItem';
+
+export const WithBigRenderItemWidth = () => (
+  <UncontrolledAutocomplete
+    source={['One', 'Two', 'Three']}
+    renderItem={(x: string) => <div style={{ width: 400 }}>Item: {x.toUpperCase()}</div>}
+  />
+);
+WithBigRenderItemWidth.storyName = 'with big renderItem width';
+
+export const WithFixedMenuSize = () => (
+  <UncontrolledAutocomplete
+    source={[
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed.',
+      'Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh.',
+      'Donec lacus nunc, viverra nec.',
+      'Sed lectus. Integer euismod lacus luctus magna.',
+      'Suspendisse potenti.',
+      ' Sed dignissim lacinia nunc.',
+    ]}
+    renderItem={(x: string) => <div>{x}</div>}
+    menuWidth={400}
+    menuMaxHeight={150}
+  />
+);
+WithFixedMenuSize.storyName = 'with fixed menu size';
+
+export const WithOnBlurOnFocusHandlers = () => <WithBlurFocusHandlersExample />;
+WithOnBlurOnFocusHandlers.storyName = 'with onBlur/onFocus handlers';
+
+WithOnBlurOnFocusHandlers.parameters = {
+  creevey: {
+    skip: true,
+  },
+};
+
+interface UncontrolledAutocompleteState {
+  value: string;
+}
+class UncontrolledAutocomplete extends React.Component<Partial<AutocompleteProps>> {
+  public state: UncontrolledAutocompleteState = {
+    value: '',
+  };
+
+  public render() {
+    return (
+      <Autocomplete
+        {...this.props}
+        value={this.state.value}
+        onValueChange={(value) => {
+          this.setState({ value });
+        }}
+      />
+    );
+  }
+}
+
+class WithBlurFocusHandlersExample extends React.Component {
+  public state = {
+    focusCount: 0,
+    blurCount: 0,
+  };
+
+  public render() {
+    return (
+      <Gapped vertical>
+        <UncontrolledAutocomplete
+          onFocus={() => {
+            const { focusCount } = this.state;
+            this.setState({ focusCount: focusCount + 1 });
+          }}
+          onBlur={() => {
+            const { blurCount } = this.state;
+            this.setState({ blurCount: blurCount + 1 });
+          }}
+          source={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.split(
+            ' ',
+          )}
+        />
+        <span>Focuses count: {this.state.focusCount}</span>
+        <span>Blures count: {this.state.blurCount}</span>
+      </Gapped>
+    );
+  }
+}
+
+export const WithPercentageWidth = () => (
+  <div style={{ width: '600px' }}>
+    <UncontrolledAutocomplete
+      width="50%"
+      source={['One', 'Two', 'Three']}
+      renderItem={(x: string) => <div>Item: {x.toUpperCase()}</div>}
+    />
+  </div>
+);
+WithPercentageWidth.storyName = 'with percentage width';
+
+export const WithFixedWidth = () => (
+  <UncontrolledAutocomplete
+    width="200px"
+    source={['One', 'Two', 'Three']}
+    renderItem={(x: string) => <div>Item: {x.toUpperCase()}</div>}
+  />
+);
+WithFixedWidth.storyName = 'with fixed width';
+
+export const WithZeroWidth = () => (
+  <UncontrolledAutocomplete
+    width={0}
+    source={['One', 'Two', 'Three']}
+    renderItem={(x: string) => <div>Item: {x.toUpperCase()}</div>}
+  />
+);
+WithZeroWidth.storyName = 'with zero width';
+
+export const MobileSimple = () => (
+  <>
+    <UncontrolledAutocomplete source={['One', 'Two', 'Three']} />
+    <span>With caption</span>
+    <UncontrolledAutocomplete source={['One', 'Two', 'Three']} mobileMenuHeaderText={'With caption'} />
+    <span>With many items</span>
+    <UncontrolledAutocomplete
+      source={flatten(
+        new Array(10).fill(['One', 'Two', 'Three']).map((arr, index) => arr.map((i: string) => `${i} ${index}`)),
+      )}
+    />
+  </>
+);
+MobileSimple.title = 'Mobile autocomplete stories';
+MobileSimple.parameters = {
+  viewport: {
+    defaultViewport: 'iphone',
+  },
+  creevey: { skip: true },
+};
+
+export const MobileHints: Story = () => <UncontrolledAutocomplete source={['one', 'two', 'three']} />;
+MobileHints.parameters = {
+  viewport: {
+    defaultViewport: 'iphone',
+  },
+};
+
+export const MobileHintsEN: Story = () => (
+  <LocaleContext.Provider value={{ langCode: LangCodes.en_GB }}>
+    <UncontrolledAutocomplete source={['one', 'two', 'three']} />
+  </LocaleContext.Provider>
+);
+MobileHintsEN.parameters = {
+  viewport: {
+    defaultViewport: 'iphone',
+  },
+};
+
+export const MobileWithTitle: Story = () => (
+  <UncontrolledAutocomplete mobileMenuHeaderText="Заголовок" source={['one', 'two', 'three']} />
+);
+MobileWithTitle.parameters = {
+  viewport: {
+    defaultViewport: 'iphone',
+  },
+};
+
+export const WithManualPosition: Story = () => {
+  const [menuPos, setMenuPos] = React.useState<'top' | 'bottom'>('top');
+
+  return (
+    <div style={{ marginTop: '300px', paddingBottom: '300px' }}>
+      <UncontrolledAutocomplete menuPos={menuPos} source={['One', 'Two', 'Three']} />
+      <button data-tid="pos" onClick={() => setMenuPos(menuPos === 'top' ? 'bottom' : 'top')}>
+        change pos to {menuPos === 'top' ? 'bottom' : 'top'}
+      </button>
+    </div>
+  );
+};
+WithManualPosition.storyName = 'with manual position';
+
+export const Size = () => {
+  const source = [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed.',
+    'Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh.',
+    'Donec lacus nunc, viverra nec.',
+    'Sed lectus. Integer euismod lacus luctus magna.',
+    'Suspendisse potenti.',
+    ' Sed dignissim lacinia nunc.',
+  ];
+
+  return (
+    <div>
+      <Gapped style={{ height: '300px', width: '1000px' }}>
+        <UncontrolledAutocomplete source={source} size={'small'} />
+        <UncontrolledAutocomplete source={source} size={'medium'} />
+        <UncontrolledAutocomplete source={source} size={'large'} />
+      </Gapped>
+    </div>
+  );
+};
+Size.storyName = 'size';
