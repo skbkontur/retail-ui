8c59154c5a4d821ef3fd45d3095532837ff65351
Elena Ishmatova <ishmatova@skbkontur.ru>
2024-05-14 13:18:11 +0500

    chore: fix MenuFooter test

---
+++ packages/react-ui/components/MenuFooter/__stories__/MenuFooter.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Meta } from '../../../typings/stories';
+import { MenuFooter } from '../MenuFooter';
+import { Gapped } from '../../Gapped';
+
+export default {
+  title: 'MenuFooter',
+} as Meta;
+
+export const Size = () => {
+  return (
+    <Gapped>
+      <MenuFooter size={'small'}>Маленький</MenuFooter>
+      <MenuFooter size={'medium'}>Средний</MenuFooter>
+      <MenuFooter size={'large'}>Большой</MenuFooter>
+    </Gapped>
+  );
+};
+Size.storyName = 'size';
