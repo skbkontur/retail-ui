7a2a58c2de2a62d43b3f52b0649ee69cd7f21ef0
Elena Ishmatova <ishmatova@skbkontur.ru>
2024-05-15 14:33:09 +0500

    chore: fix ThemePlayground test

---
+++ packages/react-ui/internal/ThemePlayground/__stories__/ThemePlayground.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Story } from '../../../typings/stories';
+import { ThemeContextPlayground } from '../ThemeContextPlayground';
+
+export default { title: 'ThemeProvider' };
+
+export const Playground: Story = () => <ThemeContextPlayground />;
+Playground.storyName = 'playground';
