a02f11b1f481656ab946f57f1e4883ef249222d2
Elena Ishmatova <ishmatova@skbkontur.ru>
2024-05-15 14:15:37 +0500

    chore: fix IgnoreLayerClick test

---
+++ packages/react-ui/internal/IgnoreLayerClick/__stories__/IgnoreLayerClick.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { SidePage } from '../../../components/SidePage';
+import { IgnoreLayerClick } from '../IgnoreLayerClick';
+import { Button } from '../../../components/Button';
+import { Toggle } from '../../../components/Toggle';
+
+interface SampleState {
+  open: boolean;
+  active: boolean;
+}
+class Sample extends React.Component {
+  public state: SampleState = {
+    active: false,
+    open: false,
+  };
+
+  public render() {
+    return (
+      <div>
+        {this.state.open && (
+          <SidePage onClose={() => this.setState({ open: false })}>
+            <SidePage.Header>Голова</SidePage.Header>
+            <SidePage.Body>
+              <div style={{ padding: 20 }}>Туловище</div>
+            </SidePage.Body>
+          </SidePage>
+        )}
+        <IgnoreLayerClick active={this.state.active}>
+          <Button onClick={() => this.setState({ open: true })}>Open</Button>
+        </IgnoreLayerClick>
+        <Toggle
+          checked={this.state.active}
+          onValueChange={() =>
+            this.setState((state: SampleState) => {
+              return { active: !state.active };
+            })
+          }
+        />
+      </div>
+    );
+  }
+}
+
+export default { title: 'IgnoreLayerClick', parameters: { creevey: { skip: true } } };
+
+export const Common = () => <Sample />;
