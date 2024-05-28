95b20dcb7e765c3e69588414000a34bc9c871901
Elena Ishmatova <ishmatova@skbkontur.ru>
2024-05-28 15:58:28 +0500

    fix: js test setup

--- packages/react-ui/test-setup.js
+++ packages/react-ui/test-setup.js
@@ -1,6 +1,6 @@
 import 'core-js/stable';
 import '@testing-library/jest-dom';
-import { configure } from '@testing-library/dom';
+import { configure } from '@testing-library/react';
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
