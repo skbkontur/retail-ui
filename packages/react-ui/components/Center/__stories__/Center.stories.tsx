0b7bf192190931d672d414ec861aca5b2e0c3833
SchwJ <schwj@yandex.ru>
2024-06-03 12:26:33 +0500

    chore(Button): props, folders, examples view, no examples code, have stories

--- packages/react-ui/components/Center/__stories__/Center.stories.tsx
+++ packages/react-ui/components/Center/__stories__/Center.stories.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import { Center } from '../Center';
 import { Meta } from '../../../typings/stories';
 
-export default { title: 'Center', component: Center, parameters: { creevey: { skip: true } } } as Meta;
+export default {
+  title: 'Layout/Center',
+  component: Center,
+  parameters: { creevey: { skip: true } }
+} as Meta;
 
 export const Simple = () => (
   <div style={{ width: 200, height: 200, border: '1px solid #dfdede' }}>
