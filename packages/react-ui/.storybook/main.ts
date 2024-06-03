267e270472c29cb1cb888c12e7a8f7dda7bdcab2
SchwJ <schwj@yandex.ru>
2024-06-03 17:07:16 +0500

    chore(): add storybook documentation comand, remove stories from docs

--- packages/react-ui/.storybook/main.ts
+++ packages/react-ui/.storybook/main.ts
@@ -1,24 +1,29 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
+const isDocsEnv = Boolean(process.env.STORYBOOK_REACT_UI_DOCS);
+
 const config: StorybookConfig = {
   stories: ['../components/**/*.stories.tsx', '../internal/**/*.stories.tsx', '../components/**/*.mdx'],
+  docs: {
+    docsMode: isDocsEnv ? true : false,
+  },
   addons: [
     'creevey',
     '@storybook/addon-links',
     '@storybook/addon-a11y',
     '@storybook/blocks',
     '@storybook/addon-docs',
-    //'storybook-addon-live-examples',
     {
       name: '@storybook/addon-essentials',
       options: {
-        docs: false,
+        docsMode: true,
       },
     },
   ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {
+      legacyRootApi: true,
       fastRefresh: true,
       strictMode: true,
     },
