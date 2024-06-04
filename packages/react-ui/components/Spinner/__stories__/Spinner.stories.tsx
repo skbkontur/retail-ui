2421acb15014f8c917f0ae60dca72e347c954664
HelenaIsh <32093844+HelenaIsh@users.noreply.github.com>
2024-06-04 16:02:22 +0500

    feat: remove spinnerLoaderRemoveDefaultCaption feature flag (#3436)

--- packages/react-ui/components/Spinner/__stories__/Spinner.stories.tsx
+++ packages/react-ui/components/Spinner/__stories__/Spinner.stories.tsx
@@ -42,6 +42,7 @@ export const Normal = () => <Spinner />;
 export const Big = () => <Spinner type="big" />;
 export const Mini = () => <Spinner type="mini" />;
 export const MiniDimmed = () => <Spinner type="mini" dimmed />;
+export const WithCaption = () => <Spinner caption={'Loading'} />;
 export const WithCustomColor = () => <Spinner type="big" color={'pink'} />;
 export const WithCustomWidth = () => <Spinner type="big" width={10} />;
 MiniDimmed.storyName = 'Mini dimmed';
