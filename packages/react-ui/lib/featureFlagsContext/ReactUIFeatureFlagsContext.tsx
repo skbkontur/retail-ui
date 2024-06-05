f3488554685e24fd0392561dff518640e2059c2a
HelenaIsh <32093844+HelenaIsh@users.noreply.github.com>
2024-06-05 10:13:17 +0500

    feat: remove sidePageEnableFocusLockWhenBackgroundBlocked feature flag (#3437)

--- packages/react-ui/lib/featureFlagsContext/ReactUIFeatureFlagsContext.tsx
+++ packages/react-ui/lib/featureFlagsContext/ReactUIFeatureFlagsContext.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export interface ReactUIFeatureFlags {
-  sidePageEnableFocusLockWhenBackgroundBlocked?: boolean;
   menuItemsAtAnyLevel?: boolean;
   textareaUseSafari17Workaround?: boolean;
   linkFocusOutline?: boolean;
@@ -11,7 +10,6 @@ export interface ReactUIFeatureFlags {
 }
 
 export const reactUIFeatureFlagsDefault: ReactUIFeatureFlags = {
-  sidePageEnableFocusLockWhenBackgroundBlocked: false,
   menuItemsAtAnyLevel: false,
   textareaUseSafari17Workaround: false,
   linkFocusOutline: false,
