9a03686e6f864bcc0f6539b42e64db3c9d23d9c3
Elena Ishmatova <ishmatova@skbkontur.ru>
2024-06-17 15:50:17 +0500

    chore: remove unnecessary code

--- packages/react-ui-validations/src/utils/getValidationTextColor.ts
+++ packages/react-ui-validations/src/utils/getValidationTextColor.ts
@@ -4,14 +4,6 @@ import type { ThemeValidations } from '../../typings/theme-context';
 export const DEFAULT_TEXT_COLOR = '#d43517';
 
 export function getValidationTextColor(theme: ThemeValidations, level: ValidationLevel = 'error') {
-  if (!theme.validationsTextColorWarning && !theme.validationsTextColorError) {
-    switch (level) {
-      case 'warning':
-        return '#ef8b17';
-      case 'error':
-        return '#cb3d35';
-    }
-  }
   return (
     (level === 'warning' ? theme.validationsTextColorWarning : theme.validationsTextColorError) || DEFAULT_TEXT_COLOR
   );
