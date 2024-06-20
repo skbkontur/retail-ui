e6dee301fce9e7f8076cfd4515cdad0a0592c84f
Eazymov <azymov@kontur.ru>
2024-06-20 17:42:33 +0500

    feat(MenuItem): add scrollIntoView prop

--- packages/react-ui/components/MenuItem/MenuItem.tsx
+++ packages/react-ui/components/MenuItem/MenuItem.tsx
@@ -104,6 +104,7 @@ export interface MenuItemProps
   isNotSelectable?: boolean;
 
   isMobile?: boolean;
+  scrollIntoView?: boolean;
 }
 
 export const MenuItemDataTids = {
@@ -189,6 +190,11 @@ export class MenuItem extends React.Component<MenuItemProps> {
   }
 
   public componentDidMount() {
+    if (this.props.scrollIntoView) {
+      this.rootRef?.scrollIntoView({
+        block: 'center',
+      });
+    }
     if (this.rootRef && isBrowser(globalObject)) {
       this.setState({ iconOffsetTop: globalObject.getComputedStyle(this.rootRef).getPropertyValue('padding-top') });
     }
@@ -303,6 +309,7 @@ export class MenuItem extends React.Component<MenuItemProps> {
       isMobile,
       href,
       disabled,
+      scrollIntoView,
       rel = this.props.href && isExternalLink(this.props.href) ? 'noopener noreferrer' : this.props.rel,
       isNotSelectable,
       ...rest
