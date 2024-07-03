c434be8cafa5ba5df10b73610a54fe71902b568f
Funkicide <knel@skbkontur.ru>
2024-07-03 17:55:32 +0300

    docs(ThemeContext): update theme table

--- packages/react-ui/lib/theming/themes/DarkTheme.ts
+++ packages/react-ui/lib/theming/themes/DarkTheme.ts
@@ -2,4 +2,7 @@ import { ThemeFactory } from '../ThemeFactory';
 
 import { THEME_2022_DARK } from './Theme2022Dark';
 
+/**
+ * @deprecated Use THEME_2022_DARK
+ */
 export const DARK_THEME = ThemeFactory.create({}, THEME_2022_DARK);
--- packages/react-ui/lib/theming/themes/DefaultTheme.ts
+++ packages/react-ui/lib/theming/themes/DefaultTheme.ts
@@ -1,4 +1,8 @@
 import { ThemeFactory } from '../ThemeFactory';
 
 import { THEME_2022 } from './Theme2022';
+
+/**
+ * @deprecated Use THEME_2022
+ */
 export const DEFAULT_THEME = ThemeFactory.create({}, THEME_2022);
