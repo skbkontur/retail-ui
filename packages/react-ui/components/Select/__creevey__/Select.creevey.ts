2240ee10c2f71e690509ff7439907d4f80c26965
Funkicide <knel@skbkontur.ru>
2024-07-04 17:55:58 +0300

    test(Select): skip test without hover

--- packages/react-ui/components/Select/__creevey__/Select.creevey.ts
+++ packages/react-ui/components/Select/__creevey__/Select.creevey.ts
@@ -101,6 +101,8 @@ kind('Select', () => {
 
         // TODO @Khlutkova fix after update browsers
         'story-skip-1': { in: ['chrome8px', 'chromeFlat8px', 'chrome', 'chromeDark'], tests: ['MenuItem hover'] },
+
+        'hover does not work in chrome': { in: ['chrome2022', 'chrome2022Dark'], tests: ['MenuItem hover'] },
       },
     });
 
