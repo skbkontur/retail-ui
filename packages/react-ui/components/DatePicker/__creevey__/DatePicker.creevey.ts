79f8f250a63740356b8772c4b5ee2202b4451a46
Eazymov <eazymov@mail.ru>
2024-07-08 17:37:18 +0500

    test(DatePicker): add screenshots for disablePortal

--- packages/react-ui/components/DatePicker/__creevey__/DatePicker.creevey.ts
+++ packages/react-ui/components/DatePicker/__creevey__/DatePicker.creevey.ts
@@ -198,4 +198,10 @@ kind('DatePicker', () => {
       await this.expect(await this.browser.takeScreenshot()).to.matchImage('MobilePicker on iphone opened');
     });
   });
+  story('DisablePortal', () => {
+    test('opened', async function () {
+      await delay(1000);
+      await this.expect(await this.takeScreenshot()).to.matchImage('opened');
+    });
+  });
 });
