00a4eee96f9a8a27c6259bbdff6a8a28f2ec08f0
Funkicide <knel@skbkontur.ru>
2024-07-24 12:27:02 +0300

    chore(ThemeSwitcher): remove old themes

--- packages/react-ui/.styleguide/components/ThemeSwitcher/constants.ts
+++ packages/react-ui/.styleguide/components/ThemeSwitcher/constants.ts
@@ -1,11 +1,7 @@
-import { DARK_THEME } from '../../../lib/theming/themes/DarkTheme';
-import { DEFAULT_THEME } from '../../../lib/theming/themes/DefaultTheme';
 import { THEME_2022 } from '../../../lib/theming/themes/Theme2022';
 import { THEME_2022_DARK } from '../../../lib/theming/themes/Theme2022Dark';
 
 export const THEMES = {
-  DEFAULT_THEME,
-  DARK_THEME,
   THEME_2022,
   THEME_2022_DARK,
 } as const;
