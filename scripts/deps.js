a60d10dfcf9e222d21014baba82657fc371be8f8
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2024-07-29 12:38:27 +0300

    chore: comment empty catch

--- scripts/deps.js
+++ scripts/deps.js
@@ -3,7 +3,9 @@ const { execSync } = require('child_process');
 execSync(`node scripts/update-package-registry`, { stdio: 'inherit' });
 
 try {
-    execSync(`yarn ${process.argv.slice(2).join(' ')}`, { stdio: 'inherit' });
-} catch (e) {}
+  execSync(`yarn ${process.argv.slice(2).join(' ')}`, { stdio: 'inherit' });
+} catch (e) {
+  // error message will be forwarded to stdio anyway
+}
 
 execSync(`node scripts/update-package-registry back`, { stdio: 'inherit' });
