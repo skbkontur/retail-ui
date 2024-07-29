cd2042ab080196ea1a55583ca32a29d8f6e08b8a
Sasha Chabin <alex.chabin@gmail.com>
2024-07-29 06:01:58 +0500

    refactor(DateRangePicker): Update context

--- packages/react-ui/components/DateRangePicker/__stories__/DateRangePicker.stories.tsx
+++ packages/react-ui/components/DateRangePicker/__stories__/DateRangePicker.stories.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Story } from '@storybook/react';
 
 import { Meta } from '../../../typings/stories';
 import { DateRangePicker } from '../DateRangePicker';
-import { ThemeContext } from '../../../lib/theming/ThemeContext';
-import { ThemeFactory } from '../../../lib/theming/ThemeFactory';
-import { THEME_2022 } from '../../../lib/theming/themes/Theme2022';
+import { Gapped } from '../../Gapped';
+import { LangCodes, LocaleContext } from '../../../lib/locale';
 
 export default {
   title: 'DateRangePicker',
@@ -13,45 +12,120 @@ export default {
 
 export const Component = () => {
   return (
-    <DateRangePicker />
+    <DateRangePicker onValueChange={console.log} />
   );
 };
+Component.storyName = 'DateRangePicker';
+Component.parameters = {};
 
-export const WithMouseeventHandlers: Story = () => {
-  const [date, setDate] = useState('02.07.2017');
+export const Mobile: Story = () => {
+  return (
+    <DateRangePicker onValueChange={console.log} />
+  );
+};
+Mobile.parameters = { viewport: { defaultViewport: 'iphone' } };
 
+export const Sizes: Story = () => {
+  return (
+    <>
+      <Gapped vertical>
+        <DateRangePicker onValueChange={console.log} size="large" />
+        <DateRangePicker onValueChange={console.log} size="medium" />
+        <DateRangePicker onValueChange={console.log} size="small" />
+      </Gapped>
+    </>
+  );
+};
+Sizes.parameters = {};
+
+export const MinMax: Story = () => {
   return (
     <DateRangePicker
-      width={200}
-      value={date}
-      onMouseEnter={() => console.count('enter')}
-      onMouseLeave={() => console.count('leave')}
-      onValueChange={setDate}
+      minDate="05.07.2024"
+      maxDate="15.08.2024"
+      onValueChange={console.log}
     />
   );
 };
-WithMouseeventHandlers.storyName = 'with mouseevent handlers';
+MinMax.parameters = {};
 
-export const MobilePicker: Story = () => {
-  const [date, setDate] = useState('02.07.2017');
-
+export const Autofocus: Story = () => {
   return (
-    <ThemeContext.Consumer>
-      {(theme) => {
-        return (
-          <ThemeContext.Provider value={ThemeFactory.create(theme, THEME_2022)}>
-            <DateRangePicker enableTodayLink width="auto" value={date} onValueChange={setDate} />
-          </ThemeContext.Provider>
-        );
-      }}
-    </ThemeContext.Consumer>
+    <DateRangePicker
+      autoFocus
+      onValueChange={console.log}
+    />
   );
 };
-MobilePicker.storyName = 'MobilePicker';
-MobilePicker.parameters = {
-  viewport: {
-    defaultViewport: 'iphone',
-  },
-};
+Autofocus.parameters = {};
 
-Component.storyName = 'Default';
+export const MenuAlign: Story = () => {
+  return (
+    <>
+    </>
+  );
+};
+MenuAlign.parameters = {};
+
+export const MenuPos: Story = () => {
+  return (
+    <Gapped vertical gap={16} style={{ margin: 120 }}>
+      <DateRangePicker
+        menuPos="top"
+        onValueChange={console.log}
+      />
+      <DateRangePicker
+        menuPos="bottom"
+        onValueChange={console.log}
+      />
+    </Gapped>
+  );
+};
+MenuPos.storyName = 'MenuPos';
+MenuPos.parameters = {};
+
+export const DateRangePickerLocaleProvider = () => {
+  return (
+    <div style={{ paddingTop: 200 }}>
+      <LocaleContext.Provider value={{ langCode: LangCodes.en_GB }}>
+        <DateRangePicker
+          value="10.07.2024"
+          minDate="05.07.2024"
+          maxDate="30.08.2024"
+          onValueChange={console.log}
+        />
+      </LocaleContext.Provider>
+    </div>
+  );
+};
+DateRangePickerLocaleProvider.storyName = 'LocaleProvider';
+DateRangePickerLocaleProvider.parameters = { creevey: { skip: true } };
+
+export const Disabled: Story = () => {
+  return (
+    <DateRangePicker onValueChange={() => void (0)} disabled />
+  );
+};
+Disabled.storyName = 'Disabled';
+Disabled.parameters = {};
+
+export const CustomChildren: Story = () => {
+  return (
+    <DateRangePicker onValueChange={console.log}>
+      <Gapped vertical gap={4}>
+        <DateRangePicker.From />
+        <DateRangePicker.To />
+      </Gapped>
+    </DateRangePicker>
+  );
+};
+CustomChildren.storyName = 'CustomChildren';
+CustomChildren.parameters = {};
+
+export const Range: Story = () => {
+  return (
+    <>
+    </>
+  );
+};
+Range.parameters = {};
