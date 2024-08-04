b7fcabd49b91a525fb1a801d0ce08b2719e1cad4
Eazymov <azymov@kontur.ru>
2024-08-04 17:02:18 +0500

    test: add test for getMenuPositions

--- packages/react-ui/lib/getMenuPositions.ts
+++ packages/react-ui/lib/getMenuPositions.ts
@@ -1,4 +1,9 @@
-export function getMenuPositions(menuPos?: 'top' | 'middle' | 'bottom', menuAlign?: 'left' | 'right') {
+import type { PopupPositionsType } from '../internal/Popup';
+
+export function getMenuPositions(
+  menuPos?: 'top' | 'middle' | 'bottom',
+  menuAlign?: 'left' | 'right',
+): PopupPositionsType[] {
   if (menuAlign === undefined) {
     return menuPos === undefined ? ['bottom left', 'top left'] : [`${menuPos} left`];
   }
