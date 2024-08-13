7dc656d347d210aea21a155ac87029eac42e22aa
Funkicide <knel@skbkontur.ru>
2024-08-13 12:22:02 +0300

    chore: remove deprecated warning

--- packages/react-ui/lib/theming/themes/DarkTheme.ts
+++ packages/react-ui/lib/theming/themes/DarkTheme.ts
@@ -2,7 +2,4 @@ import { ThemeFactory } from '../ThemeFactory';
 
 import { THEME_2022_DARK } from './Theme2022Dark';
 
-/**
- * @deprecated Use THEME_2022_DARK
- */
 export const DARK_THEME = ThemeFactory.create({}, THEME_2022_DARK);
--- packages/react-ui/lib/theming/themes/DefaultTheme.ts
+++ packages/react-ui/lib/theming/themes/DefaultTheme.ts
@@ -2,7 +2,4 @@ import { ThemeFactory } from '../ThemeFactory';
 
 import { THEME_2022 } from './Theme2022';
 
-/**
- * @deprecated Use THEME_2022
- */
 export const DEFAULT_THEME = ThemeFactory.create({}, THEME_2022);
