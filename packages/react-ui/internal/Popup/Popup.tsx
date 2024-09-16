2600fa253e463c6acb9153d1c887277403f45736
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2024-09-16 10:34:26 +0500

    chore: wip

--- packages/react-ui/internal/Popup/Popup.tsx
+++ packages/react-ui/internal/Popup/Popup.tsx
@@ -251,7 +251,7 @@ export class Popup extends React.Component<PopupProps, PopupState> {
     disableAnimations: isTestEnv,
     useWrapper: false,
     ignoreHover: false,
-    disablePortal: false,
+    disablePortal: true,
     width: 'auto',
     priority: ZIndex.priorities.Popup,
   };
