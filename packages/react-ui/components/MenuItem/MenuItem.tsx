31aa42a438b70f414274c0eadb42da7de45824e8
Funkicide <knel@skbkontur.ru>
2024-09-25 12:21:57 +0300

    refactor(Menu): remove legacy var usage

--- packages/react-ui/components/MenuItem/MenuItem.tsx
+++ packages/react-ui/components/MenuItem/MenuItem.tsx
@@ -307,7 +307,7 @@ export class MenuItem extends React.Component<MenuItemProps> {
         <div
           style={{ top: this.state.iconOffsetTop }}
           className={cx({
-            [styles.icon(this.theme)]: true,
+            [styles.icon()]: true,
             [this.getIconSizeClassName()]: true,
           })}
         >
