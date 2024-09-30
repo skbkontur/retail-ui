be5ba45becf3505f550c766bfd05585a11369d7d
SchwJ <juliachirkova@e1.ru>
2024-09-30 12:48:13 +0500

    chore(): draw examples

---
+++ packages/react-ui-validations/docs/Pages_NEW/Concepts/InlineValidations/InlineValidation.docs.stories.tsx
@@ -0,0 +1,86 @@
+import { Meta, Story } from '@skbkontur/react-ui/typings/stories';
+import React from 'react';
+import { Button } from '@skbkontur/react-ui/components/Button';
+
+import { ValidationContainer } from '../../../../src';
+import { Form } from '../../../Common/Form';
+import { Nullable } from '../../../../typings/Types';
+
+import { Input, DatePicker, lessThanDate } from './ControlsWithValidations';
+
+export default {
+  title: 'Концепции/Встраиваемые валидации',
+  parameters: { creevey: { skip: true } },
+} as Meta;
+
+export const InlineValidations: Story = () => {
+  interface ContactInfo {
+    name: string;
+    email: string;
+    born: string;
+  }
+
+  interface InlineValidationsDemoState {
+    data: ContactInfo;
+  }
+  class InlineValidationsDemo extends React.Component {
+    public state: InlineValidationsDemoState = {
+      data: {
+        name: '',
+        email: '',
+        born: '',
+      },
+    };
+
+    private container: Nullable<ValidationContainer> = null;
+
+    public render() {
+      const { data } = this.state;
+      return (
+        <ValidationContainer ref={this.refContainer}>
+          <Form>
+            <Form.Line title="Имя">
+              <Input required value={data.name} onValueChange={(value) => this.handleChange({ name: value })} />
+            </Form.Line>
+
+            <Form.Line title="E-mail">
+              <Input required email value={data.email} onValueChange={(value) => this.handleChange({ email: value })} />
+            </Form.Line>
+
+            <Form.Line title="Дата рождения">
+              <DatePicker
+                required
+                validations={[lessThanDate(new Date('2010-01-01'))]}
+                value={data.born}
+                onValueChange={(value) => this.handleChange({ born: value })}
+              />
+            </Form.Line>
+
+            <Form.ActionsBar>
+              <Button use={'primary'} onClick={this.handleSubmit}>
+                Submit
+              </Button>
+            </Form.ActionsBar>
+          </Form>
+        </ValidationContainer>
+      );
+    }
+
+    private handleChange = (data: Partial<ContactInfo>): void => {
+      this.setState({ data: { ...this.state.data, ...data } });
+    };
+
+    private handleSubmit = async (): Promise<void> => {
+      if (!this.container) {
+        throw new Error('invalid state');
+      }
+      if (await this.container.validate()) {
+        alert('success');
+      }
+    };
+
+    private refContainer = (el: Nullable<ValidationContainer>) => (this.container = el);
+  }
+
+  return <InlineValidationsDemo />;
+};
