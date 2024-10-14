1dad9d3715d2712bda08d3d57cc900d2f519d674
Funkicide <knel@skbkontur.ru>
2024-10-14 09:11:57 +0300

    chore(TokenInput): remove mention of whitespace from default locale

--- packages/react-ui/components/TokenInput/locale/locales/en.ts
+++ packages/react-ui/components/TokenInput/locale/locales/en.ts
@@ -1,6 +1,6 @@
 import { TokenInputLocale } from '../types';
 
 export const componentsLocales: TokenInputLocale = {
-  addButtonComment: 'Type comma or space',
+  addButtonComment: 'Type comma',
   addButtonTitle: 'Add',
 };
