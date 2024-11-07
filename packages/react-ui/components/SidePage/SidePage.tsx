69ad757bdd234c2d6b93ff7831c1ec03a867db79
lossir <lossir1@gmail.com>
2024-11-07 13:30:32 +0500

    fix: tests and fixes

--- packages/react-ui/components/SidePage/SidePage.tsx
+++ packages/react-ui/components/SidePage/SidePage.tsx
@@ -118,6 +118,10 @@ export class SidePage extends React.Component<SidePageProps, SidePageState> {
   public componentDidMount() {
     globalObject.addEventListener?.('keydown', this.handleKeyDown);
     this.stackSubscription = ModalStack.add(this, this.handleStackChange);
+
+    if (this.layout) {
+      this.layout.addEventListener('scroll', LayoutEvents.emit);
+    }
   }
 
   public componentDidUpdate(prevProps: SidePageProps) {
@@ -135,6 +139,10 @@ export class SidePage extends React.Component<SidePageProps, SidePageState> {
       this.stackSubscription.remove();
     }
     ModalStack.remove(this);
+
+    if (this.layout) {
+      this.layout.removeEventListener('scroll', LayoutEvents.emit);
+    }
   }
 
   /**
