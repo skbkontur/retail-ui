f601f046429c31f780fdea9890fe0be625776488
SchwJ <juliachirkova@e1.ru>
2024-11-12 16:35:14 +0500

    chore(): fix review comments

--- packages/react-ui/internal/ZIndex/ZIndex.tsx
+++ packages/react-ui/internal/ZIndex/ZIndex.tsx
@@ -189,8 +189,7 @@ export class ZIndex extends React.Component<ZIndexProps, ZIndexState> {
 
       if (isInstanceOf(portal, globalObject.HTMLElement)) {
         const portalID = portal.getAttribute(PORTAL_OUTLET_ATTR);
-        const root = element.getRootNode() as HTMLElement;
-        const noscript = root.querySelector(`noscript[${PORTAL_INLET_ATTR}="${portalID}"]`);
+        const noscript = globalObject.document?.querySelector(`noscript[${PORTAL_INLET_ATTR}="${portalID}"]`);
         const parent = noscript?.parentElement?.closest('[style*=z-index]');
 
         if (isInstanceOf(parent, globalObject.HTMLElement)) {
