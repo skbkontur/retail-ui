d10e4f0e3dc2ee9829ec167cbba0965b141d6eb3
Sasha Chabin <nibach@ya.ru>
2024-11-15 16:12:49 +0500

    feat(creevey): get storybook urls from creevey config

--- scripts/creevey-prepare.js
+++ scripts/creevey-prepare.js
@@ -1,10 +1,9 @@
 require('dotenv').config({ path: '../../.env' });
 const waitOn = require('wait-on');
+const config = require(`${process.cwd()}/${process.argv[2]}`);
 
-const STORYBOOK_USER_NOTICE_TIMEOUT = 3000;
+const STORYBOOK_USER_NOTICE_TIMEOUT = 2000;
 const STORYBOOK_NOT_RESPOND_TIMEOUT = 300000;
-
-const urls = process.argv[2].split(',');
 
 if (!process.env.GRID_URL || !process.env.GET_IP_URL) {
     error('Для запуска Creevey создайте в корне файл .env c переменными GRID_URL= и GET_IP_URL=');
@@ -15,11 +14,13 @@ if (!process.env.GRID_URL || !process.env.GET_IP_URL) {
 info('Waiting Storybook...\n');
 
 setTimeout(() => {
-    info('Check Storybook is running parallel `yarn storybook:test`\n');
+    warn('It looks like Storybook is not running!')
+    warn(`Run Storybook via \`yarn storybook:test\` and check ${config.storybookUrl}\n`);
+    info('Waiting Storybook...');
 }, STORYBOOK_USER_NOTICE_TIMEOUT);
 
 waitOn({
-    resources: urls.map(url => `http-get://${url}`),
+    resources: [config.storybookUrl],
     timeout: STORYBOOK_NOT_RESPOND_TIMEOUT
 }).then(() => {
     info('Storybook connected\n');
@@ -33,6 +34,10 @@ waitOn({
 
 function info(text) {
     console.info(`\x1b[34mINFO\x1b[0m => ${text}`);
+}
+
+function warn(text) {
+    console.info(`\x1b[33mWARNING\x1b[0m => ${text}`);
 }
 
 function error(text) {
