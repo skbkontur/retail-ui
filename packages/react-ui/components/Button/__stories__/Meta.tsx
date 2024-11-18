e82e143694aeac4aba07d917746e062c83a5eb84
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2024-11-18 10:14:07 +0500

    chore: add globals panel to Meta

--- packages/react-ui/components/Button/__stories__/Meta.tsx
+++ packages/react-ui/components/Button/__stories__/Meta.tsx
@@ -1,11 +1,29 @@
 import { DocsContext } from '@storybook/blocks';
 import type { ModuleExports } from '@storybook/types';
-import { useContext } from 'react';
+import React, { useContext } from 'react';
+
+import { Sticky } from '../../Sticky';
+import { Switcher } from '../../Switcher';
 
 export const Meta = ({ of }: { of: ModuleExports }) => {
   const context = useContext(DocsContext);
+
   if (of) {
     context.referenceMeta(of, true);
   }
-  return null;
-}
+
+  return (
+    <Sticky side="top">
+      <div style={{ padding: '20px', margin: '0 -20px', background: 'white' }}>
+        <Switcher
+          items={['LIGHT_THEME', 'DARK_THEME']}
+          //@ts-expect-error: store is not public
+          value={context.store.globals.globals.theme}
+          onValueChange={(value) => {
+            context.channel.emit('updateGlobals', { globals: { theme: value } });
+          }}
+        />
+      </div>
+    </Sticky>
+  );
+};
