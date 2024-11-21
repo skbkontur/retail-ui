6c018b1b8503e4250ad5a31138710060942960ec
Sasha Chabin <nibach@ya.ru>
2024-11-21 19:03:54 +0500

    chore: use `globalObject` instead `window`

--- packages/react-ui/lib/dom/scrollYCenterIntoNearestScrollable.ts
+++ packages/react-ui/lib/dom/scrollYCenterIntoNearestScrollable.ts
@@ -1,22 +1,32 @@
+import { globalObject, isBrowser } from "@skbkontur/global-object";
+
 export function scrollYCenterIntoNearestScrollable(element: HTMLElement) {
+  if (!isBrowser(globalObject)) {
+    return;
+  }
+
   const parent = findNearestScrollableParent(element);
   const elementRect = element.getBoundingClientRect();
-  const parentRect = parent.getBoundingClientRect();
+  const parentRect = parent!.getBoundingClientRect();
 
   const scrollTopPos = elementRect.top - parentRect.top;
   const scrollBottomPos = elementRect.bottom - parentRect.bottom;
   const scrollCenterPos = (scrollTopPos + scrollBottomPos) / 2;
 
   if (scrollCenterPos !== 0) {
-    parent.scrollTo({ top: scrollCenterPos });
+    parent!.scrollTo({ top: scrollCenterPos });
   }
 }
 
 function findNearestScrollableParent(element: HTMLElement) {
   let parent : Element | null = element.parentElement;
 
+  if (!isBrowser(globalObject)) {
+    return;
+  }
+
   while (parent) {
-    const { overflow } = window.getComputedStyle(parent);
+    const { overflow } = globalObject.getComputedStyle(parent);
     const overflowValues = overflow.split(' ');
 
     if (overflowValues.every((val) => val === 'auto' || val === 'scroll')) {
