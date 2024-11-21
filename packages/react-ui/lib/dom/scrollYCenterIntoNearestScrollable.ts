d031e0dbdb7242e3fc9cf3c5c725fb1731c76c44
Sasha Chabin <nibach@ya.ru>
2024-11-21 23:28:59 +0500

    chore: add `getParentOrShadowHost` function

--- packages/react-ui/lib/dom/scrollYCenterIntoNearestScrollable.ts
+++ packages/react-ui/lib/dom/scrollYCenterIntoNearestScrollable.ts
@@ -1,4 +1,6 @@
-import { globalObject, isBrowser } from "@skbkontur/global-object";
+import { globalObject, isBrowser } from '@skbkontur/global-object';
+
+import { getParentOrShadowHost } from './getParentOrShadowHost';
 
 export function scrollYCenterIntoNearestScrollable(element: HTMLElement) {
   if (!isBrowser(globalObject)) {
@@ -6,35 +8,40 @@ export function scrollYCenterIntoNearestScrollable(element: HTMLElement) {
   }
 
   const parent = findNearestScrollableParent(element);
+
+  if (!parent) {
+    return;
+  }
+
   const elementRect = element.getBoundingClientRect();
-  const parentRect = parent!.getBoundingClientRect();
+  const parentRect = parent.getBoundingClientRect();
 
   const scrollTopPos = elementRect.top - parentRect.top;
   const scrollBottomPos = elementRect.bottom - parentRect.bottom;
   const scrollCenterPos = (scrollTopPos + scrollBottomPos) / 2;
 
   if (scrollCenterPos !== 0) {
-    parent!.scrollTo({ top: scrollCenterPos });
+    parent.scrollTo({ top: scrollCenterPos });
   }
 }
 
-function findNearestScrollableParent(element: HTMLElement) {
-  let parent : Element | null = element.parentElement;
-
+export function findNearestScrollableParent(element: HTMLElement) {
   if (!isBrowser(globalObject)) {
     return;
   }
+
+  let parent = getParentOrShadowHost(element);
 
   while (parent) {
     const { overflow } = globalObject.getComputedStyle(parent);
     const overflowValues = overflow.split(' ');
     const isScrollbale = overflowValues.every((val) => val === 'auto' || val === 'scroll');
-    
+
     if (isScrollbale) {
       return parent;
     }
 
-    parent = parent.parentElement || parent.shadowRoot!.host
+    parent = getParentOrShadowHost(parent);
   }
 
   return element.ownerDocument.documentElement;
