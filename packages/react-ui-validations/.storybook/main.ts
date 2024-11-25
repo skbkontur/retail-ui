5b76eb7e8bcef2eb4f430f2ba2379eeb44a3d801
SchwJ <juliachirkova@e1.ru>
2024-11-25 16:20:37 +0500

    chore(): make addons and story files conditional

--- packages/react-ui-validations/.storybook/main.ts
+++ packages/react-ui-validations/.storybook/main.ts
@@ -2,24 +2,31 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const isDocsEnv = Boolean(process.env.STORYBOOK_REACT_UI_VALIDATIONS_DOCS);
 
+const conditionalAddons = isDocsEnv
+  ? [
+      '@skbkontur/storybook-addon-live-examples',
+      'storybook-addon-multiselect',
+      '@storybook/blocks',
+      '@storybook/addon-docs',
+      {
+        name: '@storybook/addon-essentials',
+        options: {
+          docsMode: true,
+        },
+      },
+    ]
+  : ['creevey'];
+
+const conditionalStories = isDocsEnv
+  ? ['../stories/**/*.stories.tsx', '../stories/**/*.mdx', '../docs/**/*.mdx', '../docs/**/*.docs.stories.tsx']
+  : ['../stories/**/*.stories.tsx'];
+
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.stories.tsx', '../stories/**/*.mdx', '../docs/**/*.mdx', '../docs/**/*.docs.stories.tsx'],
+  stories: conditionalStories,
   docs: {
     docsMode: isDocsEnv,
   },
-  addons: [
-    '@skbkontur/storybook-addon-live-examples',
-    'creevey',
-    'storybook-addon-multiselect',
-    '@storybook/blocks',
-    '@storybook/addon-docs',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        docsMode: true,
-      },
-    },
-  ],
+  addons: conditionalAddons,
   framework: {
     name: '@storybook/react-webpack5',
     options: {
@@ -29,6 +36,7 @@ const config: StorybookConfig = {
   },
   core: {
     disableWhatsNewNotifications: true,
+    disableTelemetry: true,
   },
 };
 
--- packages/react-ui/.storybook/main.ts
+++ packages/react-ui/.storybook/main.ts
@@ -2,31 +2,50 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const isDocsEnv = Boolean(process.env.STORYBOOK_REACT_UI_DOCS);
 
+const conditionalAddons = isDocsEnv
+  ? [
+      'creevey',
+      '@storybook/addon-links',
+      '@storybook/addon-a11y',
+      '@storybook/blocks',
+      '@storybook/addon-docs',
+      {
+        name: '@storybook/addon-essentials',
+        options: {
+          docsMode: true,
+        },
+      },
+      'storybook-addon-multiselect',
+    ]
+  : [
+      'creevey',
+      '@storybook/addon-links',
+      '@storybook/addon-a11y',
+      {
+        name: '@storybook/addon-essentials',
+        options: {
+          docs: false,
+        },
+      },
+      'storybook-addon-multiselect',
+    ];
+
+const conditionalStories = isDocsEnv
+  ? [
+      '../components/**/*.stories.tsx',
+      '../internal/**/*.stories.tsx',
+      '../components/**/*.mdx',
+      '../.storybook/**/*.mdx',
+      '../**/*.mdx',
+    ]
+  : ['../components/**/*.stories.tsx', '../internal/**/*.stories.tsx'];
+
 const config: StorybookConfig = {
-  stories: [
-    '../components/**/*.stories.tsx',
-    '../internal/**/*.stories.tsx',
-    '../components/**/*.mdx',
-    '../.storybook/**/*.mdx',
-    '../**/*.mdx',
-  ],
+  stories: conditionalStories,
   docs: {
     docsMode: isDocsEnv,
   },
-  addons: [
-    '@skbkontur/storybook-addon-live-examples',
-    'creevey',
-    '@storybook/addon-links',
-    '@storybook/addon-a11y',
-    '@storybook/blocks',
-    '@storybook/addon-docs',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        docsMode: true,
-      },
-    },
-  ],
+  addons: conditionalAddons,
   framework: {
     name: '@storybook/react-webpack5',
     options: {
@@ -37,6 +56,7 @@ const config: StorybookConfig = {
   },
   core: {
     disableWhatsNewNotifications: true,
+    disableTelemetry: true,
   },
 };
 export default config;
