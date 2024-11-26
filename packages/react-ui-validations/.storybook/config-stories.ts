56d22312e4db1ac6aad79a1e19d15fe8f69a82ec
SchwJ <juliachirkova@e1.ru>
2024-11-26 15:30:33 +0500

    chore(): remove docs.stories from test storybook

--- packages/react-ui-validations/.storybook/config-stories.ts
+++ packages/react-ui-validations/.storybook/config-stories.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.stories.tsx'],
+  stories: ['../stories/**/!(*.docs)*.stories.tsx'],
   docs: {
     docsMode: false,
   },
