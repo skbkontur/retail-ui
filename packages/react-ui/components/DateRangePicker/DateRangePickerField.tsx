b988b4eb70a1d74a371f87b2d88881578db7e48e
Sasha Chabin <nibach@ya.ru>
2024-11-27 06:14:33 +0500

    chore(): fix ESLint

--- packages/react-ui/components/DateRangePicker/DateRangePickerField.tsx
+++ packages/react-ui/components/DateRangePicker/DateRangePickerField.tsx
@@ -1,53 +1,55 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
 
-import { DateInput, DateInputProps } from "../DateInput";
+import { DateInput, DateInputProps } from '../DateInput';
 
-import { DateRangePickerContext } from "./DateRangePickerContext";
-import { DateRangePickerDataTids } from "./DateRangePicker";
+import { DateRangePickerContext } from './DateRangePickerContext';
+import { DateRangePickerDataTids } from './DateRangePicker';
 
-export const DateRangePickerField: React.FC<DateInputProps & { type: "start" | "end" }> = (props) => {
-    const state = useContext(DateRangePickerContext);
+export const DateRangePickerField: React.FC<DateInputProps & { type: 'start' | 'end' }> = (props) => {
+  const state = useContext(DateRangePickerContext);
 
-    return (
-        <div style={{ display: 'inline-block' }}>
-            <DateInput
-                width="100%"
-                value={props.type === 'start' ? state.periodStart : state.periodEnd}
-                withIcon
-                size={props.size || state.size}
-                minDate={props.type === 'start' ? state.minDate : state.maxDate}
-                disabled={props.disabled}
-                // ref={state.start} или ref={state.end} в зависимости от focus
-                onFocus={() => {
-                    if (state.setCurrentFocus) {
-                        state.setCurrentFocus(props.type);
-                    }
+  return (
+    <div style={{ display: 'inline-block' }}>
+      <DateInput
+        width="100%"
+        value={props.type === 'start' ? state.periodStart : state.periodEnd}
+        withIcon
+        size={props.size || state.size}
+        minDate={props.type === 'start' ? state.minDate : state.maxDate}
+        disabled={props.disabled}
+        // ref={state.start} или ref={state.end} в зависимости от focus
+        onFocus={() => {
+          if (state.setCurrentFocus) {
+            state.setCurrentFocus(props.type);
+          }
 
-                    if (state.setShowCalendar) {
-                        state.setShowCalendar(true);
-                    }
+          if (state.setShowCalendar) {
+            state.setShowCalendar(true);
+          }
 
-                    if (props.type === 'start' ? state.periodStart : state.periodEnd) {
-                        const [, month, year] = (props.type === 'start' ? state.periodStart || "" : state.periodEnd || "")?.split('.').map(Number);
-                        if (month && state.calendarRef) {
-                            state.calendarRef?.current?.scrollToMonth(month - 1, year);
-                        }
-                    }
-                }}
-                onValueChange={(value) => {
-                    if (props.type === 'start' && state.setPeriodStart) {
-                        state.setPeriodStart(value);
-                        return;
-                    }
+          if (props.type === 'start' ? state.periodStart : state.periodEnd) {
+            const [, month, year] = (props.type === 'start' ? state.periodStart || '' : state.periodEnd || '')
+              ?.split('.')
+              .map(Number);
+            if (month && state.calendarRef) {
+              state.calendarRef?.current?.scrollToMonth(month - 1, year);
+            }
+          }
+        }}
+        onValueChange={(value) => {
+          if (props.type === 'start' && state.setPeriodStart) {
+            state.setPeriodStart(value);
+            return;
+          }
 
-                    if (props.type === 'end' && state.setPeriodEnd) {
-                        state.setPeriodEnd(value);
-                        return;
-                    }
-                }}
-                data-tid={props.type === 'start' ? DateRangePickerDataTids.from : DateRangePickerDataTids.to}
-                {...props}
-            />
-        </div>
-    );
+          if (props.type === 'end' && state.setPeriodEnd) {
+            state.setPeriodEnd(value);
+            return;
+          }
+        }}
+        data-tid={props.type === 'start' ? DateRangePickerDataTids.from : DateRangePickerDataTids.to}
+        {...props}
+      />
+    </div>
+  );
 };
