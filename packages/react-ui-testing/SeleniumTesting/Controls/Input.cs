11227a005873c5c452911e2ff7ddde3b14bfddbe
Aleksandr Kostarev <60134280+kostarev-a@users.noreply.github.com>
2024-12-09 11:25:21 +0500

    fix(selenium-testing): speed up input's "clear" method  (#3555)

--- packages/react-ui-testing/SeleniumTesting/Controls/Input.cs
+++ packages/react-ui-testing/SeleniumTesting/Controls/Input.cs
@@ -43,13 +43,8 @@ namespace SKBKontur.SeleniumTesting.Controls
                 x =>
                     {
                         var inputElement = x.FindElement(By.CssSelector("input"));
-                        inputElement.SendKeys(Keys.End);
-                        var length = inputElement.GetAttribute("value").Length;
-                        while(length > 0)
-                        {
-                            inputElement.SendKeys(Keys.Backspace);
-                            length--;
-                        }
+                        inputElement.SendKeys(Keys.Control + "a");
+                        inputElement.SendKeys(Keys.Delete);
                     },
                 "Clear");
         }
