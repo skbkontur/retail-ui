e8882c5407c99959ca65493a072b1cee453c8683
SchwJ <juliachirkova@e1.ru>
2024-12-09 15:31:46 +0500

    chore(): fix radio size examples

--- packages/react-ui/components/Radio/__docs__/Radio.docs.stories.tsx
+++ packages/react-ui/components/Radio/__docs__/Radio.docs.stories.tsx
@@ -13,7 +13,7 @@ export const Example1: Story = () => {
   const [chosen, setChosen] = React.useState(null);
 
   return (
-    <RadioGroup onValueChange={(value) => setChosen(value)}>
+    <RadioGroup value={chosen} onValueChange={setChosen}>
       <Gapped gap={3} vertical>
         <Radio value={1}>Обычная радио-кнопка</Radio>
         <Radio value={2} disabled>
@@ -40,7 +40,7 @@ Example1.storyName = 'Виды радио-кнопок';
 /** Радио-кнопки могут иметь сразу несколько состояний. */
 export const Example2: Story = () => {
   return (
-    <Radio disabled checked warning>
+    <Radio disabled checked warning value={true}>
       Отключенная, отмеченная радио-кнопка в состоянии <b>warning</b>
     </Radio>
   );
@@ -48,18 +48,21 @@ export const Example2: Story = () => {
 Example2.storyName = 'Состояния';
 
 export const Example3: Story = () => {
+  const [chosen, setChosen] = React.useState(null);
   return (
-    <Gapped vertical>
-      <Radio size="small" value="value">
-        Маленький
-      </Radio>
-      <Radio size="medium" value="value">
-        Средний
-      </Radio>
-      <Radio size="large" value="value">
-        Большой
-      </Radio>
-    </Gapped>
+    <RadioGroup value={chosen} onValueChange={setChosen}>
+      <Gapped vertical>
+        <Radio size="small" value={1}>
+          Маленький
+        </Radio>
+        <Radio size="medium" value={2}>
+          Средний
+        </Radio>
+        <Radio size="large" value={3}>
+          Большой
+        </Radio>
+      </Gapped>
+    </RadioGroup>
   );
 };
 Example3.storyName = 'Размер';
