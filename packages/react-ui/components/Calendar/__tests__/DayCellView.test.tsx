5b6095a984a8a6dad8f24bebd118699fda8076b2
Roman Knel <knel@skbkontur.ru>
2024-12-24 08:34:30 +0300

    fix(Calendar, DatePicker): fix click on disabled dates (#3574)
    Co-authored-by: Egor Pogadaev <pogadaev.e@skbkontur.ru>

--- packages/react-ui/components/Calendar/__tests__/DayCellView.test.tsx
+++ packages/react-ui/components/Calendar/__tests__/DayCellView.test.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { LangCodes, LocaleContext } from '../../../lib/locale';
-import { Calendar, CalendarDataTids } from '../Calendar';
+import { Calendar, CalendarProps, CalendarDataTids } from '../Calendar';
+import { CalendarDay } from '../CalendarDay';
 import { componentsLocales as DayCellViewLocalesRu } from '../locale/locales/ru';
 import { componentsLocales as DayCellViewLocalesEn } from '../locale/locales/en';
 import { InternalDate } from '../../../lib/date/InternalDate';
+import { CalendarLocaleHelper } from '../locale';
 
 describe('DayCellView', () => {
   describe('a11y', () => {
@@ -50,5 +53,52 @@ describe('DayCellView', () => {
 
       expect(screen.getAllByTestId(CalendarDataTids.dayCell)[0]).toHaveAttribute('aria-label', ariaLabel);
     });
+  });
+
+  it('should not call onClick when disabled', async () => {
+    const initialDate = '03.11.2021';
+    const minDate = '02.11.2021';
+    const maxDate = '05.11.2021';
+    const expectedDate = '06.11.2021';
+    const onValueChange = jest.fn();
+
+    render(<Calendar value={initialDate} minDate={minDate} maxDate={maxDate} onValueChange={onValueChange} />);
+
+    const outOfRangeDay = screen.getByRole('button', {
+      name: `${CalendarLocaleHelper.get(LangCodes.ru_RU).dayCellChooseDateAriaLabel}: ${new InternalDate({
+        langCode: LangCodes.ru_RU,
+        value: expectedDate,
+      }).toA11YFormat()}`,
+    });
+
+    userEvent.click(outOfRangeDay, undefined, {
+      skipPointerEventsCheck: true,
+    });
+
+    expect(onValueChange).not.toHaveBeenCalled();
+  });
+
+  it('should not loose users click handler', async () => {
+    const initialDate = '03.11.2021';
+    const expectedDate = '06.11.2021';
+    const onValueChange = jest.fn();
+    const customDayOnClick = jest.fn();
+    const renderDay: CalendarProps['renderDay'] = (props) => <CalendarDay {...props} onClick={customDayOnClick} />;
+
+    render(<Calendar value={initialDate} renderDay={renderDay} onValueChange={onValueChange} />);
+
+    const dayToClickOn = screen.getByRole('button', {
+      name: `${CalendarLocaleHelper.get(LangCodes.ru_RU).dayCellChooseDateAriaLabel}: ${new InternalDate({
+        langCode: LangCodes.ru_RU,
+        value: expectedDate,
+      }).toA11YFormat()}`,
+    });
+
+    userEvent.click(dayToClickOn, undefined, {
+      skipPointerEventsCheck: true,
+    });
+
+    expect(onValueChange).toHaveBeenCalled();
+    expect(customDayOnClick).toHaveBeenCalled();
   });
 });
