bb16b81afaa9539706a5634ef2f547beb196c7d5
Sasha Chabin <nibach@ya.ru>
2024-12-26 07:45:13 +0500

    chore(): add ru/en locales

---
+++ packages/react-ui/components/DateRangePicker/locale/locales/ru.ts
@@ -0,0 +1,6 @@
+import { DateRangePickerLocale } from '../types';
+import { componentsLocales as DatePickerLocale } from '../../../DatePicker/locale/locales/ru';
+
+export const componentsLocales: DateRangePickerLocale = {
+  ...DatePickerLocale,
+};
