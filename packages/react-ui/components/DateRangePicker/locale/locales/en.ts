1cccf286663b946c4e876e8d688291f6999ebe1a
Sasha Chabin <nibach@ya.ru>
2024-12-28 14:47:35 +0500

    chore(): add locales to optional buttons

--- packages/react-ui/components/DateRangePicker/locale/locales/en.ts
+++ packages/react-ui/components/DateRangePicker/locale/locales/en.ts
@@ -2,5 +2,7 @@ import { DateRangePickerLocale } from '../types';
 import { componentsLocales as DatePickerLocale } from '../../../DatePicker/locale/locales/en';
 
 export const componentsLocales: DateRangePickerLocale = {
+  withoutFirstDate: 'No first date',
+  withoutSecondDate: 'No second date',
   ...DatePickerLocale,
 };
