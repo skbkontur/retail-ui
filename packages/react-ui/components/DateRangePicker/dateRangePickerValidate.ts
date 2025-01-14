f8e2fb9a9f06a65f93bd3867ffc0fd1ce8521fc3
Sasha Chabin <nibach@ya.ru>
2025-01-14 09:18:47 +0500

    chore(): refactor validations

--- packages/react-ui/components/DateRangePicker/dateRangePickerValidate.ts
+++ packages/react-ui/components/DateRangePicker/dateRangePickerValidate.ts
@@ -1,27 +1,24 @@
-import { InternalDateOrder, InternalDateSeparator, InternalDateValidateCheck } from "../../lib/date/types";
-import { InternalDate } from "../../lib/date/InternalDate";
-import { MAX_FULLDATE, MIN_FULLDATE } from "../../lib/date/constants";
+import { InternalDateOrder, InternalDateSeparator, InternalDateValidateCheck } from '../../lib/date/types';
+import { InternalDate } from '../../lib/date/InternalDate';
+import { MAX_FULLDATE, MIN_FULLDATE } from '../../lib/date/constants';
 
-export function dateRangePickerValidate(
-  value: string[],
-  range: { minDate?: string; maxDate?: string } = {}
-) {
+export function dateRangePickerValidate(value: string[], range: { minDate?: string; maxDate?: string } = {}) {
   if (!value || !value[0] || !value[1]) {
     return [false, false];
   }
 
   const { minDate = MIN_FULLDATE, maxDate = MAX_FULLDATE } = range;
-  const internalDates = new InternalDate({
+  const { parseValue } = new InternalDate({
     order: InternalDateOrder.DMY,
     separator: InternalDateSeparator.Dot,
   })
     .setRangeStart(new InternalDate({ value: minDate }))
     .setRangeEnd(new InternalDate({ value: maxDate }));
 
-  return [
-    checkDate(internalDates.parseValue(value[0])),
-    checkDate(internalDates.parseValue(value[1])),
-  ];
+  const startInternalDate = parseValue(value[0]);
+  const endInternalDate = parseValue(value[1]);
+
+  return [checkDate(startInternalDate), checkDate(endInternalDate)];
 }
 
 function checkDate(internalDate: InternalDate) {
