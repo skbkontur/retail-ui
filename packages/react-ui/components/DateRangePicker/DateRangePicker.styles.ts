4b18d0877d5b2023ff079db9fbd40d29c496f631
Sasha Chabin <nibach@ya.ru>
2025-01-16 08:58:39 +0500

    chore(): move css styles to file

--- packages/react-ui/components/DateRangePicker/DateRangePicker.styles.ts
+++ packages/react-ui/components/DateRangePicker/DateRangePicker.styles.ts
@@ -1,5 +1,7 @@
+import { SizeProp } from '../../lib/types/props';
 import { css, memoizeStyle } from '../../lib/theming/Emotion';
 import { Theme } from '../../lib/theming/Theme';
+import { CalendarDataTids } from '../Calendar';
 
 export const styles = memoizeStyle({
   root() {
@@ -17,20 +19,14 @@ export const styles = memoizeStyle({
       }
     `;
   },
-  inputVisuallyFocus(t: Theme) {
-    return css`
-      box-shadow: ${t.inputFocusShadow};
-    `;
-  },
   inputWrapperWidth(t: Theme) {
     return css`
       min-width: ${parseInt(t.calendarCellWidth) * 7 + parseInt(t.calendarPaddingX) * 2}px;
     `;
   },
-
-  calendarMobile() {
+  inputVisuallyFocus(t: Theme) {
     return css`
-      width: auto;
+      box-shadow: ${t.inputFocusShadow};
     `;
   },
   calendarWrapper(t: Theme) {
@@ -42,4 +38,67 @@ export const styles = memoizeStyle({
       touch-action: none;
     `;
   },
+  calendarWidthAuto() {
+    return css`
+      width: auto;
+    `;
+  },
+  rangeCalendarDay() {
+    return css`
+      width: 100%;
+      height: 100%;
+    `;
+  },
+  rangeCalendarDayEnd(t: Theme) {
+    return css`
+      position: relative;
+
+      &:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: ${t.rangeCalendarCellEndBg};
+        border-radius: ${t.calendarCellBorderRadius};
+      }
+
+      [data-tid=${CalendarDataTids.dayCell}] {
+        color: ${t.rangeCalendarCellEndColor};
+
+        @media (hover: hover) {
+          &:hover {
+            background: none;
+          }
+        }
+      }
+    `;
+  },
+  rangeCalendarDayInHoveredPeriod(t: Theme) {
+    return css`
+      background: ${t.rangeCalendarCellBg};
+    `;
+  },
+  rangeCalendarDayHoverInPeriod(t: Theme) {
+    return css`
+      @media (hover: hover) {
+        &:hover [data-tid=${CalendarDataTids.dayCell}] {
+          background: ${t.rangeCalendarCellHoverBg};
+        }
+      }
+    `;
+  },
 });
+
+export function getFontSize(t: Theme, size?: SizeProp) {
+  switch (size) {
+    case 'large':
+      return t.fontSizeLarge;
+    case 'medium':
+      return t.fontSizeMedium;
+    case 'small':
+    default:
+      return t.fontSizeSmall;
+  }
+}
