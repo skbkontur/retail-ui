322012afe4aa969e8300ccd248a3301441ada530
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2025-01-31 10:39:58 +0500

    chore: wip

--- packages/react-ui/internal/themes/LightTheme5_0.ts
+++ packages/react-ui/internal/themes/LightTheme5_0.ts
@@ -2,14 +2,7 @@ import { createTheme, markThemeVersion } from '../../lib/theming/ThemeHelpers';
 
 import { BasicTheme, BasicThemeClass } from './BasicTheme';
 
-export const LightTheme5_0 = createTheme(
-  class extends (class {} as typeof BasicThemeClass) {
-    public static get errorText() {
-      return this.bgActive;
-    }
-  },
-  {
-    prototypeTheme: BasicTheme,
-    themeMarkers: [markThemeVersion(5.0)],
-  },
-);
+export const LightTheme5_0 = createTheme(class extends (class {} as typeof BasicThemeClass) {}, {
+  prototypeTheme: BasicTheme,
+  themeMarkers: [markThemeVersion(5.0)],
+});
