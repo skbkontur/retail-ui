bba277cc2af15b5e15ca22baec63fd95cb5e3577
SchwJ <juliachirkova@e1.ru>
2025-02-03 16:40:17 +0500

    chore(): add cross in InputLayout

--- packages/react-ui/components/Input/InputLayout/InputLayout.tsx
+++ packages/react-ui/components/Input/InputLayout/InputLayout.tsx
@@ -3,20 +3,25 @@ import React from 'react';
 import { forwardRefAndName } from '../../../lib/forwardRefAndName';
 import { InputDataTids, InputProps } from '../Input';
 import { CommonProps, CommonWrapper } from '../../../internal/CommonWrapper';
+import { CleanCrossIcon } from '../../../internal/CleanCrossIcon/CleanCrossIcon';
 
 import { InputLayoutAside } from './InputLayoutAside';
 import { InputLayoutContext, InputLayoutContextDefault, InputLayoutContextProps } from './InputLayoutContext';
 import { stylesLayout } from './InputLayout.styles';
 
-type InputLayoutRootFromInputProps = Pick<InputProps, 'leftIcon' | 'rightIcon' | 'prefix' | 'suffix'>;
+type InputLayoutRootFromInputProps = Pick<
+  InputProps,
+  'showCleanCross' | 'leftIcon' | 'rightIcon' | 'prefix' | 'suffix'
+>;
 
 export interface InputLayoutRootProps extends InputLayoutRootFromInputProps, CommonProps {
   labelProps: React.LabelHTMLAttributes<HTMLLabelElement>;
   context: Partial<InputLayoutContextProps>;
+  clearInput: () => void;
 }
 
 export const InputLayout = forwardRefAndName<HTMLLabelElement, InputLayoutRootProps>('InputLayout', (props, ref) => {
-  const { leftIcon, rightIcon, prefix, suffix, labelProps, context, children } = props;
+  const { showCleanCross, clearInput, leftIcon, rightIcon, prefix, suffix, labelProps, context, children } = props;
   const _context: InputLayoutContextProps = { ...InputLayoutContextDefault, ...context };
 
   return (
@@ -25,7 +30,24 @@ export const InputLayout = forwardRefAndName<HTMLLabelElement, InputLayoutRootPr
         <label ref={ref} data-tid={InputDataTids.root} {...labelProps}>
           <InputLayoutAside icon={leftIcon} text={prefix} side="left" />
           <span className={stylesLayout.input()}>{children}</span>
-          <InputLayoutAside icon={rightIcon} text={suffix} side="right" />
+          {showCleanCross ? (
+            <InputLayoutAside
+              icon={
+                <CleanCrossIcon
+                  data-tid={InputDataTids.close}
+                  size={_context.size}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    clearInput();
+                  }}
+                />
+              }
+              text={suffix}
+              side="right"
+            />
+          ) : (
+            <InputLayoutAside icon={rightIcon} text={suffix} side="right" />
+          )}
         </label>
       </CommonWrapper>
     </InputLayoutContext.Provider>
