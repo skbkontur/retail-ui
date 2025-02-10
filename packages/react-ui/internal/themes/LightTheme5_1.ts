9c1ea8327073b7ad8e4f5e0498b72dd9aacfe6a3
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2025-02-10 10:12:20 +0500

    chore: wip

--- packages/react-ui/internal/themes/LightTheme5_1.ts
+++ packages/react-ui/internal/themes/LightTheme5_1.ts
@@ -5,5 +5,5 @@ import { LightTheme5_0 } from './LightTheme5_0';
 
 export const LightTheme5_1 = createThemeFromClass(class LightTheme5_1 extends (class {} as typeof BasicThemeClass) {}, {
   prototypeTheme: LightTheme5_0,
-  themeMarkers: [markThemeVersion(5.1)],
+  themeMarkers: [markThemeVersion('5_1')],
 });
