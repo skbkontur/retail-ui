9486f030da24a32e276ed6e6412b60c37eeb0e2d
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2025-02-11 17:19:53 +0500

    chore: wip

--- packages/react-ui/lib/theming/ThemeVersionsComparator.ts
+++ packages/react-ui/lib/theming/ThemeVersionsComparator.ts
@@ -1,26 +1,37 @@
-const compareVersions = (ver1: string, ver2: string, comparator: (a: number, b: number) => boolean) => {
-  if (!ver1 || !ver2) {
-    return false;
-  }
+import warning from 'warning';
 
-  const [arr1, arr2] = [ver1, ver2].map((version) => version.split('_').map(Number));
+const versionToNumbers = (version: string, delimiter: string): number[] => {
+  return version.split(delimiter).map((i) => (/^\d+$/.test(i) ? Number(i) : Number.NaN));
+};
+
+const compareVersions = (
+  ver1: string,
+  ver2: string,
+  comparator: (a: number, b: number) => boolean,
+  delimiter: string = '_',
+): boolean => {
+  const arr1 = versionToNumbers(ver1, delimiter);
+  const arr2 = versionToNumbers(ver2, delimiter);
   const maxArrLength = Math.max(arr1.length, arr2.length);
 
   for (let i = 0; i < maxArrLength; i++) {
-    const item1 = arr1[i] || 0;
-    const item2 = arr2[i] || 0;
+    const item1 = arr1[i] ?? 0;
+    const item2 = arr2[i] ?? 0;
 
-    if ([item1, item2].find(Number.isNaN)) {
-      return false;
+    if (Number.isNaN(item1) || Number.isNaN(item2)) {
+      warning(
+        true,
+        `[ThemeVersionsComparator]: unsupported version format. Only numbers and "${delimiter}" are allowed in ${ver1} and ${ver2}.`,
+      );
+      break;
     }
 
-    if (item1 === item2) {
-      continue;
-    } else {
+    if (item1 !== item2 || i === maxArrLength - 1) {
       return comparator(item1, item2);
     }
   }
-  return comparator(1, 1);
+
+  return false;
 };
 
 export const isVersionEQ = (ver1: string, ver2: string) => compareVersions(ver1, ver2, (a, b) => a === b);
