7f0102d85206b4b9f74c25cd68d71e4b07bfd3a3
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2025-02-12 15:56:42 +0500

    chore: wip

--- packages/react-ui/lib/theming/themes/DarkTheme.ts
+++ packages/react-ui/lib/theming/themes/DarkTheme.ts
@@ -1,8 +1,7 @@
-import { ThemeFactory } from '../ThemeFactory';
 import { DarkTheme5_0 } from '../../../internal/themes/DarkTheme5_0';
 import { DarkTheme5_1 } from '../../../internal/themes/DarkTheme5_1';
 
-export const DARK_THEME_5_0 = ThemeFactory.create({}, DarkTheme5_0);
-export const DARK_THEME_5_1 = ThemeFactory.create({}, DarkTheme5_1);
+export const DARK_THEME_5_0 = DarkTheme5_0;
+export const DARK_THEME_5_1 = DarkTheme5_1;
 
 export const DARK_THEME = DARK_THEME_5_0;
