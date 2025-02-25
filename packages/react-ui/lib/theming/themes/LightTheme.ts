229e95e61f444e4471efb4f046fcfae05d56ff49
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2025-02-25 13:03:03 +0500

    chore: actualize default themes versions

--- packages/react-ui/lib/theming/themes/LightTheme.ts
+++ packages/react-ui/lib/theming/themes/LightTheme.ts
@@ -4,4 +4,4 @@ import { LightTheme5_1 } from '../../../internal/themes/LightTheme5_1';
 export const LIGHT_THEME_5_0 = LightTheme5_0;
 export const LIGHT_THEME_5_1 = LightTheme5_1;
 
-export const LIGHT_THEME = LIGHT_THEME_5_0;
+export const LIGHT_THEME = LIGHT_THEME_5_1;
