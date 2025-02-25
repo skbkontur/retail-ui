6a42eec7a47ea0931f922a55023b50ab8ee75be8
Alexander Chabin <chabin@skbkontur.ru>
2025-02-25 15:22:19 +0500

    feat(DateRangePicker): add validations example

---
+++ packages/react-ui-validations/stories/DateRangePicker.stories.tsx
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from 'react';
+import { Meta } from '@storybook/react';
+import { Button } from '@skbkontur/react-ui/components/Button';
+import { DateRangePicker } from '@skbkontur/react-ui/components/DateRangePicker';
+
+import { ValidationContainer, ValidationInfo, ValidationWrapper } from '../src';
+
+export default {
+  title: 'DateRangePicker',
+} as Meta;
+
+export const Example1 = () => {
+  const refContainer = useRef<ValidationContainer>(null);
+  const [valueStart, setValueStart] = useState('');
+  const [valueEnd, setValueEnd] = useState('');
+  const minDate = '11.01.2025';
+  const maxDate = '23.05.2025';
+
+  const validate = () => {
+    const message = (
+      <>
+        Укажите даты в промежутке
+        <br />
+        {minDate}—{maxDate}
+        <br />в формате ДД.ММ.ГГГГ
+      </>
+    );
+    const validationInfoSubmit: ValidationInfo = { message, type: 'submit' };
+    const validationInfoFocus: ValidationInfo = { message, type: 'lostfocus' };
+
+    if ((valueStart && !valueEnd) || (!valueStart && valueEnd)) {
+      return {
+        start: validationInfoSubmit,
+        end: validationInfoSubmit,
+      };
+    }
+
+    const [isStartValid, isEndValid] = DateRangePicker.validate(valueStart, valueEnd, { minDate, maxDate });
+    return {
+      start: !isStartValid ? validationInfoFocus : null,
+      end: !isEndValid ? validationInfoFocus : null,
+    };
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', gap: 20, padding: 20 }}>
+      <ValidationContainer ref={refContainer}>
+        <DateRangePicker>
+          <ValidationWrapper validationInfo={validate().start}>
+            <DateRangePicker.Start value={valueStart} minDate={minDate} onValueChange={setValueStart} />
+          </ValidationWrapper>
+          <DateRangePicker.Separator />
+          <ValidationWrapper validationInfo={validate().end}>
+            <DateRangePicker.End value={valueEnd} maxDate={maxDate} onValueChange={setValueEnd} />
+          </ValidationWrapper>
+        </DateRangePicker>
+      </ValidationContainer>
+
+      <Button onClick={() => refContainer.current?.validate()}>Submit</Button>
+    </div>
+  );
+};
