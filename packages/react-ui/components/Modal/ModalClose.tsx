b8973f55df600ae478f9309da5749fd9f35d03e6
SchwJ <juliachirkova@e1.ru>
2025-02-27 19:36:26 +0500

    chore(): fix lint errors

--- packages/react-ui/components/Modal/ModalClose.tsx
+++ packages/react-ui/components/Modal/ModalClose.tsx
@@ -8,7 +8,7 @@ import { keyListener } from '../../lib/events/keyListener';
 import { ResponsiveLayout } from '../ResponsiveLayout';
 import { CommonWrapper } from '../../internal/CommonWrapper';
 import { XIcon20Regular } from '../../internal/icons2022/XIcon/XIcon20Regular';
-import { isThemeVersionGTE } from '../../lib/theming/ThemeHelpers';
+import { isThemeVersion } from '../../lib/theming/ThemeHelpers';
 
 import { CloseProps } from './ModalContext';
 import { styles } from './Modal.styles';
@@ -35,6 +35,7 @@ export function ModalClose({ disableClose, requestClose, ...otherProps }: CloseP
   };
 
   const icon = <XIcon20Regular align="none" />;
+  const versionGTE5_1 = isThemeVersion(theme, '5.1');
 
   return (
     <CommonWrapper {...otherProps}>
@@ -44,12 +45,12 @@ export function ModalClose({ disableClose, requestClose, ...otherProps }: CloseP
             aria-label={locale.closeButtonAriaLabel}
             className={cx({
               [styles.close(theme)]: true,
-              [styles.close5_1(theme)]: isThemeVersionGTE(theme, 5, 1),
+              [styles.close5_1(theme)]: versionGTE5_1,
               [styles.closeMobile(theme)]: isMobile,
-              [styles.closeMobile5_1(theme)]: isMobile && isThemeVersionGTE(theme, 5, 1),
+              [styles.closeMobile5_1(theme)]: isMobile && versionGTE5_1,
               [styles.disabled(theme)]: disableClose,
-              [styles.focus(theme)]: focusedByTab && !isThemeVersionGTE(theme, 5, 1),
-              [styles.focus5_1(theme)]: focusedByTab && isThemeVersionGTE(theme, 5, 1),
+              [styles.focus(theme)]: focusedByTab && !versionGTE5_1,
+              [styles.focus5_1(theme)]: focusedByTab && versionGTE5_1,
             })}
             onClick={requestClose}
             onFocus={handleFocus}
