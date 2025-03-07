35bcf9b438927c9e9f83d0c764746490c9d360a7
Alexander Chabin <chabin@skbkontur.ru>
2025-03-07 10:01:20 +0500

    chore(): create 2 groups of tests, fix typos

--- packages/react-ui/components/DateRangePicker/__tests__/getStateForValue-test.ts
+++ packages/react-ui/components/DateRangePicker/__tests__/getStateForValue-test.ts
@@ -15,21 +15,7 @@ describe('DateRangePicker value pick', () => {
     });
   });
 
-  it(`should set value to 2nd field, save focus in 2nd and close picker`, () => {
-    expect(
-      getStateForValue('end', '10.10.2025', {
-        currentStart: '10.10.2000',
-        currentEnd: '',
-      }),
-    ).toEqual({
-      start: '10.10.2000',
-      end: '10.10.2025',
-      focus: 'end',
-      isOpen: false,
-    });
-  });
-
-  it(`should set value to 2nd field, preserve save focus in 2nd and close picker`, () => {
+  it(`should set value to 2nd field, save save focus in 2nd and close picker`, () => {
     expect(
       getStateForValue('end', '20.10.2025', {
         currentStart: '10.10.2000',
@@ -53,76 +39,6 @@ describe('DateRangePicker value pick', () => {
       start: '10.10.2000',
       end: '15.10.2000',
       focus: 'end',
-      isOpen: false,
-    });
-  });
-
-  it(`should update 2nd field, save focus in 2nd and close picker`, () => {
-    expect(
-      getStateForValue('end', '15.10.2000', {
-        currentStart: '10.10.2000',
-        currentEnd: '20.10.2000',
-      }),
-    ).toEqual({
-      start: '10.10.2000',
-      end: '15.10.2000',
-      focus: 'end',
-      isOpen: false,
-    });
-  });
-
-  it(`should preserve equal value in 2nd field, save focus in 2nd and close picker`, () => {
-    expect(
-      getStateForValue('end', '15.10.2000', {
-        currentStart: '10.10.2000',
-        currentEnd: '15.10.2000',
-      }),
-    ).toEqual({
-      start: '10.10.2000',
-      end: '15.10.2000',
-      focus: 'end',
-      isOpen: false,
-    });
-  });
-
-  it(`should set equally value to 2nd field, save focus in 2nd and close picker`, () => {
-    expect(
-      getStateForValue('end', '10.10.2000', {
-        currentStart: '10.10.2000',
-        currentEnd: '15.10.2000',
-      }),
-    ).toEqual({
-      start: '10.10.2000',
-      end: '10.10.2000',
-      focus: 'end',
-      isOpen: false,
-    });
-  });
-
-  it(`should set equally value to 1st field, save focus in 1st and close picker`, () => {
-    expect(
-      getStateForValue('start', '10.10.2000', {
-        currentStart: '10.10.2000',
-        currentEnd: '10.10.2000',
-      }),
-    ).toEqual({
-      start: '10.10.2000',
-      end: '10.10.2000',
-      focus: 'start',
-      isOpen: false,
-    });
-  });
-
-  it(`should set value to 1st field, save focus in 1st and close picker`, () => {
-    expect(
-      getStateForValue('start', '10.10.2000', {
-        currentStart: '10.10.2000',
-        currentEnd: '10.10.2000',
-      }),
-    ).toEqual({
-      start: '10.10.2000',
-      end: '10.10.2000',
-      focus: 'start',
       isOpen: false,
     });
   });
@@ -155,7 +71,7 @@ describe('DateRangePicker value pick', () => {
     });
   });
 
-  it(`should save state when 1st value < minDate`, () => {
+  it(`should save state if 1st value < minDate`, () => {
     expect(
       getStateForValue('start', '01.10.2000', {
         currentStart: '20.10.2010',
@@ -170,7 +86,7 @@ describe('DateRangePicker value pick', () => {
     });
   });
 
-  it(`should save state when if 2nd value > maxDate`, () => {
+  it(`should save state if 2nd value > maxDate`, () => {
     expect(
       getStateForValue('end', '10.10.2030', {
         currentStart: '20.10.2010',
@@ -182,6 +98,80 @@ describe('DateRangePicker value pick', () => {
       end: '05.10.2020',
       focus: 'end',
       isOpen: true,
+    });
+  });
+});
+
+describe('select dates when start equals end', () => {
+  it(`should set equal value to 1st field, save focus in 1st and close picker`, () => {
+    expect(
+      getStateForValue('start', '10.10.2000', {
+        currentStart: '10.10.2000',
+        currentEnd: '10.10.2000',
+      }),
+    ).toEqual({
+      start: '10.10.2000',
+      end: '10.10.2000',
+      focus: 'start',
+      isOpen: false,
+    });
+  });
+
+  it(`should set equal value to 2nd field, save focus in 2nd and close picker`, () => {
+    expect(
+      getStateForValue('end', '10.10.2025', {
+        currentStart: '10.10.2000',
+        currentEnd: '',
+      }),
+    ).toEqual({
+      start: '10.10.2000',
+      end: '10.10.2025',
+      focus: 'end',
+      isOpen: false,
+    });
+  });
+
+  it(`should set equal value to filled 2nd field, save focus in 2nd and close picker`, () => {
+    expect(
+      getStateForValue('end', '10.10.2000', {
+        currentStart: '10.10.2000',
+        currentEnd: '15.10.2000',
+      }),
+    ).toEqual({
+      start: '10.10.2000',
+      end: '10.10.2000',
+      focus: 'end',
+      isOpen: false,
+    });
+  });
+});
+
+describe('select date equals to current field', () => {
+  it(`should save equal value in 1st field, save focus in 1st and close picker`, () => {
+    expect(
+      getStateForValue('start', '10.10.2000', {
+        currentStart: '10.10.2000',
+        currentEnd: '15.10.2000',
+      }),
+    ).toEqual({
+      start: '10.10.2000',
+      end: '15.10.2000',
+      focus: 'start',
+      isOpen: false,
+    });
+  });
+
+  it(`should save equal value in 2nd field, save focus in 2nd and close picker`, () => {
+    expect(
+      getStateForValue('end', '15.10.2000', {
+        currentStart: '10.10.2000',
+        currentEnd: '15.10.2000',
+      }),
+    ).toEqual({
+      start: '10.10.2000',
+      end: '15.10.2000',
+      focus: 'end',
+      isOpen: false,
     });
   });
 });
