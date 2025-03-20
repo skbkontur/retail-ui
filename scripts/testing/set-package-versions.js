df8cac5fca5a9443aa654c960a106fb192f6889c
kuzmich <kuzmich@skbkontur.ru>
2025-03-20 14:24:26 +0500

    chore: add script

--- scripts/testing/set-package-versions.js
+++ scripts/testing/set-package-versions.js
@@ -1,13 +1,30 @@
 const { execSync } = require('child_process');
+const path = require('path');
 
-//copied
+const fs = require('fs-extra');
 
-execSync(`node scripts/update-package-registry`, { stdio: 'inherit' });
+const reactVersion = process.env.REACT_VERSION;
+const tsVersion = process.env.TYPESCRIPT_VERSION;
+const {react, typescript} = fs.readJsonSync(path.resolve(__dirname, "package-versions.json"))
+
+const packagesForReact = react[reactVersion];
+const packagesForTypescript = typescript[tsVersion];
+
+const allPackages = convertPackagesToString({...packagesForReact, ...packagesForTypescript});
+
+const pathToReactUi = path.resolve(__dirname, "packages", "react-ui");
+const pathToReactUiValidation = path.resolve(__dirname, "packages", "react-ui-validations");
 
 try {
-  execSync(`yarn ${process.argv.slice(2).join(' ')}`, { stdio: 'inherit' });
+  [pathToReactUi, pathToReactUiValidation].forEach(p => {
+    execSync(`yarn add ${allPackages} `, { cwd: p });
+  })
 } catch (e) {
   // error message will be forwarded to stdio anyway
 }
 
-execSync(`node scripts/update-package-registry back`, { stdio: 'inherit' });
+console.log("r");
+
+function convertPackagesToString(packages) {
+  return Object.entries(packages).reduce((acc, [name, version]) => acc.concat(`${name}@${version} `), "");
+}
