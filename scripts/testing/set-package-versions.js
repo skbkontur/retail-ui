ff79e8f7f30c42dd9b32de5c43db300eba89624d
kuzmich <kuzmich@skbkontur.ru>
2025-03-20 15:54:49 +0500

    chore: script to install deps is ready

--- scripts/testing/set-package-versions.js
+++ scripts/testing/set-package-versions.js
@@ -5,6 +5,7 @@ const fs = require('fs-extra');
 
 const reactVersion = process.env.REACT_VERSION;
 const tsVersion = process.env.TYPESCRIPT_VERSION;
+const registry = process.env.PACKAGE_REGISTRY;
 const {react, typescript} = fs.readJsonSync(path.resolve(__dirname, "package-versions.json"))
 
 const packagesForReact = react[reactVersion];
@@ -12,18 +13,16 @@ const packagesForTypescript = typescript[tsVersion];
 
 const allPackages = convertPackagesToString({...packagesForReact, ...packagesForTypescript});
 
-const pathToReactUi = path.resolve(__dirname, "packages", "react-ui");
-const pathToReactUiValidation = path.resolve(__dirname, "packages", "react-ui-validations");
+const pathToReactUi = path.resolve(__dirname, "..", "..", "packages", "react-ui");
+const pathToReactUiValidation = path.resolve(__dirname, "..", "..", "packages", "react-ui-validations");
 
 try {
-  [pathToReactUi, pathToReactUiValidation].forEach(p => {
-    execSync(`yarn add ${allPackages} `, { cwd: p });
+  [pathToReactUi, pathToReactUiValidation].forEach(packagePath => {
+    execSync(`yarn add -D ${allPackages} --registry ${registry}`, { stdio: 'inherit', cwd: packagePath });
   })
 } catch (e) {
   // error message will be forwarded to stdio anyway
 }
-
-console.log("r");
 
 function convertPackagesToString(packages) {
   return Object.entries(packages).reduce((acc, [name, version]) => acc.concat(`${name}@${version} `), "");
