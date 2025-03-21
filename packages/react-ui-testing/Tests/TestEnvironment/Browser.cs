6ef09d85eaefe34cb49dd665c8e97a048b26de19
lossir <lossir1@gmail.com>
2025-03-21 11:33:59 +0500

    ci(react-ui-testing): wip 1
    Signed-off-by: lossir <lossir1@gmail.com>

--- packages/react-ui-testing/Tests/TestEnvironment/Browser.cs
+++ packages/react-ui-testing/Tests/TestEnvironment/Browser.cs
@@ -1,10 +1,12 @@
 using System;
+using System.IO;
 using System.Drawing;
 using Kontur.Selone.Extensions;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
+using DotNetEnv;
 
 namespace SKBKontur.SeleniumTesting.Tests.TestEnvironment
 {
@@ -65,7 +67,11 @@ namespace SKBKontur.SeleniumTesting.Tests.TestEnvironment
             {
                 if (webDriver != null) return webDriver;
 
-                var wdHub = Environment.GetEnvironmentVariable("GRID_URL");
+                string fullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../.env");
+                Env.Load(fullPath);
+
+                var wdHub = Env.GetString("GRID_URL");
+                Console.WriteLine($"Current Working Directory: {wdHub}");
                 ChromeOptions options = new ChromeOptions();
 
                 options.AddAdditionalOption(CapabilityType.Platform, "windows");
