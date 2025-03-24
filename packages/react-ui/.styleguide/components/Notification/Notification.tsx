4993d008b1de7c6145de1638b214777f183cd773
Sasha Chabin <chabin@skbkontur.ru>
2025-03-24 16:05:02 +0500

    docs: update new docs banner (#3627)

--- packages/react-ui/.styleguide/components/Notification/Notification.tsx
+++ packages/react-ui/.styleguide/components/Notification/Notification.tsx
@@ -8,20 +8,13 @@ export function Notification() {
   return (
     <div className={cx(styles.notification())}>
       <div className={cx(styles.notificationContent())}>
-        <b>Делаем новую доку.</b> Собираем в ней все наши пакеты, уже сделали навигацию по&nbsp;разделам, управление
-        фича-флагами и&nbsp;добавили песочницу для&nbsp;валидаций. Тестируем и&nbsp;собираем обратную связь
-        до&nbsp;конца года. Используйте новую доку и рассказывайте нам в&nbsp;
-        <a href="https://chat.skbkontur.ru/kontur/channels/new-documentation-react-ui" target="_blank">
-          Маттермосте
-        </a>{' '}
-        как она вам.
+        <b>Документация больше не обновляется.</b> Новая документация на{' '}
+        <a href="https://tech.skbkontur.ru/kontur-ui/" target="_blank">
+          tech.skbkontur.ru/kontur-ui
+        </a>
       </div>
-      <a
-        href="https://ui.gitlab-pages.kontur.host/storybook-documentation/"
-        className={cx(styles.notificationButton())}
-        target="_blank"
-      >
-        Попробовать
+      <a href="https://tech.skbkontur.ru/kontur-ui/" className={cx(styles.notificationButton())} target="_blank">
+        Поехали
       </a>
     </div>
   );
