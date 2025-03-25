4b6b52b41ca585060573cb6641428b4fcbf4f600
kuzmich <kuzmich@skbkontur.ru>
2025-03-25 13:19:11 +0500

    chore: на 17 проходят

--- packages/react-ui-testing/TestPages/src/index.jsx
+++ packages/react-ui-testing/TestPages/src/index.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDom from 'react-dom';
 import TestPage from './ReactTestApplication';
 
 window.__RETAIL_UI_VERSION__ = process.env.retailUIVersion;
 
-const container = document.getElementById('content');
-if (container) {
-  const root = createRoot(container);
-  root.render(<TestPage data-tid="testpage" />);
-}
+ReactDom.render(<TestPage data-tid="testpage" />, document.getElementById('content'));
