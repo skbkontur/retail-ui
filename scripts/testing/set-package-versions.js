b8a37c115f62b106527f10a3255e7293bfedbe42
kuzmich <kuzmich@skbkontur.ru>
2025-03-28 15:52:09 +0500

    chore: update set versions script

--- scripts/testing/set-package-versions.js
+++ scripts/testing/set-package-versions.js
@@ -1,4 +1,3 @@
-const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
@@ -9,30 +8,30 @@ if (!(reactVersion && tsVersion)) {
   return;
 }
 
-const registry = process.env.PACKAGE_REGISTRY ?? "https://nexus.kontur.host/repository/kontur-npm-group";
 const packagesPath = path.resolve(__dirname, "..", "..", "packages");
-const {react, typescript} = JSON.parse(fs.readFileSync(path.resolve(__dirname, "package-versions.json"), 'utf8'));
+const {react, typescript} = getJsonFile(path.resolve(__dirname, "package-versions.json"));
 
 const packagesForReact = react[reactVersion];
 const packagesForTypescript = typescript[tsVersion];
 
-const allPackages = convertPackagesToString({...packagesForReact, ...packagesForTypescript});
-
-const pathToReactUi = path.resolve(packagesPath, "react-ui");
-const pathToReactUiValidation = path.resolve(packagesPath, "react-ui-validations");
+const allPackages = {...packagesForReact, ...packagesForTypescript};
 
 try {
   patchSmokeTestPackage();
   patchSeleniumTestsPackage();
-  [pathToReactUi, pathToReactUiValidation].forEach(packagePath => {
-    execSync(`yarn add -D ${allPackages} --registry ${registry}`, { stdio: 'inherit', cwd: packagePath });
-  })
+  patchPackageJsons();
 } catch (e) {
   //log
 }
 
-function convertPackagesToString(packages) {
-  return Object.entries(packages).reduce((acc, [name, version]) => acc.concat(`${name}@${version} `), "");
+function patchPackageJsons() {
+  const pathToReactUi = path.resolve(packagesPath, "react-ui", "package.json");
+  const pathToReactUiValidation = path.resolve(packagesPath, "react-ui-validations", "package.json");
+  [pathToReactUi, pathToReactUiValidation].forEach(packagePath => {
+    const json = getJsonFile(packagePath);
+    json.devDependencies = {...json.devDependencies, ...allPackages}
+    writeJsonFile(packagePath, json);
+  })
 }
 
 function patchSeleniumTestsPackage() {
@@ -43,8 +42,16 @@ function patchSeleniumTestsPackage() {
 
 function patchSmokeTestPackage() {
   const pathToConfig = path.resolve(packagesPath, "react-ui-smoke-test", "cra-template-react-ui", "template.json");
-  const package = JSON.parse(fs.readFileSync(pathToConfig, 'utf8'));
+  const package = getJsonFile(pathToConfig);
   package.package.dependencies = {...package.package.dependencies, ...packagesForReact};
-  fs.writeFileSync(pathToConfig, JSON.stringify(package));
+  writeJsonFile(pathToConfig, package);
   console.log(pathToConfig, "has been patched");
+}
+
+function getJsonFile(path) {
+  return JSON.parse(fs.readFileSync(path, 'utf8'));
+}
+
+function writeJsonFile(path, content) {
+  fs.writeFileSync(path, JSON.stringify(content));
 }
