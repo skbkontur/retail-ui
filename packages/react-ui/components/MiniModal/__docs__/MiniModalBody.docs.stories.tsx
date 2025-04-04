15be6ff1b6682b7d028de6a4da59ba30a0c3a15b
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2025-04-04 12:44:47 +0500

    chore: add storybook docs

---
+++ packages/react-ui/components/MiniModal/__docs__/MiniModalBody.docs.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Meta } from '../../../typings/stories';
+import { MiniModalBody } from '../MiniModalBody';
+
+export default {
+  title: 'Overlay/MiniModal/MiniModalBody',
+  component: MiniModalBody,
+  parameters: { creevey: { skip: true } },
+} as Meta;
+
+export const Default = () => {
+  return <div />;
+};
---
+++ packages/react-ui/components/Tabs/__docs__/Tab.docs.stories.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from 'react';
+import { Tab, ThemeContext, ThemeFactory, Button, Tabs } from '@skbkontur/react-ui';
+
+import { Meta, Story } from '../../../typings/stories';
+
+export default {
+  title: 'Display data/Tabs/Tab',
+  component: Tab,
+  parameters: { creevey: { skip: true } },
+} as Meta;
+
+/** Используя переменные `tabColorPrimary`, `tabColorSuccess`, `tabColorWarning` и `tabColorError` можно изменить активный цвет состояния, а библиотека автоматически подберёт цвет подчёркивания при наведении. */
+export const Example1: Story = () => {
+  const getRandomColor = () => '#' + Math.random().toString(16).substr(-6);
+  const updateColors = () => {
+    return {
+      tabColorPrimary: getRandomColor(),
+      tabColorSuccess: getRandomColor(),
+      tabColorWarning: getRandomColor(),
+      tabColorError: getRandomColor(),
+    };
+  };
+
+  const [activeBase, setActiveBase] = React.useState('error');
+  const [activeRandom, setActiveRandom] = React.useState('error');
+  const [colors, setColors] = React.useState(updateColors());
+
+  return (
+    <>
+      <p style={{ fontSize: '17px' }}>C цветами по умолчанию</p>
+      <Tabs value={activeBase} onValueChange={setActiveBase}>
+        <Tabs.Tab primary id="primary">
+          Primary
+        </Tabs.Tab>
+        <Tabs.Tab success id="success">
+          Success
+        </Tabs.Tab>
+        <Tabs.Tab warning id="warning">
+          Warning
+        </Tabs.Tab>
+        <Tabs.Tab error id="error">
+          Error
+        </Tabs.Tab>
+      </Tabs>
+
+      <p style={{ fontSize: '17px' }}>Со случайным основным цветом</p>
+      <div
+        style={{ display: 'inline-flex', flexDirection: 'column', justifyContent: 'space-between', height: '100px' }}
+      >
+        <ThemeContext.Consumer>
+          {(theme) => {
+            return (
+              <ThemeContext.Provider value={ThemeFactory.create(colors, theme)}>
+                <Tabs value={activeRandom} onValueChange={setActiveRandom}>
+                  <Tabs.Tab primary id="primary">
+                    Primary
+                  </Tabs.Tab>
+                  <Tabs.Tab success id="success">
+                    Success
+                  </Tabs.Tab>
+                  <Tabs.Tab warning id="warning">
+                    Warning
+                  </Tabs.Tab>
+                  <Tabs.Tab error id="error">
+                    Error
+                  </Tabs.Tab>
+                </Tabs>
+              </ThemeContext.Provider>
+            );
+          }}
+        </ThemeContext.Consumer>
+        <Button onClick={() => setColors(updateColors)}>Получить новый набор цветов</Button>
+      </div>
+    </>
+  );
+};
+Example1.storyName = 'Кастомизация';
+
+/** С помощью пропа `component` можно изменять корневой элемент `<Tab />`.
+Проп может принимать: компоненты, функции и строки. */
+export const Example2: Story = () => {
+  const [active, setActive] = React.useState('/fuji');
+
+  const NavLink = (props) => <a {...props} />;
+
+  return (
+    <Tabs value={active} onValueChange={setActive}>
+      {/** Кастомный компонент **/}
+      <Tabs.Tab component={(props) => <NavLink {...props} />} id="/fuji">
+        🌋 Fuji
+      </Tabs.Tab>
+      {/** Функция **/}
+      <Tabs.Tab component={(props) => <a {...props} />} id="/tahat">
+        ⛰ Tahat
+      </Tabs.Tab>
+      {/** Строка **/}
+      <Tabs.Tab component="a" id="/alps">
+        🗻 Alps
+      </Tabs.Tab>
+    </Tabs>
+  );
+};
+Example2.storyName = 'Изменение корневого компонента Tab';
