d091850cdbe0f4960b8105bc2dacfad75c95f60b
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2025-04-04 12:44:47 +0500

    chore: add storybook docs

---
+++ packages/react-ui/components/Paging/__docs__/Paging.docs.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Paging } from '@skbkontur/react-ui';
+
+import { Meta, Story } from '../../../typings/stories';
+
+export default {
+  title: 'Display data/Paging',
+  component: Paging,
+  parameters: { creevey: { skip: true } },
+} as Meta;
+
+export const Example1: Story = () => {
+  class Paginator3000 extends React.Component {
+    constructor() {
+      super();
+      this.state = { active: 1 };
+      this._handlePageChange = this._handlePageChange.bind(this);
+    }
+
+    render() {
+      return <Paging activePage={this.state.active} onPageChange={this._handlePageChange} pagesCount={12} />;
+    }
+
+    _handlePageChange(pageNumber) {
+      this.setState({ active: pageNumber });
+    }
+  }
+
+  return <Paginator3000 />;
+};
+Example1.storyName = 'Базовый пример';
+
+export const Example2: Story = () => {
+  const [activePage, setActivePage] = React.useState(3);
+
+  return (
+    <Paging disabled onPageChange={(activePage) => setActivePage(activePage)} activePage={activePage} pagesCount={8} />
+  );
+};
+Example2.storyName = 'Пейджинг в отключенном состоянии';
---
+++ packages/react-ui/components/Spinner/__docs__/Spinner.docs.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Spinner, Gapped } from '@skbkontur/react-ui';
+
+import { Meta, Story } from '../../../typings/stories';
+
+export default {
+  title: 'Display data/Spinner',
+  component: Spinner,
+  parameters: { creevey: { skip: true } },
+} as Meta;
+
+export const Example1: Story = () => {
+  const reactNodeCaption = (
+    <div>
+      <Spinner type="mini" caption={null} /> <span style={{ color: 'tomato', fontSize: '1.3em' }}>З</span>
+      агрузка
+    </div>
+  );
+
+  return (
+    <Gapped>
+      <Spinner type="big" caption="big" />
+      <Spinner type="normal" caption="normal" />
+      <Spinner type="mini" caption="mini" />
+      <Spinner type="mini" dimmed caption="mini dimmed" />
+      <Spinner type="big" caption={reactNodeCaption} />
+      <Spinner type="big" caption="custom" width={8} color={'#538A1B'} />
+    </Gapped>
+  );
+};
+Example1.storyName = 'Базовый пример';
