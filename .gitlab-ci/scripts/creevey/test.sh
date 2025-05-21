8e82da75b6576e740b8cbb2e2af216cf13d0bab0
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2025-05-21 12:46:51 +0500

    chore: wip

--- .gitlab-ci/scripts/creevey/test.sh
+++ .gitlab-ci/scripts/creevey/test.sh
@@ -3,18 +3,12 @@ source ./.gitlab-ci/scripts/logging/logging.sh
 
 workspace=$1
 package_path=$2
-
 section_id="$workspace UI Сreevey"
+
 startLogCollapsedSection "$section_id"
 
 CONTAINER_IP=$(ip a | grep "global eth0" | cut -c 10- | sed -r 's/[/].+//')
 
 exe "GET_IP_URL=https://fake.testkontur.ru/ip GRID_URL=https://grid.skbkontur.ru/common/wd/hub CONTAINER_IP=$CONTAINER_IP yarn workspace $workspace creevey:ci"
-return_code=$?
-
-if [ $return_code -ne 0 ]; then
-    logError "Не смогли запустить тесты Сreevey (exitCode $return_code)" "off"
-	exit $return_code
-fi
 
 endLogSection "$section_id"
--- .gitlab-ci/scripts/install/install.sh
+++ .gitlab-ci/scripts/install/install.sh
@@ -2,14 +2,9 @@
 source ./.gitlab-ci/scripts/logging/logging.sh
 
 section_id="yarn install"
+
 startLogCollapsedSection "$section_id"
 
-exe "yarn install"
-return_code_yarn_ci=$?
+exe "yarn install" "$section_1"
 
 endLogSection "$section_id"
-
-if [ $return_code_yarn_ci -ne 0 ]; then
-    logError "Не смогли выполнить команду yarn install (exitCode $return_code_yarn_ci)"
-	exit $return_code_yarn_ci
-fi
