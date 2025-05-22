d67d2ac6949e47d4190119674a5789968a6b8abc
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2025-05-22 09:27:21 +0500

    chore: fix sb configs

--- packages/react-ui-validations/.storybook/main.ts
+++ packages/react-ui-validations/.storybook/main.ts
@@ -10,7 +10,7 @@ const config: StorybookConfig = {
       fastRefresh: !isTestEnv,
       strictMode: process?.env?.STRICT_MODE === 'true',
       //Для версионного прогона убран, чтобы 18 реакт гонялся по честному
-      legacyRootApi: !process?.env?.REACT_VERSION,
+      legacyRootApi: Number(process?.env?.REACT_VERSION || 17) < 18,
     },
   },
   core: {
