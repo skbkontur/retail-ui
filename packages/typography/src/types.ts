3c7729664a9859af56a747ccd1d7daa7c9335bd9
Пахомов Максим Валерьевич <m.pakhomov@skbkontur.ru>
2025-12-03 14:10:38 +0500

    fix(typography): specified undefined type explicitly

--- packages/typography/src/types.ts
+++ packages/typography/src/types.ts
@@ -1,34 +1,29 @@
-import type { AriaAttributes, CSSProperties, HTMLAttributes } from 'react';
+import type { AriaAttributes, CSSProperties, HTMLAttributes, PropsWithChildren } from 'react';
 
 export type TTextTags = 'span' | 'p' | 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 export type TTextSizes = 12 | 14 | 16 | 18 | 20 | 22 | 24 | 28 | 32 | 36 | 40 | 48 | 56;
 
 export interface TTextProps
   extends Pick<AriaAttributes, 'aria-describedby' | 'aria-label' | 'aria-labelledby'>,
-    Pick<HTMLAttributes<HTMLElement>, 'id' | 'role' | 'title'> {
-  /**
-   * Размер шрифта
-   */
-  size: TTextSizes;
-  /**
-   * HTML-тег
-   */
-  tag: TTextTags;
-  /**
-   * Отключить отступы
-   * @default false
-   */
-  noSpacing?: boolean;
-  /**
-   * Широкая колонка — от 40 до 60 символов в строке
-   * @default false
-   */
-  wideColumn?: boolean;
-  /**
-   * Надпись
-   */
-  children: React.ReactNode;
-  className?: string;
-  style?: CSSProperties;
-  'data-tid'?: string;
-}
+    Pick<HTMLAttributes<HTMLElement>, 'id' | 'role' | 'title' | 'className' | 'style'>,
+    PropsWithChildren<{
+      /**
+       * Размер шрифта
+       */
+      size: TTextSizes;
+      /**
+       * HTML-тег
+       */
+      tag: TTextTags;
+      /**
+       * Отключить отступы
+       * @default false
+       */
+      noSpacing?: boolean;
+      /**
+       * Широкая колонка — от 40 до 60 символов в строке
+       * @default false
+       */
+      wideColumn?: boolean;
+      'data-tid'?: string;
+    }> {}
