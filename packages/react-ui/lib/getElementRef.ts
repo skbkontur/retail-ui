9b0762a792589c98c3bb801c459e9fd4881a785e
Васеньков Артемий Владимирович <a.vasenkov@skbkontur.ru>
2025-12-03 18:00:46 +0500

    chore: fix getElementRef func

--- packages/react-ui/lib/getElementRef.ts
+++ packages/react-ui/lib/getElementRef.ts
@@ -1,8 +1,14 @@
-import type React from 'react';
+import React from 'react';
 
 // Access the element's ref
 export function getElementRef(element: React.ReactElement) {
-  return element.props && typeof element.props === 'object' && 'ref' in element.props
-    ? element.props.ref
-    : (element as any).ref;
+  if (
+    parseInt(React.version, 10) >= 19 &&
+    element.props &&
+    typeof element.props === 'object' &&
+    'ref' in element.props
+  ) {
+    return element.props.ref;
+  }
+  return (element as any).ref;
 }
